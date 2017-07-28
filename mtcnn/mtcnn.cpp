53a54407a22d0dd1c0726bd29744fd0817f005ef
ElegantGod <838060390@qq.com>
2017-07-28 20:18:01 +0800

    add resize, no opencv in detect

--- mtcnn/mtcnn.cpp
+++ mtcnn/mtcnn.cpp
@@ -36,11 +36,11 @@ bool cmpScore(orderScore lsh, orderScore rsh){
 }
 class mtcnn{
 public:
-    mtcnn();
-    void detect(cv::Mat& image);
+    mtcnn(int img_w_, int img_h_);
+    void detect(const unsigned char* image_data, std::vector<Bbox>& finalBbox);
 private:
-    void generateBbox(ncnn::Mat score, ncnn::Mat location, std::vector<Bbox>& boundingBox_, std::vector<orderScore>& bboxScore_, float scale);
-    void nms(std::vector<Bbox> &boundingBox_, std::vector<orderScore> &bboxScore_, const float overlap_threshold, string modelname="Union");
+    void generateBbox(ncnn::Mat score, ncnn::Mat location, vector<Bbox>& boundingBox_, vector<orderScore>& bboxScore_, float scale);
+    void nms(vector<Bbox> &boundingBox_, std::vector<orderScore> &bboxScore_, const float overlap_threshold, string modelname="Union");
     void refineAndSquareBbox(vector<Bbox> &vecBbox, const int &height, const int &width);
 
     ncnn::Net Pnet, Rnet, Onet;
@@ -55,7 +55,9 @@ private:
     int img_w, img_h;
 };
 
-mtcnn::mtcnn(){
+mtcnn::mtcnn(int img_w_, int img_h_){
+	img_w = img_w_;
+	img_h = img_h_;
     Pnet.load_param("det1.param");
     Pnet.load_model("det1.bin");
     Rnet.load_param("det2.param");
@@ -65,7 +67,6 @@ mtcnn::mtcnn(){
 }
 
 void mtcnn::generateBbox(ncnn::Mat score, ncnn::Mat location, std::vector<Bbox>& boundingBox_, std::vector<orderScore>& bboxScore_, float scale){
-    //for pooling 
     int stride = 2;
     int cellsize = 12;
     int count = 0;
@@ -158,7 +159,7 @@ void mtcnn::refineAndSquareBbox(vector<Bbox> &vecBbox, const int &height, const 
     float bbw=0, bbh=0, maxSide=0;
     float h = 0, w = 0;
     float x1=0, y1=0, x2=0, y2=0;
-    for(vector<struct Bbox>::iterator it=vecBbox.begin(); it!=vecBbox.end();it++){
+    for(vector<Bbox>::iterator it=vecBbox.begin(); it!=vecBbox.end();it++){
         if((*it).exist){
             bbw = (*it).x2 - (*it).x1 + 1;
             bbh = (*it).y2 - (*it).y1 + 1;
@@ -188,10 +189,8 @@ void mtcnn::refineAndSquareBbox(vector<Bbox> &vecBbox, const int &height, const 
         }
     }
 }
-void mtcnn::detect(cv::Mat &image){
-    img_w = image.cols;
-    img_h = image.rows;
-	img = ncnn::Mat::from_pixels(image.data, ncnn::Mat::PIXEL_RGB, img_w, img_h);
+void mtcnn::detect(const unsigned char* image_data, std::vector<Bbox>& finalBbox_){
+	img = ncnn::Mat::from_pixels(image_data, ncnn::Mat::PIXEL_RGB, img_w, img_h);
 	img.substract_mean_normalize(mean_vals, norm_vals);
 	
     float minl = img_w<img_h?img_w:img_h;
@@ -214,7 +213,7 @@ void mtcnn::detect(cv::Mat &image){
     for (size_t i = 0; i < scales_.size(); i++) {
         int hs = (int)ceil(img_h*scales_[i]);
         int ws = (int)ceil(img_w*scales_[i]);
-        //ncnn::Mat in = ncnn::Mat::from_pixels_resize(image.data, ncnn::Mat::PIXEL_RGB, img_w, img_h, ws, hs);
+        //ncnn::Mat in = ncnn::Mat::from_pixels_resize(image_data, ncnn::Mat::PIXEL_RGB, img_w, img_h, ws, hs);
         ncnn::Mat in(ws, hs, 3);
 		resize_image(img, in);
 		//in.substract_mean_normalize(mean_vals, norm_vals);
@@ -244,11 +243,11 @@ void mtcnn::detect(cv::Mat &image){
     //the first stage's nms
     if(count<1)return;
     nms(firstBbox_, firstOrderScore_, nms_threshold[0]);
-    refineAndSquareBbox(firstBbox_, image.rows, image.cols);
+    refineAndSquareBbox(firstBbox_, img_h, img_w);
 	printf("firstBbox_.size()=%d\n", firstBbox_.size());
     //second stage
     count = 0;
-    for(vector<struct Bbox>::iterator it=firstBbox_.begin(); it!=firstBbox_.end();it++){
+    for(vector<Bbox>::iterator it=firstBbox_.begin(); it!=firstBbox_.end();it++){
         if((*it).exist){
 			ncnn::Mat tempIm;
 			copy_cut_border(img, tempIm, (*it).y1, img_h-(*it).y2, (*it).x1, img_w-(*it).x2);
@@ -278,11 +277,11 @@ void mtcnn::detect(cv::Mat &image){
     printf("secondBbox_.size()=%d\n", secondBbox_.size());
     if(count<1)return;
     nms(secondBbox_, secondBboxScore_, nms_threshold[1]);
-    refineAndSquareBbox(secondBbox_, image.rows, image.cols);
+    refineAndSquareBbox(secondBbox_, img_h, img_w);
 
     //third stage 
     count = 0;
-    for(vector<struct Bbox>::iterator it=secondBbox_.begin(); it!=secondBbox_.end();it++){
+    for(vector<Bbox>::iterator it=secondBbox_.begin(); it!=secondBbox_.end();it++){
         if((*it).exist){
             ncnn::Mat tempIm;
 			copy_cut_border(img, tempIm, (*it).y1, img_h-(*it).y2, (*it).x1, img_w-(*it).x2);
@@ -317,14 +316,11 @@ void mtcnn::detect(cv::Mat &image){
 		
 	printf("thirdBbox_.size()=%d\n", thirdBbox_.size());
     if(count<1)return;
-    refineAndSquareBbox(thirdBbox_, image.rows, image.cols);
+    refineAndSquareBbox(thirdBbox_, img_h, img_w);
     nms(thirdBbox_, thirdBboxScore_, nms_threshold[2], "Min");
-    for(vector<struct Bbox>::iterator it=thirdBbox_.begin(); it!=thirdBbox_.end();it++){
-        if((*it).exist){
-            rectangle(image, Point((*it).x1, (*it).y1), Point((*it).x2, (*it).y2), Scalar(0,0,255), 2,8,0);
-            for(int num=0;num<5;num++)circle(image,Point((int)*(it->ppoint+num), (int)*(it->ppoint+num+5)),3,Scalar(0,255,255), -1);
-        }
-    }
+    
+	finalBbox_ = thirdBbox_;
+	
     firstBbox_.clear();
     firstOrderScore_.clear();
     secondBbox_.clear();
@@ -337,14 +333,21 @@ int main(int argc, char** argv)
 {
     const char* imagepath = argv[1];
 
-    cv::Mat m = cv::imread(imagepath, CV_LOAD_IMAGE_COLOR);
-    if (m.empty())
+    cv::Mat cv_img = cv::imread(imagepath, CV_LOAD_IMAGE_COLOR);
+    if (cv_img.empty())
     {
         fprintf(stderr, "cv::imread %s failed\n", imagepath);
         return -1;
     }
-    mtcnn mm;
-    mm.detect(m);
-	imwrite("result.jpg",m);
+	std::vector<Bbox> finalBbox;
+    mtcnn mm(cv_img.cols, cv_img.rows);
+    mm.detect(cv_img.data, finalBbox);
+	for(vector<Bbox>::iterator it=finalBbox.begin(); it!=finalBbox.end();it++){
+        if((*it).exist){
+            rectangle(cv_img, Point((*it).x1, (*it).y1), Point((*it).x2, (*it).y2), Scalar(0,0,255), 2,8,0);
+            for(int num=0;num<5;num++)circle(cv_img,Point((int)*(it->ppoint+num), (int)*(it->ppoint+num+5)),3,Scalar(0,255,255), -1);
+        }
+    }
+	imwrite("result.jpg",cv_img);
     return 0;
 }
