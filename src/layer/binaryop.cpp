091a43676a654b17d25261780ccc9bb1bc8ad302
nihuini <nihuini@tencent.com>
2017-09-01 14:28:56 +0800

    implement binaryop and unaryop with operator template, binaryop between blobs with different dims

--- src/layer/binaryop.cpp
+++ src/layer/binaryop.cpp
@@ -15,6 +15,7 @@
 #include "binaryop.h"
 #include <math.h>
 #include <algorithm>
+#include <functional>
 
 namespace ncnn {
 
@@ -56,126 +57,278 @@ int BinaryOp::load_param(const unsigned char*& mem)
     return 0;
 }
 
+template<typename Op>
+static int binary_op(const Mat& a, const Mat& b, Mat& c)
+{
+    Op op;
+
+    int w = a.w;
+    int h = a.h;
+    int channels = a.c;
+    int size = w * h;
+
+    int w1 = b.w;
+    int h1 = b.h;
+    int channels1 = b.c;
+
+    if (a.dims == 3)
+    {
+        c.create(w, h, channels);
+        if (c.empty())
+            return -100;
+
+        if (b.dims == 3)
+        {
+            #pragma omp parallel for
+            for (int q=0; q<channels; q++)
+            {
+                const float* ptr = a.channel(q);
+                const float* ptr1 = b.channel(q);
+                float* outptr = c.channel(q);
+
+                for (int i=0; i<size; i++)
+                {
+                    outptr[i] = op(ptr[i], ptr1[i]);
+                }
+            }
+
+            return 0;
+        }
+
+        if (b.dims == 2)
+        {
+            #pragma omp parallel for
+            for (int q=0; q<channels; q++)
+            {
+                const float* ptr = a.channel(q);
+                const float* ptr1 = b.data + h * q;
+                float* outptr = c.channel(q);
+
+                for (int y=0; y<h; y++)
+                {
+                    const float b0 = ptr1[y];
+                    for (int x=0; x<w; x++)
+                    {
+                        outptr[x] = op(ptr[x], b0);
+                    }
+
+                    ptr += w;
+                    outptr += w;
+                }
+            }
+
+            return 0;
+        }
+
+        if (b.dims == 1)
+        {
+            #pragma omp parallel for
+            for (int q=0; q<channels; q++)
+            {
+                const float* ptr = a.channel(q);
+                const float b0 = b.data[q];
+                float* outptr = c.channel(q);
+
+                for (int i=0; i<size; i++)
+                {
+                    outptr[i] = op(ptr[i], b0);
+                }
+
+                ptr += w;
+                outptr += w;
+            }
+
+            return 0;
+        }
+    }
+    else if (a.dims == 2)
+    {
+        if (b.dims == 3)
+        {
+            c.create(w1, h1, channels1);
+            if (c.empty())
+                return -100;
+
+            #pragma omp parallel for
+            for (int q=0; q<channels1; q++)
+            {
+                const float* ptr = a.data + h1 * q;
+                const float* ptr1 = b.channel(q);
+                float* outptr = c.channel(q);
+
+                for (int y=0; y<h1; y++)
+                {
+                    const float a0 = ptr[y];
+                    for (int x=0; x<w1; x++)
+                    {
+                        outptr[x] = op(a0, ptr1[x]);
+                    }
+
+                    ptr1 += w1;
+                    outptr += w1;
+                }
+            }
+
+            return 0;
+        }
+
+        c.create(w, h);
+        if (c.empty())
+            return -100;
+
+        if (b.dims == 2)
+        {
+            const float* ptr = a;
+            const float* ptr1 = b;
+            float* outptr = c;
+
+            for (int i=0; i<size; i++)
+            {
+                outptr[i] = op(ptr[i], ptr1[i]);
+            }
+
+            return 0;
+        }
+
+        if (b.dims == 1)
+        {
+            c.create(w, h);
+            if (c.empty())
+                return -100;
+
+            const float* ptr = a;
+            const float* ptr1 = b;
+            float* outptr = c;
+
+            for (int y=0; y<h; y++)
+            {
+                const float b0 = ptr1[y];
+                for (int x=0; x<w; x++)
+                {
+                    outptr[x] = op(ptr[x], b0);
+                }
+
+                ptr += w;
+                outptr += w;
+            }
+
+            return 0;
+        }
+    }
+    else if (a.dims == 1)
+    {
+        if (b.dims == 3)
+        {
+            c.create(w1, h1, channels1);
+            if (c.empty())
+                return -100;
+
+            #pragma omp parallel for
+            for (int q=0; q<channels1; q++)
+            {
+                const float a0 = a.data[q];
+                const float* ptr1 = b.channel(q);
+                float* outptr = c.channel(q);
+
+                for (int i=0; i<size; i++)
+                {
+                    outptr[i] = op(a0, ptr1[i]);
+                }
+
+                ptr1 += w1;
+                outptr += w1;
+            }
+
+            return 0;
+        }
+
+        if (b.dims == 2)
+        {
+            c.create(w1, h1);
+            if (c.empty())
+                return -100;
+
+            const float* ptr = a;
+            const float* ptr1 = b;
+            float* outptr = c;
+
+            for (int y=0; y<h1; y++)
+            {
+                const float a0 = ptr[y];
+                for (int x=0; x<w1; x++)
+                {
+                    outptr[x] = op(a0, ptr1[x]);
+                }
+
+                ptr1 += w1;
+                outptr += w1;
+            }
+
+            return 0;
+        }
+
+        if (b.dims == 1)
+        {
+            c.create(w);
+            if (c.empty())
+                return -100;
+
+            const float* ptr = a;
+            const float* ptr1 = b;
+            float* outptr = c;
+
+            for (int i=0; i<size; i++)
+            {
+                outptr[i] = op(ptr[i], ptr1[i]);
+            }
+        }
+    }
+
+    return 0;
+}
+
+template<typename T>
+struct binary_op_max : std::binary_function<T,T,T> {
+    T operator() (const T& x, const T& y) const { return std::max(x, y); }
+};
+
+template<typename T>
+struct binary_op_min : std::binary_function<T,T,T> {
+    T operator() (const T& x, const T& y) const { return std::min(x, y); }
+};
+
+template<typename T>
+struct binary_op_pow : std::binary_function<T,T,T> {
+    T operator() (const T& x, const T& y) const { return pow(x, y); }
+};
+
 int BinaryOp::forward(const std::vector<Mat>& bottom_blobs, std::vector<Mat>& top_blobs) const
 {
     const Mat& bottom_blob = bottom_blobs[0];
     const Mat& bottom_blob1 = bottom_blobs[1];
 
-    int w = bottom_blob.w;
-    int h = bottom_blob.h;
-    int channels = bottom_blob.c;
-    int size = w * h;
-
     Mat& top_blob = top_blobs[0];
-    top_blob.create(w, h, channels);
-    if (top_blob.empty())
-        return -100;
 
     if (op_type == Operation_ADD)
-    {
-        #pragma omp parallel for
-        for (int q=0; q<channels; q++)
-        {
-            const float* ptr = bottom_blob.channel(q);
-            const float* ptr1 = bottom_blob1.channel(q);
-            float* outptr = top_blob.channel(q);
+        return binary_op< std::plus<float> >(bottom_blob, bottom_blob1, top_blob);
 
-            for (int i=0; i<size; i++)
-            {
-                outptr[i] = ptr[i] + ptr1[i];
-            }
-        }
-    }
-    else if (op_type == Operation_SUB)
-    {
-        #pragma omp parallel for
-        for (int q=0; q<channels; q++)
-        {
-            const float* ptr = bottom_blob.channel(q);
-            const float* ptr1 = bottom_blob1.channel(q);
-            float* outptr = top_blob.channel(q);
+    if (op_type == Operation_SUB)
+        return binary_op< std::minus<float> >(bottom_blob, bottom_blob1, top_blob);
 
-            for (int i=0; i<size; i++)
-            {
-                outptr[i] = ptr[i] - ptr1[i];
-            }
-        }
-    }
-    else if (op_type == Operation_MUL)
-    {
-        #pragma omp parallel for
-        for (int q=0; q<channels; q++)
-        {
-            const float* ptr = bottom_blob.channel(q);
-            const float* ptr1 = bottom_blob1.channel(q);
-            float* outptr = top_blob.channel(q);
+    if (op_type == Operation_MUL)
+        return binary_op< std::multiplies<float> >(bottom_blob, bottom_blob1, top_blob);
 
-            for (int i=0; i<size; i++)
-            {
-                outptr[i] = ptr[i] * ptr1[i];
-            }
-        }
-    }
-    else if (op_type == Operation_DIV)
-    {
-        #pragma omp parallel for
-        for (int q=0; q<channels; q++)
-        {
-            const float* ptr = bottom_blob.channel(q);
-            const float* ptr1 = bottom_blob1.channel(q);
-            float* outptr = top_blob.channel(q);
+    if (op_type == Operation_DIV)
+        return binary_op< std::divides<float> >(bottom_blob, bottom_blob1, top_blob);
 
-            for (int i=0; i<size; i++)
-            {
-                outptr[i] = ptr[i] / ptr1[i];
-            }
-        }
-    }
-    else if (op_type == Operation_MAX)
-    {
-        #pragma omp parallel for
-        for (int q=0; q<channels; q++)
-        {
-            const float* ptr = bottom_blob.channel(q);
-            const float* ptr1 = bottom_blob1.channel(q);
-            float* outptr = top_blob.channel(q);
+    if (op_type == Operation_MAX)
+        return binary_op< binary_op_max<float> >(bottom_blob, bottom_blob1, top_blob);
 
-            for (int i=0; i<size; i++)
-            {
-                outptr[i] = std::max(ptr[i], ptr1[i]);
-            }
-        }
-    }
-    else if (op_type == Operation_MIN)
-    {
-        #pragma omp parallel for
-        for (int q=0; q<channels; q++)
-        {
-            const float* ptr = bottom_blob.channel(q);
-            const float* ptr1 = bottom_blob1.channel(q);
-            float* outptr = top_blob.channel(q);
+    if (op_type == Operation_MIN)
+        return binary_op< binary_op_min<float> >(bottom_blob, bottom_blob1, top_blob);
 
-            for (int i=0; i<size; i++)
-            {
-                outptr[i] = std::min(ptr[i], ptr1[i]);
-            }
-        }
-    }
-    else if (op_type == Operation_POW)
-    {
-        #pragma omp parallel for
-        for (int q=0; q<channels; q++)
-        {
-            const float* ptr = bottom_blob.channel(q);
-            const float* ptr1 = bottom_blob1.channel(q);
-            float* outptr = top_blob.channel(q);
-
-            for (int i=0; i<size; i++)
-            {
-                outptr[i] = pow(ptr[i], ptr1[i]);
-            }
-        }
-    }
+    if (op_type == Operation_POW)
+        return binary_op< binary_op_pow<float> >(bottom_blob, bottom_blob1, top_blob);
 
     return 0;
 }
--- src/layer/unaryop.cpp
+++ src/layer/unaryop.cpp
@@ -14,6 +14,7 @@
 
 #include "unaryop.h"
 #include <math.h>
+#include <functional>
 
 namespace ncnn {
 
@@ -55,433 +56,236 @@ int UnaryOp::load_param(const unsigned char*& mem)
     return 0;
 }
 
+template<typename Op>
+static int unary_op(const Mat& a, Mat& b)
+{
+    Op op;
+
+    int w = a.w;
+    int h = a.h;
+    int channels = a.c;
+    int size = w * h * channels;
+
+    if (a.dims == 3)
+    {
+        b.create(w, h, channels);
+        if (b.empty())
+            return -100;
+    }
+    else if (a.dims == 2)
+    {
+        b.create(w, h);
+        if (b.empty())
+            return -100;
+    }
+    else if (a.dims == 1)
+    {
+        b.create(w);
+        if (b.empty())
+            return -100;
+    }
+
+    const float* ptr = a;
+    float* outptr = b;
+
+    #pragma omp parallel for
+    for (int i=0; i<size; i++)
+    {
+        outptr[i] = op(ptr[i]);
+    }
+
+    return 0;
+}
+
+template<typename Op>
+static int unary_op_inplace(Mat& a)
+{
+    Op op;
+
+    int size = a.total();
+
+    float* ptr = a;
+
+    #pragma omp parallel for
+    for (int i=0; i<size; i++)
+    {
+        ptr[i] = op(ptr[i]);
+    }
+
+    return 0;
+}
+
+template<typename T>
+struct binary_op_abs : std::unary_function<T,T> {
+    T operator() (const T& x) const { return fabs(x); }
+};
+
+template<typename T>
+struct binary_op_neg : std::unary_function<T,T> {
+    T operator() (const T& x) const { return -x; }
+};
+
+template<typename T>
+struct binary_op_floor : std::unary_function<T,T> {
+    T operator() (const T& x) const { return floor(x); }
+};
+
+template<typename T>
+struct binary_op_ceil : std::unary_function<T,T> {
+    T operator() (const T& x) const { return ceil(x); }
+};
+
+template<typename T>
+struct binary_op_square : std::unary_function<T,T> {
+    T operator() (const T& x) const { return x * x; }
+};
+
+template<typename T>
+struct binary_op_sqrt : std::unary_function<T,T> {
+    T operator() (const T& x) const { return sqrt(x); }
+};
+
+template<typename T>
+struct binary_op_rsqrt : std::unary_function<T,T> {
+    T operator() (const T& x) const { return 1.f / sqrt(x); }
+};
+
+template<typename T>
+struct binary_op_exp : std::unary_function<T,T> {
+    T operator() (const T& x) const { return exp(x); }
+};
+
+template<typename T>
+struct binary_op_log : std::unary_function<T,T> {
+    T operator() (const T& x) const { return log(x); }
+};
+
+template<typename T>
+struct binary_op_sin : std::unary_function<T,T> {
+    T operator() (const T& x) const { return sin(x); }
+};
+
+template<typename T>
+struct binary_op_cos : std::unary_function<T,T> {
+    T operator() (const T& x) const { return cos(x); }
+};
+
+template<typename T>
+struct binary_op_tan : std::unary_function<T,T> {
+    T operator() (const T& x) const { return tan(x); }
+};
+
+template<typename T>
+struct binary_op_asin : std::unary_function<T,T> {
+    T operator() (const T& x) const { return asin(x); }
+};
+
+template<typename T>
+struct binary_op_acos : std::unary_function<T,T> {
+    T operator() (const T& x) const { return acos(x); }
+};
+
+template<typename T>
+struct binary_op_atan : std::unary_function<T,T> {
+    T operator() (const T& x) const { return atan(x); }
+};
+
 int UnaryOp::forward(const Mat& bottom_blob, Mat& top_blob) const
 {
-    int w = bottom_blob.w;
-    int h = bottom_blob.h;
-    int channels = bottom_blob.c;
-    int size = w * h;
-
-    top_blob.create(w, h, channels);
-    if (top_blob.empty())
-        return -100;
-
     if (op_type == Operation_ABS)
-    {
-        #pragma omp parallel for
-        for (int q=0; q<channels; q++)
-        {
-            const float* ptr = bottom_blob.channel(q);
-            float* outptr = top_blob.channel(q);
+        return unary_op< binary_op_abs<float> >(bottom_blob, top_blob);
 
-            for (int i=0; i<size; i++)
-            {
-                outptr[i] = fabs(ptr[i]);
-            }
-        }
-    }
-    else if (op_type == Operation_NEG)
-    {
-        #pragma omp parallel for
-        for (int q=0; q<channels; q++)
-        {
-            const float* ptr = bottom_blob.channel(q);
-            float* outptr = top_blob.channel(q);
+    if (op_type == Operation_NEG)
+        return unary_op< binary_op_neg<float> >(bottom_blob, top_blob);
 
-            for (int i=0; i<size; i++)
-            {
-                outptr[i] = -ptr[i];
-            }
-        }
-    }
-    else if (op_type == Operation_FLOOR)
-    {
-        #pragma omp parallel for
-        for (int q=0; q<channels; q++)
-        {
-            const float* ptr = bottom_blob.channel(q);
-            float* outptr = top_blob.channel(q);
+    if (op_type == Operation_FLOOR)
+        return unary_op< binary_op_floor<float> >(bottom_blob, top_blob);
 
-            for (int i=0; i<size; i++)
-            {
-                outptr[i] = floor(ptr[i]);
-            }
-        }
-    }
-    else if (op_type == Operation_CEIL)
-    {
-        #pragma omp parallel for
-        for (int q=0; q<channels; q++)
-        {
-            const float* ptr = bottom_blob.channel(q);
-            float* outptr = top_blob.channel(q);
+    if (op_type == Operation_CEIL)
+        return unary_op< binary_op_ceil<float> >(bottom_blob, top_blob);
 
-            for (int i=0; i<size; i++)
-            {
-                outptr[i] = ceil(ptr[i]);
-            }
-        }
-    }
-    else if (op_type == Operation_SQUARE)
-    {
-        #pragma omp parallel for
-        for (int q=0; q<channels; q++)
-        {
-            const float* ptr = bottom_blob.channel(q);
-            float* outptr = top_blob.channel(q);
+    if (op_type == Operation_SQUARE)
+        return unary_op< binary_op_square<float> >(bottom_blob, top_blob);
 
-            for (int i=0; i<size; i++)
-            {
-                outptr[i] = ptr[i] * ptr[i];
-            }
-        }
-    }
-    else if (op_type == Operation_SQRT)
-    {
-        #pragma omp parallel for
-        for (int q=0; q<channels; q++)
-        {
-            const float* ptr = bottom_blob.channel(q);
-            float* outptr = top_blob.channel(q);
+    if (op_type == Operation_SQRT)
+        return unary_op< binary_op_sqrt<float> >(bottom_blob, top_blob);
 
-            for (int i=0; i<size; i++)
-            {
-                outptr[i] = sqrt(ptr[i]);
-            }
-        }
-    }
-    else if (op_type == Operation_RSQRT)
-    {
-        #pragma omp parallel for
-        for (int q=0; q<channels; q++)
-        {
-            const float* ptr = bottom_blob.channel(q);
-            float* outptr = top_blob.channel(q);
+    if (op_type == Operation_RSQRT)
+        return unary_op< binary_op_rsqrt<float> >(bottom_blob, top_blob);
 
-            for (int i=0; i<size; i++)
-            {
-                outptr[i] = 1.f / sqrt(ptr[i]);
-            }
-        }
-    }
-    else if (op_type == Operation_EXP)
-    {
-        #pragma omp parallel for
-        for (int q=0; q<channels; q++)
-        {
-            const float* ptr = bottom_blob.channel(q);
-            float* outptr = top_blob.channel(q);
+    if (op_type == Operation_EXP)
+        return unary_op< binary_op_exp<float> >(bottom_blob, top_blob);
 
-            for (int i=0; i<size; i++)
-            {
-                outptr[i] = exp(ptr[i]);
-            }
-        }
-    }
-    else if (op_type == Operation_LOG)
-    {
-        #pragma omp parallel for
-        for (int q=0; q<channels; q++)
-        {
-            const float* ptr = bottom_blob.channel(q);
-            float* outptr = top_blob.channel(q);
+    if (op_type == Operation_LOG)
+        return unary_op< binary_op_log<float> >(bottom_blob, top_blob);
 
-            for (int i=0; i<size; i++)
-            {
-                outptr[i] = log(ptr[i]);
-            }
-        }
-    }
-    else if (op_type == Operation_SIN)
-    {
-        #pragma omp parallel for
-        for (int q=0; q<channels; q++)
-        {
-            const float* ptr = bottom_blob.channel(q);
-            float* outptr = top_blob.channel(q);
+    if (op_type == Operation_SIN)
+        return unary_op< binary_op_sin<float> >(bottom_blob, top_blob);
 
-            for (int i=0; i<size; i++)
-            {
-                outptr[i] = sin(ptr[i]);
-            }
-        }
-    }
-    else if (op_type == Operation_COS)
-    {
-        #pragma omp parallel for
-        for (int q=0; q<channels; q++)
-        {
-            const float* ptr = bottom_blob.channel(q);
-            float* outptr = top_blob.channel(q);
+    if (op_type == Operation_COS)
+        return unary_op< binary_op_cos<float> >(bottom_blob, top_blob);
 
-            for (int i=0; i<size; i++)
-            {
-                outptr[i] = cos(ptr[i]);
-            }
-        }
-    }
-    else if (op_type == Operation_TAN)
-    {
-        #pragma omp parallel for
-        for (int q=0; q<channels; q++)
-        {
-            const float* ptr = bottom_blob.channel(q);
-            float* outptr = top_blob.channel(q);
+    if (op_type == Operation_TAN)
+        return unary_op< binary_op_tan<float> >(bottom_blob, top_blob);
 
-            for (int i=0; i<size; i++)
-            {
-                outptr[i] = tan(ptr[i]);
-            }
-        }
-    }
-    else if (op_type == Operation_ASIN)
-    {
-        #pragma omp parallel for
-        for (int q=0; q<channels; q++)
-        {
-            const float* ptr = bottom_blob.channel(q);
-            float* outptr = top_blob.channel(q);
+    if (op_type == Operation_ASIN)
+        return unary_op< binary_op_asin<float> >(bottom_blob, top_blob);
 
-            for (int i=0; i<size; i++)
-            {
-                outptr[i] = asin(ptr[i]);
-            }
-        }
-    }
-    else if (op_type == Operation_ACOS)
-    {
-        #pragma omp parallel for
-        for (int q=0; q<channels; q++)
-        {
-            const float* ptr = bottom_blob.channel(q);
-            float* outptr = top_blob.channel(q);
+    if (op_type == Operation_ACOS)
+        return unary_op< binary_op_acos<float> >(bottom_blob, top_blob);
 
-            for (int i=0; i<size; i++)
-            {
-                outptr[i] = acos(ptr[i]);
-            }
-        }
-    }
-    else if (op_type == Operation_ATAN)
-    {
-        #pragma omp parallel for
-        for (int q=0; q<channels; q++)
-        {
-            const float* ptr = bottom_blob.channel(q);
-            float* outptr = top_blob.channel(q);
-
-            for (int i=0; i<size; i++)
-            {
-                outptr[i] = atan(ptr[i]);
-            }
-        }
-    }
+    if (op_type == Operation_ATAN)
+        return unary_op< binary_op_atan<float> >(bottom_blob, top_blob);
 
     return 0;
 }
 
 int UnaryOp::forward_inplace(Mat& bottom_top_blob) const
 {
-    int w = bottom_top_blob.w;
-    int h = bottom_top_blob.h;
-    int channels = bottom_top_blob.c;
-    int size = w * h;
-
     if (op_type == Operation_ABS)
-    {
-        #pragma omp parallel for
-        for (int q=0; q<channels; q++)
-        {
-            float* ptr = bottom_top_blob.channel(q);
+        return unary_op_inplace< binary_op_abs<float> >(bottom_top_blob);
 
-            for (int i=0; i<size; i++)
-            {
-                ptr[i] = fabs(ptr[i]);
-            }
-        }
-    }
-    else if (op_type == Operation_NEG)
-    {
-        #pragma omp parallel for
-        for (int q=0; q<channels; q++)
-        {
-            float* ptr = bottom_top_blob.channel(q);
+    if (op_type == Operation_NEG)
+        return unary_op_inplace< binary_op_neg<float> >(bottom_top_blob);
 
-            for (int i=0; i<size; i++)
-            {
-                ptr[i] = -ptr[i];
-            }
-        }
-    }
-    else if (op_type == Operation_FLOOR)
-    {
-        #pragma omp parallel for
-        for (int q=0; q<channels; q++)
-        {
-            float* ptr = bottom_top_blob.channel(q);
+    if (op_type == Operation_FLOOR)
+        return unary_op_inplace< binary_op_floor<float> >(bottom_top_blob);
 
-            for (int i=0; i<size; i++)
-            {
-                ptr[i] = floor(ptr[i]);
-            }
-        }
-    }
-    else if (op_type == Operation_CEIL)
-    {
-        #pragma omp parallel for
-        for (int q=0; q<channels; q++)
-        {
-            float* ptr = bottom_top_blob.channel(q);
+    if (op_type == Operation_CEIL)
+        return unary_op_inplace< binary_op_ceil<float> >(bottom_top_blob);
 
-            for (int i=0; i<size; i++)
-            {
-                ptr[i] = ceil(ptr[i]);
-            }
-        }
-    }
-    else if (op_type == Operation_SQUARE)
-    {
-        #pragma omp parallel for
-        for (int q=0; q<channels; q++)
-        {
-            float* ptr = bottom_top_blob.channel(q);
+    if (op_type == Operation_SQUARE)
+        return unary_op_inplace< binary_op_square<float> >(bottom_top_blob);
 
-            for (int i=0; i<size; i++)
-            {
-                ptr[i] = ptr[i] * ptr[i];
-            }
-        }
-    }
-    else if (op_type == Operation_SQRT)
-    {
-        #pragma omp parallel for
-        for (int q=0; q<channels; q++)
-        {
-            float* ptr = bottom_top_blob.channel(q);
+    if (op_type == Operation_SQRT)
+        return unary_op_inplace< binary_op_sqrt<float> >(bottom_top_blob);
 
-            for (int i=0; i<size; i++)
-            {
-                ptr[i] = sqrt(ptr[i]);
-            }
-        }
-    }
-    else if (op_type == Operation_RSQRT)
-    {
-        #pragma omp parallel for
-        for (int q=0; q<channels; q++)
-        {
-            float* ptr = bottom_top_blob.channel(q);
+    if (op_type == Operation_RSQRT)
+        return unary_op_inplace< binary_op_rsqrt<float> >(bottom_top_blob);
 
-            for (int i=0; i<size; i++)
-            {
-                ptr[i] = 1.f / sqrt(ptr[i]);
-            }
-        }
-    }
-    else if (op_type == Operation_EXP)
-    {
-        #pragma omp parallel for
-        for (int q=0; q<channels; q++)
-        {
-            float* ptr = bottom_top_blob.channel(q);
+    if (op_type == Operation_EXP)
+        return unary_op_inplace< binary_op_exp<float> >(bottom_top_blob);
 
-            for (int i=0; i<size; i++)
-            {
-                ptr[i] = exp(ptr[i]);
-            }
-        }
-    }
-    else if (op_type == Operation_LOG)
-    {
-        #pragma omp parallel for
-        for (int q=0; q<channels; q++)
-        {
-            float* ptr = bottom_top_blob.channel(q);
+    if (op_type == Operation_LOG)
+        return unary_op_inplace< binary_op_log<float> >(bottom_top_blob);
 
-            for (int i=0; i<size; i++)
-            {
-                ptr[i] = log(ptr[i]);
-            }
-        }
-    }
-    else if (op_type == Operation_SIN)
-    {
-        #pragma omp parallel for
-        for (int q=0; q<channels; q++)
-        {
-            float* ptr = bottom_top_blob.channel(q);
+    if (op_type == Operation_SIN)
+        return unary_op_inplace< binary_op_sin<float> >(bottom_top_blob);
 
-            for (int i=0; i<size; i++)
-            {
-                ptr[i] = sin(ptr[i]);
-            }
-        }
-    }
-    else if (op_type == Operation_COS)
-    {
-        #pragma omp parallel for
-        for (int q=0; q<channels; q++)
-        {
-            float* ptr = bottom_top_blob.channel(q);
+    if (op_type == Operation_COS)
+        return unary_op_inplace< binary_op_cos<float> >(bottom_top_blob);
 
-            for (int i=0; i<size; i++)
-            {
-                ptr[i] = cos(ptr[i]);
-            }
-        }
-    }
-    else if (op_type == Operation_TAN)
-    {
-        #pragma omp parallel for
-        for (int q=0; q<channels; q++)
-        {
-            float* ptr = bottom_top_blob.channel(q);
+    if (op_type == Operation_TAN)
+        return unary_op_inplace< binary_op_tan<float> >(bottom_top_blob);
 
-            for (int i=0; i<size; i++)
-            {
-                ptr[i] = tan(ptr[i]);
-            }
-        }
-    }
-    else if (op_type == Operation_ASIN)
-    {
-        #pragma omp parallel for
-        for (int q=0; q<channels; q++)
-        {
-            float* ptr = bottom_top_blob.channel(q);
+    if (op_type == Operation_ASIN)
+        return unary_op_inplace< binary_op_asin<float> >(bottom_top_blob);
 
-            for (int i=0; i<size; i++)
-            {
-                ptr[i] = asin(ptr[i]);
-            }
-        }
-    }
-    else if (op_type == Operation_ACOS)
-    {
-        #pragma omp parallel for
-        for (int q=0; q<channels; q++)
-        {
-            float* ptr = bottom_top_blob.channel(q);
+    if (op_type == Operation_ACOS)
+        return unary_op_inplace< binary_op_acos<float> >(bottom_top_blob);
 
-            for (int i=0; i<size; i++)
-            {
-                ptr[i] = acos(ptr[i]);
-            }
-        }
-    }
-    else if (op_type == Operation_ATAN)
-    {
-        #pragma omp parallel for
-        for (int q=0; q<channels; q++)
-        {
-            float* ptr = bottom_top_blob.channel(q);
-
-            for (int i=0; i<size; i++)
-            {
-                ptr[i] = atan(ptr[i]);
-            }
-        }
-    }
+    if (op_type == Operation_ATAN)
+        return unary_op_inplace< binary_op_atan<float> >(bottom_top_blob);
 
     return 0;
 }
