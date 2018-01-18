d92be40a58a0c6271747e6f75e3b801d80b96c37
Linghan Cheung <lhcheung1991@gmail.com>
2018-01-19 00:19:14 +0800

    move the function implementation to cpp file to avoid multiple definitions

--- src/benchmark.h
+++ src/benchmark.h
@@ -21,7 +21,7 @@
 #ifdef _WIN32
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
-#include <stdint.h> // portable: uint64_t   MSVC: __int64 
+#include <stdint.h> // portable: uint64_t   MSVC: __int64
 
 typedef struct timeval 
 {
@@ -29,26 +29,7 @@ typedef struct timeval
     long tv_usec;
 } timeval;
 
-int gettimeofday(struct timeval * tp, struct timezone * tzp)
-{
-    // Note: some broken versions only have 8 trailing zero's, the correct epoch has 9 trailing zero's
-    // This magic number is the number of 100 nanosecond intervals since January 1, 1601 (UTC)
-    // until 00:00:00 January 1, 1970 
-    static const uint64_t EPOCH = ((uint64_t) 116444736000000000ULL);
-
-    SYSTEMTIME  system_time;
-    FILETIME    file_time;
-    uint64_t    time;
-
-    GetSystemTime( &system_time );
-    SystemTimeToFileTime( &system_time, &file_time );
-    time =  ((uint64_t)file_time.dwLowDateTime )      ;
-    time += ((uint64_t)file_time.dwHighDateTime) << 32;
-
-    tp->tv_sec  = (long) ((time - EPOCH) / 10000000L);
-    tp->tv_usec = (long) (system_time.wMilliseconds * 1000);
-    return 0;
-}
+int gettimeofday(struct timeval * tp, struct timezone * tzp);
 #elif defined (__unix__) || (defined (__APPLE__) && defined (__MACH__))
 #include <sys/time.h>
 #endif // _WIN32
