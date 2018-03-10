7d1e49584da92af9a372af4f898cb9c5321e2782
nihui <shuizhuyuanluo@126.com>
2018-03-10 15:14:56 +0800

    call Innerproduct for convolution on flattened blob

--- src/layer/arm/convolution_arm.cpp
+++ src/layer/arm/convolution_arm.cpp
@@ -64,6 +64,11 @@ int Convolution_arm::forward(const Mat& bottom_blob, Mat& top_blob) const
     // convolv with NxN kernel
     // value = value + bias
 
+    if (bottom_blob.dims != 3)
+    {
+        return Convolution::forward(bottom_blob, top_blob);
+    }
+
     if (kernel_w != kernel_h || stride_w != stride_h)
     {
         return Convolution::forward(bottom_blob, top_blob);
