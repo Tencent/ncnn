bc1a84dee51fd76aeb1d3a717df30670d83db8c9
nihui <shuizhuyuanluo@126.com>
2018-04-23 23:27:07 +0800

    fix mingw32 build

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -14,7 +14,7 @@
 
 #ifdef _WIN32
 #define WIN32_LEAN_AND_MEAN
-#include <Windows.h>
+#include <windows.h>
 #include <stdint.h> // portable: uint64_t   MSVC: __int64
 #else // _WIN32
 #include <sys/time.h>
