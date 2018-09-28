455f1785994a52c0e254e03d2ad75d95b32de242
nihuini <nihuini@tencent.com>
2018-09-28 10:51:33 +0800

    vulkan infrastructure

---
+++ src/gpu.cpp
@@ -0,0 +1,455 @@
+// Tencent is pleased to support the open source community by making ncnn available.
+//
+// Copyright (C) 2018 THL A29 Limited, a Tencent company. All rights reserved.
+//
+// Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
+// in compliance with the License. You may obtain a copy of the License at
+//
+// https://opensource.org/licenses/BSD-3-Clause
+//
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
+#include "gpu.h"
+
+#if NCNN_VULKAN
+
+#include <vulkan/vulkan.h>
+
+#include <stdio.h>
+#include <string.h>
+
+#include <vector>
+
+#define ENABLE_VALIDATION_LAYER 1
+
+namespace ncnn {
+
+// global
+static VkInstance g_instance = 0;
+static int g_gpu_count = 0;
+
+// NOTE 8 is large enough i think ...
+static GpuInfo g_gpu_infos[8];
+static VkDevice g_gpu_devices[8] = {0};
+
+#if ENABLE_VALIDATION_LAYER
+static VkDebugUtilsMessengerEXT callback;
+
+const char* validationLayers[] = {
+    "VK_LAYER_LUNARG_standard_validation"
+};
+
+const char* validationExtensions[] = {
+    VK_EXT_DEBUG_UTILS_EXTENSION_NAME
+};
+
+bool checkValidationLayerSupport()
+{
+    uint32_t layerCount;
+    vkEnumerateInstanceLayerProperties(&layerCount, NULL);
+
+    std::vector<VkLayerProperties> availableLayers(layerCount);
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+    const char* layerName = validationLayers[0];
+    for (size_t i=0; i<availableLayers.size(); i++)
+    {
+//         fprintf(stderr, "%s\n", availableLayers[i].layerName);
+        if (strcmp(layerName, availableLayers[i].layerName) == 0)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+    VkDebugUtilsMessageSeverityFlagBitsEXT /*messageSeverity*/,
+    VkDebugUtilsMessageTypeFlagsEXT /*messageType*/,
+    const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+    void* /*pUserData*/)
+{
+    fprintf(stderr, "validation layer: %s\n", pCallbackData->pMessage);
+
+    return VK_FALSE;
+}
+
+VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pCallback)
+{
+    PFN_vkCreateDebugUtilsMessengerEXT func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+    if (func)
+        return func(instance, pCreateInfo, pAllocator, pCallback);
+
+    return VK_ERROR_EXTENSION_NOT_PRESENT;
+}
+
+void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT callback, const VkAllocationCallbacks* pAllocator)
+{
+    PFN_vkDestroyDebugUtilsMessengerEXT func = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+    if (func)
+        func(instance, callback, pAllocator);
+}
+#endif // ENABLE_VALIDATION_LAYER
+
+static uint32_t find_device_compute_queue(const std::vector<VkQueueFamilyProperties>& queueFamilyProperties)
+{
+    // first try, compute only queue
+    for (uint32_t i=0; i<queueFamilyProperties.size(); i++)
+    {
+        const VkQueueFamilyProperties& queueFamilyProperty = queueFamilyProperties[i];
+
+        if ((queueFamilyProperty.queueFlags & VK_QUEUE_COMPUTE_BIT) && !(queueFamilyProperty.queueFlags & VK_QUEUE_GRAPHICS_BIT))
+        {
+            return i;
+        }
+    }
+
+    // second try, any queue with compute
+    for (uint32_t i=0; i<queueFamilyProperties.size(); i++)
+    {
+        const VkQueueFamilyProperties& queueFamilyProperty = queueFamilyProperties[i];
+
+        if (queueFamilyProperty.queueFlags & VK_QUEUE_COMPUTE_BIT)
+        {
+            return i;
+        }
+    }
+
+    fprintf(stderr, "no compute queue\n");
+    return -1;
+}
+
+static uint32_t find_device_local_memory(VkPhysicalDeviceMemoryProperties physicalDeviceMemoryProperties)
+{
+    // first try, device local only
+    for (uint32_t i=0; i<physicalDeviceMemoryProperties.memoryTypeCount; i++)
+    {
+        const VkMemoryType& memoryType = physicalDeviceMemoryProperties.memoryTypes[i];
+
+        if (memoryType.propertyFlags == VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT)
+        {
+            return i;
+        }
+    }
+
+    // second try, with device local bit
+    for (uint32_t i=0; i<physicalDeviceMemoryProperties.memoryTypeCount; i++)
+    {
+        const VkMemoryType& memoryType = physicalDeviceMemoryProperties.memoryTypes[i];
+
+        if (memoryType.propertyFlags & VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT)
+        {
+            return i;
+        }
+    }
+
+    fprintf(stderr, "no device local memory\n");
+    return -1;
+}
+
+static uint32_t find_host_visible_memory(VkPhysicalDeviceMemoryProperties physicalDeviceMemoryProperties)
+{
+    // first try, host visible + host coherent + device local
+    for (uint32_t i=0; i<physicalDeviceMemoryProperties.memoryTypeCount; i++)
+    {
+        const VkMemoryType& memoryType = physicalDeviceMemoryProperties.memoryTypes[i];
+
+        if ((memoryType.propertyFlags & VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT)
+            && (memoryType.propertyFlags & VK_MEMORY_PROPERTY_HOST_COHERENT_BIT)
+            && (memoryType.propertyFlags & VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT))
+        {
+            return i;
+        }
+    }
+
+    // second try, host visible + host coherent
+    for (uint32_t i=0; i<physicalDeviceMemoryProperties.memoryTypeCount; i++)
+    {
+        const VkMemoryType& memoryType = physicalDeviceMemoryProperties.memoryTypes[i];
+
+        if ((memoryType.propertyFlags & VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT)
+            && (memoryType.propertyFlags & VK_MEMORY_PROPERTY_HOST_COHERENT_BIT))
+        {
+            return i;
+        }
+    }
+
+    // third try, with host visible bit
+    for (uint32_t i=0; i<physicalDeviceMemoryProperties.memoryTypeCount; i++)
+    {
+        const VkMemoryType& memoryType = physicalDeviceMemoryProperties.memoryTypes[i];
+
+        if (memoryType.propertyFlags & VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT)
+        {
+            return i;
+        }
+    }
+
+    fprintf(stderr, "no host visible memory\n");
+    return -1;
+}
+
+int create_gpu_instance()
+{
+#if ENABLE_VALIDATION_LAYER
+    if (!checkValidationLayerSupport())
+    {
+        fprintf(stderr, "validation layers requested, but not available!\n");
+        return -1;
+    }
+#endif // ENABLE_VALIDATION_LAYER
+
+    VkResult ret;
+
+    VkApplicationInfo applicationInfo;
+    applicationInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    applicationInfo.pNext = 0;
+    applicationInfo.pApplicationName = "ncnn";
+    applicationInfo.applicationVersion = 0;
+    applicationInfo.pEngineName = "ncnn";
+    applicationInfo.engineVersion = 20180927;
+    applicationInfo.apiVersion = VK_MAKE_VERSION(1, 0, 0);
+
+    VkInstanceCreateInfo instanceCreateInfo;
+    instanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    instanceCreateInfo.pNext = 0;
+    instanceCreateInfo.flags = 0;
+    instanceCreateInfo.pApplicationInfo = &applicationInfo;
+#if ENABLE_VALIDATION_LAYER
+    instanceCreateInfo.enabledLayerCount = 1;
+    instanceCreateInfo.ppEnabledLayerNames = validationLayers;
+    instanceCreateInfo.enabledExtensionCount = 1;
+    instanceCreateInfo.ppEnabledExtensionNames = validationExtensions;
+#else
+    instanceCreateInfo.enabledLayerCount = 0;
+    instanceCreateInfo.ppEnabledLayerNames = 0;
+    instanceCreateInfo.enabledExtensionCount = 0;
+    instanceCreateInfo.ppEnabledExtensionNames = 0;
+#endif // ENABLE_VALIDATION_LAYER
+
+    ret = vkCreateInstance(&instanceCreateInfo, 0, &g_instance);
+    if (ret != VK_SUCCESS)
+    {
+        fprintf(stderr, "vkCreateInstance failed %d\n", ret);
+        return -1;
+    }
+
+#if ENABLE_VALIDATION_LAYER
+    VkDebugUtilsMessengerCreateInfoEXT createInfo = {};
+    createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+    createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+    createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+    createInfo.pfnUserCallback = debugCallback;
+    createInfo.pUserData = 0;
+    ret = CreateDebugUtilsMessengerEXT(g_instance, &createInfo, nullptr, &callback);
+    if (ret != VK_SUCCESS)
+    {
+        fprintf(stderr, "CreateDebugUtilsMessengerEXT failed %d\n", ret);
+        return -1;
+    }
+#endif // ENABLE_VALIDATION_LAYER
+
+    uint32_t physicalDeviceCount = 0;
+    ret = vkEnumeratePhysicalDevices(g_instance, &physicalDeviceCount, 0);
+    if (ret != VK_SUCCESS)
+    {
+        fprintf(stderr, "vkEnumeratePhysicalDevices failed %d\n", ret);
+        return -1;
+    }
+
+    // NOTE 8 is large enough i think ...
+    if (physicalDeviceCount > 8)
+        physicalDeviceCount = 8;
+
+    std::vector<VkPhysicalDevice> physicalDevices(physicalDeviceCount);
+
+    ret = vkEnumeratePhysicalDevices(g_instance, &physicalDeviceCount, physicalDevices.data());
+    if (ret != VK_SUCCESS)
+    {
+        fprintf(stderr, "vkEnumeratePhysicalDevices failed %d\n", ret);
+        return -1;
+    }
+
+    g_gpu_count = physicalDeviceCount;
+
+    // find proper device and queue
+    for (uint32_t i=0; i<physicalDeviceCount; i++)
+    {
+        const VkPhysicalDevice& physicalDevice = physicalDevices[i];
+        GpuInfo& gpu_info = g_gpu_infos[i];
+
+        gpu_info.physical_device = physicalDevice;
+
+        // device type
+        VkPhysicalDeviceProperties physicalDeviceProperties;
+        vkGetPhysicalDeviceProperties(physicalDevice, &physicalDeviceProperties);
+
+//         fprintf(stderr, "[%u] apiVersion = %u.%u.%u\n", i, VK_VERSION_MAJOR(physicalDeviceProperties.apiVersion),
+//             VK_VERSION_MINOR(physicalDeviceProperties.apiVersion), VK_VERSION_PATCH(physicalDeviceProperties.apiVersion));
+//         fprintf(stderr, "[%u] driverVersion = %u.%u.%u\n", i, VK_VERSION_MAJOR(physicalDeviceProperties.driverVersion),
+//             VK_VERSION_MINOR(physicalDeviceProperties.driverVersion), VK_VERSION_PATCH(physicalDeviceProperties.driverVersion));
+//         fprintf(stderr, "[%u] vendorID = %x\n", i, physicalDeviceProperties.vendorID);
+//         fprintf(stderr, "[%u] deviceID = %x\n", i, physicalDeviceProperties.deviceID);
+//         fprintf(stderr, "[%u] deviceType = %x\n", i, physicalDeviceProperties.deviceType);
+//         fprintf(stderr, "[%u] deviceName = %s\n", i, physicalDeviceProperties.deviceName);
+//         fprintf(stderr, "[%u] pipelineCacheUUID = %u\n", i, physicalDeviceProperties.pipelineCacheUUID);
+
+        if (physicalDeviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU)
+            gpu_info.type = 0;
+        else if (physicalDeviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU)
+            gpu_info.type = 1;
+        else if (physicalDeviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_VIRTUAL_GPU)
+            gpu_info.type = 2;
+        else if (physicalDeviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_CPU)
+            gpu_info.type = 3;
+        else
+            gpu_info.type = -1;
+
+        // device capability
+        gpu_info.max_shared_memory_size = physicalDeviceProperties.limits.maxComputeSharedMemorySize;
+
+        gpu_info.max_image_dimension[0] = physicalDeviceProperties.limits.maxImageDimension1D;
+        gpu_info.max_image_dimension[1] = physicalDeviceProperties.limits.maxImageDimension2D;
+        gpu_info.max_image_dimension[2] = physicalDeviceProperties.limits.maxImageDimension3D;
+
+        gpu_info.max_workgroup_count[0] = physicalDeviceProperties.limits.maxComputeWorkGroupCount[0];
+        gpu_info.max_workgroup_count[1] = physicalDeviceProperties.limits.maxComputeWorkGroupCount[1];
+        gpu_info.max_workgroup_count[2] = physicalDeviceProperties.limits.maxComputeWorkGroupCount[2];
+
+        gpu_info.max_workgroup_invocations = physicalDeviceProperties.limits.maxComputeWorkGroupInvocations;
+
+        gpu_info.max_workgroup_size[0] = physicalDeviceProperties.limits.maxComputeWorkGroupSize[0];
+        gpu_info.max_workgroup_size[1] = physicalDeviceProperties.limits.maxComputeWorkGroupSize[1];
+        gpu_info.max_workgroup_size[2] = physicalDeviceProperties.limits.maxComputeWorkGroupSize[2];
+
+//         // TODO check features
+//         VkPhysicalDeviceFeatures features;
+//         vkGetPhysicalDeviceFeatures(physicalDevice, &features);
+//
+//         // TODO check formatProperties
+//         VkFormat format = VK_FORMAT_R32_SFLOAT;
+//         VkFormatProperties formatProperties;
+//         vkGetPhysicalDeviceFormatProperties(physicalDevice, format, &formatProperties);
+
+        // find compute queue
+        uint32_t queueFamilyPropertiesCount;
+        vkGetPhysicalDeviceQueueFamilyProperties(physicalDevice, &queueFamilyPropertiesCount, 0);
+
+        std::vector<VkQueueFamilyProperties> queueFamilyProperties(queueFamilyPropertiesCount);
+        vkGetPhysicalDeviceQueueFamilyProperties(physicalDevice, &queueFamilyPropertiesCount, queueFamilyProperties.data());
+
+        gpu_info.compute_queue_index = find_device_compute_queue(queueFamilyProperties);
+
+        // find memory type index
+        VkPhysicalDeviceMemoryProperties physicalDeviceMemoryProperties;
+        vkGetPhysicalDeviceMemoryProperties(physicalDevice, &physicalDeviceMemoryProperties);
+
+        gpu_info.device_local_memory_index = find_device_local_memory(physicalDeviceMemoryProperties);
+        gpu_info.host_visible_memory_index = find_host_visible_memory(physicalDeviceMemoryProperties);
+
+//     // TODO get device extension
+//     uint32_t deviceExtensionPropertyCount = 0;
+//     ret = vkEnumerateDeviceExtensionProperties(physicalDevice, NULL, &deviceExtensionPropertyCount, NULL);
+//     if (ret != VK_SUCCESS)
+//     {
+//         fprintf(stderr, "vkEnumerateDeviceExtensionProperties failed %d\n", ret);
+//         return -1;
+//     }
+//
+//     fprintf(stderr, "deviceExtensionPropertyCount = %d\n", deviceExtensionPropertyCount);
+//
+//     std::vector<VkExtensionProperties> deviceExtensionProperties(deviceExtensionPropertyCount);
+//     ret = vkEnumerateDeviceExtensionProperties(physicalDevice, NULL, &deviceExtensionPropertyCount, deviceExtensionProperties.data());
+//     if (ret != VK_SUCCESS)
+//     {
+//         fprintf(stderr, "vkEnumerateDeviceExtensionProperties failed %d\n", ret);
+//         return -1;
+//     }
+//
+//     for (uint32_t i=0; i<deviceExtensionPropertyCount; i++)
+//     {
+//         const VkExtensionProperties exp = deviceExtensionProperties[i];
+//         fprintf(stderr, "%s = %u\n", exp.extensionName, exp.specVersion);
+//     }
+
+        fprintf(stderr, "[%u %s]  queue=%u  memDL=%u  memHV=%u\n", i, physicalDeviceProperties.deviceName,
+                gpu_info.compute_queue_index, gpu_info.device_local_memory_index, gpu_info.host_visible_memory_index);
+    }
+
+    return 0;
+}
+
+void destroy_gpu_instance()
+{
+#if ENABLE_VALIDATION_LAYER
+    DestroyDebugUtilsMessengerEXT(g_instance, callback, NULL);
+#endif // ENABLE_VALIDATION_LAYER
+
+    vkDestroyInstance(g_instance, 0);
+}
+
+int get_gpu_count()
+{
+    return g_gpu_count;
+}
+
+const GpuInfo& get_gpu_info(int i)
+{
+    return g_gpu_infos[i];
+}
+
+int create_gpu_device(int i)
+{
+    const float queuePriorities[1] = { 1.f };// 0.f ~ 1.f
+
+    VkDeviceQueueCreateInfo deviceQueueCreateInfo;
+    deviceQueueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+    deviceQueueCreateInfo.pNext = 0;
+    deviceQueueCreateInfo.flags = 0;
+    deviceQueueCreateInfo.queueFamilyIndex = g_gpu_infos[i].compute_queue_index;
+    deviceQueueCreateInfo.queueCount = 1;
+    deviceQueueCreateInfo.pQueuePriorities = queuePriorities;
+
+    VkDeviceCreateInfo deviceCreateInfo;
+    deviceCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+    deviceCreateInfo.pNext = 0;
+    deviceCreateInfo.flags = 0;
+    deviceCreateInfo.queueCreateInfoCount = 1;
+    deviceCreateInfo.pQueueCreateInfos = &deviceQueueCreateInfo;
+    deviceCreateInfo.enabledLayerCount = 0;
+    deviceCreateInfo.ppEnabledLayerNames = 0;
+    deviceCreateInfo.enabledExtensionCount = 0;
+    deviceCreateInfo.ppEnabledExtensionNames = 0;
+    deviceCreateInfo.pEnabledFeatures = 0;// VkPhysicalDeviceFeatures pointer
+
+    VkDevice device;
+    VkResult ret = vkCreateDevice(g_gpu_infos[i].physical_device, &deviceCreateInfo, 0, &device);
+    if (ret != VK_SUCCESS)
+    {
+        fprintf(stderr, "vkCreateDevice failed %d\n", ret);
+    }
+
+    g_gpu_devices[i] = device;
+
+    return 0;
+}
+
+void destroy_gpu_device(int i)
+{
+    vkDestroyDevice(g_gpu_devices[i], 0);
+
+    g_gpu_devices[i] = 0;
+}
+
+VkDevice get_gpu_device(int i)
+{
+    return g_gpu_devices[i];
+}
+
+} // namespace ncnn
+
+#endif // NCNN_VULKAN
---
+++ src/gpu.h
@@ -0,0 +1,82 @@
+// Tencent is pleased to support the open source community by making ncnn available.
+//
+// Copyright (C) 2018 THL A29 Limited, a Tencent company. All rights reserved.
+//
+// Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
+// in compliance with the License. You may obtain a copy of the License at
+//
+// https://opensource.org/licenses/BSD-3-Clause
+//
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
+#ifndef NCNN_GPU_H
+#define NCNN_GPU_H
+
+#include "platform.h"
+
+#if NCNN_VULKAN
+
+#include <vulkan/vulkan.h>
+
+namespace ncnn {
+
+// instance
+int create_gpu_instance();
+void destroy_gpu_instance();
+
+// get info
+int get_gpu_count();
+
+class GpuInfo
+{
+public:
+    // vulkan physical device
+    VkPhysicalDevice physical_device;
+
+    // 0 = discrete gpu
+    // 1 = integrated gpu
+    // 2 = virtual gpu
+    // 3 = cpu
+    int type;
+
+    // hardware capability
+    int max_shared_memory_size;
+    int max_image_dimension[3];
+    int max_workgroup_count[3];
+    int max_workgroup_invocations;
+    int max_workgroup_size[3];
+
+    // TODO extension capability
+    int support_VK_KHR_descriptor_update_template;
+    int support_VK_KHR_push_descriptor;
+    int support_VK_AMD_gpu_shader_half_float;
+
+    // runtime
+    uint32_t compute_queue_index;
+
+    uint32_t device_local_memory_index;
+    uint32_t host_visible_memory_index;
+};
+
+const GpuInfo& get_gpu_info(int i);
+
+// device
+int create_gpu_device(int i);
+void destroy_gpu_device(int i);
+
+// get device
+VkDevice get_gpu_device(int i);
+
+// TODO shader management
+// VkShaderModule shaderModule;
+// VkDescriptorSetLayout descriptorSetLayout;
+// VkPipelineLayout pipelineLayout;
+
+} // namespace ncnn
+
+#endif // NCNN_VULKAN
+
+#endif // NCNN_GPU_H
