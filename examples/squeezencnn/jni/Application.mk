c1c72ec1b76554eb7d38acefc429f67d72b84e4d
nihuini <nihuini@tencent.com>
2019-03-18 17:00:36 +0800

    add gpu button for squeezencnn sample, require api android-24 and ndk-r18b

--- examples/squeezencnn/jni/Application.mk
+++ examples/squeezencnn/jni/Application.mk
@@ -1,7 +1,3 @@
-
-# APP_STL := stlport_static
-APP_STL := gnustl_static
-# APP_ABI := armeabi armeabi-v7a
+APP_STL := c++_static
 APP_ABI := armeabi-v7a arm64-v8a
-APP_PLATFORM := android-9
-# NDK_TOOLCHAIN_VERSION := 4.9
+APP_PLATFORM := android-24
