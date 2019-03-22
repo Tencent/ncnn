de540b04ef5d2dc354f8c118cd53be3721509374
ShuangLiu1992 <ShuangLiu1992@users.noreply.github.com>
2019-03-22 15:48:48 +0000

    Update quantize_arm.cpp

--- src/layer/arm/quantize_arm.cpp
+++ src/layer/arm/quantize_arm.cpp
@@ -125,7 +125,7 @@ int Quantize_arm::forward(const Mat& bottom_blob, Mat& top_blob, const Option& o
                 : "0"(nn),
                   "1"(ptr),
                   "2"(outptr),
-                  "r"(scale)     // %6
+                  "r"(scale)      // %6
                 : "cc", "memory", "v0", "v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8"
             );
             }
