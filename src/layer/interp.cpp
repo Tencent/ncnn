24312e33ee046709cb43e1fe31dc5521c9920c44
nihuini <nihuini@tencent.com>
2019-03-27 17:19:05 +0800

    move resize bilinear function to operator

--- src/layer/interp.cpp
+++ src/layer/interp.cpp
@@ -42,6 +42,152 @@ int Interp::load_param(const ParamDict& pd)
     return 0;
 }
 
+static void resize_bilinear_image(const Mat& src, Mat& dst)
+{
+    int w = dst.w;
+    int h = dst.w;
+
+    double scale_x = (double)src.w / w;
+    double scale_y = (double)src.h / h;
+
+    int* buf = new int[w + h + w*2 + h*2];
+
+    int* xofs = buf;//new int[w];
+    int* yofs = buf + w;//new int[h];
+
+    float* alpha = (float*)(buf + w + h);//new float[w * 2];
+    float* beta = (float*)(buf + w + h + w*2);//new float[h * 2];
+
+    float fx;
+    float fy;
+    int sx;
+    int sy;
+
+    for (int dx = 0; dx < w; dx++)
+    {
+        fx = (float)((dx + 0.5) * scale_x - 0.5);
+        sx = floor(fx);
+        fx -= sx;
+
+        if (sx < 0)
+        {
+            sx = 0;
+            fx = 0.f;
+        }
+        if (sx >= src.w - 1)
+        {
+            sx = src.w - 2;
+            fx = 1.f;
+        }
+
+        xofs[dx] = sx;
+
+        alpha[dx*2    ] = 1.f - fx;
+        alpha[dx*2 + 1] = fx;
+    }
+
+    for (int dy = 0; dy < h; dy++)
+    {
+        fy = (float)((dy + 0.5) * scale_y - 0.5);
+        sy = floor(fy);
+        fy -= sy;
+
+        if (sy < 0)
+        {
+            sy = 0;
+            fy = 0.f;
+        }
+        if (sy >= src.h - 1)
+        {
+            sy = src.h - 2;
+            fy = 1.f;
+        }
+
+        yofs[dy] = sy;
+
+        beta[dy*2    ] = 1.f - fy;
+        beta[dy*2 + 1] = fy;
+    }
+
+    // loop body
+    Mat rowsbuf0(w + 1);
+    Mat rowsbuf1(w + 1);
+    float* rows0 = rowsbuf0;
+    float* rows1 = rowsbuf1;
+
+    int prev_sy1 = -1;
+
+    for (int dy = 0; dy < h; dy++ )
+    {
+        int sy = yofs[dy];
+
+        if (sy == prev_sy1)
+        {
+            // hresize one row
+            float* rows0_old = rows0;
+            rows0 = rows1;
+            rows1 = rows0_old;
+            const float* S1 = src.row(sy+1);
+
+            const float* alphap = alpha;
+            float* rows1p = rows1;
+            for (int dx = 0; dx < w; dx++)
+            {
+                int sx = xofs[dx];
+                const float* S1p = S1 + sx;
+
+                float a0 = alphap[0];
+                float a1 = alphap[1];
+                rows1p[dx] = S1p[0]*a0 + S1p[1]*a1;
+
+                alphap += 2;
+            }
+        }
+        else
+        {
+            // hresize two rows
+            const float* S0 = src.row(sy);
+            const float* S1 = src.row(sy+1);
+
+            const float* alphap = alpha;
+            float* rows0p = rows0;
+            float* rows1p = rows1;
+            for (int dx = 0; dx < w; dx++)
+            {
+                int sx = xofs[dx];
+                const float* S0p = S0 + sx;
+                const float* S1p = S1 + sx;
+
+                float a0 = alphap[0];
+                float a1 = alphap[1];
+                rows0p[dx] = S0p[0]*a0 + S0p[1]*a1;
+                rows1p[dx] = S1p[0]*a0 + S1p[1]*a1;
+
+                alphap += 2;
+            }
+        }
+
+        prev_sy1 = sy + 1;
+
+        // vresize
+        float b0 = beta[0];
+        float b1 = beta[1];
+
+        float* rows0p = rows0;
+        float* rows1p = rows1;
+        float* Dp = dst.row(dy);
+        for (int dx = 0; dx < w; dx++)
+        {
+//             D[x] = rows0[x]*b0 + rows1[x]*b1;
+            *Dp++ = *rows0p++ * b0 + *rows1p++ * b1;
+        }
+
+        beta += 2;
+    }
+
+    delete[] buf;
+}
+
 int Interp::forward(const Mat &bottom_blob, Mat &top_blob, const Option& opt) const
 {
     int h = bottom_blob.h;
@@ -105,9 +251,16 @@ int Interp::forward(const Mat &bottom_blob, Mat &top_blob, const Option& opt) co
     }
     else if (resize_type == 2)// bilinear
     {
-        resize_bilinear(bottom_blob, top_blob, ow, oh);
-        return 0;
+        #pragma omp parallel for num_threads(opt.num_threads)
+        for (int q = 0; q < c; ++q)
+        {
+            const Mat src = bottom_blob.channel(q);
+            Mat dst = top_blob.channel(q);
 
+            resize_bilinear_image(src, dst);
+        }
+
+        return 0;
     }
     else
     {
