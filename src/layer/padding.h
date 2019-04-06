c93d23dffe9374e8825a0a3c2ed23dac125a0f9a
nihui <shuizhuyuanluo@126.com>
2019-04-06 17:18:37 +0800

    dynamic padding and crop offset size

--- src/layer/padding.h
+++ src/layer/padding.h
@@ -28,14 +28,19 @@ public:
 
     virtual int forward(const Mat& bottom_blob, Mat& top_blob, const Option& opt) const;
 
+    virtual int forward(const std::vector<Mat>& bottom_blobs, std::vector<Mat>& top_blobs, const Option& opt) const;
+
 #if NCNN_VULKAN
     virtual int create_pipeline();
     virtual int destroy_pipeline();
 
     virtual int forward(const VkMat& bottom_blob, VkMat& top_blob, VkCompute& cmd, const Option& opt) const;
+
+    virtual int forward(const std::vector<VkMat>& bottom_blobs, std::vector<VkMat>& top_blobs, VkCompute& cmd, const Option& opt) const;
 #endif // NCNN_VULKAN
 
 public:
+    // -233 = dynamic offset from reference blob
     int top;
     int bottom;
     int left;
