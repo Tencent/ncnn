2560bceb8b028cf2db01c9b0c365d8cb82a1b182
BUG1989 <248857878@qq.com>
2019-05-31 17:05:27 +0800

    optimize the x86 winograd F43 with avx2

--- src/layer/arm/quantize_arm.cpp
+++ src/layer/arm/quantize_arm.cpp
@@ -1,6 +1,6 @@
 // Tencent is pleased to support the open source community by making ncnn available.
 //
-// Copyright (C) 2018 SenseNets Technology Ltd. All rights reserved.
+// Copyright (C) 2019 BUG1989. All rights reserved.
 // Copyright (C) 2018 THL A29 Limited, a Tencent company. All rights reserved.
 //
 // Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
