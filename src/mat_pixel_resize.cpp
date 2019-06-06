4a4ae995207e8bcfdfc7383244d1a0aa0648fda3
nihuini <nihuini@tencent.com>
2019-06-06 17:46:14 +0800

    fix out of range load and store in bilinear resize c2/c3 neon block

--- src/mat_pixel_resize.cpp
+++ src/mat_pixel_resize.cpp
@@ -358,8 +358,8 @@ void resize_bilinear_c2(const unsigned char* src, int srcw, int srch, unsigned c
 #undef SATURATE_CAST_SHORT
 
     // loop body
-    Mat rowsbuf0(w*2, (size_t)2u);
-    Mat rowsbuf1(w*2, (size_t)2u);
+    Mat rowsbuf0(w*2+2, (size_t)2u);
+    Mat rowsbuf1(w*2+2, (size_t)2u);
     short* rows0 = (short*)rowsbuf0.data;
     short* rows1 = (short*)rowsbuf1.data;
 
@@ -391,7 +391,8 @@ void resize_bilinear_c2(const unsigned char* src, int srcw, int srch, unsigned c
 #if __ARM_NEON
                 int16x4_t _a0a1XX = vld1_s16(ialphap);
                 int16x4_t _a0a0a1a1 = vzip_s16(_a0a1XX, _a0a1XX).val[0];
-                uint8x8_t _S1 = vld1_u8(S1p);
+                uint8x8_t _S1;
+                _S1 = vreinterpret_u8_u32(vld1_lane_u32((const unsigned int*)S1p, vreinterpret_u32_u8(_S1), 0));
                 int16x8_t _S116 = vreinterpretq_s16_u16(vmovl_u8(_S1));
                 int16x4_t _S1lowhigh = vget_low_s16(_S116);
                 int32x4_t _S1ma0a1 = vmull_s16(_S1lowhigh, _a0a0a1a1);
@@ -431,8 +432,10 @@ void resize_bilinear_c2(const unsigned char* src, int srcw, int srch, unsigned c
 #if __ARM_NEON
                 int16x4_t _a0 = vdup_n_s16(a0);
                 int16x4_t _a1 = vdup_n_s16(a1);
-                uint8x8_t _S0 = vld1_u8(S0p);
-                uint8x8_t _S1 = vld1_u8(S1p);
+                uint8x8_t _S0;
+                uint8x8_t _S1;
+                _S0 = vreinterpret_u8_u32(vld1_lane_u32((const unsigned int*)S0p, vreinterpret_u32_u8(_S0), 0));
+                _S1 = vreinterpret_u8_u32(vld1_lane_u32((const unsigned int*)S1p, vreinterpret_u32_u8(_S1), 0));
                 int16x8_t _S016 = vreinterpretq_s16_u16(vmovl_u8(_S0));
                 int16x8_t _S116 = vreinterpretq_s16_u16(vmovl_u8(_S1));
                 int16x4_t _S0lowhigh = vget_low_s16(_S016);
@@ -651,8 +654,8 @@ void resize_bilinear_c3(const unsigned char* src, int srcw, int srch, unsigned c
 #undef SATURATE_CAST_SHORT
 
     // loop body
-    Mat rowsbuf0(w*3, (size_t)2u);
-    Mat rowsbuf1(w*3, (size_t)2u);
+    Mat rowsbuf0(w*3+1, (size_t)2u);
+    Mat rowsbuf1(w*3+1, (size_t)2u);
     short* rows0 = (short*)rowsbuf0.data;
     short* rows1 = (short*)rowsbuf1.data;
 
@@ -686,7 +689,9 @@ void resize_bilinear_c3(const unsigned char* src, int srcw, int srch, unsigned c
 #if __ARM_NEON
                 int16x4_t _a0 = vdup_n_s16(a0);
                 int16x4_t _a1 = vdup_n_s16(a1);
-                uint8x8_t _S1 = vld1_u8(S1p);
+                uint8x8_t _S1;
+                _S1 = vreinterpret_u8_u32(vld1_lane_u32((const unsigned int*)S1p, vreinterpret_u32_u8(_S1), 0));
+                _S1 = vreinterpret_u8_u16(vld1_lane_u16((const unsigned short*)(S1p+4), vreinterpret_u16_u8(_S1), 2));
                 int16x8_t _S116 = vreinterpretq_s16_u16(vmovl_u8(_S1));
                 int16x4_t _S1low = vget_low_s16(_S116);
                 int16x4_t _S1high = vext_s16(_S1low, vget_high_s16(_S116), 3);
@@ -724,8 +729,12 @@ void resize_bilinear_c3(const unsigned char* src, int srcw, int srch, unsigned c
 #if __ARM_NEON
                 int16x4_t _a0 = vdup_n_s16(a0);
                 int16x4_t _a1 = vdup_n_s16(a1);
-                uint8x8_t _S0 = vld1_u8(S0p);
-                uint8x8_t _S1 = vld1_u8(S1p);
+                uint8x8_t _S0;
+                uint8x8_t _S1;
+                _S0 = vreinterpret_u8_u32(vld1_lane_u32((const unsigned int*)S0p, vreinterpret_u32_u8(_S0), 0));
+                _S1 = vreinterpret_u8_u32(vld1_lane_u32((const unsigned int*)S1p, vreinterpret_u32_u8(_S1), 0));
+                _S0 = vreinterpret_u8_u16(vld1_lane_u16((const unsigned short*)(S0p+4), vreinterpret_u16_u8(_S0), 2));
+                _S1 = vreinterpret_u8_u16(vld1_lane_u16((const unsigned short*)(S1p+4), vreinterpret_u16_u8(_S1), 2));
                 int16x8_t _S016 = vreinterpretq_s16_u16(vmovl_u8(_S0));
                 int16x8_t _S116 = vreinterpretq_s16_u16(vmovl_u8(_S1));
                 int16x4_t _S0low = vget_low_s16(_S016);
