cb994315a430200156157f4cc0f1475215d3c197
akemimadoka <jxhzq1996@126.com>
2019-07-18 17:49:53 +0800

    Make cmake clean

--- cmake/ncnn-FindOpenMP.cmake
+++
@@ -1,1 +0,0 @@
-find_package(OpenMP)
--- cmake/ncnn-FindVulkan.cmake
+++
@@ -1,5 +0,0 @@
-find_package(Vulkan REQUIRED)
-
-add_library(Vulkan UNKNOWN IMPORTED)
-set_target_properties(Vulkan PROPERTIES IMPORTED_LOCATION ${Vulkan_LIBRARY})
-set_target_properties(Vulkan PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${Vulkan_INCLUDE_DIR})
