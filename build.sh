f118c328e7b806ca49d0251a2b1d96bb40013592
nihuini <nihuini@tencent.com>
2019-09-06 15:26:40 +0800

    bump android platform version to 19, that makes ndk-r19c happy

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 ##### android armv7
 mkdir -p build-android-armv7
 pushd build-android-armv7
-cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake -DANDROID_ABI="armeabi-v7a" -DANDROID_ARM_NEON=ON -DANDROID_PLATFORM=android-14 ..
+cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake -DANDROID_ABI="armeabi-v7a" -DANDROID_ARM_NEON=ON -DANDROID_PLATFORM=android-19 ..
 make
 make install
 popd
@@ -19,7 +19,7 @@ popd
 ##### android armv7 without neon
 mkdir -p build-android-armv7-without-neon
 pushd build-android-armv7-without-neon
-cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake -DANDROID_ABI="armeabi-v7a" -DANDROID_PLATFORM=android-14 ..
+cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake -DANDROID_ABI="armeabi-v7a" -DANDROID_PLATFORM=android-19 ..
 make
 make install
 popd
