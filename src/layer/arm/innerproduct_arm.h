b37ecab630cf2bed1c3848a81962f35cd926ef3c
nihui <shuizhuyuanluo@126.com>
2019-10-08 22:48:19 +0800

    auto flatten before innerproduct pack4

--- src/layer/arm/innerproduct_arm.h
+++ src/layer/arm/innerproduct_arm.h
@@ -25,10 +25,13 @@ public:
     InnerProduct_arm();
 
     virtual int create_pipeline(const Option& opt);
+    virtual int destroy_pipeline(const Option& opt);
 
     virtual int forward(const Mat& bottom_blob, Mat& top_blob, const Option& opt) const;
 
 public:
+    ncnn::Layer* flatten;
+
     // pack4
     Mat weight_data_pack4;
     Mat weight_data_pack1to4;
