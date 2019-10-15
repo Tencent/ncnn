01955232b7c5e5e8fa7486fe46b75249bef914e0
tpoisonooo <khj.application@aliyun.com>
2019-10-15 18:33:15 +0000

    rewrite reorder b and test passed, 10% faster than im2row version

--- src/layer/arm/convolution_sgemm_int8.h
+++ src/layer/arm/convolution_sgemm_int8.h
@@ -23,6 +23,255 @@ extern "C" {
     void int8kernel_m1_requant(int8_t* dst, const int8_t* sa, const int8_t* sb, size_t k, size_t n, size_t ldc, float* scales, float* bias);
 }
 
+static void reorder_b(int8_t* b, int8_t* sb, const int k, const int n, const int ldx) {
+
+    int i = 0;
+    for (; i+3 < n; i += 4) {
+        int8_t *p0 = b + i;
+        int8_t *p1 = b + 1 * ldx + i;
+        int8_t *p2 = b + 2 * ldx + i;
+        int8_t *p3 = b + 3 * ldx + i;
+
+        int8_t *p4 = b + 4 * ldx + i;
+        int8_t *p5 = b + 5 * ldx + i;
+        int8_t *p6 = b + 6 * ldx + i;
+        int8_t *p7 = b + 7 * ldx + i;
+
+        int j = 0;
+        for (; j+7 < k; j += 8) {
+            sb[0]  = p0[0];
+            sb[1]  = p1[0];
+            sb[2]  = p2[0];
+            sb[3]  = p3[0];
+            sb[4]  = p4[0];
+            sb[5]  = p5[0];
+            sb[6]  = p6[0];
+            sb[7]  = p7[0];
+
+            sb[8]  = p0[1];
+            sb[9]  = p1[1];
+            sb[10] = p2[1];
+            sb[11] = p3[1];
+            sb[12] = p4[1];
+            sb[13] = p5[1];
+            sb[14] = p6[1];
+            sb[15] = p7[1];
+
+            sb[16] = p0[2];
+            sb[17] = p1[2];
+            sb[18] = p2[2];
+            sb[19] = p3[2];
+            sb[20] = p4[2];
+            sb[21] = p5[2];
+            sb[22] = p6[2];
+            sb[23] = p7[2];
+
+            sb[24] = p0[3];
+            sb[25] = p1[3];
+            sb[26] = p2[3];
+            sb[27] = p3[3];
+            sb[28] = p4[3];
+            sb[29] = p5[3];
+            sb[30] = p6[3];
+            sb[31] = p7[3];
+
+            sb += 32;
+            p0 += 8 * ldx;
+            p1 += 8 * ldx;
+            p2 += 8 * ldx;
+            p3 += 8 * ldx;
+            p4 += 8 * ldx;
+            p5 += 8 * ldx;
+            p6 += 8 * ldx;
+            p7 += 8 * ldx;
+        }
+        for (; j+3 < k; j += 4) {
+            sb[0]  = p0[0];
+            sb[1]  = p1[0];
+            sb[2]  = p2[0];
+            sb[3]  = p3[0];
+
+            sb[4]  = p0[1];
+            sb[5]  = p1[1];
+            sb[6]  = p2[1];
+            sb[7]  = p3[1];
+
+            sb[8]  = p0[2];
+            sb[9]  = p1[2];
+            sb[10] = p2[2];
+            sb[11] = p3[2];
+
+            sb[12] = p0[3];
+            sb[13] = p1[3];
+            sb[14] = p2[3];
+            sb[15] = p3[3];
+
+            sb += 16;
+            p0 += 4 * ldx;
+            p1 += 4 * ldx;
+            p2 += 4 * ldx;
+            p3 += 4 * ldx;
+        }
+        for (; j+1 < k; j += 2) {
+            sb[0] = p0[0];
+            sb[1] = p1[0];
+            sb[2] = p0[1];
+            sb[3] = p1[1];
+            sb[4] = p0[2];
+            sb[5] = p1[2];
+            sb[6] = p0[3];
+            sb[7] = p1[3];
+
+            sb += 8;
+            p0 += 2 * ldx;
+            p1 += 2 * ldx;
+        }
+        for (; j < k; ++j) {
+            sb[0] = p0[0];
+            sb[1] = p0[1];
+            sb[2] = p0[2];
+            sb[3] = p0[3];
+
+            sb += 4;
+            p0 += ldx;
+        }
+    }
+    for (; i+1 < n; i += 2) {
+        int8_t *p0 = b + i;
+        int8_t *p1 = b + 1 * ldx + i;
+        int8_t *p2 = b + 2 * ldx + i;
+        int8_t *p3 = b + 3 * ldx + i;
+
+        int8_t *p4 = b + 4 * ldx + i;
+        int8_t *p5 = b + 5 * ldx + i;
+        int8_t *p6 = b + 6 * ldx + i;
+        int8_t *p7 = b + 7 * ldx + i;
+
+        int j = 0;
+        for (; j+7 < k; j += 8) {
+            sb[0]  = p0[0];
+            sb[1]  = p1[0];
+            sb[2]  = p2[0];
+            sb[3]  = p3[0];
+            sb[4]  = p4[0];
+            sb[5]  = p5[0];
+            sb[6]  = p6[0];
+            sb[7]  = p7[0];
+
+            sb[8]  = p0[1];
+            sb[9]  = p1[1];
+            sb[10] = p2[1];
+            sb[11] = p3[1];
+            sb[12] = p4[1];
+            sb[13] = p5[1];
+            sb[14] = p6[1];
+            sb[15] = p7[1];
+
+            sb += 16;
+            p0 += 8 * ldx;
+            p1 += 8 * ldx;
+            p2 += 8 * ldx;
+            p3 += 8 * ldx;
+            p4 += 8 * ldx;
+            p5 += 8 * ldx;
+            p6 += 8 * ldx;
+            p7 += 8 * ldx;
+        }
+        for (; j+3 < k; j += 4) {
+            sb[0]  = p0[0];
+            sb[1]  = p1[0];
+            sb[2]  = p2[0];
+            sb[3]  = p3[0];
+
+            sb[4]  = p0[1];
+            sb[5]  = p1[1];
+            sb[6]  = p2[1];
+            sb[7]  = p3[1];
+
+            sb += 8;
+            p0 += 4 * ldx;
+            p1 += 4 * ldx;
+            p2 += 4 * ldx;
+            p3 += 4 * ldx;
+        }
+        for (; j+1 < k; j += 2) {
+            sb[0] = p0[0];
+            sb[1] = p1[0];
+            sb[2] = p0[1];
+            sb[3] = p1[1];
+
+            sb += 4;
+            p0 += 2 * ldx;
+            p1 += 2 * ldx;
+        }
+        for (; j < k; ++j) {
+            sb[0] = p0[0];
+            sb[1] = p0[1];
+
+            sb += 2;
+            p0 += ldx;
+        }
+    }
+    for (; i < n; ++i) {
+        int8_t *p0 = b + i;
+        int8_t *p1 = b + 1 * ldx + i;
+        int8_t *p2 = b + 2 * ldx + i;
+        int8_t *p3 = b + 3 * ldx + i;
+        int8_t *p4 = b + 4 * ldx + i;
+        int8_t *p5 = b + 5 * ldx + i;
+        int8_t *p6 = b + 6 * ldx + i;
+        int8_t *p7 = b + 7 * ldx + i;
+
+        int j = 0;
+        for (; j+7 < k; j += 8) {
+            sb[0]  = p0[0];
+            sb[1]  = p1[0];
+            sb[2]  = p2[0];
+            sb[3]  = p3[0];
+            sb[4]  = p4[0];
+            sb[5]  = p5[0];
+            sb[6]  = p6[0];
+            sb[7]  = p7[0];
+
+            sb += 8;
+            p0 += 8 * ldx;
+            p1 += 8 * ldx;
+            p2 += 8 * ldx;
+            p3 += 8 * ldx;
+            p4 += 8 * ldx;
+            p5 += 8 * ldx;
+            p6 += 8 * ldx;
+            p7 += 8 * ldx;
+        }
+        for (; j+3 < k; j += 4) {
+            sb[0]  = p0[0];
+            sb[1]  = p1[0];
+            sb[2]  = p2[0];
+            sb[3]  = p3[0];
+
+            sb += 4;
+            p0 += 4 * ldx;
+            p1 += 4 * ldx;
+            p2 += 4 * ldx;
+            p3 += 4 * ldx;
+        }
+        for (; j+1 < k; j += 2) {
+            sb[0] = p0[0];
+            sb[1] = p1[0];
+
+            sb += 2;
+            p0 += 2 * ldx;
+            p1 += 2 * ldx;
+        }
+        for (; j < k; ++j) {
+            sb[0] = p0[0];
+
+            sb += 1;
+            p0 += ldx;
+        }
+    }
+}
+
 static void conv_im2col_sgemm_transform_kernel_int8_neon(const Mat& _kernel, Mat& kernel_tm, int inch, int outch, int maxk)
 {
     const int m = outch;
@@ -80,23 +329,9 @@ static void conv_im2col_sgemm_int8_neon(const Mat &bottom_blob, Mat &top_blob, c
 
     ncnn::Mat bottom_tm(k * n, (size_t)1u, opt.workspace_allocator);
     {
-        // split input into block8x4
-        // first tranpose, then pack with block4x8
-        ncnn::Mat bottom_trans(k * n, (size_t)1u, opt.workspace_allocator);
-        
         int8_t *pData    = static_cast<int8_t*>(bottom_im2col.data);
-        int8_t *pTrans   = static_cast<int8_t*>(bottom_trans.data);
         int8_t *pReorder = static_cast<int8_t*>(bottom_tm.data);
-        // transpose
-        // #pragma omp parallel for num_threads(opt.num_threads)
-        for (int i = 0; i < k; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                pTrans[j * k + i] = pData[i * n + j];
-            }
-        }
-        reorder_a(pTrans, pReorder, n, k, k);
+        reorder_b(pData, pReorder, k, n, n);
     }
     // GEMM
     int32_t *pc = static_cast<int32_t*>(top_blob.data);
