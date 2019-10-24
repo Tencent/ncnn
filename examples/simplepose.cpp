9bf30d1040be1c524368d496f9f53d2694d62ad1
zoezoe <fuzuoyi@alumni.hust.edu.cn>
2019-10-24 16:47:57 +0800

    Update simplepose.cpp
    opencv 4
    
    CV_LOAD_IMAGE_COLOR error
    
    fix it

--- examples/simplepose.cpp
+++ examples/simplepose.cpp
@@ -18,6 +18,7 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/imgcodecs/legacy/constants_c.h>
 
 #include "net.h"
 #include "gpu.h"
