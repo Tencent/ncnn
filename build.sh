e54a5c97e6b50ff242f7c526362d0587c225674f
nihuini <nihuini@tencent.com>
2019-11-12 19:36:39 +0800

    android x86 x86_64 cpu/gpu build

--- build.sh
+++ build.sh
@@ -24,6 +24,22 @@ make
 make install
 popd
 
+##### android x86
+mkdir -p build-android-x86
+pushd build-android-x86
+cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake -DANDROID_ABI="x86" -DANDROID_PLATFORM=android-19 ..
+make
+make install
+popd
+
+##### android x86_64
+mkdir -p build-android-x86_64
+pushd build-android-x86_64
+cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake -DANDROID_ABI="x86_64" -DANDROID_PLATFORM=android-21 ..
+make
+make install
+popd
+
 ##### android armv7 vulkan
 mkdir -p build-android-armv7-vulkan
 pushd build-android-armv7-vulkan
@@ -36,6 +52,22 @@ popd
 mkdir -p build-android-aarch64-vulkan
 pushd build-android-aarch64-vulkan
 cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake -DANDROID_ABI="arm64-v8a" -DANDROID_PLATFORM=android-24 -DNCNN_VULKAN=ON ..
+make
+make install
+popd
+
+##### android x86 vulkan
+mkdir -p build-android-x86-vulkan
+pushd build-android-x86-vulkan
+cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake -DANDROID_ABI="x86" -DANDROID_PLATFORM=android-24 -DNCNN_VULKAN=ON ..
+make
+make install
+popd
+
+##### android x86_64 vulkan
+mkdir -p build-android-x86_64-vulkan
+pushd build-android-x86_64-vulkan
+cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake -DANDROID_ABI="x86_64" -DANDROID_PLATFORM=android-24 -DNCNN_VULKAN=ON ..
 make
 make install
 popd
