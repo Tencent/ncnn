a1a1304d218f44c724f21306e2090bc19b11a19a
tpoisonooo <khj.application@aliyun.com>
2019-12-20 13:28:49 +0800

    Update gemm_symm_int8.h
    add license

--- src/layer/arm/gemm_symm_int8.h
+++ src/layer/arm/gemm_symm_int8.h
@@ -1,3 +1,19 @@
+// chgemm is pleased to support the open source community by supporting ncnn available.
+//
+// author:tpoisonooo (https://github.com/tpoisonooo/chgemm) implement symmetric int8 GEMM on aarch64.
+//
+// Copyright (C) 2019 tpoisonooo. All rights reserved.
+//
+// Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
+// in compliance with the License. You may obtain a copy of the License at
+//
+// https://opensource.org/licenses/BSD-3-Clause
+//
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
 #if __aarch64__
 
 #define DECOMPOSE_K\
