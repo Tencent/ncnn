0d6e5129963eb285625882965d0922bd1c846de6
Leo <leo@nullptr.com.cn>
2019-12-25 17:16:43 +0800

    Add some mips layers (#1442)
    * Add mips architecture optimization support
    
    * Add mipsisa32r6 toolchain files
    
    * Add AbsVal_mips layer
    
    * Remove unused constructor override
    
    * Keep the same programming style with upstream
    
    * Add Bias_mips layer
    
    * Add Clip_mips layer
    
    * Update copyright header
    
    * Add mips-mti-gnu compile to travis
    
    * Add codescape mips toolchain config
    
    * Update the toolchain comments
    
    * Fix conflicts in travis config file
    
    * Add travis status in README.md

---
+++ toolchains/mips-mti-linux-gnu.toolchain.cmake
@@ -0,0 +1,19 @@
+# set cross-compiled system type, it's better not use the type which cmake cannot recognized.
+SET ( CMAKE_SYSTEM_NAME Linux )
+SET ( CMAKE_SYSTEM_PROCESSOR mips )
+# make sure mips-mti-linux-gnu-gcc and mips-mti-linux-gnu-g++ can be found in $PATH:
+SET ( CMAKE_C_COMPILER "mips-mti-linux-gnu-gcc" )
+SET ( CMAKE_CXX_COMPILER "mips-mti-linux-gnu-g++" )
+
+# set searching rules for cross-compiler
+SET ( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER )
+SET ( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY )
+SET ( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY )
+
+# set ${CMAKE_C_FLAGS} and ${CMAKE_CXX_FLAGS}flag for cross-compiled process
+SET ( CMAKE_CXX_FLAGS "-std=c++11 -march=mips32r6 -mmsa -fopenmp ${CMAKE_CXX_FLAGS}" )
+
+# other settings
+add_definitions(-D__MIPS_MSA)
+add_definitions(-DLINUX)
+SET ( LINUX true)
