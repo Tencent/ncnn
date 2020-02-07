d9041363515ac9531e685b842954a2d033db1c43
corleonechensiyu <39826330+corleonechensiyu@users.noreply.github.com>
2020-02-07 18:19:44 +0800

    Update lstm.cpp

--- src/layer/lstm.cpp
+++ src/layer/lstm.cpp
@@ -21,7 +21,7 @@ DEFINE_LAYER_CREATOR(LSTM)
 
 LSTM::LSTM()
 {
-    one_blob_only = false;
+    one_blob_only = true;
     support_inplace = false;
 }
 
@@ -38,52 +38,47 @@ int LSTM::load_model(const ModelBin& mb)
     int size = weight_data_size / num_output / 4;
 
     // raw weight data
-    weight_xc_data = mb.load(size, num_output * 4, 0);
-    if (weight_xc_data.empty())
+    weight_i_data = mb.load(size, num_output * 4, 0);
+    if (weight_i_data.empty())
         return -100;
 
-   
-    bias_c_data = mb.load(4, num_output, 0);
+    weight_h_data = mb.load(num_output, num_output * 4, 0);
+    if (weight_h_data.empty())
+        return -100;
+
+    bias_c_data = mb.load(num_output*4, 0);
     if (bias_c_data.empty())
         return -100;
 
-    weight_hc_data = mb.load(num_output, num_output * 4, 0);
-    if (weight_hc_data.empty())
-        return -100;
 
     return 0;
 }
 
-int LSTM::forward(const std::vector<Mat>& bottom_blobs, std::vector<Mat>& top_blobs, const Option& opt) const
+int LSTM::forward(const Mat& bottom_blob, Mat& top_blob, const Option& opt) const
 {
-    // size x T
-    const Mat& input_blob = bottom_blobs[0];
+    size_t elemsize = bottom_blob.elemsize;
 
-    size_t elemsize = input_blob.elemsize;
-
-    // T, 0 or 1 each
-    const Mat& cont_blob = bottom_blobs[1];
-
-    int T = input_blob.h;
-    int size = input_blob.w;
+    int T = bottom_blob.c;
+    int N=bottom_blob.h;
+    int size = bottom_blob.w;
 
     // initial hidden state
-    Mat hidden(num_output, 4u, opt.workspace_allocator);
+    Mat hidden(num_output, 4u, opt.workspace_allocator);//256
     if (hidden.empty())
         return -100;
     hidden.fill(0.f);
 
     // internal cell state
-    Mat cell(num_output, 4u, opt.workspace_allocator);
+    Mat cell(num_output, 4u, opt.workspace_allocator);//256
     if (cell.empty())
         return -100;
     // 4 x num_output
-    Mat gates(4, num_output, 4u, opt.workspace_allocator);
+    Mat gates(num_output,4, 4u, opt.workspace_allocator);//256 4
     if (gates.empty())
         return -100;
 
-    Mat& top_blob = top_blobs[0];
-    top_blob.create(num_output, T, elemsize, opt.blob_allocator);
+ 
+    top_blob.create(num_output,N, T, elemsize, opt.blob_allocator);
     if (top_blob.empty())
         return -100;
 
@@ -96,8 +91,8 @@ int LSTM::forward(const std::vector<Mat>& bottom_blobs, std::vector<Mat>& top_bl
         //                0       otherwise
         // calculate hidden
         // gate_input_t := W_hc * h_conted_{t-1} + W_xc * x_t + b_c
-        const int cont = ((const int*)cont_blob)[t];
-        const float* x = input_blob.row(t);
+        const bool cont = t > 0;
+        const float* x = bottom_blob.channel(t);
         for (int q=0; q<num_output; q++)
         {
             //float h_cont = cont ? hidden[q] : 0.f;
@@ -107,43 +102,54 @@ int LSTM::forward(const std::vector<Mat>& bottom_blobs, std::vector<Mat>& top_bl
             const float* O_bias_c_data_ptr = (const float*)bias_c_data + 2 * num_output;
             const float* G_bias_c_data_ptr = (const float*)bias_c_data + 3 * num_output;
 
-            //const float* bias_c_data_ptr = (const float*)bias_c_data + 4 * q;
-            float* gates_data = (float*)gates + 4 * q;
+            float* gates_data_I = (float*)gates;
+            float* gates_data_F = (float*)gates+ num_output;
+            float* gates_data_O = (float*)gates+ 2 * num_output;
+            float* gates_data_G = (float*)gates+ 3 * num_output;
+
+
 
             // gate I F O G
-            const float* weight_hc_data_I = (const float*)weight_hc_data + weight_hc_data.w * q;
-            const float* weight_xc_data_I = (const float*)weight_xc_data + weight_xc_data.w * q;
-            const float* weight_hc_data_F = (const float*)weight_hc_data + weight_hc_data.w * q + num_output * num_output;
-            const float* weight_xc_data_F = (const float*)weight_xc_data + weight_xc_data.w * q + num_output * size;
-            const float* weight_hc_data_O = (const float*)weight_hc_data + weight_hc_data.w * q + num_output * num_output * 2;
-            const float* weight_xc_data_O = (const float*)weight_xc_data + weight_xc_data.w * q + num_output * size * 2;
-            const float* weight_hc_data_G = (const float*)weight_hc_data + weight_hc_data.w * q + num_output * num_output * 3;
-            const float* weight_xc_data_G = (const float*)weight_xc_data + weight_xc_data.w * q + num_output * size * 3;
+            const float* weight_h_data_I = (const float*)weight_h_data + weight_h_data.w * q;
+            const float* weight_i_data_I = (const float*)weight_i_data + weight_i_data.w * q;
+            const float* weight_h_data_F = (const float*)weight_h_data + weight_h_data.w * q + num_output * num_output;
+            const float* weight_i_data_F = (const float*)weight_i_data + weight_i_data.w * q + num_output * size;
+            const float* weight_h_data_O = (const float*)weight_h_data + weight_h_data.w * q + num_output * num_output * 2;
+            const float* weight_i_data_O = (const float*)weight_i_data + weight_i_data.w * q + num_output * size * 2;
+            const float* weight_h_data_G = (const float*)weight_h_data + weight_h_data.w * q + num_output * num_output * 3;
+            const float* weight_i_data_G = (const float*)weight_i_data + weight_i_data.w * q + num_output * size * 3;
 
+           
             float I = I_bias_c_data_ptr[q];
             float F = F_bias_c_data_ptr[q];
             float O = O_bias_c_data_ptr[q];
             float G = G_bias_c_data_ptr[q];
 
+
             for (int i=0; i<size; i++)
             {
-                I += weight_xc_data_I[i] * x[i];
-                F += weight_xc_data_F[i] * x[i];
-                O += weight_xc_data_O[i] * x[i];
-                G += weight_xc_data_G[i] * x[i];
+                I += x[i] * weight_i_data_I[i];
+                F += x[i] * weight_i_data_F[i];
+                O += x[i] * weight_i_data_O[i];
+                G += x[i] * weight_i_data_G[i];
+
+                I += weight_h_data_I[i] * (!cont ? 0: hidden[i]);
+                F += weight_h_data_F[i] * (!cont ? 0: hidden[i]);
+                O += weight_h_data_O[i] * (!cont ? 0: hidden[i]);
+                G += weight_h_data_G[i] * (!cont ? 0: hidden[i]);
             }
 
-            for (int i=0; i<num_output; ++i){
-                I += weight_hc_data_I[i] * (cont == 0? 0: hidden[i]);
-                F += weight_hc_data_F[i] * (cont == 0? 0: hidden[i]);
-                O += weight_hc_data_O[i] * (cont == 0? 0: hidden[i]);
-                G += weight_hc_data_G[i] * (cont == 0? 0: hidden[i]);
-            }
+            // for (int i=0; i<num_output; ++i){
+            //     I += weight_h_data_I[i] * (!cont ? 0: hidden[i]);
+            //     F += weight_h_data_F[i] * (!cont ? 0: hidden[i]);
+            //     O += weight_h_data_O[i] * (!cont ? 0: hidden[i]);
+            //     G += weight_h_data_G[i] * (!cont ? 0: hidden[i]);
 
-            gates_data[0] = I;
-            gates_data[1] = F;
-            gates_data[2] = O;
-            gates_data[3] = G;
+            // }
+            gates_data_I[q] = I;
+            gates_data_F[q] = F;
+            gates_data_O[q] = O;
+            gates_data_G[q] = G;
         }
 
         // lstm unit
@@ -153,15 +159,18 @@ int LSTM::forward(const std::vector<Mat>& bottom_blobs, std::vector<Mat>& top_bl
         // tanh(G)
         // c_t := f_t .* c_{t-1} + i_t .* g_t
         // h_t := o_t .* tanh[c_t]
-        float* output_data = top_blob.row(t);
+        float* output_data = top_blob.channel(t);
         for (int q=0; q<num_output; q++)
         {
-            float* gates_data = (float*)gates + 4 * q;
+            float* gates_data_I = (float*)gates;
+            float* gates_data_F = (float*)gates+ num_output;
+            float* gates_data_O = (float*)gates+ 2 * num_output;
+            float* gates_data_G = (float*)gates+ 3 * num_output;
 
-            float I = gates_data[0];
-            float F = gates_data[1];
-            float O = gates_data[2];
-            float G = gates_data[3];
+            float I = gates_data_I[q];
+            float F = gates_data_F[q];
+            float O = gates_data_O[q];
+            float G = gates_data_G[q];
 	    
             I = 1.f / (1.f + exp(-I));
             F = cont ? 1.f / (1.f + exp(-F)) : 0.f;
@@ -178,6 +187,8 @@ int LSTM::forward(const std::vector<Mat>& bottom_blobs, std::vector<Mat>& top_bl
 
         // no cell output here
     }
+
+
     return 0;
 }
 
