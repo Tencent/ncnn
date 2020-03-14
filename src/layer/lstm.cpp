9fa5b2b249c13fdfbdca054344b527d9f92e8fa7
nihui <shuizhuyuanluo@126.com>
2020-03-14 15:49:02 +0800

    onnx lstm (#1613)
    * lstm with direction
    
    * convert onnx lstm
    
    * lstm works

--- src/layer/lstm.cpp
+++ src/layer/lstm.cpp
@@ -21,7 +21,7 @@ DEFINE_LAYER_CREATOR(LSTM)
 
 LSTM::LSTM()
 {
-    one_blob_only = false;
+    one_blob_only = true;
     support_inplace = false;
 }
 
@@ -29,63 +29,57 @@ int LSTM::load_param(const ParamDict& pd)
 {
     num_output = pd.get(0, 0);
     weight_data_size = pd.get(1, 0);
+    direction = pd.get(2, 0);
 
     return 0;
 }
 
 int LSTM::load_model(const ModelBin& mb)
 {
-    int size = weight_data_size / num_output / 4;
+    int num_directions = direction == 2 ? 2 : 1;
+
+    int size = weight_data_size / num_directions / num_output / 4;
 
     // raw weight data
-    weight_xc_data = mb.load(size, num_output * 4, 0);
+    weight_xc_data = mb.load(size, num_output * 4, num_directions, 0);
     if (weight_xc_data.empty())
         return -100;
 
-   
-    bias_c_data = mb.load(4, num_output, 0);
+    bias_c_data = mb.load(num_output, 4, num_directions, 0);
     if (bias_c_data.empty())
         return -100;
 
-    weight_hc_data = mb.load(num_output, num_output * 4, 0);
+    weight_hc_data = mb.load(num_output, num_output * 4, num_directions, 0);
     if (weight_hc_data.empty())
         return -100;
 
     return 0;
 }
 
-int LSTM::forward(const std::vector<Mat>& bottom_blobs, std::vector<Mat>& top_blobs, const Option& opt) const
+static int lstm(const Mat& bottom_blob, Mat& top_blob, int reverse, const Mat& weight_xc, const Mat& bias_c, const Mat& weight_hc, const Option& opt)
 {
-    // size x T
-    const Mat& input_blob = bottom_blobs[0];
+    int size = bottom_blob.w;
+    int T = bottom_blob.h;
 
-    size_t elemsize = input_blob.elemsize;
-
-    // T, 0 or 1 each
-    const Mat& cont_blob = bottom_blobs[1];
-
-    int T = input_blob.h;
-    int size = input_blob.w;
+    int num_output = top_blob.w;
 
     // initial hidden state
     Mat hidden(num_output, 4u, opt.workspace_allocator);
     if (hidden.empty())
         return -100;
-    hidden.fill(0.f);
 
     // internal cell state
     Mat cell(num_output, 4u, opt.workspace_allocator);
     if (cell.empty())
         return -100;
+
     // 4 x num_output
     Mat gates(4, num_output, 4u, opt.workspace_allocator);
     if (gates.empty())
         return -100;
 
-    Mat& top_blob = top_blobs[0];
-    top_blob.create(num_output, T, elemsize, opt.blob_allocator);
-    if (top_blob.empty())
-        return -100;
+    hidden.fill(0.f);
+    cell.fill(0.f);
 
     // unroll
     for (int t=0; t<T; t++)
@@ -96,48 +90,54 @@ int LSTM::forward(const std::vector<Mat>& bottom_blobs, std::vector<Mat>& top_bl
         //                0       otherwise
         // calculate hidden
         // gate_input_t := W_hc * h_conted_{t-1} + W_xc * x_t + b_c
-        const int cont = ((const int*)cont_blob)[t];
-        const float* x = input_blob.row(t);
+        int cont = t > 0;
+
+        int ti = reverse ? T-1-t : t;
+
+        const float* x = bottom_blob.row(ti);
         for (int q=0; q<num_output; q++)
         {
-            //float h_cont = cont ? hidden[q] : 0.f;
+            const float* bias_c_I = bias_c.row(0);
+            const float* bias_c_F = bias_c.row(1);
+            const float* bias_c_O = bias_c.row(2);
+            const float* bias_c_G = bias_c.row(3);
 
-            const float* I_bias_c_data_ptr = (const float*)bias_c_data;
-            const float* F_bias_c_data_ptr = (const float*)bias_c_data + num_output;
-            const float* O_bias_c_data_ptr = (const float*)bias_c_data + 2 * num_output;
-            const float* G_bias_c_data_ptr = (const float*)bias_c_data + 3 * num_output;
-
-            //const float* bias_c_data_ptr = (const float*)bias_c_data + 4 * q;
-            float* gates_data = (float*)gates + 4 * q;
+            float* gates_data = gates.row(q);
 
             // gate I F O G
-            const float* weight_hc_data_I = (const float*)weight_hc_data + weight_hc_data.w * q;
-            const float* weight_xc_data_I = (const float*)weight_xc_data + weight_xc_data.w * q;
-            const float* weight_hc_data_F = (const float*)weight_hc_data + weight_hc_data.w * q + num_output * num_output;
-            const float* weight_xc_data_F = (const float*)weight_xc_data + weight_xc_data.w * q + num_output * size;
-            const float* weight_hc_data_O = (const float*)weight_hc_data + weight_hc_data.w * q + num_output * num_output * 2;
-            const float* weight_xc_data_O = (const float*)weight_xc_data + weight_xc_data.w * q + num_output * size * 2;
-            const float* weight_hc_data_G = (const float*)weight_hc_data + weight_hc_data.w * q + num_output * num_output * 3;
-            const float* weight_xc_data_G = (const float*)weight_xc_data + weight_xc_data.w * q + num_output * size * 3;
+            const float* weight_xc_I = weight_xc.row(num_output * 0 + q);
+            const float* weight_xc_F = weight_xc.row(num_output * 1 + q);
+            const float* weight_xc_O = weight_xc.row(num_output * 2 + q);
+            const float* weight_xc_G = weight_xc.row(num_output * 3 + q);
 
-            float I = I_bias_c_data_ptr[q];
-            float F = F_bias_c_data_ptr[q];
-            float O = O_bias_c_data_ptr[q];
-            float G = G_bias_c_data_ptr[q];
+            const float* weight_hc_I = weight_hc.row(num_output * 0 + q);
+            const float* weight_hc_F = weight_hc.row(num_output * 1 + q);
+            const float* weight_hc_O = weight_hc.row(num_output * 2 + q);
+            const float* weight_hc_G = weight_hc.row(num_output * 3 + q);
+
+            float I = bias_c_I[q];
+            float F = bias_c_F[q];
+            float O = bias_c_O[q];
+            float G = bias_c_G[q];
 
             for (int i=0; i<size; i++)
             {
-                I += weight_xc_data_I[i] * x[i];
-                F += weight_xc_data_F[i] * x[i];
-                O += weight_xc_data_O[i] * x[i];
-                G += weight_xc_data_G[i] * x[i];
+                float xi = x[i];
+
+                I += weight_xc_I[i] * xi;
+                F += weight_xc_F[i] * xi;
+                O += weight_xc_O[i] * xi;
+                G += weight_xc_G[i] * xi;
             }
 
-            for (int i=0; i<num_output; ++i){
-                I += weight_hc_data_I[i] * (cont == 0? 0: hidden[i]);
-                F += weight_hc_data_F[i] * (cont == 0? 0: hidden[i]);
-                O += weight_hc_data_O[i] * (cont == 0? 0: hidden[i]);
-                G += weight_hc_data_G[i] * (cont == 0? 0: hidden[i]);
+            for (int i=0; i<num_output; i++)
+            {
+                float h_cont = cont ? hidden[i] : 0.f;
+
+                I += weight_hc_I[i] * h_cont;
+                F += weight_hc_F[i] * h_cont;
+                O += weight_hc_O[i] * h_cont;
+                G += weight_hc_G[i] * h_cont;
             }
 
             gates_data[0] = I;
@@ -153,23 +153,22 @@ int LSTM::forward(const std::vector<Mat>& bottom_blobs, std::vector<Mat>& top_bl
         // tanh(G)
         // c_t := f_t .* c_{t-1} + i_t .* g_t
         // h_t := o_t .* tanh[c_t]
-        float* output_data = top_blob.row(t);
+        float* output_data = top_blob.row(ti);
         for (int q=0; q<num_output; q++)
         {
-            float* gates_data = (float*)gates + 4 * q;
+            const float* gates_data = gates.row(q);
 
             float I = gates_data[0];
             float F = gates_data[1];
             float O = gates_data[2];
             float G = gates_data[3];
-	    
+
             I = 1.f / (1.f + exp(-I));
             F = cont ? 1.f / (1.f + exp(-F)) : 0.f;
             O = 1.f / (1.f + exp(-O));
             G = tanh(G);
 
-            //cell[q] is not initialized and so might be nan, and 0*nan evals to nan
-            float cell2 = cont ? F * cell[q] + I * G  : I * G;
+            float cell2 = F * cell[q] + I * G;
             float H = O * tanh(cell2);
             cell[q] = cell2;
             hidden[q] = H;
@@ -178,6 +177,65 @@ int LSTM::forward(const std::vector<Mat>& bottom_blobs, std::vector<Mat>& top_bl
 
         // no cell output here
     }
+
+    return 0;
+}
+
+int LSTM::forward(const Mat& bottom_blob, Mat& top_blob, const Option& opt) const
+{
+    int T = bottom_blob.h;
+
+    int num_directions = direction == 2 ? 2 : 1;
+
+    top_blob.create(num_output * num_directions, T, 4u, opt.blob_allocator);
+    if (top_blob.empty())
+        return -100;
+
+    // forward
+    if (direction == 0)
+    {
+        int ret = lstm(bottom_blob, top_blob, 0, weight_xc_data.channel(0), bias_c_data.channel(0), weight_hc_data.channel(0), opt);
+        if (ret != 0)
+            return ret;
+    }
+
+    if (direction == 1)
+    {
+        int ret = lstm(bottom_blob, top_blob, 1, weight_xc_data.channel(0), bias_c_data.channel(0), weight_hc_data.channel(0), opt);
+        if (ret != 0)
+            return ret;
+    }
+
+    if (direction == 2)
+    {
+        Mat top_blob_forward(num_output, T, 4u, opt.workspace_allocator);
+        if (top_blob_forward.empty())
+            return -100;
+
+        Mat top_blob_reverse(num_output, T, 4u, opt.workspace_allocator);
+        if (top_blob_reverse.empty())
+            return -100;
+
+        int ret0 = lstm(bottom_blob, top_blob_forward, 0, weight_xc_data.channel(0), bias_c_data.channel(0), weight_hc_data.channel(0), opt);
+        if (ret0 != 0)
+            return ret0;
+
+        int ret1 = lstm(bottom_blob, top_blob_reverse, 1, weight_xc_data.channel(1), bias_c_data.channel(1), weight_hc_data.channel(1), opt);
+        if (ret1 != 0)
+            return ret1;
+
+        // concat w
+        for (int i=0; i<T; i++)
+        {
+            const float* pf = top_blob_forward.row(i);
+            const float* pr = top_blob_reverse.row(i);
+            float* ptr = top_blob.row(i);
+
+            memcpy(ptr, pf, num_output * sizeof(float));
+            memcpy(ptr + num_output, pr, num_output * sizeof(float));
+        }
+    }
+
     return 0;
 }
 
