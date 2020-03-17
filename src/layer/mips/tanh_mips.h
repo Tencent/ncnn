6bf96c4635596d199d444dd61eaead1eb285f156
Leo <leo@nullptr.com.cn>
2020-03-17 18:17:58 +0800

    Add mips layers (#1496)
    * Add mips softmax layer
    
    * Fix bias value error in bias_layer
    
    * Fix max and min value error in clip_layer
    
    * Remove unused elempack variable
    
    * Add sigmoid mips layer
    
    * Add mips tanh math func
    
    * Add mips tanh layer
    
    * Add msa_fill_w_f32 for load float type data
    
    * Remove conv layer header

---
+++ src/layer/mips/tanh_mips.h
@@ -0,0 +1,30 @@
+// Leo is pleased to support the open source community by making ncnn available.
+//
+// Copyright (C) 2020 Leo <leo@nullptr.com.cn>. All rights reserved.
+//
+// Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
+// in compliance with the License. You may obtain a copy of the License at
+//
+// https://opensource.org/licenses/BSD-3-Clause
+//
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
+#ifndef LAYER_TANH_MIPS_H
+#define LAYER_TANH_MIPS_H
+
+#include "tanh.h"
+
+namespace ncnn {
+
+class TanH_mips : virtual public TanH
+{
+public:
+    virtual int forward_inplace(Mat& bottom_top_blob, const Option& opt) const;
+};
+
+} // namespace ncnn
+
+#endif // LAYER_TANH_MIPS_H
