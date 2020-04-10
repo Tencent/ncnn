1eac109f9e1688b58fb463dd9e8f7e2bec60838a
nihuini <nihuini@tencent.com>
2020-04-10 19:52:02 +0800

    bring back layer forward

--- src/layer.cpp
+++ src/layer.cpp
@@ -69,14 +69,32 @@ int Layer::destroy_pipeline(const Option& /*opt*/)
     return 0;
 }
 
-int Layer::forward(const std::vector<Mat>& /*bottom_blobs*/, std::vector<Mat>& /*top_blobs*/, const Option& /*opt*/) const
+int Layer::forward(const std::vector<Mat>& bottom_blobs, std::vector<Mat>& top_blobs, const Option& opt) const
 {
-    return -1;
+    if (!support_inplace)
+        return -1;
+
+    top_blobs = bottom_blobs;
+    for (int i = 0; i < (int)top_blobs.size(); i++)
+    {
+        top_blobs[i] = bottom_blobs[i].clone(opt.blob_allocator);
+        if (top_blobs[i].empty())
+            return -100;
+    }
+
+    return forward_inplace(top_blobs, opt);
 }
 
-int Layer::forward(const Mat& /*bottom_blob*/, Mat& /*top_blob*/, const Option& /*opt*/) const
+int Layer::forward(const Mat& bottom_blob, Mat& top_blob, const Option& opt) const
 {
-    return -1;
+    if (!support_inplace)
+        return -1;
+
+    top_blob = bottom_blob.clone(opt.blob_allocator);
+    if (top_blob.empty())
+        return -100;
+
+    return forward_inplace(top_blob, opt);
 }
 
 int Layer::forward_inplace(std::vector<Mat>& /*bottom_top_blobs*/, const Option& /*opt*/) const
@@ -95,14 +113,28 @@ int Layer::upload_model(VkTransfer& /*cmd*/, const Option& /*opt*/)
     return 0;
 }
 
-int Layer::forward(const std::vector<VkMat>& /*bottom_blobs*/, std::vector<VkMat>& /*top_blobs*/, VkCompute& /*cmd*/, const Option& /*opt*/) const
+int Layer::forward(const std::vector<VkMat>& bottom_blobs, std::vector<VkMat>& top_blobs, VkCompute& cmd, const Option& opt) const
 {
-    return -1;
+    if (!support_inplace)
+        return -1;
+
+    top_blobs.resize(bottom_blobs.size());
+    for (int i = 0; i < (int)top_blobs.size(); i++)
+    {
+        cmd.record_clone(bottom_blobs[i], top_blobs[i], opt);
+    }
+
+    return forward_inplace(top_blobs, cmd, opt);
 }
 
-int Layer::forward(const VkMat& /*bottom_blob*/, VkMat& /*top_blob*/, VkCompute& /*cmd*/, const Option& /*opt*/) const
+int Layer::forward(const VkMat& bottom_blob, VkMat& top_blob, VkCompute& cmd, const Option& opt) const
 {
-    return -1;
+    if (!support_inplace)
+        return -1;
+
+    cmd.record_clone(bottom_blob, top_blob, opt);
+
+    return forward_inplace(top_blob, cmd, opt);
 }
 
 int Layer::forward_inplace(std::vector<VkMat>& /*bottom_top_blobs*/, VkCompute& /*cmd*/, const Option& /*opt*/) const
