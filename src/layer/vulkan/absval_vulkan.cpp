acafa3dcde1d898ccca331c0f7c20b239ee32fd0
nihui <shuizhuyuanluo@126.com>
2020-04-23 09:06:44 +0800

    absval image shader

--- src/layer/vulkan/absval_vulkan.cpp
+++ src/layer/vulkan/absval_vulkan.cpp
@@ -148,4 +148,28 @@ int AbsVal_vulkan::forward_inplace(VkMat& bottom_top_blob, VkCompute& cmd, const
     return 0;
 }
 
+int AbsVal_vulkan::forward_inplace(VkImageMat& bottom_top_blob, VkCompute& cmd, const Option& /*opt*/) const
+{
+    int elempack = bottom_top_blob.elempack;
+
+    std::vector<VkImageMat> bindings(2);
+    bindings[0] = bottom_top_blob;
+    bindings[1] = bottom_top_blob;
+
+    std::vector<vk_constant_type> constants(5);
+    constants[0].i = bottom_top_blob.dims;
+    constants[1].i = bottom_top_blob.w;
+    constants[2].i = bottom_top_blob.h;
+    constants[3].i = bottom_top_blob.c;
+    constants[4].i = 0;//bottom_top_blob.cstep;
+
+    const Pipeline* pipeline = elempack == 8 ? pipeline_absval_pack8
+                             : elempack == 4 ? pipeline_absval_pack4
+                             : pipeline_absval;
+
+    cmd.record_pipeline(pipeline, bindings, constants, bottom_top_blob);
+
+    return 0;
+}
+
 } // namespace ncnn
