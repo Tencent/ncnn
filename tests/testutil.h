19e7179bed47b86e1b55dcdd6172c5063e660c84
nihui <shuizhuyuanluo@126.com>
2020-05-08 18:58:59 +0800

    enable image storage test (#1744)
    * enable image storage test
    
    * update swiftshader, use subzero backend, opt default

--- tests/testutil.h
+++ tests/testutil.h
@@ -673,8 +673,7 @@ int test_layer(const char* layer_type, const ncnn::ParamDict& pd, const std::vec
     opts[1].use_image_storage = false;
     opts[2] = _opt;
     opts[2].use_packing_layout = true;
-    opts[2].use_vulkan_compute = false;//TODO enable me
-    opts[2].use_fp16_packed = false;
+    opts[2].use_fp16_packed = true;
     opts[2].use_fp16_storage = false;
     opts[2].use_bf16_storage = true;
     opts[2].use_shader_pack8 = true;
@@ -761,8 +760,7 @@ int test_layer(const char* layer_type, const ncnn::ParamDict& pd, const std::vec
     opts[1].use_image_storage = false;
     opts[2] = _opt;
     opts[2].use_packing_layout = true;
-    opts[2].use_vulkan_compute = false;//TODO enable me
-    opts[2].use_fp16_packed = false;
+    opts[2].use_fp16_packed = true;
     opts[2].use_fp16_storage = false;
     opts[2].use_bf16_storage = true;
     opts[2].use_shader_pack8 = true;
