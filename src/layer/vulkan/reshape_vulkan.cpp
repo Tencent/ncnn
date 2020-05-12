d8db3e5c835fa9888c748508dfdf9979e33a9a58
nihui <shuizhuyuanluo@126.com>
2020-05-13 00:00:06 +0800

    reshape image shader

--- src/layer/vulkan/reshape_vulkan.cpp
+++ src/layer/vulkan/reshape_vulkan.cpp
@@ -23,6 +23,7 @@ DEFINE_LAYER_CREATOR(Reshape_vulkan)
 Reshape_vulkan::Reshape_vulkan()
 {
     support_vulkan = true;
+    support_image_storage = true;
 
     pipeline_reshape = 0;
     pipeline_reshape_pack4 = 0;
@@ -377,6 +378,178 @@ int Reshape_vulkan::forward(const VkMat& bottom_blob, VkMat& top_blob, VkCompute
     constants[7].i = top_blob.h;
     constants[8].i = top_blob.c;
     constants[9].i = top_blob.cstep;
+
+    if (elempack == 1 && out_elempack == 1)
+    {
+        cmd.record_pipeline(pipeline_reshape, bindings, constants, top_blob);
+    }
+    else if (elempack == 4 && out_elempack == 4)
+    {
+        cmd.record_pipeline(pipeline_reshape_pack4, bindings, constants, top_blob);
+    }
+    else if (elempack == 1 && out_elempack == 4)
+    {
+        cmd.record_pipeline(pipeline_reshape_pack1to4, bindings, constants, top_blob);
+    }
+    else if (elempack == 4 && out_elempack == 1)
+    {
+        cmd.record_pipeline(pipeline_reshape_pack4to1, bindings, constants, bottom_blob);
+    }
+    else if (elempack == 8 && out_elempack == 8)
+    {
+        cmd.record_pipeline(pipeline_reshape_pack8, bindings, constants, top_blob);
+    }
+    else if (elempack == 1 && out_elempack == 8)
+    {
+        cmd.record_pipeline(pipeline_reshape_pack1to8, bindings, constants, top_blob);
+    }
+    else if (elempack == 4 && out_elempack == 8)
+    {
+        cmd.record_pipeline(pipeline_reshape_pack4to8, bindings, constants, top_blob);
+    }
+    else if (elempack == 8 && out_elempack == 4)
+    {
+        cmd.record_pipeline(pipeline_reshape_pack8to4, bindings, constants, top_blob);
+    }
+    else if (elempack == 8 && out_elempack == 1)
+    {
+        cmd.record_pipeline(pipeline_reshape_pack8to1, bindings, constants, bottom_blob);
+    }
+
+    return 0;
+}
+
+int Reshape_vulkan::forward(const VkImageMat& bottom_blob, VkImageMat& top_blob, VkCompute& cmd, const Option& opt) const
+{
+    int dims = bottom_blob.dims;
+    size_t elemsize = bottom_blob.elemsize;
+    int elempack = bottom_blob.elempack;
+    int out_elempack;
+
+    int total = bottom_blob.w * bottom_blob.h * bottom_blob.c * elempack;
+
+    if (ndim == 1)
+    {
+        int _w = w;
+
+        if (_w == 0)
+            _w = dims == 1 ? bottom_blob.w * elempack : bottom_blob.w;
+
+        if (_w == -1)
+            _w = total;
+
+        // TODO permute support
+
+        out_elempack = opt.use_shader_pack8 && _w % 8 == 0 ? 8 : _w % 4 == 0 ? 4 : 1;
+        size_t out_elemsize = elemsize / elempack * out_elempack;
+
+        if (opt.use_fp16_packed && !opt.use_fp16_storage)
+        {
+            if (out_elempack == 8) out_elemsize = 8*2u;
+            if (out_elempack == 4) out_elemsize = 4*2u;
+            if (out_elempack == 1) out_elemsize = 4u;
+        }
+
+        if (dims == 1 && bottom_blob.w == _w && elempack == out_elempack)
+        {
+            top_blob = bottom_blob;
+            return 0;
+        }
+
+        top_blob.create(_w / out_elempack, out_elemsize, out_elempack, opt.blob_vkallocator);
+    }
+    else if (ndim == 2)
+    {
+        int _w = w;
+        int _h = h;
+
+        if (_w == 0)
+            _w = dims == 1 ? bottom_blob.w * elempack : bottom_blob.w;
+        if (_h == 0)
+            _h = dims == 2 ? bottom_blob.h * elempack : bottom_blob.h;
+
+        if (_w == -1)
+            _w = total / _h;
+        if (_h == -1)
+            _h = total / _w;
+
+        out_elempack = opt.use_shader_pack8 && _h % 8 == 0 ? 8 : _h % 4 == 0 ? 4 : 1;
+        size_t out_elemsize = elemsize / elempack * out_elempack;
+
+        if (opt.use_fp16_packed && !opt.use_fp16_storage)
+        {
+            if (out_elempack == 8) out_elemsize = 8*2u;
+            if (out_elempack == 4) out_elemsize = 4*2u;
+            if (out_elempack == 1) out_elemsize = 4u;
+        }
+
+        if (dims == 2 && bottom_blob.w == _w && bottom_blob.h == _h && elempack == out_elempack)
+        {
+            top_blob = bottom_blob;
+            return 0;
+        }
+
+        top_blob.create(_w, _h / out_elempack, out_elemsize, out_elempack, opt.blob_vkallocator);
+    }
+    else // if (ndim == 3)
+    {
+        int _w = w;
+        int _h = h;
+        int _c = c;
+
+        if (_w == 0)
+            _w = dims == 1 ? bottom_blob.w * elempack : bottom_blob.w;
+        if (_h == 0)
+            _h = dims == 2 ? bottom_blob.h * elempack : bottom_blob.h;
+        if (_c == 0)
+            _c = dims == 3 ? bottom_blob.c * elempack : bottom_blob.c;
+
+        if (_w == -1)
+            _w = total / _c / _h;
+        if (_h == -1)
+            _h = total / _c / _w;
+        if (_c == -1)
+            _c = total / _h / _w;
+
+        out_elempack = opt.use_shader_pack8 && _c % 8 == 0 ? 8 : _c % 4 == 0 ? 4 : 1;
+        size_t out_elemsize = elemsize / elempack * out_elempack;
+
+        if (opt.use_fp16_packed && !opt.use_fp16_storage)
+        {
+            if (out_elempack == 8) out_elemsize = 8*2u;
+            if (out_elempack == 4) out_elemsize = 4*2u;
+            if (out_elempack == 1) out_elemsize = 4u;
+        }
+
+        if (dims == 3 && bottom_blob.w == _w && bottom_blob.h == _h && bottom_blob.c == _c && elempack == out_elempack)
+        {
+            top_blob = bottom_blob;
+            top_blob.w = _w;
+            top_blob.h = _h;
+            return 0;
+        }
+
+        top_blob.create(_w, _h, _c / out_elempack, out_elemsize, out_elempack, opt.blob_vkallocator);
+    }
+
+    if (top_blob.empty())
+        return -100;
+
+    std::vector<VkImageMat> bindings(2);
+    bindings[0] = bottom_blob;
+    bindings[1] = top_blob;
+
+    std::vector<vk_constant_type> constants(10);
+    constants[0].i = bottom_blob.dims;
+    constants[1].i = bottom_blob.w;
+    constants[2].i = bottom_blob.h;
+    constants[3].i = bottom_blob.c;
+    constants[4].i = 0;//bottom_blob.cstep;
+    constants[5].i = top_blob.dims;
+    constants[6].i = top_blob.w;
+    constants[7].i = top_blob.h;
+    constants[8].i = top_blob.c;
+    constants[9].i = 0;//top_blob.cstep;
 
     if (elempack == 1 && out_elempack == 1)
     {
