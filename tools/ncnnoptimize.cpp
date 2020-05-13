fb7e9bda423f759d77930363a14be33a0713fcc3
zhiliu6 <zhiliu6@gmail.com>
2020-05-13 16:52:51 +0800

    Add fused convolution and mish layer support. The vulkan version is enabled by default on x86 platform only. ARM/MIPS user may manual enable it through the NCNN_vulkan_fuse_conv_mish option.

--- tools/ncnnoptimize.cpp
+++ tools/ncnnoptimize.cpp
@@ -884,7 +884,7 @@ int NetOptimize::fuse_convolution_activation()
         int j = i + 1;
         for (; j<layer_count; j++)
         {
-            if (layers[j]->type != "ReLU" && layers[j]->type != "Clip" && layers[j]->type != "Sigmoid")
+            if (layers[j]->type != "ReLU" && layers[j]->type != "Clip" && layers[j]->type != "Sigmoid" && layers[j]->type != "Mish")
                 continue;
 
             if (layers[j]->bottoms.size() != 1)
@@ -930,6 +930,10 @@ int NetOptimize::fuse_convolution_activation()
         else if (activation->type == "Sigmoid")
         {
             convolution->activation_type = 4;
+        }
+        else if (activation->type == "Mish")
+        {
+            convolution->activation_type = 5;
         }
 
         int top_blob_index_final = activation->tops[0];
