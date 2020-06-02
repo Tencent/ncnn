f3b182da1f1428c2d9ca0b1c57515dfb83159b2e
nihuini <nihuini@tencent.com>
2020-06-02 11:24:09 +0800

    fix ci build

--- cmake/ncnn_generate_shader_comp_header.cmake
+++ cmake/ncnn_generate_shader_comp_header.cmake
@@ -10,13 +10,13 @@ string(SUBSTRING "${comp_data}" ${version_start} -1 comp_data)
 # remove whitespace
 string(REGEX REPLACE "\n +" "\n" comp_data "${comp_data}")
 
+get_filename_component(SHADER_SRC_NAME_WE ${SHADER_SRC} NAME_WE)
+
 # text to hex
-file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/${SHADER_SRC}.text2hex.txt "${comp_data}")
-file(READ ${CMAKE_CURRENT_BINARY_DIR}/${SHADER_SRC}.text2hex.txt comp_data_hex HEX)
+file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/${SHADER_SRC_NAME_WE}.text2hex.txt "${comp_data}")
+file(READ ${CMAKE_CURRENT_BINARY_DIR}/${SHADER_SRC_NAME_WE}.text2hex.txt comp_data_hex HEX)
 string(REGEX REPLACE "([0-9a-f][0-9a-f])" "0x\\1," comp_data_hex ${comp_data_hex})
 string(FIND "${comp_data_hex}" "," tail_comma REVERSE)
 string(SUBSTRING "${comp_data_hex}" 0 ${tail_comma} comp_data_hex)
-
-get_filename_component(SHADER_SRC_NAME_WE ${SHADER_SRC} NAME_WE)
 
 file(WRITE ${SHADER_COMP_HEADER} "static const char ${SHADER_SRC_NAME_WE}_comp_data[] = {${comp_data_hex}};\n")
