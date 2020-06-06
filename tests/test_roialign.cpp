a1b433bfae73782847d30ddf037433b35fcbdc74
wkcn <wkcn@live.cn>
2020-06-07 07:48:25 +0800

    fix

--- tests/test_roialign.cpp
+++ tests/test_roialign.cpp
@@ -12,6 +12,7 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
+#include <initializer_list>
 #include "testutil.h"
 
 #include "layer.h"
