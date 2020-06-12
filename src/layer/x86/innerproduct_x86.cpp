4a09faaff706dc302d10214f3d78dc3b0ca94640
Tijmen Verhulsdonck <tijmenlv@me.com>
2020-06-12 15:53:35 +0200

    Small fixes and using Fused avx activations

--- src/layer/x86/innerproduct_x86.cpp
+++ src/layer/x86/innerproduct_x86.cpp
@@ -1,16 +1,19 @@
-// Tencent is pleased to support the open source community by making ncnn available.
+// Tencent is pleased to support the open source community by making ncnn
+// available.
 //
 // Copyright (C) 2017 THL A29 Limited, a Tencent company. All rights reserved.
 //
-// Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
-// in compliance with the License. You may obtain a copy of the License at
+// Licensed under the BSD 3-Clause License (the "License"); you may not use this
+// file except in compliance with the License. You may obtain a copy of the
+// License at
 //
 // https://opensource.org/licenses/BSD-3-Clause
 //
-// Unless required by applicable law or agreed to in writing, software distributed
-// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-// CONDITIONS OF ANY KIND, either express or implied. See the License for the
-// specific language governing permissions and limitations under the License.
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
 
 #include "innerproduct_x86.h"
 
@@ -20,9 +23,9 @@
 
 #if NCNN_AVX2
 
+#include "avx_usability.h"
 #include <emmintrin.h>
 #include <immintrin.h>
-#include "avx_usability.h"
 
 #endif // __AVX__
 
@@ -30,316 +33,322 @@ namespace ncnn {
 
 DEFINE_LAYER_CREATOR(InnerProduct_x86)
 
-InnerProduct_x86::InnerProduct_x86()
-{
+InnerProduct_x86::InnerProduct_x86() { flatten = 0; }
+
+int InnerProduct_x86::create_pipeline(const Option &opt) { return 0; }
+
+int InnerProduct_x86::destroy_pipeline(const Option &opt) {
+  if (flatten) {
+    flatten->destroy_pipeline(opt);
+    delete flatten;
     flatten = 0;
+  }
+
+  return 0;
 }
 
-int InnerProduct_x86::create_pipeline(const Option& opt)
-{
+int InnerProduct_x86::forward(const Mat &bottom_blob, Mat &top_blob,
+                              const Option &opt) const {
+  if (opt.use_int8_inference && weight_data.elemsize == (size_t)1u) {
+    // TODO
+    return InnerProduct::forward(bottom_blob, top_blob, opt);
+  }
 
-    return 0;
-}
+  int w = bottom_blob.w;
+  int h = bottom_blob.h;
+  int channels = bottom_blob.c;
+  size_t elemsize = bottom_blob.elemsize;
+  int size = w * h;
 
-int InnerProduct_x86::destroy_pipeline(const Option& opt)
-{
-    if (flatten)
-    {
-        flatten->destroy_pipeline(opt);
-        delete flatten;
-        flatten = 0;
-    }
+  top_blob.create(num_output, elemsize, opt.blob_allocator);
+  if (top_blob.empty())
+    return -100;
 
-    return 0;
-}
+  const float *weight_data_ptr = weight_data;
 
-int InnerProduct_x86::forward(const Mat& bottom_blob, Mat& top_blob, const Option& opt) const
-{
-    if (opt.use_int8_inference && weight_data.elemsize == (size_t)1u)
-    {
-        // TODO
-        return InnerProduct::forward(bottom_blob, top_blob, opt);
-    }
- 
-    int w = bottom_blob.w;
-    int h = bottom_blob.h;
-    int channels = bottom_blob.c;
-    size_t elemsize = bottom_blob.elemsize;
-    int size = w * h;
+  int nn_num_output = num_output >> 3;
+  int remain_num_output_start = nn_num_output << 3;
 
-
-    top_blob.create(num_output, elemsize, opt.blob_allocator);
-    if (top_blob.empty())
-        return -100;
-
-    const float* weight_data_ptr = weight_data;
-
-    int nn_num_output = num_output >> 3;
-    int remain_num_output_start = nn_num_output << 3;
-
-    #pragma omp parallel for num_threads(opt.num_threads)
-    for (int pp=0; pp<nn_num_output; pp++)
-    {
-        int p = pp * 8;
-
-        float sum0 = 0.f;
-        float sum1 = 0.f;
-        float sum2 = 0.f;
-        float sum3 = 0.f;
-        float sum4 = 0.f;
-        float sum5 = 0.f;
-        float sum6 = 0.f;
-        float sum7 = 0.f;
-
-        if (bias_term)
-        {
-            sum0 = bias_data[p];
-            sum1 = bias_data[p+1];
-            sum2 = bias_data[p+2];
-            sum3 = bias_data[p+3];
-            sum4 = bias_data[p+4];
-            sum5 = bias_data[p+5];
-            sum6 = bias_data[p+6];
-            sum7 = bias_data[p+7];
-        }
-
-        const float* w0 = weight_data_ptr + size * channels * p;
-        const float* w1 = weight_data_ptr + size * channels * (p+1);
-        const float* w2 = weight_data_ptr + size * channels * (p+2);
-        const float* w3 = weight_data_ptr + size * channels * (p+3);
-        const float* w4 = weight_data_ptr + size * channels * (p+4);
-        const float* w5 = weight_data_ptr + size * channels * (p+5);
-        const float* w6 = weight_data_ptr + size * channels * (p+6);
-        const float* w7 = weight_data_ptr + size * channels * (p+7);
-
+#pragma omp parallel for num_threads(opt.num_threads)
+  for (int pp = 0; pp < nn_num_output; pp++) {
+    int p = pp * 8;
 #if __AVX__
-        __m256 _sum0 = _mm256_set1_ps(0.f);
-        __m256 _sum1 = _mm256_set1_ps(0.f);
-        __m256 _sum2 = _mm256_set1_ps(0.f);
-        __m256 _sum3 = _mm256_set1_ps(0.f);
-        __m256 _sum4 = _mm256_set1_ps(0.f);
-        __m256 _sum5 = _mm256_set1_ps(0.f);
-        __m256 _sum6 = _mm256_set1_ps(0.f);
-        __m256 _sum7 = _mm256_set1_ps(0.f);
-#endif // __AVX__
-
-        // channels
-        for (int q=0; q<channels; q++)
-        {
-            const float* m = bottom_blob.channel(q);
-
-#if __AVX__
-            int nn = size >> 3;
-            int remain = size & 7;
-            nn = 0;
+    __m256 _sum = _mm256_set1_ps(0.f);
+    if (bias_term) {
+      _sum = _mm256_loadu_ps(&bias_data[p]);
+    }
 #else
-            int remain = size;
-#endif // __AVX__
-            
-            remain = size;
+    float sum0 = 0.f;
+    float sum1 = 0.f;
+    float sum2 = 0.f;
+    float sum3 = 0.f;
+    float sum4 = 0.f;
+    float sum5 = 0.f;
+    float sum6 = 0.f;
+    float sum7 = 0.f;
 
-#if __AVX__
-            for (; nn>0; nn--)
-            {
-                __m256 _m = _mm256_loadu_ps(m);
-
-                __m256 _w0 = _mm256_loadu_ps(w0);
-                _sum0 = _mm256_fmadd_ps(_m, _w0,_sum0);
-
-                __m256 _w1 = _mm256_loadu_ps(w1);
-                _sum1 = _mm256_fmadd_ps(_m, _w1,_sum1);
-
-                __m256 _w2 = _mm256_loadu_ps(w2);
-                _sum2 = _mm256_fmadd_ps(_m, _w2,_sum2);
-
-                __m256 _w3 = _mm256_loadu_ps(w3);
-                _sum3 = _mm256_fmadd_ps(_m, _w3,_sum3);
-
-                 __m256 _w4 = _mm256_loadu_ps(w4);
-                _sum4 = _mm256_fmadd_ps(_m, _w4,_sum4);
-
-                __m256 _w5 = _mm256_loadu_ps(w5);
-                _sum5 = _mm256_fmadd_ps(_m, _w5,_sum5);
-
-                __m256 _w6 = _mm256_loadu_ps(w6);
-                _sum6 = _mm256_fmadd_ps(_m, _w6,_sum6);
-
-                __m256 _w7 = _mm256_loadu_ps(w7);
-                _sum7 = _mm256_fmadd_ps(_m, _w7,_sum7);
-
-                m += 8;
-                w0 += 8;
-                w1 += 8;
-                w2 += 8;
-                w3 += 8;
-                w4 += 8;
-                w5 += 8;
-                w6 += 8;
-                w7 += 8;
-            }
-#endif // __AVX__
-            for (; remain>0; remain--)
-            {
-                sum0 += *m * *w0;
-                sum1 += *m * *w1;
-                sum2 += *m * *w2;
-                sum3 += *m * *w3;
-                sum4 += *m * *w4;
-                sum5 += *m * *w5;
-                sum6 += *m * *w6;
-                sum7 += *m * *w7;
-
-                m++;
-                w0++;
-                w1++;
-                w2++;
-                w3++;
-                w4++;
-                w5++;
-                w6++;
-                w7++;
-            }
-
-        }
-
-#if __AVX__
-        __m256 sums = HorizontalSums(_sum0,_sum1,_sum2,_sum3,_sum4,_sum5,_sum6,_sum7);
-
-        sum0 += sums[0];
-        sum1 += sums[1];
-        sum2 += sums[2];
-        sum3 += sums[3];
-        sum4 += sums[4];
-        sum5 += sums[5];
-        sum6 += sums[6];
-        sum7 += sums[7];
-
-#endif // __AVX__
-
-        if (activation_type == 1)
-        {
-            sum0 = std::max(sum0, 0.f);
-            sum1 = std::max(sum1, 0.f);
-            sum2 = std::max(sum2, 0.f);
-            sum3 = std::max(sum3, 0.f);
-            sum4 = std::max(sum4, 0.f);
-            sum5 = std::max(sum5, 0.f);
-            sum6 = std::max(sum6, 0.f);
-            sum7 = std::max(sum7, 0.f);
-        }
-        else if (activation_type == 2)
-        {
-            float slope = activation_params[0];
-            sum0 = sum0 > 0.f ? sum0 : sum0 * slope;
-            sum1 = sum1 > 0.f ? sum1 : sum1 * slope;
-            sum2 = sum2 > 0.f ? sum2 : sum2 * slope;
-            sum3 = sum3 > 0.f ? sum3 : sum3 * slope;
-            sum4 = sum4 > 0.f ? sum4 : sum4 * slope;
-            sum5 = sum5 > 0.f ? sum5 : sum5 * slope;
-            sum6 = sum6 > 0.f ? sum6 : sum6 * slope;
-            sum7 = sum7 > 0.f ? sum7 : sum7 * slope;
-        }
-        else if (activation_type == 3)
-        {
-            float min = activation_params[0];
-            float max = activation_params[1];
-            if (sum0 < min) sum0 = min;
-            if (sum0 > max) sum0 = max;
-            if (sum1 < min) sum1 = min;
-            if (sum1 > max) sum1 = max;
-            if (sum2 < min) sum2 = min;
-            if (sum2 > max) sum2 = max;
-            if (sum3 < min) sum3 = min;
-            if (sum3 > max) sum3 = max;
-            if (sum4 < min) sum4 = min;
-            if (sum4 > max) sum4 = max;
-            if (sum5 < min) sum5 = min;
-            if (sum5 > max) sum5 = max;
-            if (sum6 < min) sum6 = min;
-            if (sum6 > max) sum6 = max;
-            if (sum7 < min) sum7 = min;
-            if (sum7 > max) sum7 = max;
-        }
-        else if (activation_type == 4)
-        {
-            sum0 = static_cast<float>(1.f / (1.f + exp(-sum0)));
-            sum1 = static_cast<float>(1.f / (1.f + exp(-sum1)));
-            sum2 = static_cast<float>(1.f / (1.f + exp(-sum2)));
-            sum3 = static_cast<float>(1.f / (1.f + exp(-sum3)));
-            sum4 = static_cast<float>(1.f / (1.f + exp(-sum4)));
-            sum5 = static_cast<float>(1.f / (1.f + exp(-sum5)));
-            sum6 = static_cast<float>(1.f / (1.f + exp(-sum6)));
-            sum7 = static_cast<float>(1.f / (1.f + exp(-sum7)));
-        }
-
-        top_blob[p] = sum0;
-        top_blob[p+1] = sum1;
-        top_blob[p+2] = sum2;
-        top_blob[p+3] = sum3;
-        top_blob[p+4] = sum4;
-        top_blob[p+5] = sum5;
-        top_blob[p+6] = sum6;
-        top_blob[p+7] = sum7;
+    if (bias_term) {
+      sum0 = bias_data[p];
+      sum1 = bias_data[p + 1];
+      sum2 = bias_data[p + 2];
+      sum3 = bias_data[p + 3];
+      sum4 = bias_data[p + 4];
+      sum5 = bias_data[p + 5];
+      sum6 = bias_data[p + 6];
+      sum7 = bias_data[p + 7];
     }
+#endif
 
-    // num_output
-    #pragma omp parallel for num_threads(opt.num_threads)
-    for (int p=remain_num_output_start; p<num_output; p++)
-    {
-        float sum = 0.f;
-
-        if (bias_term)
-            sum = bias_data[p];
-
-        const float* w = weight_data_ptr + size * channels * p;
+    const float *w0 = weight_data_ptr + size * channels * p;
+    const float *w1 = weight_data_ptr + size * channels * (p + 1);
+    const float *w2 = weight_data_ptr + size * channels * (p + 2);
+    const float *w3 = weight_data_ptr + size * channels * (p + 3);
+    const float *w4 = weight_data_ptr + size * channels * (p + 4);
+    const float *w5 = weight_data_ptr + size * channels * (p + 5);
+    const float *w6 = weight_data_ptr + size * channels * (p + 6);
+    const float *w7 = weight_data_ptr + size * channels * (p + 7);
 
 #if __AVX__
-        __m256 _sum = _mm256_set1_ps(0.f);
+    __m256 _sum0 = _mm256_set1_ps(0.f);
+    __m256 _sum1 = _mm256_set1_ps(0.f);
+    __m256 _sum2 = _mm256_set1_ps(0.f);
+    __m256 _sum3 = _mm256_set1_ps(0.f);
+    __m256 _sum4 = _mm256_set1_ps(0.f);
+    __m256 _sum5 = _mm256_set1_ps(0.f);
+    __m256 _sum6 = _mm256_set1_ps(0.f);
+    __m256 _sum7 = _mm256_set1_ps(0.f);
 #endif // __AVX__
 
-        // channels
-        for (int q=0; q<channels; q++)
-        {
-            const float* m = bottom_blob.channel(q);
+    // channels
+    for (int q = 0; q < channels; q++) {
+      const float *m = bottom_blob.channel(q);
 
 #if __AVX__
-            int nn = size >> 3;
-            int remain = size & 7;
-            nn = 0;
+      int nn = size >> 3;
+      int remain = size & 7;
+      nn = 0;
 #else
-            int remain = size;
+      int remain = size;
 #endif // __AVX__
-            remain = size;
-#if __AVX__
-            for (; nn>0; nn--)
-            {
-                __m256 _m = _mm256_loadu_ps(m);
 
-                __m256 _w = _mm256_loadu_ps(w);
-                _sum = _mm256_fmadd_ps(_m, _w,_sum);
-
-                m += 8;
-                w += 8;
-            }
-#endif // __AVX__
-            for (; remain>0; remain--)
-            {
-                sum += *m * *w;
-
-                m++;
-                w++;
-            }
-        }
+      remain = size;
 
 #if __AVX__
-        sum += _mm256_reduce_add_ps(_sum);
-#endif // __AVX__
-       sum = activation_ss(sum,activation_type,activation_params);
+      for (; nn > 0; nn--) {
+        __m256 _m = _mm256_loadu_ps(m);
 
-        top_blob[p] = sum;
+        __m256 _w0 = _mm256_loadu_ps(w0);
+        _sum0 = _mm256_fmadd_ps(_m, _w0, _sum0);
+
+        __m256 _w1 = _mm256_loadu_ps(w1);
+        _sum1 = _mm256_fmadd_ps(_m, _w1, _sum1);
+
+        __m256 _w2 = _mm256_loadu_ps(w2);
+        _sum2 = _mm256_fmadd_ps(_m, _w2, _sum2);
+
+        __m256 _w3 = _mm256_loadu_ps(w3);
+        _sum3 = _mm256_fmadd_ps(_m, _w3, _sum3);
+
+        __m256 _w4 = _mm256_loadu_ps(w4);
+        _sum4 = _mm256_fmadd_ps(_m, _w4, _sum4);
+
+        __m256 _w5 = _mm256_loadu_ps(w5);
+        _sum5 = _mm256_fmadd_ps(_m, _w5, _sum5);
+
+        __m256 _w6 = _mm256_loadu_ps(w6);
+        _sum6 = _mm256_fmadd_ps(_m, _w6, _sum6);
+
+        __m256 _w7 = _mm256_loadu_ps(w7);
+        _sum7 = _mm256_fmadd_ps(_m, _w7, _sum7);
+
+        m += 8;
+        w0 += 8;
+        w1 += 8;
+        w2 += 8;
+        w3 += 8;
+        w4 += 8;
+        w5 += 8;
+        w6 += 8;
+        w7 += 8;
+      }
+      for (; remain > 0; remain--) {
+        _sum[0] += *m * *w0;
+        _sum[1] += *m * *w1;
+        _sum[2] += *m * *w2;
+        _sum[3] += *m * *w3;
+        _sum[4] += *m * *w4;
+        _sum[5] += *m * *w5;
+        _sum[6] += *m * *w6;
+        _sum[7] += *m * *w7;
+
+        m++;
+        w0++;
+        w1++;
+        w2++;
+        w3++;
+        w4++;
+        w5++;
+        w6++;
+        w7++;
+      }
+      __m256 sums = HorizontalSums(_sum0, _sum1, _sum2, _sum3, _sum4, _sum5,
+                                   _sum6, _sum7);
+      sums = activation_ps(_mm256_add_ps(sums, _sum), activation_type,
+                           activation_params);
+      _mm256_storeu_ps(&top_blob[p], sums);
+#else
+      for (; remain > 0; remain--) {
+        sum0 += *m * *w0;
+        sum1 += *m * *w1;
+        sum2 += *m * *w2;
+        sum3 += *m * *w3;
+        sum4 += *m * *w4;
+        sum5 += *m * *w5;
+        sum6 += *m * *w6;
+        sum7 += *m * *w7;
+
+        m++;
+        w0++;
+        w1++;
+        w2++;
+        w3++;
+        w4++;
+        w5++;
+        w6++;
+        w7++;
+      }
+
+      if (activation_type == 1) {
+        sum0 = std::max(sum0, 0.f);
+        sum1 = std::max(sum1, 0.f);
+        sum2 = std::max(sum2, 0.f);
+        sum3 = std::max(sum3, 0.f);
+        sum4 = std::max(sum4, 0.f);
+        sum5 = std::max(sum5, 0.f);
+        sum6 = std::max(sum6, 0.f);
+        sum7 = std::max(sum7, 0.f);
+      } else if (activation_type == 2) {
+        float slope = activation_params[0];
+        sum0 = sum0 > 0.f ? sum0 : sum0 * slope;
+        sum1 = sum1 > 0.f ? sum1 : sum1 * slope;
+        sum2 = sum2 > 0.f ? sum2 : sum2 * slope;
+        sum3 = sum3 > 0.f ? sum3 : sum3 * slope;
+        sum4 = sum4 > 0.f ? sum4 : sum4 * slope;
+        sum5 = sum5 > 0.f ? sum5 : sum5 * slope;
+        sum6 = sum6 > 0.f ? sum6 : sum6 * slope;
+        sum7 = sum7 > 0.f ? sum7 : sum7 * slope;
+      } else if (activation_type == 3) {
+        float min = activation_params[0];
+        float max = activation_params[1];
+        if (sum0 < min)
+          sum0 = min;
+        if (sum0 > max)
+          sum0 = max;
+        if (sum1 < min)
+          sum1 = min;
+        if (sum1 > max)
+          sum1 = max;
+        if (sum2 < min)
+          sum2 = min;
+        if (sum2 > max)
+          sum2 = max;
+        if (sum3 < min)
+          sum3 = min;
+        if (sum3 > max)
+          sum3 = max;
+        if (sum4 < min)
+          sum4 = min;
+        if (sum4 > max)
+          sum4 = max;
+        if (sum5 < min)
+          sum5 = min;
+        if (sum5 > max)
+          sum5 = max;
+        if (sum6 < min)
+          sum6 = min;
+        if (sum6 > max)
+          sum6 = max;
+        if (sum7 < min)
+          sum7 = min;
+        if (sum7 > max)
+          sum7 = max;
+      } else if (activation_type == 4) {
+        sum0 = static_cast<float>(1.f / (1.f + exp(-sum0)));
+        sum1 = static_cast<float>(1.f / (1.f + exp(-sum1)));
+        sum2 = static_cast<float>(1.f / (1.f + exp(-sum2)));
+        sum3 = static_cast<float>(1.f / (1.f + exp(-sum3)));
+        sum4 = static_cast<float>(1.f / (1.f + exp(-sum4)));
+        sum5 = static_cast<float>(1.f / (1.f + exp(-sum5)));
+        sum6 = static_cast<float>(1.f / (1.f + exp(-sum6)));
+        sum7 = static_cast<float>(1.f / (1.f + exp(-sum7)));
+      }
+
+      top_blob[p] = sum0;
+      top_blob[p + 1] = sum1;
+      top_blob[p + 2] = sum2;
+      top_blob[p + 3] = sum3;
+      top_blob[p + 4] = sum4;
+      top_blob[p + 5] = sum5;
+      top_blob[p + 6] = sum6;
+      top_blob[p + 7] = sum7;
+#endif
+    }
+  }
+
+// num_output
+#pragma omp parallel for num_threads(opt.num_threads)
+  for (int p = remain_num_output_start; p < num_output; p++) {
+    float sum = 0.f;
+
+    if (bias_term)
+      sum = bias_data[p];
+
+    const float *w = weight_data_ptr + size * channels * p;
+
+#if __AVX__
+    __m256 _sum = _mm256_set1_ps(0.f);
+#endif // __AVX__
+
+    // channels
+    for (int q = 0; q < channels; q++) {
+      const float *m = bottom_blob.channel(q);
+
+#if __AVX__
+      int nn = size >> 3;
+      int remain = size & 7;
+      nn = 0;
+#else
+      int remain = size;
+#endif // __AVX__
+      remain = size;
+#if __AVX__
+      for (; nn > 0; nn--) {
+        __m256 _m = _mm256_loadu_ps(m);
+
+        __m256 _w = _mm256_loadu_ps(w);
+        _sum = _mm256_fmadd_ps(_m, _w, _sum);
+
+        m += 8;
+        w += 8;
+      }
+#endif // __AVX__
+      for (; remain > 0; remain--) {
+        sum += *m * *w;
+
+        m++;
+        w++;
+      }
     }
 
-    return 0;
+#if __AVX__
+    sum += _mm256_reduce_add_ps(_sum);
+#endif // __AVX__
+    sum = activation_ss(sum, activation_type, activation_params);
+
+    top_blob[p] = sum;
+  }
+
+  return 0;
 }
 
 } // namespace ncnn
--- src/layer/x86/innerproduct_x86.h
+++ src/layer/x86/innerproduct_x86.h
@@ -1,16 +1,19 @@
-// Tencent is pleased to support the open source community by making ncnn available.
+// Tencent is pleased to support the open source community by making ncnn
+// available.
 //
 // Copyright (C) 2017 THL A29 Limited, a Tencent company. All rights reserved.
 //
-// Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
-// in compliance with the License. You may obtain a copy of the License at
+// Licensed under the BSD 3-Clause License (the "License"); you may not use this
+// file except in compliance with the License. You may obtain a copy of the
+// License at
 //
 // https://opensource.org/licenses/BSD-3-Clause
 //
-// Unless required by applicable law or agreed to in writing, software distributed
-// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-// CONDITIONS OF ANY KIND, either express or implied. See the License for the
-// specific language governing permissions and limitations under the License.
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
 
 #ifndef LAYER_INNERPRODUCT_X86_H
 #define LAYER_INNERPRODUCT_X86_H
@@ -19,22 +22,22 @@
 
 namespace ncnn {
 
-class InnerProduct_x86 : virtual public InnerProduct
-{
+class InnerProduct_x86 : virtual public InnerProduct {
 public:
-    InnerProduct_x86();
+  InnerProduct_x86();
 
-    virtual int create_pipeline(const Option& opt);
-    virtual int destroy_pipeline(const Option& opt);
+  virtual int create_pipeline(const Option &opt);
+  virtual int destroy_pipeline(const Option &opt);
 
-    virtual int forward(const Mat& bottom_blob, Mat& top_blob, const Option& opt) const;
+  virtual int forward(const Mat &bottom_blob, Mat &top_blob,
+                      const Option &opt) const;
 
 protected:
-    int forward_f16s(const Mat& bottom_blob, Mat& top_blob, const Option& opt) const;
+  int forward_f16s(const Mat &bottom_blob, Mat &top_blob,
+                   const Option &opt) const;
 
 public:
-    ncnn::Layer* flatten;
-    
+  ncnn::Layer *flatten;
 };
 
 } // namespace ncnn
