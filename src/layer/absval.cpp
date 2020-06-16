0f6b03683c0e3b719809debf9f12b613dd735f9a
Restyled.io <commits@restyled.io>
2020-06-16 09:42:04 +0000

    Restyled by clang-format

--- src/layer/absval.cpp
+++ src/layer/absval.cpp
@@ -14,12 +14,11 @@
 
 #include "absval.h"
 
-namespace ncnn
-{
+namespace ncnn {
 
 DEFINE_LAYER_CREATOR(AbsVal)
 
-    AbsVal::AbsVal()
+AbsVal::AbsVal()
 {
     one_blob_only = true;
     support_inplace = true;
@@ -28,21 +27,23 @@ DEFINE_LAYER_CREATOR(AbsVal)
 int AbsVal::forward_inplace(Mat& bottom_top_blob, const Option& opt) const
 {
     int w = bottom_top_blob.w;
-   int h = bottom_top_blob.h;
+    int h = bottom_top_blob.h;
     int channels = bottom_top_blob.c;
     int size = w * h;
 
-   #pragma omp parallel for num_threads(opt.num_threads)
-    for (int q=0; q<channels;q++)
-   {
-        float* ptr = bottom_top_blob.channel( q );
+#pragma omp parallel for num_threads(opt.num_threads)
+    for (int q = 0; q < channels; q++)
+    {
+        float* ptr = bottom_top_blob.channel(q);
 
         for (int i = 0; i < size; i++)
         {
             if (ptr[i] < 0)
-                ptr[i] = -ptr[i];        }    }
+                ptr[i] = -ptr[i];
+        }
+    }
 
-return 0;
+    return 0;
 }
 
 } // namespace ncnn
