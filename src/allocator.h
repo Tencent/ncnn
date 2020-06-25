99090376e613d3a6eee572440bc49b563642cf17
zhiliu6 <zhiliu6@gmail.com>
2020-06-25 19:06:45 +0800

    Update allocator.h
    fix windows build

--- src/allocator.h
+++ src/allocator.h
@@ -121,7 +121,7 @@ static inline void fastFree(void* ptr)
 #endif
 #endif
 #elif defined _MSC_VER && !defined RC_INVOKED
-#include <intrin.h>
+#elif !defined RC_INVOKED
 #define NCNN_XADD(addr, delta) (int)_InterlockedExchangeAdd((long volatile*)addr, delta)
 #else
 // thread-unsafe branch
