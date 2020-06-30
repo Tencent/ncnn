e2bed9cd0540f809b27fca06258a360ce55a14a9
Restyled.io <commits@restyled.io>
2020-06-30 08:48:21 +0000

    Restyled by astyle

--- src/layer/x86/batchnorm_x86.cpp
+++ src/layer/x86/batchnorm_x86.cpp
@@ -40,7 +40,7 @@ int BatchNorm_x86::forward_inplace(Mat& bottom_top_blob, const Option& opt) cons
         {
             int w = bottom_top_blob.w;
 
-#pragma omp parallel for num_threads(opt.num_threads)
+            #pragma omp parallel for num_threads(opt.num_threads)
             for (int i = 0; i < w; i++)
             {
                 float* ptr = (float*)bottom_top_blob + i * 8;
@@ -59,7 +59,7 @@ int BatchNorm_x86::forward_inplace(Mat& bottom_top_blob, const Option& opt) cons
             int w = bottom_top_blob.w;
             int h = bottom_top_blob.h;
 
-#pragma omp parallel for num_threads(opt.num_threads)
+            #pragma omp parallel for num_threads(opt.num_threads)
             for (int i = 0; i < h; i++)
             {
                 __m256 _a = _mm256_loadu_ps((const float*)a_data + i * 8);
@@ -85,7 +85,7 @@ int BatchNorm_x86::forward_inplace(Mat& bottom_top_blob, const Option& opt) cons
             int c = bottom_top_blob.c;
             int size = w * h;
 
-#pragma omp parallel for num_threads(opt.num_threads)
+            #pragma omp parallel for num_threads(opt.num_threads)
             for (int q = 0; q < c; q++)
             {
                 __m256 _a = _mm256_loadu_ps((const float*)a_data + q * 8);
@@ -116,7 +116,7 @@ int BatchNorm_x86::forward_inplace(Mat& bottom_top_blob, const Option& opt) cons
     // int c = bottom_top_blob.c;
     int size = w * h;
 
-#pragma omp parallel for num_threads(opt.num_threads)
+    #pragma omp parallel for num_threads(opt.num_threads)
     for (int q = 0; q < channels; q++)
     {
         float* ptr = bottom_top_blob.channel(q);
