ee2dcdb855e360ce0096a1c3482762b6561f550a
nihui <shuizhuyuanluo@126.com>
2020-07-08 22:03:43 +0800

    test deconvolution with output shape and padding

--- src/layer/vulkan/deconvolution_vulkan.cpp
+++ src/layer/vulkan/deconvolution_vulkan.cpp
@@ -584,7 +584,7 @@ int Deconvolution_vulkan::forward(const VkMat& bottom_blob, VkMat& top_blob, VkC
             crop_params[2] = 0;
             crop_params[3] = top_blob_bordered_adj.w - wcut;
             crop_params[4] = top_blob_bordered_adj.h - hcut;
-            crop_params[5] = top_blob_bordered_adj.c;
+            crop_params[5] = top_blob_bordered_adj.c * out_elempack;
         }
         else if (pad_left == -234 || pad_right == -234 || pad_top == -234 || pad_bottom == -234)
         {
@@ -594,7 +594,7 @@ int Deconvolution_vulkan::forward(const VkMat& bottom_blob, VkMat& top_blob, VkC
             crop_params[2] = 0;
             crop_params[3] = top_blob_bordered_adj.w - wcut;
             crop_params[4] = top_blob_bordered_adj.h - hcut;
-            crop_params[5] = top_blob_bordered_adj.c;
+            crop_params[5] = top_blob_bordered_adj.c * out_elempack;
         }
 
         std::vector<VkMat> crop_inputs(2);
@@ -777,7 +777,7 @@ int Deconvolution_vulkan::forward(const VkImageMat& bottom_blob, VkImageMat& top
             crop_params[2] = 0;
             crop_params[3] = top_blob_bordered_adj.w - wcut;
             crop_params[4] = top_blob_bordered_adj.h - hcut;
-            crop_params[5] = top_blob_bordered_adj.c;
+            crop_params[5] = top_blob_bordered_adj.c * out_elempack;
         }
         else if (pad_left == -234 || pad_right == -234 || pad_top == -234 || pad_bottom == -234)
         {
@@ -787,7 +787,7 @@ int Deconvolution_vulkan::forward(const VkImageMat& bottom_blob, VkImageMat& top
             crop_params[2] = 0;
             crop_params[3] = top_blob_bordered_adj.w - wcut;
             crop_params[4] = top_blob_bordered_adj.h - hcut;
-            crop_params[5] = top_blob_bordered_adj.c;
+            crop_params[5] = top_blob_bordered_adj.c * out_elempack;
         }
 
         std::vector<VkImageMat> crop_inputs(2);
