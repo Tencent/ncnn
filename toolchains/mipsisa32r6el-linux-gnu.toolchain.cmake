47626a4b30e738b3d906a3a071ee2be63b55499a
nihuini <nihuini@tencent.com>
2020-07-21 20:09:09 +0800

    wip

--- toolchains/mipsisa32r6el-linux-gnu.toolchain.cmake
+++ toolchains/mipsisa32r6el-linux-gnu.toolchain.cmake
@@ -8,9 +8,5 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
-set(CMAKE_C_FLAGS "-march=mips32r6 -mmsa")
-set(CMAKE_CXX_FLAGS "-march=mips32r6 -mmsa")
-
-# cache flags
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "c flags")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}" CACHE STRING "c++ flags")
+set(CMAKE_C_FLAGS "-march=mips32r6")
+set(CMAKE_CXX_FLAGS "-march=mips32r6")
