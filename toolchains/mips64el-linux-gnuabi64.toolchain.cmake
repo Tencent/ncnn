aee9f8a6374e3876aa861ed34dbd73f3ed5d5ebf
nihui <shuizhuyuanluo@126.com>
2020-07-21 22:24:32 +0800

    Ci mips mips64 (#1948)

---
+++ toolchains/mips64el-linux-gnuabi64.toolchain.cmake
@@ -0,0 +1,16 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR mips64el)
+
+set(CMAKE_C_COMPILER "mips64el-linux-gnuabi64-gcc")
+set(CMAKE_CXX_COMPILER "mips64el-linux-gnuabi64-g++")
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+set(CMAKE_C_FLAGS "-march=mips64")
+set(CMAKE_CXX_FLAGS "-march=mips64")
+
+# cache flags
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "c flags")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}" CACHE STRING "c++ flags")
