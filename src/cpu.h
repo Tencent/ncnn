b1e22b012aa6f3da1ead8f46909733604e50364e
tpoisonooo <khj.application@aliyun.com>
2020-07-27 22:01:21 +0800

    fix(cpu): cpu number bigger than 64

--- src/cpu.h
+++ src/cpu.h
@@ -16,8 +16,60 @@
 #define NCNN_CPU_H
 
 #include <stddef.h>
+#if defined __ANDROID__ || defined __linux__
+#include <sched.h>
+#endif
 
 namespace ncnn {
+
+#if defined __ANDROID__ || defined __linux__
+class CpuSet
+{
+public:
+    CpuSet()
+    {
+        zero();
+    }
+
+    void set(int cpu)
+    {
+        CPU_SET(cpu, &m_bits);
+    }
+
+    void zero()
+    {
+        CPU_ZERO(&m_bits);
+    }
+
+    void clr(int cpu)
+    {
+        CPU_CLR(cpu, &m_bits);
+    }
+
+    bool isset(int cpu) const
+    {
+        return CPU_ISSET(cpu, &m_bits);
+    }
+    friend int set_sched_affinity(const CpuSet&);
+
+protected:
+    void* data_ptr() const
+    {
+        return (void*)(&m_bits);
+    }
+
+    size_t data_size() const
+    {
+        return sizeof(m_bits);
+    }
+
+private:
+    cpu_set_t m_bits;
+};
+#else
+class CpuSet{
+};
+#endif
 
 // test optional cpu features
 // neon = armv7 neon or aarch64 asimd
@@ -45,10 +97,10 @@ int get_cpu_powersave();
 int set_cpu_powersave(int powersave);
 
 // convenient wrapper
-size_t get_cpu_thread_affinity_mask(int powersave);
+const CpuSet& get_cpu_thread_affinity_mask(int powersave);
 
 // set explicit thread affinity
-int set_cpu_thread_affinity(size_t thread_affinity_mask);
+int set_cpu_thread_affinity(const CpuSet& thread_affinity_mask);
 
 // misc function wrapper for openmp routines
 int get_omp_num_threads();
