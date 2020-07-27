baed5e662b82b098feee6e4063143614ace19fc8
tpoisonooo <khj.application@aliyun.com>
2020-07-27 22:01:21 +0800

    fix(cpu): cpu number bigger than 64

--- src/cpu.h
+++ src/cpu.h
@@ -16,9 +16,75 @@
 #define NCNN_CPU_H
 
 #include <stddef.h>
+#if defined __ANDROID__ || defined __linux__
+#include <sched.h>
+#else
+#include <cstring>
+#endif
+
+#if defined __ANDROID__ || defined __linux__
+#include <sched.h>
+#endif
 
 namespace ncnn {
 
+#define NCNN_CPU_SETSIZE 1024
+#define NCNN_NCPUBITS    (8 * sizeof(unsigned long))
+class CpuSet
+{
+public:
+    CpuSet()
+    {
+#if defined __ANDROID__ || defined __linux__
+        zero();
+#endif
+    }
+
+    void set(int cpu)
+    {
+#if defined __ANDROID__ || defined __linux__
+        CPU_SET(cpu, &m_bits);
+#endif
+    }
+
+    void zero()
+    {
+#if defined __ANDROID__ || defined __linux__
+        CPU_ZERO(&m_bits);
+#endif
+    }
+
+    void clr(int cpu)
+    {
+#if defined __ANDROID__ || defined __linux__
+        CPU_CLR(cpu, &m_bits);
+#endif
+    }
+
+    bool isset(int cpu) const
+    {
+#if defined __ANDROID__ || defined __linux__
+        return CPU_ISSET(cpu, &m_bits);
+#endif
+    }
+    friend int set_sched_affinity(const CpuSet&);
+
+protected:
+    void* data_ptr() const
+    {
+        return (void*)(&m_bits);
+    }
+
+    size_t data_size() const
+    {
+        return sizeof(m_bits);
+    }
+
+private:
+#if defined __ANDROID__ || defined __linux__
+    cpu_set_t m_bits;
+#endif
+};
 // test optional cpu features
 // neon = armv7 neon or aarch64 asimd
 int cpu_support_arm_neon();
@@ -45,10 +111,10 @@ int get_cpu_powersave();
 int set_cpu_powersave(int powersave);
 
 // convenient wrapper
-size_t get_cpu_thread_affinity_mask(int powersave);
+const CpuSet& get_cpu_thread_affinity_mask(int powersave);
 
 // set explicit thread affinity
-int set_cpu_thread_affinity(size_t thread_affinity_mask);
+int set_cpu_thread_affinity(const CpuSet& thread_affinity_mask);
 
 // misc function wrapper for openmp routines
 int get_omp_num_threads();
