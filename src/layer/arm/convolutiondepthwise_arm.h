1a57600bd7e533631f9878f4c2c432703322ee9f
nihui <shuizhuyuanluo@126.com>
2020-07-28 21:37:20 +0800

    fix ci crash

--- src/layer/arm/convolutiondepthwise_arm.h
+++ src/layer/arm/convolutiondepthwise_arm.h
@@ -30,11 +30,13 @@ public:
     virtual int forward(const Mat& bottom_blob, Mat& top_blob, const Option& opt) const;
 
 protected:
+    int create_group_ops(const Option& opt);
 #if __ARM_FEATURE_FP16_VECTOR_ARITHMETIC
     int forward_fp16s(const Mat& bottom_blob, Mat& top_blob, const Option& opt) const;
     int forward_fp16sa(const Mat& bottom_blob, Mat& top_blob, const Option& opt) const;
 #endif
     int forward_bf16s(const Mat& bottom_blob, Mat& top_blob, const Option& opt) const;
+    int create_pipeline_int8_arm(const Option& opt);
     int forward_int8_arm(const Mat& bottom_blob, Mat& top_blob, const Option& opt) const;
 
 public:
