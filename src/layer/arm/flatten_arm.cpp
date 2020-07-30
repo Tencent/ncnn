01b7c8299a151e44c850a9a8d11b97a550bfbe16
nihuini <nihuini@tencent.com>
2020-07-30 16:49:06 +0800

    convolution flatten arm fp16sa pack8

--- src/layer/arm/flatten_arm.cpp
+++ src/layer/arm/flatten_arm.cpp
@@ -52,38 +52,46 @@ int Flatten_arm::forward(const Mat& bottom_blob, Mat& top_blob, const Option& op
         return 0;
     }
 
-#if __ARM_NEON
+    int w = bottom_blob.w;
+    int h = bottom_blob.h;
+    int channels = bottom_blob.c;
+    size_t elemsize = bottom_blob.elemsize;
+    int elempack = bottom_blob.elempack;
+    int size = w * h;
+
+    int total = size * channels * elempack;
+
+    int out_elempack = 1;
     if (opt.use_packing_layout)
     {
-        int w = bottom_blob.w;
-        int h = bottom_blob.h;
-        int channels = bottom_blob.c;
-        size_t elemsize = bottom_blob.elemsize;
-        int elempack = bottom_blob.elempack;
-        int size = w * h;
+        out_elempack = total % 4 == 0 ? 4 : 1;
+    }
+    size_t out_elemsize = elemsize / elempack * out_elempack;
 
-        int total = size * channels * elempack;
+    if (out_elempack == 1)
+    {
+        return Flatten::forward(bottom_blob, top_blob, opt);
+    }
 
-        int out_elempack = total % 4 == 0 ? 4 : 1;
-        size_t out_elemsize = elemsize / elempack * out_elempack;
+    if (dims == 2 && elempack == 1) // out_elempack == 4
+    {
+        top_blob = bottom_blob;
+        top_blob.dims = 1;
+        top_blob.w = total / out_elempack;
+        top_blob.h = 1;
+        top_blob.cstep = top_blob.w;
+        top_blob.elemsize = out_elemsize;
+        top_blob.elempack = out_elempack;
+        return 0;
+    }
 
-        if (dims == 2 && elempack == 1)
-        {
-            top_blob = bottom_blob;
-            top_blob.dims = 1;
-            top_blob.w = total / out_elempack;
-            top_blob.h = 1;
-            top_blob.cstep = top_blob.w;
-            top_blob.elemsize = out_elemsize;
-            top_blob.elempack = out_elempack;
-            return 0;
-        }
+    top_blob.create(total / out_elempack, out_elemsize, out_elempack, opt.blob_allocator);
+    if (top_blob.empty())
+        return -100;
 
-        top_blob.create(total / out_elempack, out_elemsize, out_elempack, opt.blob_allocator);
-        if (top_blob.empty())
-            return -100;
-
-        if (dims == 2 && elempack == 4)
+    if (dims == 2)
+    {
+        if (elempack == 4) // out_elempack == 4
         {
             #pragma omp parallel for num_threads(opt.num_threads)
             for (int i = 0; i < h; i++)
@@ -95,6 +103,7 @@ int Flatten_arm::forward(const Mat& bottom_blob, Mat& top_blob, const Option& op
                 float* outptr3 = (float*)top_blob + w * (i * 4 + 3);
 
                 int j = 0;
+#if __ARM_NEON
                 for (; j + 3 < w; j += 4)
                 {
                     float32x4x4_t _v4 = vld4q_f32(ptr);
@@ -109,6 +118,7 @@ int Flatten_arm::forward(const Mat& bottom_blob, Mat& top_blob, const Option& op
                     outptr2 += 4;
                     outptr3 += 4;
                 }
+#endif
                 for (; j < w; j++)
                 {
                     *outptr0++ = ptr[0];
@@ -119,11 +129,12 @@ int Flatten_arm::forward(const Mat& bottom_blob, Mat& top_blob, const Option& op
                     ptr += 4;
                 }
             }
-
-            return 0;
         }
+    }
 
-        if (dims == 3 && elempack == 4)
+    if (dims == 3)
+    {
+        if (elempack == 4) // out_elempack == 4
         {
             #pragma omp parallel for num_threads(opt.num_threads)
             for (int q = 0; q < channels; q++)
@@ -135,6 +146,7 @@ int Flatten_arm::forward(const Mat& bottom_blob, Mat& top_blob, const Option& op
                 float* outptr3 = (float*)top_blob + size * (q * 4 + 3);
 
                 int i = 0;
+#if __ARM_NEON
                 for (; i + 3 < size; i += 4)
                 {
                     float32x4x4_t _v4 = vld4q_f32(ptr);
@@ -149,6 +161,7 @@ int Flatten_arm::forward(const Mat& bottom_blob, Mat& top_blob, const Option& op
                     outptr2 += 4;
                     outptr3 += 4;
                 }
+#endif
                 for (; i < size; i++)
                 {
                     *outptr0++ = ptr[0];
@@ -159,11 +172,9 @@ int Flatten_arm::forward(const Mat& bottom_blob, Mat& top_blob, const Option& op
                     ptr += 4;
                 }
             }
-
-            return 0;
         }
 
-        if (dims == 3 && elempack == 1 && out_elempack == 4)
+        if (elempack == 1) // out_elempack == 4
         {
             #pragma omp parallel for num_threads(opt.num_threads)
             for (int q = 0; q < channels; q++)
@@ -172,6 +183,7 @@ int Flatten_arm::forward(const Mat& bottom_blob, Mat& top_blob, const Option& op
                 float* outptr = (float*)top_blob + size * q;
 
                 int i = 0;
+#if __ARM_NEON
                 for (; i + 3 < size; i += 4)
                 {
                     float32x4_t _v = vld1q_f32(ptr);
@@ -179,19 +191,16 @@ int Flatten_arm::forward(const Mat& bottom_blob, Mat& top_blob, const Option& op
                     ptr += 4;
                     outptr += 4;
                 }
+#endif
                 for (; i < size; i++)
                 {
                     *outptr++ = *ptr++;
                 }
             }
-
-            return 0;
         }
+    }
 
-    }  // opt.use_packing_layout
-#endif // __ARM_NEON
-
-    return Flatten::forward(bottom_blob, top_blob, opt);
+    return 0;
 }
 
 int Flatten_arm::forward_bf16s_fp16s(const Mat& bottom_blob, Mat& top_blob, const Option& opt) const
@@ -204,38 +213,106 @@ int Flatten_arm::forward_bf16s_fp16s(const Mat& bottom_blob, Mat& top_blob, cons
         return 0;
     }
 
-#if __ARM_NEON
+    int w = bottom_blob.w;
+    int h = bottom_blob.h;
+    int channels = bottom_blob.c;
+    size_t elemsize = bottom_blob.elemsize;
+    int elempack = bottom_blob.elempack;
+    int size = w * h;
+
+    int total = size * channels * elempack;
+
+    int out_elempack = 1;
     if (opt.use_packing_layout)
     {
-        int w = bottom_blob.w;
-        int h = bottom_blob.h;
-        int channels = bottom_blob.c;
-        size_t elemsize = bottom_blob.elemsize;
-        int elempack = bottom_blob.elempack;
-        int size = w * h;
+        out_elempack = opt.use_fp16_arithmetic && total % 8 == 0 ? 8 : total % 4 == 0 ? 4 : 1;
+    }
+    size_t out_elemsize = elemsize / elempack * out_elempack;
 
-        int total = size * channels * elempack;
+    if (out_elempack == 1)
+    {
+        return Flatten::forward(bottom_blob, top_blob, opt);
+    }
 
-        int out_elempack = total % 4 == 0 ? 4 : 1;
-        size_t out_elemsize = elemsize / elempack * out_elempack;
+    if (dims == 2 && elempack == 1) // out_elempack == 4 || out_elempack == 8
+    {
+        top_blob = bottom_blob;
+        top_blob.dims = 1;
+        top_blob.w = total / out_elempack;
+        top_blob.h = 1;
+        top_blob.cstep = top_blob.w;
+        top_blob.elemsize = out_elemsize;
+        top_blob.elempack = out_elempack;
+        return 0;
+    }
 
-        if (dims == 2 && elempack == 1)
+    top_blob.create(total / out_elempack, out_elemsize, out_elempack, opt.blob_allocator);
+    if (top_blob.empty())
+        return -100;
+
+    if (dims == 2)
+    {
+#if __ARM_FEATURE_FP16_VECTOR_ARITHMETIC
+        if (elempack == 8) // out_elempack == 8
         {
-            top_blob = bottom_blob;
-            top_blob.dims = 1;
-            top_blob.w = total / out_elempack;
-            top_blob.h = 1;
-            top_blob.cstep = top_blob.w;
-            top_blob.elemsize = out_elemsize;
-            top_blob.elempack = out_elempack;
-            return 0;
+            #pragma omp parallel for num_threads(opt.num_threads)
+            for (int i = 0; i < h; i++)
+            {
+                const __fp16* ptr = bottom_blob.row<const __fp16>(i);
+                __fp16* outptr0 = (__fp16*)top_blob + w * i * 8;
+                __fp16* outptr1 = (__fp16*)top_blob + w * (i * 8 + 1);
+                __fp16* outptr2 = (__fp16*)top_blob + w * (i * 8 + 2);
+                __fp16* outptr3 = (__fp16*)top_blob + w * (i * 8 + 3);
+                __fp16* outptr4 = (__fp16*)top_blob + w * (i * 8 + 4);
+                __fp16* outptr5 = (__fp16*)top_blob + w * (i * 8 + 5);
+                __fp16* outptr6 = (__fp16*)top_blob + w * (i * 8 + 6);
+                __fp16* outptr7 = (__fp16*)top_blob + w * (i * 8 + 7);
+
+                int j = 0;
+                for (; j + 3 < w; j += 4)
+                {
+                    float16x8x4_t _v4 = vld4q_f16(ptr);
+                    float16x8_t _v_01 = vuzp1q_f16(_v4.val[0], _v4.val[1]);
+                    float16x8_t _v_23 = vuzp1q_f16(_v4.val[2], _v4.val[3]);
+                    float16x8_t _v_45 = vuzp2q_f16(_v4.val[0], _v4.val[1]);
+                    float16x8_t _v_67 = vuzp2q_f16(_v4.val[2], _v4.val[3]);
+                    vst1_f16(outptr0, vget_low_f16(_v_01));
+                    vst1_f16(outptr1, vget_high_f16(_v_01));
+                    vst1_f16(outptr2, vget_low_f16(_v_23));
+                    vst1_f16(outptr3, vget_high_f16(_v_23));
+                    vst1_f16(outptr4, vget_low_f16(_v_45));
+                    vst1_f16(outptr5, vget_high_f16(_v_45));
+                    vst1_f16(outptr6, vget_low_f16(_v_67));
+                    vst1_f16(outptr7, vget_high_f16(_v_67));
+
+                    ptr += 32;
+                    outptr0 += 4;
+                    outptr1 += 4;
+                    outptr2 += 4;
+                    outptr3 += 4;
+                    outptr4 += 4;
+                    outptr5 += 4;
+                    outptr6 += 4;
+                    outptr7 += 4;
+                }
+                for (; j < w; j++)
+                {
+                    *outptr0++ = ptr[0];
+                    *outptr1++ = ptr[1];
+                    *outptr2++ = ptr[2];
+                    *outptr3++ = ptr[3];
+                    *outptr4++ = ptr[4];
+                    *outptr5++ = ptr[5];
+                    *outptr6++ = ptr[6];
+                    *outptr7++ = ptr[7];
+
+                    ptr += 8;
+                }
+            }
         }
+#endif // __ARM_FEATURE_FP16_VECTOR_ARITHMETIC
 
-        top_blob.create(total / out_elempack, out_elemsize, out_elempack, opt.blob_allocator);
-        if (top_blob.empty())
-            return -100;
-
-        if (dims == 2 && elempack == 4)
+        if (elempack == 4) // out_elempack == 4 || out_elempack == 8
         {
             #pragma omp parallel for num_threads(opt.num_threads)
             for (int i = 0; i < h; i++)
@@ -247,6 +324,7 @@ int Flatten_arm::forward_bf16s_fp16s(const Mat& bottom_blob, Mat& top_blob, cons
                 unsigned short* outptr3 = (unsigned short*)top_blob + w * (i * 4 + 3);
 
                 int j = 0;
+#if __ARM_NEON
                 for (; j + 3 < w; j += 4)
                 {
                     uint16x4x4_t _v4 = vld4_u16(ptr);
@@ -261,6 +339,7 @@ int Flatten_arm::forward_bf16s_fp16s(const Mat& bottom_blob, Mat& top_blob, cons
                     outptr2 += 4;
                     outptr3 += 4;
                 }
+#endif
                 for (; j < w; j++)
                 {
                     *outptr0++ = ptr[0];
@@ -271,11 +350,72 @@ int Flatten_arm::forward_bf16s_fp16s(const Mat& bottom_blob, Mat& top_blob, cons
                     ptr += 4;
                 }
             }
-
-            return 0;
         }
+    }
 
-        if (dims == 3 && elempack == 4)
+    if (dims == 3)
+    {
+#if __ARM_FEATURE_FP16_VECTOR_ARITHMETIC
+        if (elempack == 8) // out_elempack == 8
+        {
+            #pragma omp parallel for num_threads(opt.num_threads)
+            for (int q = 0; q < channels; q++)
+            {
+                const __fp16* ptr = bottom_blob.channel(q);
+                __fp16* outptr0 = (__fp16*)top_blob + size * q * 8;
+                __fp16* outptr1 = (__fp16*)top_blob + size * (q * 8 + 1);
+                __fp16* outptr2 = (__fp16*)top_blob + size * (q * 8 + 2);
+                __fp16* outptr3 = (__fp16*)top_blob + size * (q * 8 + 3);
+                __fp16* outptr4 = (__fp16*)top_blob + size * (q * 8 + 4);
+                __fp16* outptr5 = (__fp16*)top_blob + size * (q * 8 + 5);
+                __fp16* outptr6 = (__fp16*)top_blob + size * (q * 8 + 6);
+                __fp16* outptr7 = (__fp16*)top_blob + size * (q * 8 + 7);
+
+                int i = 0;
+                for (; i + 3 < size; i += 4)
+                {
+                    float16x8x4_t _v4 = vld4q_f16(ptr);
+                    float16x8_t _v_01 = vuzp1q_f16(_v4.val[0], _v4.val[1]);
+                    float16x8_t _v_23 = vuzp1q_f16(_v4.val[2], _v4.val[3]);
+                    float16x8_t _v_45 = vuzp2q_f16(_v4.val[0], _v4.val[1]);
+                    float16x8_t _v_67 = vuzp2q_f16(_v4.val[2], _v4.val[3]);
+                    vst1_f16(outptr0, vget_low_f16(_v_01));
+                    vst1_f16(outptr1, vget_high_f16(_v_01));
+                    vst1_f16(outptr2, vget_low_f16(_v_23));
+                    vst1_f16(outptr3, vget_high_f16(_v_23));
+                    vst1_f16(outptr4, vget_low_f16(_v_45));
+                    vst1_f16(outptr5, vget_high_f16(_v_45));
+                    vst1_f16(outptr6, vget_low_f16(_v_67));
+                    vst1_f16(outptr7, vget_high_f16(_v_67));
+
+                    ptr += 32;
+                    outptr0 += 4;
+                    outptr1 += 4;
+                    outptr2 += 4;
+                    outptr3 += 4;
+                    outptr4 += 4;
+                    outptr5 += 4;
+                    outptr6 += 4;
+                    outptr7 += 4;
+                }
+                for (; i < size; i++)
+                {
+                    *outptr0++ = ptr[0];
+                    *outptr1++ = ptr[1];
+                    *outptr2++ = ptr[2];
+                    *outptr3++ = ptr[3];
+                    *outptr4++ = ptr[4];
+                    *outptr5++ = ptr[5];
+                    *outptr6++ = ptr[6];
+                    *outptr7++ = ptr[7];
+
+                    ptr += 8;
+                }
+            }
+        }
+#endif // __ARM_FEATURE_FP16_VECTOR_ARITHMETIC
+
+        if (elempack == 4) // out_elempack == 4 || out_elempack == 8
         {
             #pragma omp parallel for num_threads(opt.num_threads)
             for (int q = 0; q < channels; q++)
@@ -287,6 +427,7 @@ int Flatten_arm::forward_bf16s_fp16s(const Mat& bottom_blob, Mat& top_blob, cons
                 unsigned short* outptr3 = (unsigned short*)top_blob + size * (q * 4 + 3);
 
                 int i = 0;
+#if __ARM_NEON
                 for (; i + 3 < size; i += 4)
                 {
                     uint16x4x4_t _v4 = vld4_u16(ptr);
@@ -301,6 +442,7 @@ int Flatten_arm::forward_bf16s_fp16s(const Mat& bottom_blob, Mat& top_blob, cons
                     outptr2 += 4;
                     outptr3 += 4;
                 }
+#endif
                 for (; i < size; i++)
                 {
                     *outptr0++ = ptr[0];
@@ -311,11 +453,9 @@ int Flatten_arm::forward_bf16s_fp16s(const Mat& bottom_blob, Mat& top_blob, cons
                     ptr += 4;
                 }
             }
-
-            return 0;
         }
 
-        if (dims == 3 && elempack == 1 && out_elempack == 4)
+        if (elempack == 1) // out_elempack == 4 || out_elempack == 8
         {
             #pragma omp parallel for num_threads(opt.num_threads)
             for (int q = 0; q < channels; q++)
@@ -324,6 +464,7 @@ int Flatten_arm::forward_bf16s_fp16s(const Mat& bottom_blob, Mat& top_blob, cons
                 unsigned short* outptr = (unsigned short*)top_blob + size * q;
 
                 int i = 0;
+#if __ARM_NEON
                 for (; i + 3 < size; i += 4)
                 {
                     uint16x4_t _v = vld1_u16(ptr);
@@ -331,19 +472,16 @@ int Flatten_arm::forward_bf16s_fp16s(const Mat& bottom_blob, Mat& top_blob, cons
                     ptr += 4;
                     outptr += 4;
                 }
+#endif
                 for (; i < size; i++)
                 {
                     *outptr++ = *ptr++;
                 }
             }
-
-            return 0;
         }
+    }
 
-    }  // opt.use_packing_layout
-#endif // __ARM_NEON
-
-    return Flatten::forward(bottom_blob, top_blob, opt);
+    return 0;
 }
 
 } // namespace ncnn
