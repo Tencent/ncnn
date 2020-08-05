2439f933ec7ad4156da15aee87ecc2baf129f08e
Zhuo Zhang <imzhuo@foxmail.com>
2020-08-05 21:45:04 +0800

    fix #1984 clang<6 issue

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -237,7 +237,9 @@ int cpu_support_x86_avx2()
     return cpu_info[1] & 0x00000020;
 #else
     // TODO gcc-specific
-    __builtin_cpu_init();
+    #if !(defined(__clang__) & __clang_major__ < 6)
+        __builtin_cpu_init();
+    #endif
     return __builtin_cpu_supports("avx2");
 #endif
 #else
