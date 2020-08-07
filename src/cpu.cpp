1e1ea7b01fcdddb8cc364913bbf86149ae213a8c
Zhuo Zhang <imzhuo@foxmail.com>
2020-08-07 23:29:07 +0800

    fix gcc avx2 determine

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -235,10 +235,13 @@ int cpu_support_x86_avx2()
 
     __cpuid(cpu_info, 7);
     return cpu_info[1] & 0x00000020;
-#elif defined(__clang__) || defined(__GNUC__)
+#elif defined(__clang__)
 #if __clang_major__ >= 6
     __builtin_cpu_init();
 #endif
+    return __builtin_cpu_supports("avx2");
+#elif defined(__GNUC__)
+    __builtin_cpu_init();
     return __builtin_cpu_supports("avx2");
 #endif
 #else
