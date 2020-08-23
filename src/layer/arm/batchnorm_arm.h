738f0b39ccc99c249e4c54e07f9eabcca4074d2b
nihui <shuizhuyuanluo@126.com>
2020-08-23 10:54:24 +0800

    batchnorm convdw3x3 arm fp16sa

--- src/layer/arm/batchnorm_arm.h
+++ src/layer/arm/batchnorm_arm.h
@@ -25,6 +25,13 @@ public:
     BatchNorm_arm();
 
     virtual int forward_inplace(Mat& bottom_top_blob, const Option& opt) const;
+
+protected:
+#if __ARM_FEATURE_FP16_VECTOR_ARITHMETIC
+    int forward_inplace_fp16s(Mat& bottom_top_blob, const Option& opt) const;
+    int forward_inplace_fp16sa(Mat& bottom_top_blob, const Option& opt) const;
+#endif
+    int forward_inplace_bf16s(Mat& bottom_top_blob, const Option& opt) const;
 };
 
 } // namespace ncnn
