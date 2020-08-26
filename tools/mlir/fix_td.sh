78580dc648939124b352d47cda817c5c5694a4f0
nihuini <nihuini@tencent.com>
2020-08-26 17:31:29 +0800

    sync tf dialect, mlir fuse instancenorm

--- tools/mlir/fix_td.sh
+++ tools/mlir/fix_td.sh
@@ -3,7 +3,7 @@
 # This dirty script eat td files :P
 # https://github.com/tensorflow/tensorflow/tree/master/tensorflow/compiler/mlir/tensorflow/ir
 
-sed -i 's!tensorflow/compiler/mlir/tensorflow/ir/!!g' *.td
+sed -i 's!tensorflow/compiler/mlir/tensorflow/ir/!!g' *.td tf_traits.h tf_types.h tf_types.cc tf_attributes.cc
 
 sed -i '/let hasCanonicalizer = 1;/d' *.td
 sed -i '/let hasFolder = 1;/d' *.td
