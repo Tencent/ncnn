7ace8a933db1da8e9734b3988f332dbf672d4dd2
nihui <shuizhuyuanluo@126.com>
2020-09-05 14:15:24 +0800

    powerpc64, fix #2054 (#2058), workaround gcc altivec bug

--- src/layer/interp.cpp
+++ src/layer/interp.cpp
@@ -45,6 +45,11 @@ int Interp::load_param(const ParamDict& pd)
     return 0;
 }
 
+#if defined(__GNUC__) && defined(__powerpc__) && defined(__ALTIVEC__)
+// NOTE gcc altivec optimized version produce wrong result
+// so I have to disable vectorize here  --- nihui
+__attribute__((optimize("no-tree-vectorize")))
+#endif
 static void linear_coeffs(int w, int outw, int* xofs, float* alpha)
 {
     double scale = (double)w / outw;
