40fe0bd54a673756b588b6666ec328d976e35e2f
Restyled.io <commits@restyled.io>
2020-09-10 04:08:59 +0000

    Restyled by clang-format

--- src/layer/x86/pooling_x86.cpp
+++ src/layer/x86/pooling_x86.cpp
@@ -62,7 +62,7 @@ int Pooling_x86::forward(const Mat& bottom_blob, Mat& top_blob,
 
             if (pooling_type == PoolMethod_MAX)
             {
-                #pragma omp parallel for num_threads(opt.num_threads)
+#pragma omp parallel for num_threads(opt.num_threads)
                 for (int q = 0; q < channels; q++)
                 {
                     const float* ptr = bottom_blob.channel(q);
@@ -81,7 +81,7 @@ int Pooling_x86::forward(const Mat& bottom_blob, Mat& top_blob,
             }
             else if (pooling_type == PoolMethod_AVE)
             {
-                #pragma omp parallel for num_threads(opt.num_threads)
+#pragma omp parallel for num_threads(opt.num_threads)
                 for (int q = 0; q < channels; q++)
                 {
                     const float* ptr = bottom_blob.channel(q);
@@ -156,7 +156,7 @@ int Pooling_x86::forward(const Mat& bottom_blob, Mat& top_blob,
                 return 0;
             }
 
-            #pragma omp parallel for num_threads(opt.num_threads)
+#pragma omp parallel for num_threads(opt.num_threads)
             for (int q = 0; q < channels; q++)
             {
                 const Mat m = bottom_blob_bordered.channel(q);
@@ -196,7 +196,7 @@ int Pooling_x86::forward(const Mat& bottom_blob, Mat& top_blob,
                     htailpad = bottom_blob_bordered.h - bottom_blob.h - pad_top - pad_bottom;
                 }
 
-                #pragma omp parallel for num_threads(opt.num_threads)
+#pragma omp parallel for num_threads(opt.num_threads)
                 for (int q = 0; q < channels; q++)
                 {
                     const Mat m = bottom_blob_bordered.channel(q);
@@ -250,7 +250,7 @@ int Pooling_x86::forward(const Mat& bottom_blob, Mat& top_blob,
             }
             else // if (avgpool_count_include_pad == 1)
             {
-                #pragma omp parallel for num_threads(opt.num_threads)
+#pragma omp parallel for num_threads(opt.num_threads)
                 for (int q = 0; q < channels; q++)
                 {
                     const Mat m = bottom_blob_bordered.channel(q);
