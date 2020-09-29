972bb4b52c9b4fa6a5b937224e55fa4f904f8ed6
ncnnnnn <67086033+ncnnnnn@users.noreply.github.com>
2020-09-30 06:04:14 +0800

    softplus inplace (#2156)
    * softplus inplace
    
    * softplus inplace add src/tests CMakeLists.txt
    
    * Update CMakeLists.txt
    
    * Update softplus.cpp
    
    * Update softplus.h
    
    * Update softplus.cpp
    
    * Update test_softplus.cpp
    
    * Update softplus.cpp
    
    * Update softplus.h
    
    Co-authored-by: sf <sf@sf.sf>
    Co-authored-by: nihui <shuizhuyuanluo@126.com>

---
+++ src/layer/softplus.cpp
@@ -0,0 +1,47 @@
+// Tencent is pleased to support the open source community by making ncnn available.
+//
+// Copyright (C) 2020 THL A29 Limited, a Tencent company. All rights reserved.
+//
+// Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
+// in compliance with the License. You may obtain a copy of the License at
+//
+// https://opensource.org/licenses/BSD-3-Clause
+//
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
+#include "softplus.h"
+
+#include <math.h>
+
+namespace ncnn {
+
+Softplus::Softplus()
+{
+    one_blob_only = true;
+    support_inplace = true;
+}
+
+int Softplus::forward_inplace(Mat& bottom_top_blob, const Option& opt) const
+{
+    int w = bottom_top_blob.w;
+    int h = bottom_top_blob.h;
+    int channels = bottom_top_blob.c;
+    int size = w * h;
+
+    #pragma omp parallel for num_threads(opt.num_threads)
+    for (int q = 0; q < channels; q++)
+    {
+        float* ptr = bottom_top_blob.channel(q);
+        for (int i = 0; i < size; i++)
+        {
+            ptr[i] = log(exp(ptr[i]) + 1.0f);
+        }
+    }
+
+    return 0;
+}
+
+} // namespace ncnn
