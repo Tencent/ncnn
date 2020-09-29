30706b500068dfe3b71a0a1e513bca522e6dc743
nihui <shuizhuyuanluo@126.com>
2020-09-29 23:00:26 +0800

    Update test_softplus.cpp

--- tests/test_softplus.cpp
+++ tests/test_softplus.cpp
@@ -1,6 +1,6 @@
 // Tencent is pleased to support the open source community by making ncnn available.
 //
-// Copyright (C) 2019 THL A29 Limited, a Tencent company. All rights reserved.
+// Copyright (C) 2020 THL A29 Limited, a Tencent company. All rights reserved.
 //
 // Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
 // in compliance with the License. You may obtain a copy of the License at
@@ -15,17 +15,16 @@
 #include "layer/softplus.h"
 #include "testutil.h"
 
-static int test_softplus(const ncnn::Mat& a, float slope)
+static int test_softplus(const ncnn::Mat& a)
 {
     ncnn::ParamDict pd;
-    pd.set(0, slope); //slope
 
     std::vector<ncnn::Mat> weights(0);
 
     int ret = test_layer<ncnn::Softplus>("Softplus", pd, weights, a);
     if (ret != 0)
     {
-        fprintf(stderr, "test_softplus failed a.dims=%d a=(%d %d %d) slope=%f\n", a.dims, a.w, a.h, a.c, slope);
+        fprintf(stderr, "test_softplus failed a.dims=%d a=(%d %d %d)\n", a.dims, a.w, a.h, a.c);
     }
 
     return ret;
@@ -34,34 +33,25 @@ static int test_softplus(const ncnn::Mat& a, float slope)
 static int test_softplus_0()
 {
     return 0
-           || test_softplus(RandomMat(5, 7, 24), 0.f)
-           || test_softplus(RandomMat(5, 7, 24), 0.1f)
-           || test_softplus(RandomMat(7, 9, 12), 0.f)
-           || test_softplus(RandomMat(7, 9, 12), 0.1f)
-           || test_softplus(RandomMat(3, 5, 13), 0.f)
-           || test_softplus(RandomMat(3, 5, 13), 0.1f);
+           || test_softplus(RandomMat(5, 7, 24))
+           || test_softplus(RandomMat(7, 9, 12))
+           || test_softplus(RandomMat(3, 5, 13));
 }
 
 static int test_softplus_1()
 {
     return 0
-           || test_softplus(RandomMat(15, 24), 0.f)
-           || test_softplus(RandomMat(15, 24), 0.1f)
-           || test_softplus(RandomMat(17, 12), 0.f)
-           || test_softplus(RandomMat(17, 12), 0.1f)
-           || test_softplus(RandomMat(19, 15), 0.f)
-           || test_softplus(RandomMat(19, 15), 0.1f);
+           || test_softplus(RandomMat(15, 24))
+           || test_softplus(RandomMat(17, 12))
+           || test_softplus(RandomMat(19, 15));
 }
 
 static int test_softplus_2()
 {
     return 0
-           || test_softplus(RandomMat(128), 0.f)
-           || test_softplus(RandomMat(128), 0.1f)
-           || test_softplus(RandomMat(124), 0.f)
-           || test_softplus(RandomMat(124), 0.1f)
-           || test_softplus(RandomMat(127), 0.f)
-           || test_softplus(RandomMat(127), 0.1f);
+           || test_softplus(RandomMat(128))
+           || test_softplus(RandomMat(124))
+           || test_softplus(RandomMat(127));
 }
 
 int main()
