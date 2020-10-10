90985c3b10b4bc394326cb8161a1ec5deb068afa
deepage <604588292@qq.com>
2020-10-10 12:30:46 +0800

    Update onnx2ncnn.cpp (#2189)

--- tools/onnx/onnx2ncnn.cpp
+++ tools/onnx/onnx2ncnn.cpp
@@ -39,7 +39,7 @@ static bool read_proto_from_binary(const char* filepath, onnx::ModelProto* messa
     google::protobuf::io::IstreamInputStream input(&fs);
     google::protobuf::io::CodedInputStream codedstr(&input);
 
-#if GOOGLE_PROTOBUF_VERSION >= 3002000
+#if GOOGLE_PROTOBUF_VERSION >= 3011000
     codedstr.SetTotalBytesLimit(INT_MAX);
 #else
     codedstr.SetTotalBytesLimit(INT_MAX, INT_MAX / 2);
