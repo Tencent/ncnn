fe29b376c649cafd9ec648e089e697ab948df63b
Restyled.io <commits@restyled.io>
2020-10-13 11:20:51 +0000

    Restyled by astyle

--- src/layer/x86/concat_x86.cpp
+++ src/layer/x86/concat_x86.cpp
@@ -217,7 +217,7 @@ int Concat_x86::forward(const std::vector<Mat>& bottom_blobs, std::vector<Mat>& 
             if (top_blob.empty())
                 return -100;
 
-#pragma omp parallel for num_threads(opt.num_threads)
+            #pragma omp parallel for num_threads(opt.num_threads)
             for (int i = 0; i < h; i++)
             {
                 float* outptr = top_blob.row(i);
@@ -350,7 +350,7 @@ int Concat_x86::forward(const std::vector<Mat>& bottom_blobs, std::vector<Mat>& 
             if (top_blob.empty())
                 return -100;
 
-#pragma omp parallel for num_threads(opt.num_threads)
+            #pragma omp parallel for num_threads(opt.num_threads)
             for (int q = 0; q < channels; q++)
             {
                 float* outptr = top_blob.channel(q);
@@ -392,7 +392,7 @@ int Concat_x86::forward(const std::vector<Mat>& bottom_blobs, std::vector<Mat>& 
             if (top_blob.empty())
                 return -100;
 
-#pragma omp parallel for num_threads(opt.num_threads)
+            #pragma omp parallel for num_threads(opt.num_threads)
             for (int q = 0; q < channels; q++)
             {
                 float* outptr = top_blob.channel(q);
