b0c8ed2d8ba0089d21e509a6a1800c17f22a581a
Evgeny Proydakov <e.proydakov@gmail.com>
2020-10-15 11:54:56 +0300

    Fixed compile warnings for ios clang cpu 32bit. [-Wunused-variable] (#2223)

--- src/layer/arm/convolution_3x3_pack1to4.h
+++ src/layer/arm/convolution_3x3_pack1to4.h
@@ -21,10 +21,10 @@ static void conv3x3s1_pack1to4_neon(const Mat& bottom_blob, Mat& top_blob, const
 
     const float* bias = _bias;
 
-    int nn_outch = 0;
     int remain_outch_start = 0;
 
 #if __ARM_NEON && __aarch64__
+    int nn_outch = 0;
     nn_outch = outch >> 1;
     remain_outch_start = nn_outch << 1;
 
@@ -907,10 +907,10 @@ static void conv3x3s2_pack1to4_neon(const Mat& bottom_blob, Mat& top_blob, const
 
     const float* bias = _bias;
 
-    int nn_outch = 0;
     int remain_outch_start = 0;
 
 #if __ARM_NEON && __aarch64__
+    int nn_outch = 0;
     nn_outch = outch >> 1;
     remain_outch_start = nn_outch << 1;
 
