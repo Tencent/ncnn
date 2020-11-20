3c161c052d0d347f2a7500de5fb0d2b6f3819ae4
Restyled.io <commits@restyled.io>
2020-11-20 06:15:34 +0000

    Restyled by clang-format

--- src/datareader.h
+++ src/datareader.h
@@ -30,17 +30,25 @@ namespace ncnn {
 class DataReader
 {
 public:
-    virtual ~DataReader() {}
+    virtual ~DataReader()
+    {
+    }
 
 #if NCNN_STRING
     // parse plain param text
     // return 1 if scan success
-    virtual int scan(const char* format, void* p) const { return 0; }
+    virtual int scan(const char* format, void* p) const
+    {
+        return 0;
+    }
 #endif // NCNN_STRING
 
     // read binary param and model data
     // return bytes read
-    virtual size_t read(void* buf, size_t size) const { return 0; }
+    virtual size_t read(void* buf, size_t size) const
+    {
+        return 0;
+    }
 };
 
 #if NCNN_STDIO
