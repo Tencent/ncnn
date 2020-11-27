6f3647b6d4fd139c0e111b83d10b417e70b7b135
Ca0L <954865397@qq.com>
2020-11-27 20:15:23 +0800

    Add negative axis support in Softmax_mips

--- src/layer/mips/softmax_mips.cpp
+++ src/layer/mips/softmax_mips.cpp
@@ -29,8 +29,20 @@ int Softmax_mips::forward_inplace(Mat& bottom_top_blob, const Option& opt) const
 {
     int dims = bottom_top_blob.dims;
     size_t elemsize = bottom_top_blob.elemsize;
+    if (dims != 0)
+    {
+        if (-dims <= axis && axis < dims)
+            positive_axis = axis < 0 ? dims + axis : axis;
+        else
+            return -1;
+    }
+    else
+    {
+        if (!(axis == 0 || axis == -1))
+            return -1;
+    }
 
-    if (dims != 3 || axis != 0)
+    if (dims != 3 || positive_axis != 0)
         return Softmax::forward_inplace(bottom_top_blob, opt);
 
     // value = exp( value - global max value )
