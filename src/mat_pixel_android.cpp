8c2fa17c554574b12dd7c6211b5ed9a66dc07212
Zhuo Zhang <imzhuo@foxmail.com>
2020-12-15 09:31:48 +0800

    remove unused variable; fix wrongly used variable

--- src/mat_pixel_android.cpp
+++ src/mat_pixel_android.cpp
@@ -186,17 +186,14 @@ void Mat::to_android_bitmap(JNIEnv* env, jobject bitmap, int type_from) const
     AndroidBitmap_getInfo(env, bitmap, &info);
 
     int type_to;
-    int _elempack;
 
     if (info.format == ANDROID_BITMAP_FORMAT_A_8)
     {
         type_to = PIXEL_GRAY;
-        _elempack = 1;
     }
     else if (info.format == ANDROID_BITMAP_FORMAT_RGBA_8888)
     {
         type_to = PIXEL_RGBA;
-        _elempack = 4;
     }
     else
     {
@@ -212,7 +209,7 @@ void Mat::to_android_bitmap(JNIEnv* env, jobject bitmap, int type_from) const
 
     int type = type_from == type_to ? type_to : (type_from | (type_to << PIXEL_CONVERT_SHIFT));
 
-    to_pixels_resize((unsigned char*)data, type, info.width, info.height, info.stride);
+    to_pixels_resize((unsigned char*)_data, type, info.width, info.height, info.stride);
 
     AndroidBitmap_unlockPixels(env, bitmap);
 }
