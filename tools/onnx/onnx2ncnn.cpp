922fe2f295aa5c598ff36d38d2d7fc02fcfe05d5
GuoxiaWang <mingzilaochongtu@gmail.com>
2021-01-07 17:31:09 +0800

    fix onnx2ncnn adaptive pool conversion bug, remove no used weights node

--- tools/onnx/onnx2ncnn.cpp
+++ tools/onnx/onnx2ncnn.cpp
@@ -2227,6 +2227,13 @@ int main(int argc, char** argv)
                 node_reference[node.input(1)] -= 1;
             }
         }
+        else if (op == "adaptive_avg_pool2d" || op == "adaptive_max_pool2d")
+        {
+            if (node.input_size() >= 2)
+            {
+                node_reference[node.input(1)] -= 1;
+            }
+        }
     }
 
     //         for (auto a: node_reference)
