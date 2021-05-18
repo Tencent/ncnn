05efd92ae22996997f508aa50c34f752c4074ba0
Restyled.io <commits@restyled.io>
2021-05-18 10:45:28 +0000

    [skip ci] Restyled by astyle

--- examples/scrfd.cpp
+++ examples/scrfd.cpp
@@ -59,13 +59,13 @@ static void qsort_descent_inplace(std::vector<FaceObject>& faceobjects, int left
         }
     }
 
-#pragma omp parallel sections
+    #pragma omp parallel sections
     {
-#pragma omp section
+        #pragma omp section
         {
             if (left < j) qsort_descent_inplace(faceobjects, left, j);
         }
-#pragma omp section
+        #pragma omp section
         {
             if (i < right) qsort_descent_inplace(faceobjects, i, right);
         }
