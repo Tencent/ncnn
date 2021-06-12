4e2d8d82af2c9fc564cf1397e46047ee02086e99
Xavier Hsinyuan <thelastlin@users.noreply.github.com>
2021-06-12 08:37:57 +0000

    restyle for earier commit

--- src/layer/riscv/absval_riscv.cpp
+++ src/layer/riscv/absval_riscv.cpp
@@ -65,7 +65,7 @@ int AbsVal_riscv::forward_inplace(Mat& bottom_top_blob, const Option& opt) const
     int size = w * h;
     int elempack = bottom_top_blob.elempack;
 
-#pragma omp parallel for num_threads(opt.num_threads)
+    #pragma omp parallel for num_threads(opt.num_threads)
     for (int q = 0; q < channels; q++)
     {
         float* ptr = bottom_top_blob.channel(q);
@@ -105,7 +105,7 @@ int AbsVal_riscv::forward_inplace_fp16s(Mat& bottom_top_blob, const Option& opt)
     int size = w * h;
     int elempack = bottom_top_blob.elempack;
 
-#pragma omp parallel for num_threads(opt.num_threads)
+    #pragma omp parallel for num_threads(opt.num_threads)
     for (int q = 0; q < channels; q++)
     {
         __fp16* ptr = bottom_top_blob.channel(q);
@@ -116,7 +116,7 @@ int AbsVal_riscv::forward_inplace_fp16s(Mat& bottom_top_blob, const Option& opt)
             word_type vl = vsetvl_e16m4(n);
 
             vfloat32m8_t _p = vfwcvt_f_f_v_f32m8(vle16_v_f16m4(ptr, vl), vl);
-            _p = vfabs_v_f32m8_absval(_p,vl);
+            _p = vfabs_v_f32m8_absval(_p, vl);
             vse16_v_f16m4(ptr, vfncvt_f_f_w_f16m4(_p, vl), vl);
 
             ptr += vl;
@@ -135,7 +135,7 @@ int AbsVal_riscv::forward_inplace_fp16sa(Mat& bottom_top_blob, const Option& opt
     int size = w * h;
     int elempack = bottom_top_blob.elempack;
 
-#pragma omp parallel for num_threads(opt.num_threads)
+    #pragma omp parallel for num_threads(opt.num_threads)
     for (int q = 0; q < channels; q++)
     {
         __fp16* ptr = bottom_top_blob.channel(q);
