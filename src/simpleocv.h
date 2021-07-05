92c72c1aa0ade2f3f5fbfe76022ffd996acf0944
nihuini <nihuini@tencent.com>
2021-07-05 19:09:39 +0800

    optimize tanh

--- src/simpleocv.h
+++ src/simpleocv.h
@@ -124,8 +124,7 @@ struct Point_
     {
     }
 
-    template<typename _Tp2>
-    operator Point_<_Tp2>() const
+    template<typename _Tp2> operator Point_<_Tp2>() const
     {
         return Point_<_Tp2>(saturate_cast<_Tp2>(x), saturate_cast<_Tp2>(y));
     }
@@ -149,8 +148,7 @@ struct Size_
     {
     }
 
-    template<typename _Tp2>
-    operator Size_<_Tp2>() const
+    template<typename _Tp2> operator Size_<_Tp2>() const
     {
         return Size_<_Tp2>(saturate_cast<_Tp2>(width), saturate_cast<_Tp2>(height));
     }
@@ -178,8 +176,7 @@ struct Rect_
     {
     }
 
-    template<typename _Tp2>
-    operator Rect_<_Tp2>() const
+    template<typename _Tp2> operator Rect_<_Tp2>() const
     {
         return Rect_<_Tp2>(saturate_cast<_Tp2>(x), saturate_cast<_Tp2>(y), saturate_cast<_Tp2>(width), saturate_cast<_Tp2>(height));
     }
