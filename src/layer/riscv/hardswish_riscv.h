fdc7d98b0155296011263845174f8dabecb95944
Xavier Hsinyuan <me@lstlx.com>
2021-07-25 21:17:32 +0800

    RVV optimized HardSwish and HardSigmoid (#3108)
    * RVV optimized HardSwish, with fp16 support
    
    * RVV optimized HardSigmoid, with fp16 support
    
    * apply code-format changes
    
    Co-authored-by: thelastlin <thelastlin@users.noreply.github.com>

---
+++ src/layer/riscv/hardswish_riscv.h
@@ -0,0 +1,40 @@
+// Xavier Hsinyuan is pleased to support the open source community by making
+// ncnn available.
+//
+// Copyright (C) 2021 Xavier Hsinyuan <me@lstlx.com>. All rights reserved.
+//
+// Licensed under the BSD 3-Clause License (the "License"); you may not use this
+// file except in compliance with the License. You may obtain a copy of the
+// License at
+//
+// https://opensource.org/licenses/BSD-3-Clause
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+#ifndef LAYER_HARDSWISH_RISCV_H
+#define LAYER_HARDSWISH_RISCV_H
+
+#include "hardswish.h"
+
+namespace ncnn {
+
+class HardSwish_riscv : virtual public HardSwish
+{
+public:
+    HardSwish_riscv();
+
+    virtual int forward_inplace(Mat& bottom_top_blob, const Option& opt) const;
+
+protected:
+#if __riscv_vector && __riscv_zfh
+    int forward_inplace_fp16s(Mat& bottom_top_blob, const Option& opt) const;
+#endif
+};
+
+} // namespace ncnn
+
+#endif // LAYER_HARDSWISH_RISCV_H
