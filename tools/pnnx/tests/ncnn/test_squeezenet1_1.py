43d98f0f6ec1047f00e5df2377a57d1c6ae05bc3
nihuini <nihuini@tencent.com>
2021-11-05 17:33:53 +0800

    end2end pnnx2ncnn test

---
+++ tools/pnnx/tests/ncnn/test_squeezenet1_1.py
@@ -0,0 +1,45 @@
+# Tencent is pleased to support the open source community by making ncnn available.
+#
+# Copyright (C) 2021 THL A29 Limited, a Tencent company. All rights reserved.
+#
+# Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
+# in compliance with the License. You may obtain a copy of the License at
+#
+# https://opensource.org/licenses/BSD-3-Clause
+#
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+
+import torch
+import torchvision.models as models
+
+def test():
+    net = models.squeezenet1_1()
+    net.eval()
+
+    torch.manual_seed(0)
+    x = torch.rand(1, 3, 224, 224)
+
+    a = net(x)
+
+    # export torchscript
+    mod = torch.jit.trace(net, x)
+    mod.save("test_squeezenet1_1.pt")
+
+    # torchscript to pnnx
+    import os
+    os.system("../../src/pnnx test_squeezenet1_1.pt inputshape=[1,3,224,224]")
+
+    # ncnn inference
+    import test_squeezenet1_1_ncnn
+    b = test_squeezenet1_1_ncnn.test_inference()
+
+    return torch.allclose(a, b, 1e-4, 1e-4)
+
+if __name__ == "__main__":
+    if test():
+        exit(0)
+    else:
+        exit(1)
