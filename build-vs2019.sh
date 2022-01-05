0b223271f185a3349aaffb93f6ed0fdd0072007c
Jun <jun@jun.com>
2022-01-05 18:22:10 +0800

    xmake package

--- build-vs2019.sh
+++ build-vs2019.sh
@@ -3,3 +3,8 @@ cd build-vs2019
 cmake -G "Visual Studio 16 2019" -A x64 -DNCNN_BF16=OFF -DNCNN_INT8=OFF -DNCNN_PIXEL_DRAWING=OFF -DNCNN_PIXEL_ROTATE=OFF -DNCNN_PIXEL_AFFINE=OFF -DNCNN_ARM82=OFF  -DNCNN_BUILD_BENCHMARK=OFF -DNCNN_BUILD_EXAMPLES=OFF -DNCNN_BUILD_TOOLS=OFF ..
 cmake --build . --config Release
 cmake --install . --config Release
+cd ..
+
+cp xmake/packages ~/.KnightRepo -rf
+mkdir -p ~/.KnightRepo/packages/n/ncnn/windows/x64/release/
+cp build-vs2019/install/* ~/.KnightRepo/packages/n/ncnn/windows/x64/release/ -rf
---
+++ xmake/packages/n/ncnn/xmake.lua
@@ -0,0 +1,14 @@
+package("ncnn")
+    set_description("The ncnn package")
+
+    on_load(function (package)
+        package:set("installdir", path.join(os.scriptdir(), package:plat(), package:arch(), package:mode()))
+    end)
+
+    on_fetch(function (package)
+        local result = {}
+        result.links = "ncnn"
+        result.linkdirs = package:installdir("lib")
+        result.includedirs = package:installdir("include")
+        return result
+    end)
