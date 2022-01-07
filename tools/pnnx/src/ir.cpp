7d3503c06a22b2765b0f99c9a812da289d43cba6
nihui <shuizhuyuanluo@126.com>
2022-01-07 15:01:35 +0800

    pnnx Tensor index (#3483)
    * pnnx Tensor index
    
    * add test

--- tools/pnnx/src/ir.cpp
+++ tools/pnnx/src/ir.cpp
@@ -37,6 +37,7 @@ static bool type_is_integer(int type)
     if (type == 6) return true;
     if (type == 7) return true;
     if (type == 8) return true;
+    if (type == 9) return true;
     return false;
 }
 
@@ -50,6 +51,7 @@ static const char* type_to_string(int type)
     if (type == 6) return "i16";
     if (type == 7) return "i8";
     if (type == 8) return "u8";
+    if (type == 9) return "bool";
     return "null";
 }
 
@@ -63,6 +65,7 @@ static const char* type_to_numpy_string(int type)
     if (type == 6) return "int16";
     if (type == 7) return "int8";
     if (type == 8) return "uint8";
+    if (type == 9) return "bool8";
     return "null";
 }
 
@@ -76,6 +79,7 @@ static const char* type_to_dtype_string(int type)
     if (type == 6) return "torch.short";
     if (type == 7) return "torch.int8";
     if (type == 8) return "torch.uint8";
+    if (type == 9) return "torch.bool";
     return "null";
 }
 
@@ -89,6 +93,7 @@ static size_t type_to_elemsize(int type)
     if (type == 6) return 2;
     if (type == 7) return 1;
     if (type == 8) return 1;
+    if (type == 9) return 1;
     return 0; // null
 }
 
@@ -102,6 +107,7 @@ static int string_to_type(const char* s)
     if (strcmp(s, "i16") == 0) return 6;
     if (strcmp(s, "i8") == 0) return 7;
     if (strcmp(s, "u8") == 0) return 8;
+    if (strcmp(s, "bool") == 0) return 9;
     return 0; // null
 }
 
@@ -118,6 +124,7 @@ int get_at_tensor_type(const at::ScalarType& st)
     if (st == c10::ScalarType::QInt8) return 7;
     if (st == c10::ScalarType::Byte) return 8;
     if (st == c10::ScalarType::QUInt8) return 8;
+    if (st == c10::ScalarType::Bool) return 9;
     return 0; // unknown type
 }
 
@@ -1118,6 +1125,30 @@ static std::string make_slice_expression(const Operator* op)
     return r;
 }
 
+static std::string make_index_expression(const Operator* op)
+{
+    fprintf(stderr, "make_index_expression %s\n", op->name.c_str());
+
+    std::string index_expr = op->params.at("expr").s;
+
+    // strip out-most [ ] pair
+    index_expr = index_expr.substr(1, index_expr.size() - 2);
+
+    // None,None,   ->   ...,
+    bool leading_none = false;
+    while (index_expr.substr(0, 5) == "None,")
+    {
+        leading_none = true;
+        index_expr = index_expr.substr(5);
+    }
+    if (leading_none)
+    {
+        index_expr = "...," + index_expr;
+    }
+
+    return index_expr;
+}
+
 int Graph::python(const std::string& pypath, const std::string& pnnxbinpath)
 {
     FILE* pyfp = fopen(pypath.c_str(), "wb");
@@ -1428,6 +1459,12 @@ int Graph::python(const std::string& pypath, const std::string& pnnxbinpath)
                 // slice expr
                 std::string slice_expr = make_slice_expression(op);
                 fprintf(pyfp, "v_%s = v_%s[%s]\n", sanitize_identifier(op->outputs[0]->name).c_str(), sanitize_identifier(op->inputs[0]->name).c_str(), slice_expr.c_str());
+            }
+            else if (op->type == "Tensor.index")
+            {
+                // index expr
+                std::string index_expr = make_index_expression(op);
+                fprintf(pyfp, "v_%s = v_%s[%s]\n", sanitize_identifier(op->outputs[0]->name).c_str(), sanitize_identifier(op->inputs[0]->name).c_str(), index_expr.c_str());
             }
             else if (op->type == "Tensor.view" || op->type == "Tensor.reshape")
             {
