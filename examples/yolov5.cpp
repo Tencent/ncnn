23dd32a003f660bff2062c6e48b280f43e844fc4
pogg <82716366+ppogg@users.noreply.github.com>
2022-01-31 22:47:02 +0800

    Update yolov5.cpp

--- examples/yolov5.cpp
+++ examples/yolov5.cpp
@@ -18,13 +18,10 @@
 #if defined(USE_NCNN_SIMPLEOCV)
 #include "simpleocv.h"
 #else
-
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-
 #endif
-
 #include <float.h>
 #include <stdio.h>
 #include <vector>
@@ -57,7 +54,7 @@ public:
         if (top_blob.empty())
             return -100;
 
-#pragma omp parallel for num_threads(opt.num_threads)
+        #pragma omp parallel for num_threads(opt.num_threads)
         for (int p = 0; p < outc; p++)
         {
             const float* ptr = bottom_blob.channel(p % channels).row((p / channels) % 2) + ((p / channels) / 2);
@@ -84,30 +81,35 @@ public:
 DEFINE_LAYER_CREATOR(YoloV5Focus)
 #endif //YOLOV5_V60
 
-struct Object {
+struct Object
+{
     cv::Rect_<float> rect;
     int label;
     float prob;
 };
 
-static inline float intersection_area(const Object &a, const Object &b) {
+static inline float intersection_area(const Object& a, const Object& b)
+{
     cv::Rect_<float> inter = a.rect & b.rect;
     return inter.area();
 }
 
-static void qsort_descent_inplace(std::vector <Object> &faceobjects, int left, int right) {
+static void qsort_descent_inplace(std::vector<Object>& faceobjects, int left, int right)
+{
     int i = left;
     int j = right;
     float p = faceobjects[(left + right) / 2].prob;
 
-    while (i <= j) {
+    while (i <= j)
+    {
         while (faceobjects[i].prob > p)
             i++;
 
         while (faceobjects[j].prob < p)
             j--;
 
-        if (i <= j) {
+        if (i <= j)
+        {
             // swap
             std::swap(faceobjects[i], faceobjects[j]);
 
@@ -116,43 +118,47 @@ static void qsort_descent_inplace(std::vector <Object> &faceobjects, int left, i
         }
     }
 
-#pragma omp parallel sections
+    #pragma omp parallel sections
     {
-#pragma omp section
+        #pragma omp section
         {
             if (left < j) qsort_descent_inplace(faceobjects, left, j);
         }
-#pragma omp section
+        #pragma omp section
         {
             if (i < right) qsort_descent_inplace(faceobjects, i, right);
         }
     }
 }
 
-static void qsort_descent_inplace(std::vector <Object> &faceobjects) {
+static void qsort_descent_inplace(std::vector<Object>& faceobjects)
+{
     if (faceobjects.empty())
         return;
 
     qsort_descent_inplace(faceobjects, 0, faceobjects.size() - 1);
 }
 
-static void nms_sorted_bboxes(const std::vector <Object> &faceobjects, std::vector<int> &picked,
-                              float nms_threshold) {
+static void nms_sorted_bboxes(const std::vector<Object>& faceobjects, std::vector<int>& picked, float nms_threshold)
+{
     picked.clear();
 
     const int n = faceobjects.size();
 
     std::vector<float> areas(n);
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         areas[i] = faceobjects[i].rect.area();
     }
 
-    for (int i = 0; i < n; i++) {
-        const Object &a = faceobjects[i];
+    for (int i = 0; i < n; i++)
+    {
+        const Object& a = faceobjects[i];
 
         int keep = 1;
-        for (int j = 0; j < (int) picked.size(); j++) {
-            const Object &b = faceobjects[picked[j]];
+        for (int j = 0; j < (int)picked.size(); j++)
+        {
+            const Object& b = faceobjects[picked[j]];
 
             // intersection over union
             float inter_area = intersection_area(a, b);
@@ -167,7 +173,8 @@ static void nms_sorted_bboxes(const std::vector <Object> &faceobjects, std::vect
     }
 }
 
-static inline float sigmoid(float x) {
+static inline float sigmoid(float x)
+{
     return static_cast<float>(1.f / (1.f + exp(-x)));
 }
 
@@ -176,9 +183,8 @@ static inline float unsigmoid(float y) {
     return static_cast<float>(-1.0 * (log((1.0 / y) - 1.0)));
 }
 
-static void generate_proposals(const ncnn::Mat &anchors, int stride, const ncnn::Mat &in_pad,
-                               const ncnn::Mat &feat_blob, float prob_threshold,
-                               std::vector <Object> &objects) {
+static void generate_proposals(const ncnn::Mat &anchors, int stride, const ncnn::Mat &in_pad, const ncnn::Mat &feat_blob, float prob_threshold, std::vector <Object> &objects) 
+{
     const int num_grid = feat_blob.h;
     if (prob_threshold > 0.6)
         float unsig_pro = unsigmoid(prob_threshold);
@@ -295,9 +301,8 @@ static void generate_proposals(const ncnn::Mat &anchors, int stride, const ncnn:
         }
     }
 }
-
-
-static int detect_yolov5(const cv::Mat &bgr, std::vector <Object> &objects) {
+static int detect_yolov5(const cv::Mat& bgr, std::vector<Object>& objects)
+{
     ncnn::Net yolov5;
 
     yolov5.opt.use_vulkan_compute = true;
@@ -326,26 +331,27 @@ static int detect_yolov5(const cv::Mat &bgr, std::vector <Object> &objects) {
     int w = img_w;
     int h = img_h;
     float scale = 1.f;
-    if (w > h) {
-        scale = (float) target_size / w;
+    if (w > h)
+    {
+        scale = (float)target_size / w;
         w = target_size;
         h = h * scale;
-    } else {
-        scale = (float) target_size / h;
+    }
+    else
+    {
+        scale = (float)target_size / h;
         h = target_size;
         w = w * scale;
     }
 
-    ncnn::Mat in = ncnn::Mat::from_pixels_resize(bgr.data, ncnn::Mat::PIXEL_BGR2RGB, img_w,
-                                                 img_h, w, h);
+    ncnn::Mat in = ncnn::Mat::from_pixels_resize(bgr.data, ncnn::Mat::PIXEL_BGR2RGB, img_w, img_h, w, h);
 
     // pad to target_size rectangle
     // yolov5/utils/datasets.py letterbox
     int wpad = (w + MAX_STRIDE - 1) / MAX_STRIDE * MAX_STRIDE - w;
     int hpad = (h + MAX_STRIDE - 1) / MAX_STRIDE * MAX_STRIDE - h;
     ncnn::Mat in_pad;
-    ncnn::copy_make_border(in, in_pad, hpad / 2, hpad - hpad / 2, wpad / 2, wpad - wpad / 2,
-                           ncnn::BORDER_CONSTANT, 114.f);
+    ncnn::copy_make_border(in, in_pad, hpad / 2, hpad - hpad / 2, wpad / 2, wpad - wpad / 2, ncnn::BORDER_CONSTANT, 114.f);
 
     const float norm_vals[3] = {1 / 255.f, 1 / 255.f, 1 / 255.f};
     in_pad.substract_mean_normalize(0, norm_vals);
@@ -354,7 +360,7 @@ static int detect_yolov5(const cv::Mat &bgr, std::vector <Object> &objects) {
 
     ex.input("images", in_pad);
 
-    std::vector <Object> proposals;
+    std::vector<Object> proposals;
 
     // anchor setting from yolov5/models/yolov5s.yaml
 
@@ -371,7 +377,7 @@ static int detect_yolov5(const cv::Mat &bgr, std::vector <Object> &objects) {
         anchors[4] = 33.f;
         anchors[5] = 23.f;
 
-        std::vector <Object> objects8;
+        std::vector<Object> objects8;
         generate_proposals(anchors, 8, in_pad, out, prob_threshold, objects8);
 
         proposals.insert(proposals.end(), objects8.begin(), objects8.end());
@@ -394,7 +400,7 @@ static int detect_yolov5(const cv::Mat &bgr, std::vector <Object> &objects) {
         anchors[4] = 59.f;
         anchors[5] = 119.f;
 
-        std::vector <Object> objects16;
+        std::vector<Object> objects16;
         generate_proposals(anchors, 16, in_pad, out, prob_threshold, objects16);
 
         proposals.insert(proposals.end(), objects16.begin(), objects16.end());
@@ -416,7 +422,7 @@ static int detect_yolov5(const cv::Mat &bgr, std::vector <Object> &objects) {
         anchors[4] = 373.f;
         anchors[5] = 326.f;
 
-        std::vector <Object> objects32;
+        std::vector<Object> objects32;
         generate_proposals(anchors, 32, in_pad, out, prob_threshold, objects32);
 
         proposals.insert(proposals.end(), objects32.begin(), objects32.end());
@@ -432,7 +438,8 @@ static int detect_yolov5(const cv::Mat &bgr, std::vector <Object> &objects) {
     int count = picked.size();
 
     objects.resize(count);
-    for (int i = 0; i < count; i++) {
+    for (int i = 0; i < count; i++)
+    {
         objects[i] = proposals[picked[i]];
 
         // adjust offset to original unpadded
@@ -442,10 +449,10 @@ static int detect_yolov5(const cv::Mat &bgr, std::vector <Object> &objects) {
         float y1 = (objects[i].rect.y + objects[i].rect.height - (hpad / 2)) / scale;
 
         // clip
-        x0 = std::max(std::min(x0, (float) (img_w - 1)), 0.f);
-        y0 = std::max(std::min(y0, (float) (img_h - 1)), 0.f);
-        x1 = std::max(std::min(x1, (float) (img_w - 1)), 0.f);
-        y1 = std::max(std::min(y1, (float) (img_h - 1)), 0.f);
+        x0 = std::max(std::min(x0, (float)(img_w - 1)), 0.f);
+        y0 = std::max(std::min(y0, (float)(img_h - 1)), 0.f);
+        x1 = std::max(std::min(x1, (float)(img_w - 1)), 0.f);
+        y1 = std::max(std::min(y1, (float)(img_h - 1)), 0.f);
 
         objects[i].rect.x = x0;
         objects[i].rect.y = y0;
@@ -456,31 +463,25 @@ static int detect_yolov5(const cv::Mat &bgr, std::vector <Object> &objects) {
     return 0;
 }
 
-static void draw_objects(const cv::Mat &bgr, const std::vector <Object> &objects) {
-    static const char *class_names[] = {
-            "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck",
-            "boat", "traffic light",
-            "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog",
-            "horse", "sheep", "cow",
-            "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie",
-            "suitcase", "frisbee",
-            "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove",
-            "skateboard", "surfboard",
-            "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl",
-            "banana", "apple",
-            "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake",
-            "chair", "couch",
-            "potted plant", "bed", "dining table", "toilet", "tv", "laptop", "mouse", "remote",
-            "keyboard", "cell phone",
-            "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase",
-            "scissors", "teddy bear",
-            "hair drier", "toothbrush"
+static void draw_objects(const cv::Mat& bgr, const std::vector<Object>& objects)
+{
+    static const char* class_names[] = {
+        "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", "traffic light",
+        "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow",
+        "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee",
+        "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard",
+        "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple",
+        "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "couch",
+        "potted plant", "bed", "dining table", "toilet", "tv", "laptop", "mouse", "remote", "keyboard", "cell phone",
+        "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear",
+        "hair drier", "toothbrush"
     };
 
     cv::Mat image = bgr.clone();
 
-    for (size_t i = 0; i < objects.size(); i++) {
-        const Object &obj = objects[i];
+    for (size_t i = 0; i < objects.size(); i++)
+    {
+        const Object& obj = objects[i];
 
         fprintf(stderr, "%d = %.5f at %.2f %.2f %.2f x %.2f\n", obj.label, obj.prob,
                 obj.rect.x, obj.rect.y, obj.rect.width, obj.rect.height);
@@ -491,8 +492,7 @@ static void draw_objects(const cv::Mat &bgr, const std::vector <Object> &objects
         sprintf(text, "%s %.1f%%", class_names[obj.label], obj.prob * 100);
 
         int baseLine = 0;
-        cv::Size label_size = cv::getTextSize(text, cv::FONT_HERSHEY_SIMPLEX, 0.5, 1,
-                                              &baseLine);
+        cv::Size label_size = cv::getTextSize(text, cv::FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
 
         int x = obj.rect.x;
         int y = obj.rect.y - label_size.height - baseLine;
@@ -501,8 +501,7 @@ static void draw_objects(const cv::Mat &bgr, const std::vector <Object> &objects
         if (x + label_size.width > image.cols)
             x = image.cols - label_size.width;
 
-        cv::rectangle(image, cv::Rect(cv::Point(x, y),
-                                      cv::Size(label_size.width, label_size.height + baseLine)),
+        cv::rectangle(image, cv::Rect(cv::Point(x, y), cv::Size(label_size.width, label_size.height + baseLine)),
                       cv::Scalar(255, 255, 255), -1);
 
         cv::putText(image, text, cv::Point(x, y + label_size.height),
@@ -513,21 +512,24 @@ static void draw_objects(const cv::Mat &bgr, const std::vector <Object> &objects
     cv::waitKey(0);
 }
 
-int main(int argc, char **argv) {
-    if (argc != 2) {
+int main(int argc, char** argv)
+{
+    if (argc != 2)
+    {
         fprintf(stderr, "Usage: %s [imagepath]\n", argv[0]);
         return -1;
     }
 
-    const char *imagepath = argv[1];
+    const char* imagepath = argv[1];
 
     cv::Mat m = cv::imread(imagepath, 1);
-    if (m.empty()) {
+    if (m.empty())
+    {
         fprintf(stderr, "cv::imread %s failed\n", imagepath);
         return -1;
     }
 
-    std::vector <Object> objects;
+    std::vector<Object> objects;
     detect_yolov5(m, objects);
 
     draw_objects(m, objects);
