f5b90a53335c60ff76ea2c84b51004421f9c8c39
Yoh <550856122@qq.com>
2022-02-25 14:11:08 +0800

    Add unaryop x86 (#3579)
    Co-authored-by: Yoh <wangpeizhou@bilibili.com>
    Co-authored-by: Yoh-Z <Yoh-Z@users.noreply.github.com>

--- src/layer/x86/sse_mathfun.h
+++ src/layer/x86/sse_mathfun.h
@@ -400,9 +400,9 @@ static NCNN_FORCEINLINE v4sf sin_ps(v4sf x)
     xmm1 = *(v4sf*)_ps_minus_cephes_DP1;
     xmm2 = *(v4sf*)_ps_minus_cephes_DP2;
     xmm3 = *(v4sf*)_ps_minus_cephes_DP3;
-    xmm1 = _mm_comp_fmadd_ps(y, xmm1, xmm1);
-    xmm2 = _mm_comp_fmadd_ps(y, xmm2, xmm2);
-    xmm3 = _mm_comp_fmadd_ps(y, xmm3, xmm3);
+    x = _mm_comp_fmadd_ps(y, xmm1, x);
+    x = _mm_comp_fmadd_ps(y, xmm2, x);
+    x = _mm_comp_fmadd_ps(y, xmm3, x);
 
     /* Evaluate the first polynom  (0 <= x <= Pi/4) */
     y = *(v4sf*)_ps_coscof_p0;
@@ -412,7 +412,6 @@ static NCNN_FORCEINLINE v4sf sin_ps(v4sf x)
     y = _mm_comp_fmadd_ps(y, z, *(v4sf*)_ps_coscof_p2);
     y = _mm_mul_ps(y, z);
     y = _mm_mul_ps(y, z);
-    // y = y - z * 0.5
     y = _mm_comp_fnmadd_ps(z, *(v4sf*)_ps_0p5, y);
     y = _mm_add_ps(y, *(v4sf*)_ps_1);
 
@@ -509,9 +508,9 @@ static NCNN_FORCEINLINE v4sf cos_ps(v4sf x)
     xmm1 = *(v4sf*)_ps_minus_cephes_DP1;
     xmm2 = *(v4sf*)_ps_minus_cephes_DP2;
     xmm3 = *(v4sf*)_ps_minus_cephes_DP3;
-    xmm1 = _mm_comp_fmadd_ps(y, xmm1, xmm1);
-    xmm2 = _mm_comp_fmadd_ps(y, xmm2, xmm2);
-    xmm3 = _mm_comp_fmadd_ps(y, xmm3, xmm3);
+    x = _mm_comp_fmadd_ps(y, xmm1, x);
+    x = _mm_comp_fmadd_ps(y, xmm2, x);
+    x = _mm_comp_fmadd_ps(y, xmm3, x);
 
     /* Evaluate the first polynom  (0 <= x <= Pi/4) */
     y = *(v4sf*)_ps_coscof_p0;
@@ -521,20 +520,16 @@ static NCNN_FORCEINLINE v4sf cos_ps(v4sf x)
     y = _mm_comp_fmadd_ps(y, z, *(v4sf*)_ps_coscof_p2);
     y = _mm_mul_ps(y, z);
     y = _mm_mul_ps(y, z);
-    // y = y - z * 0.5
     y = _mm_comp_fnmadd_ps(z, *(v4sf*)_ps_0p5, y);
     y = _mm_add_ps(y, *(v4sf*)_ps_1);
 
     /* Evaluate the second polynom  (Pi/4 <= x <= 0) */
 
     v4sf y2 = *(v4sf*)_ps_sincof_p0;
+    y2 = _mm_comp_fmadd_ps(y2, z, *(v4sf*)_ps_sincof_p1);
+    y2 = _mm_comp_fmadd_ps(y2, z, *(v4sf*)_ps_sincof_p2);
     y2 = _mm_mul_ps(y2, z);
-    y2 = _mm_add_ps(y2, *(v4sf*)_ps_sincof_p1);
-    y2 = _mm_mul_ps(y2, z);
-    y2 = _mm_add_ps(y2, *(v4sf*)_ps_sincof_p2);
-    y2 = _mm_mul_ps(y2, z);
-    y2 = _mm_mul_ps(y2, x);
-    y2 = _mm_add_ps(y2, x);
+    y2 = _mm_comp_fmadd_ps(y2, x, x);
 
     /* select the correct result from the two polynoms */
     xmm3 = poly_mask;
@@ -626,9 +621,9 @@ static NCNN_FORCEINLINE void sincos_ps(v4sf x, v4sf* s, v4sf* c)
     xmm1 = *(v4sf*)_ps_minus_cephes_DP1;
     xmm2 = *(v4sf*)_ps_minus_cephes_DP2;
     xmm3 = *(v4sf*)_ps_minus_cephes_DP3;
-    xmm1 = _mm_comp_fmadd_ps(y, xmm1, xmm1);
-    xmm2 = _mm_comp_fmadd_ps(y, xmm2, xmm2);
-    xmm3 = _mm_comp_fmadd_ps(y, xmm3, xmm3);
+    x = _mm_comp_fmadd_ps(y, xmm1, x);
+    x = _mm_comp_fmadd_ps(y, xmm2, x);
+    x = _mm_comp_fmadd_ps(y, xmm3, x);
 
 #ifdef USE_SSE2
     emm4 = _mm_sub_epi32(emm4, *(v4si*)_pi32_2);
@@ -658,7 +653,6 @@ static NCNN_FORCEINLINE void sincos_ps(v4sf x, v4sf* s, v4sf* c)
     y = _mm_comp_fmadd_ps(y, z, *(v4sf*)_ps_coscof_p2);
     y = _mm_mul_ps(y, z);
     y = _mm_mul_ps(y, z);
-    // y = y - z * 0.5
     y = _mm_comp_fnmadd_ps(z, *(v4sf*)_ps_0p5, y);
     y = _mm_add_ps(y, *(v4sf*)_ps_1);
 
