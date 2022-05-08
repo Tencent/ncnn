9871d0cbe19df433682bc1d5bc259adee379a198
nihui <shuizhuyuanluo@126.com>
2022-05-08 14:49:38 +0800

    more tests

--- tests/test_einsum.cpp
+++ tests/test_einsum.cpp
@@ -39,12 +39,106 @@ static int test_einsum(const std::vector<ncnn::Mat>& a, const std::string& equat
 
 static int test_einsum_0()
 {
+    std::vector<ncnn::Mat> a(1);
+    a[0] = RandomMat(32, 32);
+
+    return test_einsum(a, "ii");
+}
+
+static int test_einsum_1()
+{
+    std::vector<ncnn::Mat> a(1);
+    a[0] = RandomMat(27, 32);
+
+    return test_einsum(a, "ij->i") || test_einsum(a, "ji->i");
+}
+
+static int test_einsum_2()
+{
+    std::vector<ncnn::Mat> a(1);
+    a[0] = RandomMat(17, 14, 32);
+
+    return 0
+           || test_einsum(a, "ijk->i")
+           || test_einsum(a, "jik->i")
+           || test_einsum(a, "jki->i")
+           || test_einsum(a, "ikj->ij")
+           || test_einsum(a, "kij->ij")
+           || test_einsum(a, "ijk->ij");
+}
+
+static int test_einsum_3()
+{
+    std::vector<ncnn::Mat> a(1);
+    a[0] = RandomMat(17, 14, 9, 32);
+
+    return 0
+           || test_einsum(a, "jkli->i")
+           || test_einsum(a, "jkil->i")
+           || test_einsum(a, "jikl->i")
+           || test_einsum(a, "ijkl->i")
+           || test_einsum(a, "iklj->ij")
+           || test_einsum(a, "klij->ij")
+           || test_einsum(a, "kijl->ij")
+           || test_einsum(a, "ijkl->ij")
+           || test_einsum(a, "ijlk->ijk")
+           || test_einsum(a, "lijk->ijk")
+           || test_einsum(a, "ijkl->ijk");
+}
+
+static int test_einsum_4()
+{
+    std::vector<ncnn::Mat> a(2);
+    a[0] = RandomMat(12, 28);
+    a[1] = RandomMat(12);
+
+    return test_einsum(a, "ij,j->i");
+}
+
+static int test_einsum_5()
+{
+    std::vector<ncnn::Mat> a(2);
+    a[0] = RandomMat(14);
+    a[1] = RandomMat(14, 7, 16);
+
+    return test_einsum(a, "k,ijk->ij");
+}
+
+static int test_einsum_6()
+{
+    std::vector<ncnn::Mat> a(2);
+    a[0] = RandomMat(27);
+    a[1] = RandomMat(32);
+
+    return test_einsum(a, "i,j->ij");
+}
+
+static int test_einsum_7()
+{
     std::vector<ncnn::Mat> a(2);
     a[0] = RandomMat(5, 2, 3);
     a[1] = RandomMat(4, 5, 3);
 
-    return 0
-           || test_einsum(a, "ijl,ilk->ijk");
+    return test_einsum(a, "ijl,ilk->ijk");
+}
+
+static int test_einsum_8()
+{
+    std::vector<ncnn::Mat> a(2);
+    a[0] = RandomMat(4, 5, 3);
+    a[1] = RandomMat(5, 2, 3);
+
+    return test_einsum(a, "ilk,ijl->ijk");
+}
+
+static int test_einsum_9()
+{
+    std::vector<ncnn::Mat> a(3);
+    a[0] = RandomMat(15, 12);
+    a[1] = RandomMat(24, 15, 13);
+    a[2] = RandomMat(24, 12);
+
+    return test_einsum(a, "ik,jkl,il->ij");
 }
 
 int main()
@@ -52,5 +146,14 @@ int main()
     SRAND(7767517);
 
     return 0
-           || test_einsum_0();
+           || test_einsum_0()
+           || test_einsum_1()
+           || test_einsum_2()
+           || test_einsum_3()
+           || test_einsum_4()
+           || test_einsum_5()
+           || test_einsum_6()
+           || test_einsum_7()
+           || test_einsum_8()
+           || test_einsum_9();
 }
