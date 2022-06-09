f77afd9665c7927bb42cfe4f576e7ae748eba982
nihui <shuizhuyuanluo@126.com>
2022-06-09 15:22:36 +0800

    pnnx export weight inside moduleop

--- tools/pnnx/src/pass_level1.h
+++ tools/pnnx/src/pass_level1.h
@@ -48,7 +48,7 @@ const std::vector<const FuseModulePass*>& get_global_pnnx_fuse_module_passes();
 #define REGISTER_GLOBAL_PNNX_FUSE_MODULE_PASS(CLASS) \
     static FuseModulePassRegister g_global_pnnx_fusemodulepass_##CLASS##_register(new CLASS);
 
-void pass_level1(const torch::jit::Module& mod, const std::shared_ptr<torch::jit::Graph>& g, Graph& pg);
+void pass_level1(const torch::jit::Module& mod, const std::shared_ptr<torch::jit::Graph>& g, const std::vector<std::string>& module_operators, Graph& pg);
 
 } // namespace pnnx
 
