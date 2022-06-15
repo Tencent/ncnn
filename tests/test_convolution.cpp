e0198a2adcf2f4308b5880257912e78c1919ac25
nihuini <nihuini@tencent.com>
2022-06-15 15:30:23 +0800

    wip

--- tests/test_convolution.cpp
+++ tests/test_convolution.cpp
@@ -155,8 +155,8 @@ static int test_convolution_0()
            || test_convolution(15, 17, 32, 26, 3, 1, 2, 0, 1)
            || test_convolution(30, 30, 32, 26, 3, 1, 1, 1, 0)
            || test_convolution(12, 18, 8, 16, 3, 1, 1, 1, 1)
-           || test_convolution(42, 18, 128, 128, 3, 1, 1, 1, 1)
-           || test_convolution(12, 18, 128, 128, 3, 1, 1, 1, 1);
+           || test_convolution(42, 18, 128, 160, 3, 1, 1, 1, 1)
+           || test_convolution(12, 18, 128, 160, 3, 1, 1, 1, 1);
 }
 
 static int test_convolution_vec(int w, int outch, int kernel, int dilation, int stride, int pad, int bias)
