39fb567d4bb8d749c57bda0b8abab37cffdc9399
nihui <shuizhuyuanluo@126.com>
2022-06-20 15:16:23 +0800

    arm neon assembly optimization for cast fp16/bf16

--- src/layer/arm/cast_fp16.h
+++ src/layer/arm/cast_fp16.h
@@ -43,6 +43,38 @@ static void cast_fp32_to_fp16_neon(const Mat& bottom_blob, Mat& top_blob, const 
 
         int i = 0;
 #if (__ARM_FP & 2)
+        for (; i + 15 < size; i += 16)
+        {
+#if __aarch64__
+            asm volatile(
+                "prfm   pldl1keep, [%0, #512]   \n"
+                "ld1    {v0.4s, v1.4s, v2.4s, v3.4s}, [%0], #64 \n"
+                "fcvtn  v0.4h, v0.4s            \n"
+                "fcvtn  v1.4h, v1.4s            \n"
+                "fcvtn  v2.4h, v2.4s            \n"
+                "fcvtn  v3.4h, v3.4s            \n"
+                "st1    {v0.4h, v1.4h, v2.4h, v3.4h}, [%1], #32 \n"
+                : "=r"(ptr), // %0
+                "=r"(outptr) // %1
+                : "0"(ptr),
+                "1"(outptr)
+                : "memory", "v0", "v1", "v2", "v3");
+#else  // __aarch64__
+            asm volatile(
+                "pld        [%0, #512]      \n"
+                "vldm       %0!, {d0-d7}    \n"
+                "vcvt.f16.f32 d0, q0        \n"
+                "vcvt.f16.f32 d1, q1        \n"
+                "vcvt.f16.f32 d2, q2        \n"
+                "vcvt.f16.f32 d3, q3        \n"
+                "vst1.u16   {d0-d3}, [%1 :128]! \n"
+                : "=r"(ptr), // %0
+                "=r"(outptr) // %1
+                : "0"(ptr),
+                "1"(outptr)
+                : "memory", "q0", "q1", "q2", "q3");
+#endif // __aarch64__
+        }
         for (; i + 7 < size; i += 8)
         {
             float32x4_t _p0_fp32 = vld1q_f32(ptr);
@@ -96,6 +128,38 @@ static void cast_fp16_to_fp32_neon(const Mat& bottom_blob, Mat& top_blob, const 
 
         int i = 0;
 #if (__ARM_FP & 2)
+        for (; i + 15 < size; i += 16)
+        {
+#if __aarch64__
+            asm volatile(
+                "prfm   pldl1keep, [%0, #256]   \n"
+                "ld1    {v0.4h, v1.4h, v2.4h, v3.4h}, [%0], #32 \n"
+                "fcvtl  v0.4s, v0.4h            \n"
+                "fcvtl  v1.4s, v1.4h            \n"
+                "fcvtl  v2.4s, v2.4h            \n"
+                "fcvtl  v3.4s, v3.4h            \n"
+                "st1    {v0.4s, v1.4s, v2.4s, v3.4s}, [%1], #64 \n"
+                : "=r"(ptr), // %0
+                "=r"(outptr) // %1
+                : "0"(ptr),
+                "1"(outptr)
+                : "memory", "v0", "v1", "v2", "v3");
+#else  // __aarch64__
+            asm volatile(
+                "pld        [%0, #256]      \n"
+                "vld1.u16   {d4-d7}, [%0 :128]! \n"
+                "vcvt.f32.f16 q0, d4        \n"
+                "vcvt.f32.f16 q1, d5        \n"
+                "vcvt.f32.f16 q2, d6        \n"
+                "vcvt.f32.f16 q3, d7        \n"
+                "vstm       %1!, {d0-d7}    \n"
+                : "=r"(ptr), // %0
+                "=r"(outptr) // %1
+                : "0"(ptr),
+                "1"(outptr)
+                : "memory", "q0", "q1", "q2", "q3");
+#endif // __aarch64__
+        }
         for (; i + 7 < size; i += 8)
         {
             uint16x8_t _p_fp16 = vld1q_u16(ptr);
