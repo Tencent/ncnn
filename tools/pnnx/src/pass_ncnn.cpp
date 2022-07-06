1892c253600e4962c309bb4c75f592b6d7ba7847
nihui <nihuini@tencent.com>
2022-07-06 17:10:30 +0800

    pnnx fuse megvii style shufflechannel slice (#3999)

--- tools/pnnx/src/pass_ncnn.cpp
+++ tools/pnnx/src/pass_ncnn.cpp
@@ -27,6 +27,7 @@
 #include "pass_ncnn/convert_Tensor_select.h"
 #include "pass_ncnn/eliminate_output.h"
 #include "pass_ncnn/expand_expression.h"
+#include "pass_ncnn/fuse_convert_shufflechannel_slice.h"
 #include "pass_ncnn/insert_split.h"
 #include "pass_ncnn/chain_multi_output.h"
 #include "pass_ncnn/solve_batch_index.h"
@@ -87,6 +88,8 @@ void pass_ncnn(Graph& g)
 
     ncnn::insert_reshape_pooling(g);
     ncnn::insert_reshape_linear(g);
+
+    ncnn::fuse_convert_shufflechannel_slice(g);
 
     ncnn::convert_torch_cat(g);
     ncnn::convert_torch_chunk(g);
