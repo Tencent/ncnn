18b90a8d3057f4edfa8d36f803edfe5122fd9929
nihui <nihui@users.noreply.github.com>
2022-07-14 02:36:45 +0000

    apply code-format changes

--- src/layer/arm/cast_bf16.h
+++ src/layer/arm/cast_bf16.h
@@ -134,7 +134,7 @@ static void cast_fp32_to_bf16_neon(const Mat& bottom_blob, Mat& top_blob, const 
                 "ldr    s0, [%0], #4    \n"
                 "bfcvt  h0, s0          \n"
                 "str    h0, [%1], #2    \n"
-                : "=r"(ptr),  // %0
+                : "=r"(ptr),   // %0
                 "=r"(outptr) // %1
                 : "0"(ptr),
                 "1"(outptr)
