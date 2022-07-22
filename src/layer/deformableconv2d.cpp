99b2dfc728621a0c9234a41e1802e24c2a3a485f
miemie2013 <53960695+miemie2013@users.noreply.github.com>
2022-07-22 16:09:00 +0800

    Add DeformableConv2D
    Add DeformableConv2D

--- src/layer/deformableconv2d.cpp
+++ src/layer/deformableconv2d.cpp
@@ -14,7 +14,7 @@
 
 #include "deformableconv2d.h"
 
-#include "layer_type.h"
+#include "fused_activation.h"
 
 namespace ncnn {
 
@@ -56,26 +56,6 @@ int DeformableConv2D::load_model(const ModelBin& mb)
         if (bias_data.empty())
             return -100;
     }
-
-    const int in_c = weight_data_size / (num_output * kernel_h * kernel_w);
-    weight_data = weight_data.reshape(kernel_w * kernel_h, in_c, num_output);
-    weight_data_t.create(in_c, kernel_w * kernel_h, num_output);
-    if (weight_data_t.empty())
-        return -100;
-    for (int q = 0; q < num_output; q++)
-    {
-        const Mat m = weight_data.channel(q);
-        float* outptr = weight_data_t.channel(q);
-
-        for (int i = 0; i < kernel_w * kernel_h; i++)
-        {
-            for (int j = 0; j < in_c; j++)
-            {
-                *outptr++ = m.row(j)[i];
-            }
-        }
-    }
-    weight_data_t = weight_data_t.reshape(in_c * kernel_w * kernel_h, num_output);
     return 0;
 }
 
@@ -96,28 +76,26 @@ int DeformableConv2D::forward(const std::vector<Mat>& bottom_blobs, std::vector<
     const int out_w = (w + pad_left + pad_right - kernel_extent_w) / stride_w + 1;
     const int out_h = (h + pad_top + pad_bottom - kernel_extent_h) / stride_h + 1;
 
-    // output = im2col matmul weight_t, im2col.shape is [out_h * out_w, kernel_h * kernel_w * in_c] (in python),
-    // weight_t.shape is [num_output, kernel_h * kernel_w * in_c] (in python),
-    // output.shape   is [out_h * out_w, num_output] (in python).
-    Mat im2col;
-    im2col.create(kernel_h * kernel_w * in_c * out_h * out_w, elemsize, opt.blob_allocator);
-    if (im2col.empty())
-        return -100;
-
+    // output.shape is [num_output, out_h, out_w] (in python).
     Mat& output = top_blobs[0];
-    output.create(num_output, out_h * out_w, elemsize, opt.blob_allocator);
+    output.create(out_w * out_h * num_output, elemsize, opt.blob_allocator);
     if (output.empty())
         return -100;
 
+    // bottom_blob.shape is [in_c, h, w] (in python).
     Mat bottom_blob_flatten = bottom_blob.reshape(w * h * in_c);
     Mat offset_flatten = offset.reshape(offset.w * offset.h * offset.c);
     Mat mask_flatten = mask.reshape(mask.w * mask.h * mask.c);
     const float* data_im_ptr = bottom_blob_flatten;
     const float* data_offset_ptr = offset_flatten;
     const float* data_mask_ptr = mask_flatten;
-    float* im2col_ptr = im2col;
+    const float* weight_ptr = weight_data;
+    const float* bias_ptr = weight_data;
+    if (bias_term)
+        bias_ptr = bias_data;
+    float* output_ptr = output;
 
-    // im2col
+    // deformable conv
     #pragma omp parallel for num_threads(opt.num_threads)
     for (int h_col = 0; h_col < out_h; h_col++)
     {
@@ -125,144 +103,109 @@ int DeformableConv2D::forward(const std::vector<Mat>& bottom_blobs, std::vector<
         {
             int h_in = h_col * stride_h - pad_top;
             int w_in = w_col * stride_w - pad_left;
-            float* data_col_ptr = im2col_ptr + (h_col * out_w + w_col) * kernel_h * kernel_w * in_c;
-            for (int i = 0; i < kernel_h; i++)
+            float* output_hw_ptr = output_ptr + (h_col * out_w + w_col);
+            for (int oc = 0; oc < num_output; oc++)
             {
-                for (int j = 0; j < kernel_w; j++)
+                float sum = 0.f;
+                if (bias_term)
+                    sum = bias_ptr[oc];
+                for (int i = 0; i < kernel_h; i++)
                 {
-                    const int data_offset_h_ptr = (((i * kernel_w + j) * 2) * out_h + h_col) * out_w + w_col;
-                    const int data_offset_w_ptr = (((i * kernel_w + j) * 2 + 1) * out_h + h_col) * out_w + w_col;
-                    const int data_mask_hw_ptr = ((i * kernel_w + j) * out_h + h_col) * out_w + w_col;
-
-                    const float offset_h = data_offset_ptr[data_offset_h_ptr];
-                    const float offset_w = data_offset_ptr[data_offset_w_ptr];
-                    const float mask_ = data_mask_ptr[data_mask_hw_ptr];
-                    const float h_im = h_in + i * dilation_h + offset_h;
-                    const float w_im = w_in + j * dilation_w + offset_w;
-
-                    // Bilinear
-                    const bool cond = h_im > -1 && w_im > -1 && h_im < h && w_im < w;
-                    float w1 = 0.f;
-                    float w2 = 0.f;
-                    float w3 = 0.f;
-                    float w4 = 0.f;
-                    bool v1_cond = false;
-                    bool v2_cond = false;
-                    bool v3_cond = false;
-                    bool v4_cond = false;
-                    int v1_pos = 0;
-                    int v2_pos = 0;
-                    int v3_pos = 0;
-                    int v4_pos = 0;
-                    if (cond) {
-                        int h_low = floor(h_im);
-                        int w_low = floor(w_im);
-                        int h_high = h_low + 1;
-                        int w_high = w_low + 1;
-
-                        float lh = h_im - h_low;
-                        float lw = w_im - w_low;
-                        float hh = 1 - lh;
-                        float hw = 1 - lw;
-
-                        v1_cond = (h_low >= 0 && w_low >= 0);
-                        v2_cond = (h_low >= 0 && w_high <= w - 1);
-                        v3_cond = (h_high <= h - 1 && w_low >= 0);
-                        v4_cond = (h_high <= h - 1 && w_high <= w - 1);
-                        if (v1_cond) {
-                            v1_pos = h_low * w + w_low;
-                        }
-                        if (v2_cond) {
-                            v2_pos = h_low * w + w_high;
-                        }
-                        if (v3_cond) {
-                            v3_pos = h_high * w + w_low;
-                        }
-                        if (v4_cond) {
-                            v4_pos = h_high * w + w_high;
-                        }
-
-                        w1 = hh * hw;
-                        w2 = hh * lw;
-                        w3 = lh * hw;
-                        w4 = lh * lw;
-                    }
-
-                    const float* data_im_channel_ptr = data_im_ptr;
-                    for (int c_im = 0; c_im < in_c; c_im++)
+                    for (int j = 0; j < kernel_w; j++)
                     {
-                        float val = 0.f;
+                        const int data_offset_h_ptr = (((i * kernel_w + j) * 2) * out_h + h_col) * out_w + w_col;
+                        const int data_offset_w_ptr = (((i * kernel_w + j) * 2 + 1) * out_h + h_col) * out_w + w_col;
+                        const int data_mask_hw_ptr = ((i * kernel_w + j) * out_h + h_col) * out_w + w_col;
+
+                        const float offset_h = data_offset_ptr[data_offset_h_ptr];
+                        const float offset_w = data_offset_ptr[data_offset_w_ptr];
+                        const float mask_ = data_mask_ptr[data_mask_hw_ptr];
+                        const float h_im = h_in + i * dilation_h + offset_h;
+                        const float w_im = w_in + j * dilation_w + offset_w;
+
+                        // Bilinear
+                        const bool cond = h_im > -1 && w_im > -1 && h_im < h && w_im < w;
+                        float w1 = 0.f;
+                        float w2 = 0.f;
+                        float w3 = 0.f;
+                        float w4 = 0.f;
+                        bool v1_cond = false;
+                        bool v2_cond = false;
+                        bool v3_cond = false;
+                        bool v4_cond = false;
+                        int v1_pos = 0;
+                        int v2_pos = 0;
+                        int v3_pos = 0;
+                        int v4_pos = 0;
                         if (cond) {
-                            float v1 = 0.f;
+                            int h_low = floor(h_im);
+                            int w_low = floor(w_im);
+                            int h_high = h_low + 1;
+                            int w_high = w_low + 1;
+
+                            float lh = h_im - h_low;
+                            float lw = w_im - w_low;
+                            float hh = 1 - lh;
+                            float hw = 1 - lw;
+
+                            v1_cond = (h_low >= 0 && w_low >= 0);
+                            v2_cond = (h_low >= 0 && w_high <= w - 1);
+                            v3_cond = (h_high <= h - 1 && w_low >= 0);
+                            v4_cond = (h_high <= h - 1 && w_high <= w - 1);
                             if (v1_cond) {
-                                v1 = data_im_channel_ptr[v1_pos];
+                                v1_pos = h_low * w + w_low;
                             }
-                            float v2 = 0.f;
                             if (v2_cond) {
-                                v2 = data_im_channel_ptr[v2_pos];
+                                v2_pos = h_low * w + w_high;
                             }
-                            float v3 = 0.f;
                             if (v3_cond) {
-                                v3 = data_im_channel_ptr[v3_pos];
+                                v3_pos = h_high * w + w_low;
                             }
-                            float v4 = 0.f;
                             if (v4_cond) {
-                                v4 = data_im_channel_ptr[v4_pos];
+                                v4_pos = h_high * w + w_high;
                             }
-                            val = w1 * v1 + w2 * v2 + w3 * v3 + w4 * v4;
+
+                            w1 = hh * hw;
+                            w2 = hh * lw;
+                            w3 = lh * hw;
+                            w4 = lh * lw;
                         }
-                        *data_col_ptr = val * mask_;
-                        data_col_ptr += 1;
-                        data_im_channel_ptr += h*w;
+
+                        const float* data_im_channel_ptr = data_im_ptr;
+                        for (int c_im = 0; c_im < in_c; c_im++)
+                        {
+                            float val = 0.f;
+                            if (cond) {
+                                float v1 = 0.f;
+                                if (v1_cond) {
+                                    v1 = data_im_channel_ptr[v1_pos];
+                                }
+                                float v2 = 0.f;
+                                if (v2_cond) {
+                                    v2 = data_im_channel_ptr[v2_pos];
+                                }
+                                float v3 = 0.f;
+                                if (v3_cond) {
+                                    v3 = data_im_channel_ptr[v3_pos];
+                                }
+                                float v4 = 0.f;
+                                if (v4_cond) {
+                                    v4 = data_im_channel_ptr[v4_pos];
+                                }
+                                val = w1 * v1 + w2 * v2 + w3 * v3 + w4 * v4;
+                            }
+                            sum += val * mask_ * weight_ptr[((oc * in_c + c_im) * kernel_h + i) * kernel_w + j];
+                            data_im_channel_ptr += h*w;
+                        }
                     }
                 }
+                *output_hw_ptr = activation_ss(sum, activation_type, activation_params);
+                output_hw_ptr += out_h * out_w;
             }
         }
     }
-    im2col = im2col.reshape(kernel_h * kernel_w * in_c, out_h * out_w);
-
-    // call InnerProduct
-    ncnn::Layer* innerProduct = ncnn::create_layer(ncnn::LayerType::InnerProduct);
-
-    // set param
-    ncnn::ParamDict pd;
-    pd.set(0, num_output);
-    pd.set(1, bias_term);
-    pd.set(2, weight_data_size);
-    pd.set(9, activation_type);
-    pd.set(10, activation_params);
-    innerProduct->load_param(pd);
-
-    // set weights
-    ncnn::Mat weights[2];
-    weights[0] = weight_data_t;
-    if (bias_term)
-    {
-        weights[1] = bias_data;
-    }
-    innerProduct->load_model(ncnn::ModelBinFromMatArray(weights));
-    innerProduct->create_pipeline(opt);
-
-    // forward
-    innerProduct->forward(im2col, output, opt);
-    innerProduct->destroy_pipeline(opt);
-    delete innerProduct;
-
-    ncnn::Mat output_t;
-    // call Permute
-    ncnn::Layer* permute = ncnn::create_layer(ncnn::LayerType::Permute);
-
-    // set param
-    ncnn::ParamDict permute_pd;
-    permute_pd.set(0, 1);
-    permute->load_param(permute_pd);
-    permute->create_pipeline(opt);
-    // forward
-    permute->forward(output, output_t, opt);
-    permute->destroy_pipeline(opt);
-    delete permute;
-    output_t = output_t.reshape(out_w, out_h, num_output);
-    top_blobs[0] = output_t;
+    output = output.reshape(out_w, out_h, num_output);
+    top_blobs[0] = output;
     return 0;
 }
 
--- src/layer/x86/deformableconv2d_x86.h
+++ src/layer/x86/deformableconv2d_x86.h
@@ -12,50 +12,26 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
-#ifndef LAYER_DEFORMABLECONV2D_H
-#define LAYER_DEFORMABLECONV2D_H
+#ifndef LAYER_DEFORMABLECONV2D_X86_H
+#define LAYER_DEFORMABLECONV2D_X86_H
 
-#include "layer.h"
+#include "deformableconv2d.h"
 
 namespace ncnn {
 
-class DeformableConv2D : public Layer
+class DeformableConv2D_x86 : virtual public DeformableConv2D
 {
 public:
-    DeformableConv2D();
-
-    virtual int load_param(const ParamDict& pd);
+    DeformableConv2D_x86();
 
     virtual int load_model(const ModelBin& mb);
 
     virtual int forward(const std::vector<Mat>& bottom_blobs, std::vector<Mat>& top_blobs, const Option& opt) const;
 
 public:
-    // param
-    int num_output;
-    int kernel_w;
-    int kernel_h;
-    int dilation_w;
-    int dilation_h;
-    int stride_w;
-    int stride_h;
-    int pad_left; // -233=SAME_UPPER -234=SAME_LOWER
-    int pad_right;
-    int pad_top;
-    int pad_bottom;
-    int bias_term;
-
-    int weight_data_size;
-
-    // 0=none 1=relu 2=leakyrelu 3=clip 4=sigmoid
-    int activation_type;
-    Mat activation_params;
-
-    // model
-    Mat weight_data;
-    Mat bias_data;
+    Mat weight_data_t;
 };
 
 } // namespace ncnn
 
-#endif // LAYER_DEFORMABLECONV2D_H
+#endif // LAYER_DEFORMABLECONV2D_X86_H
