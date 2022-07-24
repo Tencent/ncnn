abb87078443d2eeae93643bdca0bcb71927ad60f
LRY89757 <lry89757@gmail.com>
2022-07-24 11:45:13 +0800

    Merge the for loop into one when elempack==1

--- src/layer/x86/instancenorm_x86.cpp
+++ src/layer/x86/instancenorm_x86.cpp
@@ -162,7 +162,6 @@ int InstanceNorm_x86::forward_inplace(Mat& bottom_top_blob, const Option& opt) c
 
         return 0;
     }
-
 #endif
     if (elempack == 4)
     {
@@ -222,275 +221,201 @@ int InstanceNorm_x86::forward_inplace(Mat& bottom_top_blob, const Option& opt) c
 
         return 0;
     }
-
 #endif
+
+    #pragma omp parallel for num_threads(opt.num_threads)
+    for (int q = 0; q < c; q++)
+    {
+        float* ptr = bottom_top_blob.channel(q);
+
+        // mean
+        int i = 0;
+        float sum = 0.f;
+#if __SSE2__
+#if __AVX__
+#if __AVX512F__
+        __m512 _fLoad512;
+        __m512 _fsum512 = _mm512_setzero_ps();
+
+        for (; i + 15 < size; i += 16)
+        {
+            _fLoad512 = _mm512_loadu_ps(ptr);
+            _fsum512 = _mm512_add_ps(_fsum512, _fLoad512);
+            ptr += 16;
+        }
+        sum += _mm512_reduce_add_ps(_fsum512);
+#endif // __AVX512F__
+        __m256 _fLoad256;
+        __m256 _fsum256 = _mm256_setzero_ps();
+        for (; i + 7 < size; i += 8)
+        {
+            _fLoad256 = _mm256_loadu_ps(ptr);
+            _fsum256 = _mm256_add_ps(_fsum256, _fLoad256);
+            ptr += 8;
+        }
+        sum += _mm256_reduce_add_ps(_fsum256);
+#endif // __AVX__
+        __m128 _fLoad128;
+        __m128 _fsum128 = _mm_setzero_ps();
+        for (; i + 3 < size; i += 4)
+        {
+            _fLoad128 = _mm_loadu_ps(ptr);
+            _fsum128 = _mm_add_ps(_fsum128, _fLoad128);
+            ptr += 4;
+        }
+        sum += _mm_reduce_add_ps(_fsum128);
+#endif // __SSE2__
+        for (; i < size; i++)
+        {
+            sum += *ptr;
+            ptr++;
+        }
+
+        // var
+        ptr = bottom_top_blob.channel(q);
+        float sqsum = 0.f;
+        float mean = sum / size;
+        i = 0;
 
 #if __SSE2__
 #if __AVX__
 #if __AVX512F__
-    #pragma omp parallel for num_threads(opt.num_threads)
-    for (int q = 0; q < c; q++)
-    {
-        float* ptr = bottom_top_blob.channel(q);
-        int ssize = size / 16;
-        int remainsize = ssize * 16;
-
-        __m512 _fLoad;
-
-        // mean
-        float sum = 0.f;
-        float sqsum = 0.f;
-
-        __m512 _fsum = _mm512_setzero_ps();
-
-        for (int i = 0; i < ssize; i++)
+        __m512 _fsqsum512 = _mm512_setzero_ps();
+        __m512 _mean512 = _mm512_set1_ps(mean);
+        for (; i + 15 < size; i += 16)
         {
-            _fLoad = _mm512_loadu_ps(ptr + (i * 16));
-            _fsum = _mm512_add_ps(_fsum, _fLoad);
+            _fLoad512 = _mm512_loadu_ps(ptr);
+            _fLoad512 = _mm512_sub_ps(_fLoad512, _mean512);
+            _fLoad512 = _mm512_mul_ps(_fLoad512, _fLoad512);
+            _fsqsum512 = _mm512_add_ps(_fsqsum512, _fLoad512);
+            ptr += 16;
         }
 
-        sum = _mm512_reduce_add_ps(_fsum);
+        sqsum += _mm512_reduce_add_ps(_fsqsum512);
 
-        for (int i = remainsize; i < size; i++)
-            sum += ptr[i];
-
-        float mean = sum / size;
-        __m512 _mean = _mm512_set1_ps(mean);
-        __m512 _fsqsum = _mm512_setzero_ps();
-
-        for (int i = 0; i < ssize; i++)
-        {
-            _fLoad = _mm512_loadu_ps(ptr + (i * 16));
-            _fLoad = _mm512_sub_ps(_fLoad, _mean);
-            _fLoad = _mm512_mul_ps(_fLoad, _fLoad);
-            _fsqsum = _mm512_add_ps(_fsqsum, _fLoad);
-        }
-
-        sqsum = _mm512_reduce_add_ps(_fsqsum);
-
-        float tmp = 0.f;
-        for (int i = remainsize; i < size; i++)
-        {
-            tmp = ptr[i] - mean;
-            sqsum += tmp * tmp;
-        }
-
-        // var
-        float var = sqsum / size;
-        float a, b;
-        __m512 _a, _b;
-
-        if (affine)
-        {
-            float gamma = gamma_data[q];
-            float beta = beta_data[q];
-
-            a = static_cast<float>(gamma / (sqrt(var + eps)));
-            b = -mean * a + beta;
-
-            _a = _mm512_set1_ps(a);
-            _b = _mm512_set1_ps(b);
-        }
-        else
-        {
-            a = static_cast<float>(1.f / (sqrt(var + eps)));
-            b = -mean * a;
-
-            _a = _mm512_set1_ps(a);
-            _b = _mm512_set1_ps(b);
-        }
-
-        for (int i = 0; i < ssize; i++)
-        {
-            _fLoad = _mm512_loadu_ps(ptr + (i * 16));
-            _fLoad = _mm512_mul_ps(_fLoad, _a);
-            _fLoad = _mm512_add_ps(_fLoad, _b);
-
-            _mm512_storeu_ps(ptr + (i * 16), _fLoad);
-        }
-        for (int i = remainsize; i < size; i++)
-        {
-            ptr[i] = ptr[i] * a + b;
-        }
-    }
-    return 0;
+        __m256 _mean256 = _mm512_castps512_ps256(_mean512);
+#else
+        __m256 _mean256 = _mm256_set1_ps(mean);
 #endif // __AVX512F__
-    #pragma omp parallel for num_threads(opt.num_threads)
-    for (int q = 0; q < c; q++)
-    {
-        float* ptr = bottom_top_blob.channel(q);
-        int ssize = size / 8;
-        int remainsize = ssize * 8;
-
-        __m256 _fLoad;
-
-        // mean
-        float sum = 0.f;
-        float sqsum = 0.f;
-
-        __m256 _fsum = _mm256_setzero_ps();
-
-        for (int i = 0; i < ssize; i++)
+        __m256 _fsqsum256 = _mm256_setzero_ps();
+        for (; i + 7 < size; i += 8)
         {
-            _fLoad = _mm256_loadu_ps(ptr + (i * 8));
-            _fsum = _mm256_add_ps(_fsum, _fLoad);
+            _fLoad256 = _mm256_loadu_ps(ptr);
+            _fLoad256 = _mm256_sub_ps(_fLoad256, _mean256);
+            _fLoad256 = _mm256_mul_ps(_fLoad256, _fLoad256);
+            _fsqsum256 = _mm256_add_ps(_fsqsum256, _fLoad256);
+            ptr += 8;
         }
 
-        sum = _mm256_reduce_add_ps(_fsum);
+        sqsum += _mm256_reduce_add_ps(_fsqsum256);
 
-        for (int i = remainsize; i < size; i++)
-            sum += ptr[i];
-
-        float mean = sum / size;
-        __m256 _mean = _mm256_set1_ps(mean);
-        __m256 _fsqsum = _mm256_setzero_ps();
-
-        for (int i = 0; i < ssize; i++)
-        {
-            _fLoad = _mm256_loadu_ps(ptr + (i * 8));
-            _fLoad = _mm256_sub_ps(_fLoad, _mean);
-            _fLoad = _mm256_mul_ps(_fLoad, _fLoad);
-            _fsqsum = _mm256_add_ps(_fsqsum, _fLoad);
-        }
-
-        sqsum = _mm256_reduce_add_ps(_fsqsum);
-
-        float tmp = 0.f;
-        for (int i = remainsize; i < size; i++)
-        {
-            tmp = ptr[i] - mean;
-            sqsum += tmp * tmp;
-        }
-
-        // var
-        float var = sqsum / size;
-        float a, b;
-        __m256 _a, _b;
-
-        if (affine)
-        {
-            float gamma = gamma_data[q];
-            float beta = beta_data[q];
-
-            a = static_cast<float>(gamma / (sqrt(var + eps)));
-            b = -mean * a + beta;
-
-            _a = _mm256_set1_ps(a);
-            _b = _mm256_set1_ps(b);
-        }
-        else
-        {
-            a = static_cast<float>(1.f / (sqrt(var + eps)));
-            b = -mean * a;
-
-            _a = _mm256_set1_ps(a);
-            _b = _mm256_set1_ps(b);
-        }
-
-        for (int i = 0; i < ssize; i++)
-        {
-            _fLoad = _mm256_loadu_ps(ptr + (i * 8));
-            _fLoad = _mm256_mul_ps(_fLoad, _a);
-            _fLoad = _mm256_add_ps(_fLoad, _b);
-
-            _mm256_storeu_ps(ptr + (i * 8), _fLoad);
-        }
-        for (int i = remainsize; i < size; i++)
-        {
-            ptr[i] = ptr[i] * a + b;
-        }
-    }
-    return 0;
+        __m128 _mean128 = _mm256_castps256_ps128(_mean256);
+#else
+        __m128 _mean128 = _mm_set1_ps(mean);
 #endif // __AVX__
-    #pragma omp parallel for num_threads(opt.num_threads)
-    for (int q = 0; q < c; q++)
-    {
-        float* ptr = bottom_top_blob.channel(q);
-        int ssize = size / 4;
-        int remainsize = ssize * 4;
-
-        __m128 _fLoad;
-
-        // mean
-        float sum = 0.f;
-        float sqsum = 0.f;
-
-        __m128 _fsum = _mm_setzero_ps();
-
-        for (int i = 0; i < ssize; i++)
+        __m128 _fsqsum128 = _mm_setzero_ps();
+        for (; i + 3 < size; i += 4)
         {
-            _fLoad = _mm_load_ps(ptr + (i * 4));
-            _fsum = _mm_add_ps(_fsum, _fLoad);
+            _fLoad128 = _mm_loadu_ps(ptr);
+            _fLoad128 = _mm_sub_ps(_fLoad128, _mean128);
+            _fLoad128 = _mm_mul_ps(_fLoad128, _fLoad128);
+            _fsqsum128 = _mm_add_ps(_fsqsum128, _fLoad128);
+            ptr += 4;
         }
 
-        sum = _mm_reduce_add_ps(_fsum);
-
-        for (int i = remainsize; i < size; i++)
-            sum += ptr[i];
-
-        float mean = sum / size;
-        __m128 _mean = _mm_set1_ps(mean);
-        __m128 _fsqsum = _mm_setzero_ps();
-
-        for (int i = 0; i < ssize; i++)
-        {
-            _fLoad = _mm_load_ps(ptr + (i * 4));
-            _fLoad = _mm_sub_ps(_fLoad, _mean);
-            _fLoad = _mm_mul_ps(_fLoad, _fLoad);
-            _fsqsum = _mm_add_ps(_fsqsum, _fLoad);
-        }
-
-        sqsum = _mm_reduce_add_ps(_fsqsum);
-
-        float tmp = 0.f;
-        for (int i = remainsize; i < size; i++)
-        {
-            tmp = ptr[i] - mean;
-            sqsum += tmp * tmp;
-        }
-
-        // var
-        float var = sqsum / size;
-        float a, b;
-        __m128 _a, _b;
-
-        if (affine)
-        {
-            float gamma = gamma_data[q];
-            float beta = beta_data[q];
-
-            a = static_cast<float>(gamma / (sqrt(var + eps)));
-            b = -mean * a + beta;
-
-            _a = _mm_set1_ps(a);
-            _b = _mm_set1_ps(b);
-        }
-        else
-        {
-            a = static_cast<float>(1.f / (sqrt(var + eps)));
-            b = -mean * a;
-
-            _a = _mm_set1_ps(a);
-            _b = _mm_set1_ps(b);
-        }
-
-        for (int i = 0; i < ssize; i++)
-        {
-            _fLoad = _mm_load_ps(ptr + (i * 4));
-            _fLoad = _mm_mul_ps(_fLoad, _a);
-            _fLoad = _mm_add_ps(_fLoad, _b);
-
-            _mm_store_ps(ptr + (i * 4), _fLoad);
-        }
-        for (int i = remainsize; i < size; i++)
-        {
-            ptr[i] = ptr[i] * a + b;
-        }
-    }
-    return 0;
+        sqsum += _mm_reduce_add_ps(_fsqsum128);
 #endif // __SSE2__
 
-    return InstanceNorm::forward_inplace(bottom_top_blob, opt);
+        float tmp = 0.f;
+        for (; i < size; i++)
+        {
+            tmp = (*ptr) - mean;
+            sqsum += tmp * tmp;
+
+            ptr++;
+        }
+
+        float var = sqsum / size;
+        ptr = bottom_top_blob.channel(q);
+        float a, b;
+        i = 0;
+
+        if (affine)
+        {
+            float gamma = gamma_data[q];
+            float beta = beta_data[q];
+
+            a = static_cast<float>(gamma / (sqrt(var + eps)));
+            b = -mean * a + beta;
+        }
+        else
+        {
+            a = static_cast<float>(1.f / (sqrt(var + eps)));
+            b = -mean * a;
+        }
+
+#if __SSE2__
+#if __AVX__
+#if __AVX512F__
+        __m512 _a512 = _mm512_set1_ps(a);
+        __m512 _b512 = _mm512_set1_ps(b);
+        // __m512 _fLoad512 = _mm512_setzero_ps();
+
+        for (; i + 15 < size; i += 16)
+        {
+            _fLoad512 = _mm512_loadu_ps(ptr);
+            _fLoad512 = _mm512_mul_ps(_fLoad512, _a512);
+            _fLoad512 = _mm512_add_ps(_fLoad512, _b512);
+
+            _mm512_storeu_ps(ptr, _fLoad512);
+            ptr += 16;
+        }
+        __m256 _a256 = _mm512_castps512_ps256(_a512);
+        __m256 _b256 = _mm512_castps512_ps256(_b512);
+#else
+        __m256 _a256 = _mm256_set1_ps(a);
+        __m256 _b256 = _mm256_set1_ps(b);
+
+#endif // __AVX512F__
+        // __m256 _fLoad256 = _mm256_setzero_ps();
+
+        for (; i + 7 < size; i += 8)
+        {
+            _fLoad256 = _mm256_loadu_ps(ptr);
+            _fLoad256 = _mm256_mul_ps(_fLoad256, _a256);
+            _fLoad256 = _mm256_add_ps(_fLoad256, _b256);
+
+            _mm256_storeu_ps(ptr, _fLoad256);
+            ptr += 8;
+        }
+        __m128 _a128 = _mm256_castps256_ps128(_a256);
+        __m128 _b128 = _mm256_castps256_ps128(_b256);
+#else
+        __m128 _a128 = _mm_set1_ps(a);
+        __m128 _b128 = _mm_set1_ps(b);
+
+#endif // __AVX__
+        // __m128 _fLoad128 = _mm_setzero_ps();
+
+        for (; i + 3 < size; i += 4)
+        {
+            _fLoad128 = _mm_loadu_ps(ptr);
+            _fLoad128 = _mm_mul_ps(_fLoad128, _a128);
+            _fLoad128 = _mm_add_ps(_fLoad128, _b128);
+
+            _mm_storeu_ps(ptr, _fLoad128);
+            ptr += 4;
+        }
+#endif // __SSE2__
+        for (; i < size; i++)
+        {
+            *ptr = *ptr * a + b;
+
+            ptr++;
+        }
+    }
+
+    return 0;
 }
 
 } // namespace ncnn
