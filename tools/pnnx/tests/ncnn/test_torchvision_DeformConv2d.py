720f3c9aab22ba4d9ea1650d16f21d84c7ece4ed
miemie2013 <53960695+miemie2013@users.noreply.github.com>
2022-07-27 23:45:09 -0400

    Add DeformableConv2D (#4070)
    * Add DeformableConv2D
    * add unittest and docs
    * pnnx torchvision deformconv2d conversion
    
    Co-authored-by: miemie2013 <miemie2013@users.noreply.github.com>
    Co-authored-by: nihui <shuizhuyuanluo@126.com>

--- tools/pnnx/tests/ncnn/test_torchvision_DeformConv2d.py
+++ tools/pnnx/tests/ncnn/test_torchvision_DeformConv2d.py
@@ -1,6 +1,6 @@
 # Tencent is pleased to support the open source community by making ncnn available.
 #
-# Copyright (C) 2021 THL A29 Limited, a Tencent company. All rights reserved.
+# Copyright (C) 2022 THL A29 Limited, a Tencent company. All rights reserved.
 #
 # Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
 # in compliance with the License. You may obtain a copy of the License at
@@ -36,7 +36,7 @@ class Model(nn.Module):
         return x1, x2
 
 def test():
-    net = Model()
+    net = Model().half().float()
     net.eval()
 
     torch.manual_seed(0)
@@ -50,13 +50,13 @@ def test():
 
     # torchscript to pnnx
     import os
-    os.system("../src/pnnx test_torchvision_DeformConv2d.pt inputshape=[1,12,64,64]")
+    os.system("../../src/pnnx test_torchvision_DeformConv2d.pt inputshape=[1,12,64,64]")
 
-    # pnnx inference
-    import test_torchvision_DeformConv2d_pnnx
-    b0, b1 = test_torchvision_DeformConv2d_pnnx.test_inference()
+    # ncnn inference
+    import test_torchvision_DeformConv2d_ncnn
+    b0, b1 = test_torchvision_DeformConv2d_ncnn.test_inference()
 
-    return torch.equal(a0, b0) and torch.equal(a1, b1)
+    return torch.allclose(a0, b0, 1e-4, 1e-4) and torch.allclose(a1, b1, 1e-4, 1e-4)
 
 if __name__ == "__main__":
     if test():
