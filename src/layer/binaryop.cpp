a52c265132dabf0d0fa9b93d191d7e54dc825b7b
Fangjun Kuang <csukuangfj@gmail.com>
2022-08-12 14:06:19 +0800

    Fix 3-d matrix + 2-d matrix

--- src/layer/binaryop.cpp
+++ src/layer/binaryop.cpp
@@ -464,15 +464,14 @@ static int binary_op(const Mat& a, const Mat& b, Mat& c, const Option& opt)
             for (int q = 0; q < channels; q++)
             {
                 const float* ptr = a.channel(q);
-                const float* ptr1 = b.row(q);
                 float* outptr = c.channel(q);
 
                 for (int y = 0; y < h; y++)
                 {
-                    const float b0 = ptr1[y];
+                    const float* b0 = b.row(y);
                     for (int x = 0; x < w; x++)
                     {
-                        outptr[x] = op(ptr[x], b0);
+                        outptr[x] = op(ptr[x], b0[x]);
                     }
 
                     ptr += w;
