462228b975b13d414b5fecce963ab728b25acef6
nihuini <nihuini@tencent.com>
2022-11-03 16:21:02 +0800

    warning--

--- tools/pnnx/src/pass_level1/nn_Fold.cpp
+++ tools/pnnx/src/pass_level1/nn_Fold.cpp
@@ -31,7 +31,7 @@ public:
         return "nn.Fold";
     }
 
-    void write(Operator* op, const std::shared_ptr<torch::jit::Graph>& graph, const torch::jit::Module& mod) const
+    void write(Operator* op, const std::shared_ptr<torch::jit::Graph>& graph) const
     {
         const torch::jit::Node* col2im = find_node_by_kind(graph, "aten::col2im");
 
