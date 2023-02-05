51fdfc67a4c524ed0645cea10df3a35ca7ebc1b5
nihui <shuizhuyuanluo@126.com>
2023-02-05 22:56:35 +0800

    fix

--- src/layer/mips/binaryop_mips.cpp
+++ src/layer/mips/binaryop_mips.cpp
@@ -504,45 +504,6 @@ static int binary_op_scalar_inplace(Mat& a, float b, const Option& opt)
     return 0;
 }
 
-template<typename Op>
-static int binary_op_scalar_inplace(Mat& a, float b, const Option& opt)
-{
-    Op op;
-
-    int w = a.w;
-    int h = a.h;
-    int d = a.d;
-    int channels = a.c;
-    int elempack = a.elempack;
-    int size = w * h * d * elempack;
-
-    #pragma omp parallel for num_threads(opt.num_threads)
-    for (int q = 0; q < channels; q++)
-    {
-        float* ptr = a.channel(q);
-
-        int i = 0;
-#if __mips_msa
-        v4f32 _b = __msa_fill_w_f32(b);
-        for (; i + 3 < size; i += 4)
-        {
-            __builtin_prefetch(ptr + 16);
-            v4f32 _p = (v4f32)__msa_ld_w(ptr, 0);
-            _p = op(_p, _b);
-            __msa_st_w((v4i32)_p, ptr, 0);
-            ptr += 4;
-        }
-#endif // __mips_msa
-        for (; i < size; i++)
-        {
-            *ptr = op(*ptr, b);
-            ptr++;
-        }
-    }
-
-    return 0;
-}
-
 namespace BinaryOp_mips_functor {
 
 #if __mips_msa
