6572da3533d36588232a4afdc1b6184711140d9a
WuJinxuan <2456510228@qq.com>
2023-02-18 15:31:07 +0800

    [x86] GroupNorm (#4471)
    Co-authored-by: EdVince <EdVince@users.noreply.github.com>

--- tests/test_groupnorm.cpp
+++ tests/test_groupnorm.cpp
@@ -15,14 +15,23 @@
 #include "layer/groupnorm.h"
 #include "testutil.h"
 
-static int test_groupnorm(const ncnn::Mat& a, int group, float eps)
+static int test_groupnorm(const ncnn::Mat& a, int group, float eps, int affine)
 {
     int channels = a.c;
+    if (a.dims == 1)
+    {
+        channels = a.w;
+    }
+    else if (a.dims == 2)
+    {
+        channels = a.h;
+    }
 
     ncnn::ParamDict pd;
     pd.set(0, group);
     pd.set(1, channels);
     pd.set(2, eps);
+    pd.set(3, affine);
 
     std::vector<ncnn::Mat> weights(2);
     weights[0] = RandomMat(channels);
@@ -40,45 +49,69 @@ static int test_groupnorm(const ncnn::Mat& a, int group, float eps)
 static int test_groupnorm_0()
 {
     return 0
-           || test_groupnorm(RandomMat(3, 6, 4, 2), 1, 0.01f)
-           || test_groupnorm(RandomMat(2, 3, 3, 8), 2, 0.002f)
-           || test_groupnorm(RandomMat(3, 4, 5, 6), 3, 0.01f)
-           || test_groupnorm(RandomMat(4, 5, 6, 12), 4, 0.02f)
-           || test_groupnorm(RandomMat(5, 6, 7, 24), 2, 0.001f)
-           || test_groupnorm(RandomMat(2, 8, 9, 24), 3, 0.0001f);
+           || test_groupnorm(RandomMat(3, 6, 4, 2), 1, 0.01f, 0)
+           || test_groupnorm(RandomMat(2, 3, 3, 8), 2, 0.002f, 0)
+           || test_groupnorm(RandomMat(3, 4, 5, 6), 3, 0.01f, 0)
+           || test_groupnorm(RandomMat(4, 5, 6, 12), 4, 0.02f, 0)
+           || test_groupnorm(RandomMat(5, 6, 7, 24), 2, 0.001f, 0)
+           || test_groupnorm(RandomMat(2, 8, 9, 24), 3, 0.0001f, 0)
+           || test_groupnorm(RandomMat(3, 6, 4, 2), 1, 0.01f, 1)
+           || test_groupnorm(RandomMat(2, 3, 3, 8), 2, 0.002f, 1)
+           || test_groupnorm(RandomMat(3, 4, 5, 6), 3, 0.01f, 1)
+           || test_groupnorm(RandomMat(4, 5, 6, 12), 4, 0.02f, 1)
+           || test_groupnorm(RandomMat(5, 6, 7, 24), 2, 0.001f, 1)
+           || test_groupnorm(RandomMat(2, 8, 9, 24), 3, 0.0001f, 1);
 }
 
 static int test_groupnorm_1()
 {
     return 0
-           || test_groupnorm(RandomMat(6, 4, 2), 1, 0.01f)
-           || test_groupnorm(RandomMat(3, 3, 8), 2, 0.002f)
-           || test_groupnorm(RandomMat(4, 5, 6), 3, 0.01f)
-           || test_groupnorm(RandomMat(5, 6, 12), 4, 0.02f)
-           || test_groupnorm(RandomMat(6, 7, 24), 2, 0.001f)
-           || test_groupnorm(RandomMat(8, 9, 24), 3, 0.0001f);
+           || test_groupnorm(RandomMat(6, 4, 2), 1, 0.01f, 0)
+           || test_groupnorm(RandomMat(3, 3, 8), 2, 0.002f, 0)
+           || test_groupnorm(RandomMat(4, 5, 6), 3, 0.01f, 0)
+           || test_groupnorm(RandomMat(5, 6, 12), 4, 0.02f, 0)
+           || test_groupnorm(RandomMat(6, 7, 24), 2, 0.001f, 0)
+           || test_groupnorm(RandomMat(8, 9, 24), 3, 0.0001f, 0)
+           || test_groupnorm(RandomMat(6, 4, 2), 1, 0.01f, 1)
+           || test_groupnorm(RandomMat(3, 3, 8), 2, 0.002f, 1)
+           || test_groupnorm(RandomMat(4, 5, 6), 3, 0.01f, 1)
+           || test_groupnorm(RandomMat(5, 6, 12), 4, 0.02f, 1)
+           || test_groupnorm(RandomMat(6, 7, 24), 2, 0.001f, 1)
+           || test_groupnorm(RandomMat(8, 9, 24), 3, 0.0001f, 1);
 }
 
 static int test_groupnorm_2()
 {
     return 0
-           || test_groupnorm(RandomMat(24, 2), 1, 0.01f)
-           || test_groupnorm(RandomMat(23, 8), 2, 0.002f)
-           || test_groupnorm(RandomMat(25, 6), 3, 0.01f)
-           || test_groupnorm(RandomMat(26, 12), 4, 0.02f)
-           || test_groupnorm(RandomMat(27, 24), 2, 0.001f)
-           || test_groupnorm(RandomMat(29, 24), 3, 0.0001f);
+           || test_groupnorm(RandomMat(24, 2), 1, 0.01f, 0)
+           || test_groupnorm(RandomMat(23, 8), 2, 0.002f, 0)
+           || test_groupnorm(RandomMat(25, 6), 3, 0.01f, 0)
+           || test_groupnorm(RandomMat(26, 12), 4, 0.02f, 0)
+           || test_groupnorm(RandomMat(27, 24), 2, 0.001f, 0)
+           || test_groupnorm(RandomMat(29, 24), 3, 0.0001f, 0)
+           || test_groupnorm(RandomMat(24, 2), 1, 0.01f, 1)
+           || test_groupnorm(RandomMat(23, 8), 2, 0.002f, 1)
+           || test_groupnorm(RandomMat(25, 6), 3, 0.01f, 1)
+           || test_groupnorm(RandomMat(26, 12), 4, 0.02f, 1)
+           || test_groupnorm(RandomMat(27, 24), 2, 0.001f, 1)
+           || test_groupnorm(RandomMat(29, 24), 3, 0.0001f, 1);
 }
 
 static int test_groupnorm_3()
 {
     return 0
-           || test_groupnorm(RandomMat(12), 1, 0.01f)
-           || test_groupnorm(RandomMat(18), 2, 0.002f)
-           || test_groupnorm(RandomMat(36), 3, 0.01f)
-           || test_groupnorm(RandomMat(212), 4, 0.02f)
-           || test_groupnorm(RandomMat(124), 2, 0.001f)
-           || test_groupnorm(RandomMat(324), 3, 0.0001f);
+           || test_groupnorm(RandomMat(12), 1, 0.01f, 0)
+           || test_groupnorm(RandomMat(18), 2, 0.002f, 0)
+           || test_groupnorm(RandomMat(36), 3, 0.01f, 0)
+           || test_groupnorm(RandomMat(212), 4, 0.02f, 0)
+           || test_groupnorm(RandomMat(124), 2, 0.001f, 0)
+           || test_groupnorm(RandomMat(324), 3, 0.0001f, 0)
+           || test_groupnorm(RandomMat(12), 1, 0.01f, 1)
+           || test_groupnorm(RandomMat(18), 2, 0.002f, 1)
+           || test_groupnorm(RandomMat(36), 3, 0.01f, 1)
+           || test_groupnorm(RandomMat(212), 4, 0.02f, 1)
+           || test_groupnorm(RandomMat(124), 2, 0.001f, 1)
+           || test_groupnorm(RandomMat(324), 3, 0.0001f, 1);
 }
 
 int main()
