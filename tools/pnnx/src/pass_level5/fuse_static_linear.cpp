8f04040f0adbd467b662dd09df274a3b4ee440b8
nihuini <nihuini@tencent.com>
2023-06-05 16:38:20 +0800

    wip

--- tools/pnnx/src/pass_level5/fuse_static_linear.cpp
+++ tools/pnnx/src/pass_level5/fuse_static_linear.cpp
@@ -29,8 +29,8 @@ public:
         return R"PNNXIR(7767517
 4 3
 pnnx.Input              input       0 1 input
-pnnx.Attribute          op_0        0 1 weight @data=(%out_features,%in_features)f32
-F.linear                op_1        2 1 input weight out bias=None
+pnnx.Attribute          op_weight   0 1 weight @data=(%out_features,%in_features)f32
+F.linear                op_0        2 1 input weight out bias=None
 pnnx.Output             output      1 0 out
 )PNNXIR";
     }
@@ -40,7 +40,7 @@ pnnx.Output             output      1 0 out
         return R"PNNXIR(7767517
 3 2
 pnnx.Input              input       0 1 input
-nn.Linear               op_0        1 1 input out in_features=%in_features out_features=%out_features bias=False @weight=%op_0.data
+nn.Linear               linear      1 1 input out in_features=%in_features out_features=%out_features bias=False @weight=%op_weight.data
 pnnx.Output             output      1 0 out
 )PNNXIR";
     }
@@ -54,9 +54,9 @@ public:
         return R"PNNXIR(7767517
 5 4
 pnnx.Input              input       0 1 input
-pnnx.Attribute          op_0        0 1 weight @data=(%out_features,%in_features)f32
-pnnx.Attribute          op_1        0 1 bias @data=(%out_features)f32
-F.linear                op_2        3 1 input weight bias out
+pnnx.Attribute          op_weight   0 1 weight @data=(%out_features,%in_features)f32
+pnnx.Attribute          op_bias     0 1 bias @data=(%out_features)f32
+F.linear                op_0        3 1 input weight bias out
 pnnx.Output             output      1 0 out
 )PNNXIR";
     }
@@ -66,7 +66,7 @@ pnnx.Output             output      1 0 out
         return R"PNNXIR(7767517
 3 2
 pnnx.Input              input       0 1 input
-nn.Linear               op_0        1 1 input out in_features=%in_features out_features=%out_features bias=True @weight=%op_0.data @bias=%op_1.data
+nn.Linear               linear      1 1 input out in_features=%in_features out_features=%out_features bias=True @weight=%op_weight.data @bias=%op_bias.data
 pnnx.Output             output      1 0 out
 )PNNXIR";
     }
@@ -80,10 +80,10 @@ public:
         return R"PNNXIR(7767517
 6 5
 pnnx.Input              input       0 1 input
-pnnx.Attribute          op_0        0 1 weight @data=(%out_features,%in_features)f32
-pnnx.Attribute          op_1        0 1 bias @data=(%batch,%out_features,1)f32
-F.linear                op_2        2 1 input weight a
-pnnx.Expression         op_3        2 1 a bias out expr=add(@0,@1)
+pnnx.Attribute          op_weight   0 1 weight @data=(%out_features,%in_features)f32
+pnnx.Attribute          op_bias     0 1 bias @data=(1,%out_features,1)f32
+F.linear                op_0        2 1 input weight a
+pnnx.Expression         op_1        2 1 a bias out expr=add(@0,@1)
 pnnx.Output             output      1 0 out
 )PNNXIR";
     }
@@ -93,7 +93,7 @@ pnnx.Output             output      1 0 out
         return R"PNNXIR(7767517
 3 2
 pnnx.Input              input       0 1 input
-nn.Linear               op_0        1 1 input out in_features=%in_features out_features=%out_features bias=True @weight=%op_0.data @bias=%op_1.data
+nn.Linear               linear      1 1 input out in_features=%in_features out_features=%out_features bias=True @weight=%op_weight.data @bias=%op_bias.data
 pnnx.Output             output      1 0 out
 )PNNXIR";
     }
@@ -104,7 +104,7 @@ pnnx.Output             output      1 0 out
 
         // fix bias shape
         const int out_features = captured_params.at("out_features").i;
-        ops.at("op_0")->attrs.at("bias").shape = {out_features};
+        ops.at("linear")->attrs.at("bias").shape = {out_features};
     }
 };
 
