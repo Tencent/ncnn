a87be247953d45842458d182e1b8d57ca8c0c6c5
nihui <nihuini@tencent.com>
2023-07-10 23:21:07 +0800

    pnnx convert conv with non-zero padding mode (#4849)

--- tools/pnnx/src/pass_ncnn/nn_Conv1d.cpp
+++ tools/pnnx/src/pass_ncnn/nn_Conv1d.cpp
@@ -26,7 +26,7 @@ public:
         return R"PNNXIR(7767517
 3 2
 pnnx.Input              input       0 1 input
-nn.Conv1d               op_0        1 1 input out in_channels=%in_channels out_channels=%out_channels kernel_size=%kernel_size stride=%stride padding_mode=%padding_mode padding=%padding dilation=%dilation groups=1 bias=%bias @weight @bias
+nn.Conv1d               op_0        1 1 input out in_channels=%in_channels out_channels=%out_channels kernel_size=%kernel_size stride=%stride padding_mode=zeros padding=%padding dilation=%dilation groups=1 bias=%bias @weight @bias
 pnnx.Output             output      1 0 out
 )PNNXIR";
     }
@@ -43,12 +43,6 @@ pnnx.Output             output      1 0 out
 
     void write(Operator* op, const std::map<std::string, Parameter>& captured_params, const std::map<std::string, Attribute>& captured_attrs) const
     {
-        std::string padding_mode = captured_params.at("padding_mode").s;
-        if (padding_mode != "zeros")
-        {
-            fprintf(stderr, "unsupported padding_mode %s\n", padding_mode.c_str());
-        }
-
         op->params["0"] = captured_params.at("out_channels");
         op->params["1"] = captured_params.at("kernel_size").ai[0];
         op->params["2"] = captured_params.at("dilation").ai[0];
@@ -83,7 +77,7 @@ public:
         return R"PNNXIR(7767517
 3 2
 pnnx.Input              input       0 1 input
-nn.Conv1d               op_0        1 1 input out in_channels=%in_channels out_channels=%out_channels kernel_size=%kernel_size stride=%stride padding_mode=%padding_mode padding=%padding dilation=%dilation groups=%groups bias=%bias @weight @bias
+nn.Conv1d               op_0        1 1 input out in_channels=%in_channels out_channels=%out_channels kernel_size=%kernel_size stride=%stride padding_mode=zeros padding=%padding dilation=%dilation groups=%groups bias=%bias @weight @bias
 pnnx.Output             output      1 0 out
 )PNNXIR";
     }
@@ -100,12 +94,6 @@ pnnx.Output             output      1 0 out
 
     void write(Operator* op, const std::map<std::string, Parameter>& captured_params, const std::map<std::string, Attribute>& captured_attrs) const
     {
-        std::string padding_mode = captured_params.at("padding_mode").s;
-        if (padding_mode != "zeros")
-        {
-            fprintf(stderr, "unsupported padding_mode %s\n", padding_mode.c_str());
-        }
-
         op->params["0"] = captured_params.at("out_channels");
         op->params["1"] = captured_params.at("kernel_size").ai[0];
         op->params["2"] = captured_params.at("dilation").ai[0];
@@ -133,8 +121,151 @@ pnnx.Output             output      1 0 out
     }
 };
 
+class nn_Conv1d_2 : public GraphRewriterPass
+{
+public:
+    const char* match_pattern_graph() const
+    {
+        return R"PNNXIR(7767517
+3 2
+pnnx.Input              input       0 1 input
+nn.Conv1d               op_0        1 1 input out in_channels=%in_channels out_channels=%out_channels kernel_size=%kernel_size stride=%stride padding_mode=%padding_mode padding=%padding dilation=%dilation groups=1 bias=%bias @weight @bias
+pnnx.Output             output      1 0 out
+)PNNXIR";
+    }
+
+    const char* replace_pattern_graph() const
+    {
+        return R"PNNXIR(7767517
+4 3
+pnnx.Input              input       0 1 input
+Padding                 pad         1 1 input a
+Convolution1D           conv        1 1 a out
+pnnx.Output             output      1 0 out
+)PNNXIR";
+    }
+
+    bool match(const std::map<std::string, Parameter>& captured_params) const
+    {
+        const std::string& padding_mode = captured_params.at("padding_mode").s;
+        if (padding_mode == "zeros")
+            return false;
+
+        return true;
+    }
+
+    bool match(const std::map<std::string, const Operator*>& matched_operators) const
+    {
+        const Operator* conv = matched_operators.at("op_0");
+        if (conv->params.at("padding").type == 4 && conv->params.at("padding").s == "same")
+        {
+            const std::vector<int> input_shape = conv->inputs[0]->shape;
+            if (input_shape.size() != 2 && input_shape.size() != 3)
+            {
+                fprintf(stderr, "can not resolve pads without shape\n");
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    void write(const std::map<std::string, Operator*>& ops, const std::map<std::string, Parameter>& captured_params, const std::map<std::string, Attribute>& captured_attrs) const
+    {
+        std::vector<int> padding;
+        if (captured_params.at("padding").type == 4)
+        {
+            if (captured_params.at("padding").s == "same")
+            {
+                // resolve pads
+                const std::vector<int> input_shape = ops.at("pad")->inputs[0]->shape;
+                const int w = input_shape[input_shape.size() - 1];
+                const int kernel_w = captured_params.at("kernel_size").ai[0];
+                const int dilation_w = captured_params.at("dilation").ai[0];
+                const int stride_w = captured_params.at("stride").ai[0];
+
+                const int kernel_extent_w = dilation_w * (kernel_w - 1) + 1;
+
+                int wpad = kernel_extent_w + (w - 1) / stride_w * stride_w - w;
+
+                padding = std::vector<int>{wpad / 2, wpad - wpad / 2};
+            }
+            else if (captured_params.at("padding").s == "valid")
+            {
+                padding = std::vector<int>{0, 0};
+            }
+        }
+        else
+        {
+            int wpad = captured_params.at("padding").ai[0];
+            padding = std::vector<int>{wpad, wpad};
+        }
+
+        ops.at("pad")->params["0"] = 0;
+        ops.at("pad")->params["1"] = 0;
+        ops.at("pad")->params["2"] = padding[0];
+        ops.at("pad")->params["3"] = padding[1];
+
+        std::string padding_mode = captured_params.at("padding_mode").s;
+        if (padding_mode == "reflect")
+        {
+            ops.at("pad")->params["4"] = 2; // type=reflect
+        }
+        else if (padding_mode == "replicate")
+        {
+            ops.at("pad")->params["4"] = 1; // type=replicate
+        }
+        else
+        {
+            fprintf(stderr, "unsupported padding_mode %s\n", padding_mode.c_str());
+        }
+
+        ops.at("conv")->params["0"] = captured_params.at("out_channels");
+        ops.at("conv")->params["1"] = captured_params.at("kernel_size").ai[0];
+        ops.at("conv")->params["2"] = captured_params.at("dilation").ai[0];
+        ops.at("conv")->params["3"] = captured_params.at("stride").ai[0];
+        ops.at("conv")->params["4"] = 0;
+        ops.at("conv")->params["5"] = captured_params.at("bias").b ? 1 : 0;
+        ops.at("conv")->params["6"] = captured_attrs.at("op_0.weight").elemcount();
+        ops.at("conv")->params["7"] = captured_params.find("groups") != captured_params.end() ? captured_params.at("groups") : 1;
+
+        ops.at("conv")->attrs["0"] = Attribute();
+        ops.at("conv")->attrs["0"].data = {0, 0, 0, 0};
+        ops.at("conv")->attrs["1"] = captured_attrs.at("op_0.weight");
+        if (captured_params.at("bias").b)
+            ops.at("conv")->attrs["2"] = captured_attrs.at("op_0.bias");
+    }
+};
+
+class nn_Conv1d_3 : public nn_Conv1d_2
+{
+public:
+    const char* match_pattern_graph() const
+    {
+        return R"PNNXIR(7767517
+3 2
+pnnx.Input              input       0 1 input
+nn.Conv1d               op_0        1 1 input out in_channels=%in_channels out_channels=%out_channels kernel_size=%kernel_size stride=%stride padding_mode=%padding_mode padding=%padding dilation=%dilation groups=%groups bias=%bias @weight @bias
+pnnx.Output             output      1 0 out
+)PNNXIR";
+    }
+
+    const char* replace_pattern_graph() const
+    {
+        return R"PNNXIR(7767517
+4 3
+pnnx.Input              input       0 1 input
+Padding                 pad         1 1 input a
+ConvolutionDepthWise1D  conv        1 1 a out
+pnnx.Output             output      1 0 out
+)PNNXIR";
+    }
+};
+
 REGISTER_GLOBAL_PNNX_NCNN_GRAPH_REWRITER_PASS(nn_Conv1d, 20)
 REGISTER_GLOBAL_PNNX_NCNN_GRAPH_REWRITER_PASS(nn_Conv1d_1, 21)
+REGISTER_GLOBAL_PNNX_NCNN_GRAPH_REWRITER_PASS(nn_Conv1d_2, 22)
+REGISTER_GLOBAL_PNNX_NCNN_GRAPH_REWRITER_PASS(nn_Conv1d_3, 23)
 
 } // namespace ncnn
 
--- tools/pnnx/src/pass_ncnn/nn_Conv2d.cpp
+++ tools/pnnx/src/pass_ncnn/nn_Conv2d.cpp
@@ -26,7 +26,7 @@ public:
         return R"PNNXIR(7767517
 3 2
 pnnx.Input              input       0 1 input
-nn.Conv2d               op_0        1 1 input out in_channels=%in_channels out_channels=%out_channels kernel_size=%kernel_size stride=%stride padding_mode=%padding_mode padding=%padding dilation=%dilation groups=1 bias=%bias @weight @bias
+nn.Conv2d               op_0        1 1 input out in_channels=%in_channels out_channels=%out_channels kernel_size=%kernel_size stride=%stride padding_mode=zeros padding=%padding dilation=%dilation groups=1 bias=%bias @weight @bias
 pnnx.Output             output      1 0 out
 )PNNXIR";
     }
@@ -43,12 +43,6 @@ pnnx.Output             output      1 0 out
 
     void write(Operator* op, const std::map<std::string, Parameter>& captured_params, const std::map<std::string, Attribute>& captured_attrs) const
     {
-        std::string padding_mode = captured_params.at("padding_mode").s;
-        if (padding_mode != "zeros")
-        {
-            fprintf(stderr, "unsupported padding_mode %s\n", padding_mode.c_str());
-        }
-
         op->params["0"] = captured_params.at("out_channels");
         op->params["1"] = captured_params.at("kernel_size").ai[1];
         op->params["11"] = captured_params.at("kernel_size").ai[0];
@@ -87,7 +81,7 @@ public:
         return R"PNNXIR(7767517
 3 2
 pnnx.Input              input       0 1 input
-nn.Conv2d               op_0        1 1 input out in_channels=%in_channels out_channels=%out_channels kernel_size=%kernel_size stride=%stride padding_mode=%padding_mode padding=%padding dilation=%dilation groups=%groups bias=%bias @weight @bias
+nn.Conv2d               op_0        1 1 input out in_channels=%in_channels out_channels=%out_channels kernel_size=%kernel_size stride=%stride padding_mode=zeros padding=%padding dilation=%dilation groups=%groups bias=%bias @weight @bias
 pnnx.Output             output      1 0 out
 )PNNXIR";
     }
@@ -104,12 +98,6 @@ pnnx.Output             output      1 0 out
 
     void write(Operator* op, const std::map<std::string, Parameter>& captured_params, const std::map<std::string, Attribute>& captured_attrs) const
     {
-        std::string padding_mode = captured_params.at("padding_mode").s;
-        if (padding_mode != "zeros")
-        {
-            fprintf(stderr, "unsupported padding_mode %s\n", padding_mode.c_str());
-        }
-
         op->params["0"] = captured_params.at("out_channels");
         op->params["1"] = captured_params.at("kernel_size").ai[1];
         op->params["11"] = captured_params.at("kernel_size").ai[0];
@@ -141,8 +129,162 @@ pnnx.Output             output      1 0 out
     }
 };
 
+class nn_Conv2d_2 : public GraphRewriterPass
+{
+public:
+    const char* match_pattern_graph() const
+    {
+        return R"PNNXIR(7767517
+3 2
+pnnx.Input              input       0 1 input
+nn.Conv2d               op_0        1 1 input out in_channels=%in_channels out_channels=%out_channels kernel_size=%kernel_size stride=%stride padding_mode=%padding_mode padding=%padding dilation=%dilation groups=1 bias=%bias @weight @bias
+pnnx.Output             output      1 0 out
+)PNNXIR";
+    }
+
+    const char* replace_pattern_graph() const
+    {
+        return R"PNNXIR(7767517
+4 3
+pnnx.Input              input       0 1 input
+Padding                 pad         1 1 input a
+Convolution             conv        1 1 a out
+pnnx.Output             output      1 0 out
+)PNNXIR";
+    }
+
+    bool match(const std::map<std::string, Parameter>& captured_params) const
+    {
+        const std::string& padding_mode = captured_params.at("padding_mode").s;
+        if (padding_mode == "zeros")
+            return false;
+
+        return true;
+    }
+
+    bool match(const std::map<std::string, const Operator*>& matched_operators) const
+    {
+        const Operator* conv = matched_operators.at("op_0");
+        if (conv->params.at("padding").type == 4 && conv->params.at("padding").s == "same")
+        {
+            const std::vector<int> input_shape = conv->inputs[0]->shape;
+            if (input_shape.size() != 3 && input_shape.size() != 4)
+            {
+                fprintf(stderr, "can not resolve pads without shape\n");
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    void write(const std::map<std::string, Operator*>& ops, const std::map<std::string, Parameter>& captured_params, const std::map<std::string, Attribute>& captured_attrs) const
+    {
+        std::vector<int> padding;
+        if (captured_params.at("padding").type == 4)
+        {
+            if (captured_params.at("padding").s == "same")
+            {
+                // resolve pads
+                const std::vector<int> input_shape = ops.at("pad")->inputs[0]->shape;
+                const int w = input_shape[input_shape.size() - 1];
+                const int h = input_shape[input_shape.size() - 2];
+                const int kernel_w = captured_params.at("kernel_size").ai[1];
+                const int kernel_h = captured_params.at("kernel_size").ai[0];
+                const int dilation_w = captured_params.at("dilation").ai[1];
+                const int dilation_h = captured_params.at("dilation").ai[0];
+                const int stride_w = captured_params.at("stride").ai[1];
+                const int stride_h = captured_params.at("stride").ai[0];
+
+                const int kernel_extent_w = dilation_w * (kernel_w - 1) + 1;
+                const int kernel_extent_h = dilation_h * (kernel_h - 1) + 1;
+
+                int wpad = kernel_extent_w + (w - 1) / stride_w * stride_w - w;
+                int hpad = kernel_extent_h + (h - 1) / stride_h * stride_h - h;
+
+                padding = std::vector<int>{hpad / 2, hpad - hpad / 2, wpad / 2, wpad - wpad / 2};
+            }
+            else if (captured_params.at("padding").s == "valid")
+            {
+                padding = std::vector<int>{0, 0, 0, 0};
+            }
+        }
+        else
+        {
+            int hpad = captured_params.at("padding").ai[0];
+            int wpad = captured_params.at("padding").ai[1];
+            padding = std::vector<int>{hpad, hpad, wpad, wpad};
+        }
+
+        ops.at("pad")->params["0"] = padding[0];
+        ops.at("pad")->params["1"] = padding[1];
+        ops.at("pad")->params["2"] = padding[2];
+        ops.at("pad")->params["3"] = padding[3];
+
+        std::string padding_mode = captured_params.at("padding_mode").s;
+        if (padding_mode == "reflect")
+        {
+            ops.at("pad")->params["4"] = 2; // type=reflect
+        }
+        else if (padding_mode == "replicate")
+        {
+            ops.at("pad")->params["4"] = 1; // type=replicate
+        }
+        else
+        {
+            fprintf(stderr, "unsupported padding_mode %s\n", padding_mode.c_str());
+        }
+
+        ops.at("conv")->params["0"] = captured_params.at("out_channels");
+        ops.at("conv")->params["1"] = captured_params.at("kernel_size").ai[1];
+        ops.at("conv")->params["11"] = captured_params.at("kernel_size").ai[0];
+        ops.at("conv")->params["2"] = captured_params.at("dilation").ai[1];
+        ops.at("conv")->params["12"] = captured_params.at("dilation").ai[0];
+        ops.at("conv")->params["3"] = captured_params.at("stride").ai[1];
+        ops.at("conv")->params["13"] = captured_params.at("stride").ai[0];
+        ops.at("conv")->params["4"] = 0;
+        ops.at("conv")->params["14"] = 0;
+        ops.at("conv")->params["5"] = captured_params.at("bias").b ? 1 : 0;
+        ops.at("conv")->params["6"] = captured_attrs.at("op_0.weight").elemcount();
+        ops.at("conv")->params["7"] = captured_params.find("groups") != captured_params.end() ? captured_params.at("groups") : 1;
+
+        ops.at("conv")->attrs["0"] = Attribute();
+        ops.at("conv")->attrs["0"].data = {0, 0, 0, 0};
+        ops.at("conv")->attrs["1"] = captured_attrs.at("op_0.weight");
+        if (captured_params.at("bias").b)
+            ops.at("conv")->attrs["2"] = captured_attrs.at("op_0.bias");
+    }
+};
+
+class nn_Conv2d_3 : public nn_Conv2d_2
+{
+public:
+    const char* match_pattern_graph() const
+    {
+        return R"PNNXIR(7767517
+3 2
+pnnx.Input              input       0 1 input
+nn.Conv2d               op_0        1 1 input out in_channels=%in_channels out_channels=%out_channels kernel_size=%kernel_size stride=%stride padding_mode=%padding_mode padding=%padding dilation=%dilation groups=%groups bias=%bias @weight @bias
+pnnx.Output             output      1 0 out
+)PNNXIR";
+    }
+
+    const char* replace_pattern_graph() const
+    {
+        return R"PNNXIR(7767517
+4 3
+pnnx.Input              input       0 1 input
+Padding                 pad         1 1 input a
+ConvolutionDepthWise    conv        1 1 a out
+pnnx.Output             output      1 0 out
+)PNNXIR";
+    }
+};
+
 REGISTER_GLOBAL_PNNX_NCNN_GRAPH_REWRITER_PASS(nn_Conv2d, 20)
 REGISTER_GLOBAL_PNNX_NCNN_GRAPH_REWRITER_PASS(nn_Conv2d_1, 21)
+REGISTER_GLOBAL_PNNX_NCNN_GRAPH_REWRITER_PASS(nn_Conv2d_2, 22)
+REGISTER_GLOBAL_PNNX_NCNN_GRAPH_REWRITER_PASS(nn_Conv2d_3, 23)
 
 } // namespace ncnn
 
