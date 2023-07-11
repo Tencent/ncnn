472244420e6dfd12d0bf7309ca66a78e5ac28974
JeremyRand <244188+JeremyRand@users.noreply.github.com>
2023-07-11 07:56:51 +0000

    VSX toolchains: check for SSE2 support (#4845)
    Improves compatibility with Clang 11.
    
    Also rename NCNN_SSE* options to NCNN_VSX_SSE* to avoid conflict between
    x86 and POWER (went unnoticed before because x86 doesn't have an option
    for toggling SSE 4.1).
    
    Co-authored-by: Jeremy Rand <jeremyrand@danwin1210.de>

--- toolchains/power9le-linux-gnu-vsx.toolchain.cmake
+++ toolchains/power9le-linux-gnu-vsx.toolchain.cmake
@@ -8,8 +8,8 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
-set(CMAKE_C_FLAGS "-mcpu=power9 -mtune=power9 -DNO_WARN_X86_INTRINSICS -D__MMX__ -D__SSE__ -D__SSE2__ -D__SSSE3__")
-set(CMAKE_CXX_FLAGS "-mcpu=power9 -mtune=power9 -DNO_WARN_X86_INTRINSICS -D__MMX__ -D__SSE__ -D__SSE2__ -D__SSSE3__")
+set(CMAKE_C_FLAGS "-mcpu=power9 -mtune=power9 -DNO_WARN_X86_INTRINSICS -D__MMX__ -D__SSE__ -D__SSSE3__")
+set(CMAKE_CXX_FLAGS "-mcpu=power9 -mtune=power9 -DNO_WARN_X86_INTRINSICS -D__MMX__ -D__SSE__ -D__SSSE3__")
 
 # cache flags
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "c flags")
