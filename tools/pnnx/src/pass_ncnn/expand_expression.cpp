f1928611829a3b1b39690d1400d7edfe4d4729e1
nihui <nihui@users.noreply.github.com>
2023-07-24 06:57:33 +0000

    apply code-format changes

--- tools/pnnx/src/pass_ncnn/expand_expression.cpp
+++ tools/pnnx/src/pass_ncnn/expand_expression.cpp
@@ -198,8 +198,8 @@ static std::string expand_expression(Graph& graph, const Operator* op, int& pnnx
             if (t == "mul") op_binary->params["0"] = 2;
             if (t == "div") op_binary->params["0"] = 3;
             if (t == "floor_divide") fprintf(stderr, "BinaryOp floor_divide not supported yet\n"); // TODO
-            if (t == "fmod") fprintf(stderr, "BinaryOp fmod not supported yet\n"); // TODO
-            if (t == "remainder") fprintf(stderr, "BinaryOp remainder not supported yet\n"); // TODO
+            if (t == "fmod") fprintf(stderr, "BinaryOp fmod not supported yet\n");                 // TODO
+            if (t == "remainder") fprintf(stderr, "BinaryOp remainder not supported yet\n");       // TODO
             if (t == "pow") op_binary->params["0"] = 6;
             if (t == "atan2") op_binary->params["0"] = 10;
 
