9576dcb5f981b853718100bad0e38e026dbaf798
nanjoin <g850183609@gmail.com>
2023-07-28 19:33:36 +0800

    rebase

--- src/layer/arm/gemm_arm.cpp
+++ src/layer/arm/gemm_arm.cpp
@@ -51,7 +51,6 @@ void pack_A_tile(const Mat& A, Mat& AT, int i, int max_ii, int k, int max_kk)
     const int A_hstep = A.dims == 3 ? (int)A.cstep : A.w;
 
     float* pp = AT;
-
     int ii = 0;
 #if __ARM_NEON
 #if __aarch64__
