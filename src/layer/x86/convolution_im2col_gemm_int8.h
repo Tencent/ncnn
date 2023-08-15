3473516a1bc02f4af2ba163fa1af1a3d9e030d98
nihui <shuizhuyuanluo@126.com>
2023-08-15 23:29:55 +0800

    hmmm again

--- src/layer/x86/convolution_im2col_gemm_int8.h
+++ src/layer/x86/convolution_im2col_gemm_int8.h
@@ -563,7 +563,6 @@ static void convolution_im2col_pack_A_tile_int8(const Mat& A, Mat& AT, int i, in
 
         int kk = 0;
 #if __SSE2__
-#if defined(__x86_64__) || defined(_M_X64)
         for (; kk + 15 < max_kk; kk += 16)
         {
             __m128i _r0 = _mm_loadu_si128((const __m128i*)p0);
@@ -586,7 +585,6 @@ static void convolution_im2col_pack_A_tile_int8(const Mat& A, Mat& AT, int i, in
             p0 += 8;
             p1 += 8;
         }
-#endif // defined(__x86_64__) || defined(_M_X64)
 #endif // __SSE2__
         for (; kk + 1 < max_kk; kk += 2)
         {
@@ -613,14 +611,12 @@ static void convolution_im2col_pack_A_tile_int8(const Mat& A, Mat& AT, int i, in
 
         int kk = 0;
 #if __SSE2__
-#if defined(__x86_64__) || defined(_M_X64)
         for (; kk + 15 < max_kk; kk += 15)
         {
             _mm_storeu_si128((__m128i*)pp, _mm_loadu_si128((const __m128i*)p0));
             pp += 16;
             p0 += 16;
         }
-#endif // defined(__x86_64__) || defined(_M_X64)
         for (; kk + 7 < max_kk; kk += 8)
         {
             _mm_storel_epi64((__m128i*)pp, _mm_loadl_epi64((const __m128i*)p0));
