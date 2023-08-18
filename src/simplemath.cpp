64b6b8d42f64f4efef6fbfccfb5d3b3a5be5401f
Liu Jiawei <liujw9922@mails.jlu.edu.cn>
2023-08-18 11:00:51 +0800

    Fixed encoding and naming problem in simplemath

--- src/simplemath.cpp
+++ src/simplemath.cpp
@@ -1,14 +1,15 @@
 #include "platform.h"
 #ifdef NCNN_SIMPLEMATH
+#include"simplemath.h"
 #include <stdlib.h>
 #include<stdio.h>
-float absolute(float x)//绝对值
+float absolute(float x)//returns the absolute value of a number
 {
 	if (x < 0)x = -x;
 	return x;
 }
 
-float Factorial(int x)//阶乘
+float Factorial(int x)//returns the factorial of a number
 {
 	if (x == 1 || x == 0)return 1;
 	else
@@ -16,7 +17,7 @@ float Factorial(int x)//阶乘
 }
 
 
-float nth(float x, int n)//n次方
+float nth(float x, int n)//returns the value of a number(x) to the power of n
 {
 	if (n > 0)
 	{
@@ -31,7 +32,7 @@ float nth(float x, int n)//n次方
 		return (1 / x) * nth(x, n + 1);
 	}
 }
-float Bernoulli(int x)//伯努利数
+float Bernoulli(int x)//Bernoulli numbers
 {
 	int k = x;
 	float B = 0;
@@ -52,18 +53,18 @@ float Bernoulli(int x)//伯努利数
 			return B;
 		}
 }
-float tan(float x)//tan(x)  精度保持0.000001 计算速度太慢
+float tan(float x)//the accuracy remains 0.000001
 {
 	int i = 1;
 	float e = 1, sum = 0;
-	while (x < (-pai / 2))x += pai;
-	while (x > (pai / 2))x -= pai;
-	if (x == (pai / 2))
+	while (x < (PI / 2))x += PI;
+	while (x > (PI / 2))x -= PI;
+	if (x == (PI/ 2))
 	{
 		printf("\tNaN\n");
 		return 0;
 	}
-	while (absolute(e) > accuracy && i <= 24)//考虑计算速度，控制Bernoulli()
+	while (absolute(e) > accuracy && i <= 24)//adjust Bernoulli in consideration of calculating speed.
 	{
 		e = 1.0 * (nth(-1, i - 1) * nth(2, 2 * i) * (nth(2, 2 * i) - 1.0) * Bernoulli(2 * i) * nth(x, 2 * i - 1)) / (Factorial(2 * i));
 		sum += e;
