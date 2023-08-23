e116f9843bacf83f93d857612c1681ba48fbfdbb
邓实诚 <2958906959@qq.com>
2023-08-23 09:57:45 +0000

    change code format

--- src/simplemath.cpp
+++ src/simplemath.cpp
@@ -12,8 +12,8 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
-#include "simplemath.h"
 #if NCNN_SIMPLEMATH
+#include "simplemath.h"
 /*
 * ====================================================
 * util functions
