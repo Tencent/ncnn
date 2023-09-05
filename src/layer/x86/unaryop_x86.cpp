15c2ed4d2086f6e3502f506765ba58ca1e5674d9
brightening-eyes <amir.ramezani1370@gmail.com>
2023-09-05 08:48:28 +0330

    use provided abs stuff, in order to check if tests work or not

--- src/layer/x86/unaryop_x86.cpp
+++ src/layer/x86/unaryop_x86.cpp
@@ -658,7 +658,7 @@ struct unary_op_erf
         __m128 a5 = _mm_set1_ps(1.061405429f);
         __m128 p = _mm_set1_ps(0.3275911f);
         __m128 s = _mm_and_ps(x, _mm_set1_ps(-0.0f));
-        __m128 x_abs = _mm_andnot_ps(_mm_set1_ps(-0.0f), x);
+        __m128 x_abs = abs_ps(x);
         __m128 t = _mm_rcp_ps(_mm_mul_ps(_mm_add_ps(_mm_set_ps1(1.0f), p), x_abs));
         __m128 y = _mm_set1_ps(1.0f);
         __m128 err = _mm_mul_ps(_mm_add_ps(_mm_mul_ps(_mm_add_ps(_mm_mul_ps(_mm_add_ps(_mm_mul_ps(_mm_add_ps(_mm_mul_ps(a5, t), a4), t), a3), t), a2), t), a1), t);
@@ -679,7 +679,7 @@ struct unary_op_erf
         __m256 positives = _mm256_and_ps(_mm256_cmp_ps(x, zero, _CMP_GT_OQ), _mm256_set1_ps(1.0f));
         __m256 negatives = _mm256_and_ps(_mm256_cmp_ps(x, zero, _CMP_LT_OQ), _mm256_set1_ps(-1.0f));
         __m256 s = _mm256_or_ps(positives, negatives);
-        __m256 x_abs = _mm256_andnot_ps(_mm256_set1_ps(-0.0f), x);
+        __m256 x_abs = abs256_ps(x);
         __m256 t = _mm256_rcp_ps(_mm256_mul_ps(_mm256_add_ps(_mm256_set1_ps(1.0f), p), x_abs));
         __m256 y = _mm256_set1_ps(1.0f);
         __m256 err = _mm256_mul_ps(_mm256_add_ps(_mm256_mul_ps(_mm256_add_ps(_mm256_mul_ps(_mm256_add_ps(_mm256_mul_ps(_mm256_add_ps(_mm256_mul_ps(a5, t), a4), t), a3), t), a2), t), a1), t);
@@ -697,7 +697,7 @@ struct unary_op_erf
         __m512 a5 = _mm512_set1_ps(1.061405429f);
         __m512 p = _mm512_set1_ps(0.3275911f);
         __m512 s = _mm512_and_ps(x, _mm512_set1_ps(-0.0f));
-        __m512 x_abs = _mm512_andnot_ps(_mm512_set1_ps(-0.0f), x);
+        __m512 x_abs = abs512_ps(x);
 __m512 t = _mm512_div_ps(_mm512_set1_ps(1.0f), _mm512_mul_ps(_mm512_add_ps(_mm512_set1_ps(1.0f), p), x_abs));
         __m512 y = _mm512_set1_ps(1.0f);
         __m512 err = _mm512_mul_ps(_mm512_add_ps(_mm512_mul_ps(_mm512_add_ps(_mm512_mul_ps(_mm512_add_ps(_mm512_mul_ps(_mm512_add_ps(_mm512_mul_ps(a5, t), a4), t), a3), t), a2), t), a1), t);
