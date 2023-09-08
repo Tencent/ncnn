e096866080c37cbec2d7519d43aa8af96f8a7345
邓实诚 <2958906959@qq.com>
2023-09-08 12:18:00 +0000

    fix a bug

--- src/simplemath.cpp
+++ src/simplemath.cpp
@@ -673,6 +673,21 @@ float erfcf(float x)
 * ====================================================
 */
 
+int msb(unsigned int v)
+{
+    static const int pos[32] = {0, 1, 28, 2, 29, 14, 24, 3,
+                                30, 22, 20, 15, 25, 17, 4, 8, 31, 27, 13, 23, 21, 19,
+                                16, 7, 26, 12, 18, 6, 11, 5, 10, 9
+                               };
+    v |= v >> 1;
+    v |= v >> 2;
+    v |= v >> 4;
+    v |= v >> 8;
+    v |= v >> 16;
+    v = (v >> 1) + 1;
+    return pos[(v * 0x077CB531UL) >> 27];
+}
+
 float fmaf(float x, float y, float z)
 {
     float tmp = x * y;
