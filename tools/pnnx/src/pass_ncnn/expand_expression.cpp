5df5c8dd8c537897a91d6b04a1384a2a9aa7ae37
Fisher <fisheryung@outlook.com>
2023-09-13 19:44:06 +0800

    Add pnnx convertor

--- tools/pnnx/src/pass_ncnn/expand_expression.cpp
+++ tools/pnnx/src/pass_ncnn/expand_expression.cpp
@@ -199,7 +199,7 @@ static std::string expand_expression(Graph& graph, const Operator* op, int& pnnx
             if (t == "div") op_binary->params["0"] = 3;
             if (t == "floor_divide") fprintf(stderr, "BinaryOp floor_divide not supported yet\n"); // TODO
             if (t == "fmod") fprintf(stderr, "BinaryOp fmod not supported yet\n");                 // TODO
-            if (t == "remainder") fprintf(stderr, "BinaryOp remainder not supported yet\n");       // TODO
+            if (t == "remainder") op_binary->params["0"] = 12;
             if (t == "pow") op_binary->params["0"] = 6;
             if (t == "atan2") op_binary->params["0"] = 10;
 
