454cd55f01bb579f9a48b3e6c9d754c63885ce47
HonestDeng <HonestDeng@users.noreply.github.com>
2023-09-22 03:23:36 +0000

    apply code-format changes

--- src/simplemath.cpp
+++ src/simplemath.cpp
@@ -17,13 +17,13 @@
 #if NCNN_SIMPLEMATH
 
 #include "simplemath.h"
-#define __HI(X)            *(1 + (short*)&x)
-#define __LO(X)            *(short*)&x
-#define INFINITY           (1.0 / 0)
-#define FE_TONEAREST       0
-#define FE_DOWNWARD        1024
-#define FE_UPWARD          2048
-#define FE_TOWARDZERO      3072
+#define __HI(X)       *(1 + (short*)&x)
+#define __LO(X)       *(short*)&x
+#define INFINITY      (1.0 / 0)
+#define FE_TONEAREST  0
+#define FE_DOWNWARD   1024
+#define FE_UPWARD     2048
+#define FE_TOWARDZERO 3072
 
 /*
 * ====================================================
@@ -45,7 +45,6 @@ static float uint32_as_float(uint32_t a)
 
     return r;
 }
-
 
 #ifdef __cplusplus
 extern "C" {
