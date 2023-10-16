528250e8d7e09cb20f370d49728dc5807722529e
nihui <nihui@users.noreply.github.com>
2023-10-16 11:32:43 +0000

    apply code-format changes

--- src/layer/arm/convolution_3x3_winograd_int8.h
+++ src/layer/arm/convolution_3x3_winograd_int8.h
@@ -1278,7 +1278,7 @@ static void gemm_transB_packed_tile_int8(const Mat& AT_tile, const Mat& BT_tile,
                     "r"(max_kk), // %6
                     "r"(k)       // %7
                     : "cc", "memory", "x4", "v0", "v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8", "v9", "v10", "v11", "v12", "v13", "v14", "v15", "v16", "v17", "v18", "v19", "v20", "v21", "v22", "v23", "v24", "v25", "v26", "v27", "v28", "v29", "v30", "v31");
-#else // __aarch64__
+#else  // __aarch64__
                 asm volatile(
                     "cmp        %7, #0              \n"
                     "beq        0f                  \n"
