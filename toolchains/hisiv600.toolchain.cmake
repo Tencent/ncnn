7bddf3551b40d4748a66ccb4e1a55209bcae042f
Zhuo Zhang <imzhuo@foxmail.com>
2023-11-11 21:24:47 +0800

    [style] Use same style for cmake `set` command

--- toolchains/hisiv600.toolchain.cmake
+++ toolchains/hisiv600.toolchain.cmake
@@ -1,18 +1,18 @@
 # set cross-compiled system type, it's better not use the type which cmake cannot recognized.
-SET ( CMAKE_SYSTEM_NAME Linux )
-SET ( CMAKE_SYSTEM_PROCESSOR arm )
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
 # when hislicon SDK was installed, toolchain was installed in the path as below: 
-SET ( CMAKE_C_COMPILER "/opt/hisi-linux/x86-arm/arm-hisiv600-linux/target/bin/arm-hisiv600-linux-gcc" )
-SET ( CMAKE_CXX_COMPILER "/opt/hisi-linux/x86-arm/arm-hisiv600-linux/target/bin/arm-hisiv600-linux-g++" )
-SET ( CMAKE_FIND_ROOT_PATH "/opt/hisi-linux/x86-arm/arm-hisiv600-linux" )
+set(CMAKE_C_COMPILER "/opt/hisi-linux/x86-arm/arm-hisiv600-linux/target/bin/arm-hisiv600-linux-gcc")
+set(CMAKE_CXX_COMPILER "/opt/hisi-linux/x86-arm/arm-hisiv600-linux/target/bin/arm-hisiv600-linux-g++")
+set(CMAKE_FIND_ROOT_PATH "/opt/hisi-linux/x86-arm/arm-hisiv600-linux")
 
 # set searching rules for cross-compiler
-SET ( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER )
-SET ( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY )
-SET ( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY )
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
 # set ${CMAKE_C_FLAGS} and ${CMAKE_CXX_FLAGS}flag for cross-compiled process
-SET ( CMAKE_CXX_FLAGS "-march=armv7-a -mfloat-abi=softfp -mfpu=neon ${CMAKE_CXX_FLAGS}" )
+set(CMAKE_CXX_FLAGS "-march=armv7-a -mfloat-abi=softfp -mfpu=neon ${CMAKE_CXX_FLAGS}")
 
 # cache flags
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "c flags")
