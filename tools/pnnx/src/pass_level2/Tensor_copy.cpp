5499cd57f18ebc6dc800eaac2bfd051e3f2f88fe
nihuini <nihuini@tencent.com>
2024-03-07 16:02:10 +0800

    convnext

--- tools/pnnx/src/pass_level2/Tensor_copy.cpp
+++ tools/pnnx/src/pass_level2/Tensor_copy.cpp
@@ -39,6 +39,28 @@ pnnx.Output             output      1 0 out
 
 REGISTER_GLOBAL_PNNX_GRAPH_REWRITER_PASS(Tensor_copy, 20)
 
+class Tensor_copy_01 : public GraphRewriterPass
+{
+public:
+    const char* match_pattern_graph() const
+    {
+        return R"PNNXIR(7767517
+4 3
+pnnx.Input              input_0     0 1 self
+pnnx.Input              input_1     0 1 src
+aten::copy              op_1        2 1 self src out non_blocking=*
+pnnx.Output             output      1 0 out
+)PNNXIR";
+    }
+
+    const char* type_str() const
+    {
+        return "Tensor.copy";
+    }
+};
+
+REGISTER_GLOBAL_PNNX_GRAPH_REWRITER_PASS(Tensor_copy_01, 20)
+
 class Tensor_copy_1 : public GraphRewriterPass
 {
 public:
