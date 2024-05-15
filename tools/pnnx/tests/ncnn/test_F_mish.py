35fade4e5270214beb88568987c331e0269e9d01
nihuini <nihuini@tencent.com>
2024-05-15 14:36:17 +0800

    fix tests

--- tools/pnnx/tests/ncnn/test_F_mish.py
+++ tools/pnnx/tests/ncnn/test_F_mish.py
@@ -15,6 +15,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from packaging import version
 
 def mish_forward_0(x):
     return x * F.softplus(x).tanh()
@@ -38,6 +39,9 @@ class Model(nn.Module):
         return x, y, z, w
 
 def test():
+    if version.parse(torch.__version__) < version.parse('1.9'):
+        return True
+
     net = Model()
     net.eval()
 
--- tools/pnnx/tests/test_F_scaled_dot_product_attention.py
+++ tools/pnnx/tests/test_F_scaled_dot_product_attention.py
@@ -15,6 +15,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from packaging import version
 
 class Model(nn.Module):
     def __init__(self):
@@ -26,6 +27,9 @@ class Model(nn.Module):
         return x, y
 
 def test():
+    if version.parse(torch.__version__) < version.parse('2.0'):
+        return True
+
     net = Model()
     net.eval()
 
