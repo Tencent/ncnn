1df6699c86a73d1138786bdd81f85c2b02e1b751
nihuini <nihuini@tencent.com>
2024-05-22 16:12:42 +0800

    wip

--- tools/pnnx/src/pass_level2/torch_mean.cpp
+++ tools/pnnx/src/pass_level2/torch_mean.cpp
@@ -126,4 +126,31 @@ pnnx.Output             output      1 0 out
 
 REGISTER_GLOBAL_PNNX_GRAPH_REWRITER_PASS(torch_mean_onnx, 20)
 
+class torch_mean_onnx_1 : public GraphRewriterPass
+{
+public:
+    const char* match_pattern_graph() const
+    {
+        return R"PNNXIR(7767517
+3 2
+pnnx.Input              input       0 1 input
+ReduceMean              op_0        1 1 input out axes=%axes
+pnnx.Output             output      1 0 out
+)PNNXIR";
+    }
+
+    const char* type_str() const
+    {
+        return "torch.mean";
+    }
+
+    void write(Operator* op, const std::map<std::string, Parameter>& captured_params) const
+    {
+        op->params["dim"] = captured_params.at("axes");
+        op->params["keepdim"] = true;
+    }
+};
+
+REGISTER_GLOBAL_PNNX_GRAPH_REWRITER_PASS(torch_mean_onnx_1, 20)
+
 } // namespace pnnx
