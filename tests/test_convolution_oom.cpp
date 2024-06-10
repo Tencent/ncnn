4b0148cf0c4a5f10497217c18de07aac759aaf80
nihui <shuizhuyuanluo@126.com>
2024-06-10 20:15:46 +0800

    test oom

--- tests/test_convolution_oom.cpp
+++ tests/test_convolution_oom.cpp
@@ -107,10 +107,10 @@ static int test_convolution_oom_int8(int w, int h, int c, int outch, int kernel,
     }
 
     int flag = TEST_LAYER_DISABLE_GPU_TESTING;
-    int ret = test_layer("Convolution", pd, weights, a, requant ? 1.0f : 0.001f, 0, flag);
+    int ret = test_layer_oom("Convolution", pd, weights, a, flag);
     if (ret != 0)
     {
-        fprintf(stderr, "test_convolution_int8 failed w=%d h=%d c=%d outch=%d kernel=%d dilation=%d stride=%d pad=%d bias=%d requant=%d act=%d actparams=[%f,%f]\n", w, h, c, outch, kernel, dilation, stride, pad, bias, requant, activation_type, activation_params[0], activation_params[1]);
+        fprintf(stderr, "test_convolution_oom_int8 failed w=%d h=%d c=%d outch=%d kernel=%d dilation=%d stride=%d pad=%d bias=%d requant=%d act=%d actparams=[%f,%f]\n", w, h, c, outch, kernel, dilation, stride, pad, bias, requant, activation_type, activation_params[0], activation_params[1]);
         return ret;
     }
 
