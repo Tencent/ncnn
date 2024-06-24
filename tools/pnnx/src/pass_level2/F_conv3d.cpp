8c0d125048bfcdd321b709aa783c650107f4d2ee
nihuini <nihuini@tencent.com>
2024-06-24 16:36:16 +0800

    generalized conv convtranspose

--- tools/pnnx/src/pass_level2/F_conv3d.cpp
+++ tools/pnnx/src/pass_level2/F_conv3d.cpp
@@ -108,7 +108,7 @@ public:
 pnnx.Input              input_0     0 1 input
 pnnx.Input              input_1     0 1 weight
 pnnx.Input              input_2     0 1 bias
-Conv                    op_0        3 1 input weight bias out kernel_shape=%kernel_shape strides=%strides pads=%pads dilations=%dilations group=%group
+Conv                    op_0        3 1 input weight bias out %*=%*
 pnnx.Output             output      1 0 out
 )PNNXIR";
     }
@@ -120,51 +120,81 @@ pnnx.Output             output      1 0 out
 
     bool match(const std::map<std::string, Parameter>& captured_params) const
     {
-        if (captured_params.at("kernel_shape").type != 5)
-            return false;
+        if (captured_params.find("op_0.kernel_shape") != captured_params.end())
+        {
+            if (captured_params.at("op_0.kernel_shape").type != 5 || captured_params.at("op_0.kernel_shape").ai.size() != 3)
+                return false;
+        }
 
-        if (captured_params.at("kernel_shape").ai.size() != 3)
-            return false;
+        if (captured_params.find("op_0.dilations") != captured_params.end())
+        {
+            if (captured_params.at("op_0.dilations").type != 5 || captured_params.at("op_0.dilations").ai.size() != 3)
+                return false;
+        }
 
-        if (captured_params.at("strides").type != 5)
-            return false;
+        if (captured_params.find("op_0.strides") != captured_params.end())
+        {
+            if (captured_params.at("op_0.strides").type != 5 || captured_params.at("op_0.strides").ai.size() != 3)
+                return false;
+        }
 
-        if (captured_params.at("strides").ai.size() != 3)
-            return false;
+        if (captured_params.find("op_0.pads") != captured_params.end())
+        {
+            if (captured_params.at("op_0.pads").type != 5 || captured_params.at("op_0.pads").ai.size() != 6)
+                return false;
 
-        if (captured_params.at("dilations").type != 5)
-            return false;
-
-        if (captured_params.at("dilations").ai.size() != 3)
-            return false;
-
-        if (captured_params.at("group").type != 2)
-            return false;
-
-        if (captured_params.at("pads").type != 5)
-            return false;
-
-        const std::vector<int>& pads = captured_params.at("pads").ai;
-        if (pads.size() != 6 || pads[0] != pads[3] || pads[1] != pads[4] || pads[2] != pads[5])
-            return false;
+            const std::vector<int>& pads = captured_params.at("op_0.pads").ai;
+            if (pads[0] != pads[3] || pads[1] != pads[4] || pads[2] != pads[5])
+                return false;
+        }
 
         return true;
     }
 
     void write(Operator* op, const std::map<std::string, Parameter>& captured_params) const
     {
-        const std::vector<int>& pads = captured_params.at("pads").ai;
+        if (captured_params.find("op_0.dilations") != captured_params.end())
+        {
+            op->params["dilation"] = captured_params.at("op_0.dilations");
+        }
+        else
+        {
+            op->params["dilation"] = {1, 1, 1};
+        }
 
-        op->params["stride"] = captured_params.at("strides");
-        op->params["dilation"] = captured_params.at("dilations");
-        op->params["groups"] = captured_params.at("group");
-        op->params["padding"] = {pads[0], pads[1], pads[2]};
+        if (captured_params.find("op_0.strides") != captured_params.end())
+        {
+            op->params["stride"] = captured_params.at("op_0.strides");
+        }
+        else
+        {
+            op->params["stride"] = {1, 1, 1};
+        }
+
+        if (captured_params.find("op_0.pads") != captured_params.end())
+        {
+            const std::vector<int>& pads = captured_params.at("op_0.pads").ai;
+            op->params["padding"] = {pads[0], pads[1], pads[2]};
+        }
+        else
+        {
+            op->params["padding"] = {0, 0, 0};
+        }
+
+        if (captured_params.find("op_0.group") != captured_params.end())
+        {
+            op->params["groups"] = captured_params.at("op_0.group");
+        }
+        else
+        {
+            op->params["groups"] = 1;
+        }
     }
 };
 
 REGISTER_GLOBAL_PNNX_GRAPH_REWRITER_PASS(F_conv3d_onnx, 10)
 
-class F_conv3d_onnx_0 : public F_conv3d_onnx
+class F_conv3d_onnx_1 : public F_conv3d_onnx
 {
 public:
     const char* match_pattern_graph() const
@@ -173,80 +203,16 @@ public:
 4 3
 pnnx.Input              input_0     0 1 input
 pnnx.Input              input_1     0 1 weight
-Conv                    op_0        2 1 input weight out kernel_shape=%kernel_shape strides=%strides pads=%pads dilations=%dilations group=%group
+Conv                    op_0        2 1 input weight out %*=%*
 pnnx.Output             output      1 0 out
 )PNNXIR";
-    }
-
-    const char* replace_pattern_graph() const
-    {
-        return R"PNNXIR(7767517
-4 3
-pnnx.Input              input_0     0 1 input
-pnnx.Input              input_1     0 1 weight
-F.conv3d                conv        2 1 input weight out bias=None
-pnnx.Output             output      1 0 out
-)PNNXIR";
-    }
-};
-
-REGISTER_GLOBAL_PNNX_GRAPH_REWRITER_PASS(F_conv3d_onnx_0, 10)
-
-class F_conv3d_onnx_1 : public GraphRewriterPass
-{
-public:
-    const char* match_pattern_graph() const
-    {
-        return R"PNNXIR(7767517
-5 4
-pnnx.Input              input_0     0 1 input
-pnnx.Input              input_1     0 1 weight
-pnnx.Input              input_2     0 1 bias
-Conv                    op_0        3 1 input weight bias out strides=%strides pads=%pads dilations=%dilations group=%group auto_pad=NOTSET
-pnnx.Output             output      1 0 out
-)PNNXIR";
-    }
-
-    const char* type_str() const
-    {
-        return "F.conv3d";
-    }
-
-    bool match(const std::map<std::string, Parameter>& captured_params) const
-    {
-        if (captured_params.at("strides").type != 5)
-            return false;
-
-        if (captured_params.at("strides").ai.size() != 3)
-            return false;
-
-        if (captured_params.at("dilations").type != 5)
-            return false;
-
-        if (captured_params.at("dilations").ai.size() != 3)
-            return false;
-
-        if (captured_params.at("group").type != 2)
-            return false;
-
-        if (captured_params.at("pads").type != 5)
-            return false;
-
-        const std::vector<int>& pads = captured_params.at("pads").ai;
-        if (pads.size() != 6 || pads[0] != pads[3] || pads[1] != pads[4] || pads[2] != pads[5])
-            return false;
-
-        return true;
     }
 
     void write(Operator* op, const std::map<std::string, Parameter>& captured_params) const
     {
-        const std::vector<int>& pads = captured_params.at("pads").ai;
+        F_conv3d_onnx::write(op, captured_params);
 
-        op->params["stride"] = captured_params.at("strides");
-        op->params["dilation"] = captured_params.at("dilations");
-        op->params["groups"] = captured_params.at("group");
-        op->params["padding"] = {pads[0], pads[1], pads[2]};
+        op->params["bias"] = Parameter();
     }
 };
 
