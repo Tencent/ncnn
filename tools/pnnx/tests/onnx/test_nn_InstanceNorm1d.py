07d27dd441489af1325b69c57ad6d6f436bf0033
nihuini <nihuini@tencent.com>
2024-06-28 19:55:55 +0800

    torch < 1.10 can not handle track_running_stats=True

--- tools/pnnx/tests/onnx/test_nn_InstanceNorm1d.py
+++ tools/pnnx/tests/onnx/test_nn_InstanceNorm1d.py
@@ -15,6 +15,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from packaging import version
 
 class Model(nn.Module):
     def __init__(self):
@@ -24,7 +25,10 @@ class Model(nn.Module):
         self.in_0.weight = nn.Parameter(torch.rand(12))
         self.in_0.bias = nn.Parameter(torch.rand(12))
         self.in_1 = nn.InstanceNorm1d(num_features=12, eps=1e-2, affine=False)
-        self.in_2 = nn.InstanceNorm1d(num_features=12, eps=1e-4, affine=True, track_running_stats=True)
+        if version.parse(torch.__version__) < version.parse('1.10'):
+            self.in_2 = nn.InstanceNorm1d(num_features=12, eps=1e-4, affine=True, track_running_stats=False)
+        else:
+            self.in_2 = nn.InstanceNorm1d(num_features=12, eps=1e-4, affine=True, track_running_stats=True)
         self.in_2.weight = nn.Parameter(torch.rand(12))
         self.in_2.bias = nn.Parameter(torch.rand(12))
 
