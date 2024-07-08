d8f4b006bf28d2b791c66749aa0d36cb63ebde0b
nihuini <nihuini@tencent.com>
2024-07-08 19:50:04 +0800

    skip celu test for old torch

--- tools/pnnx/tests/onnx/test_nn_CELU.py
+++ tools/pnnx/tests/onnx/test_nn_CELU.py
@@ -15,6 +15,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from packaging import version
 
 class Model(nn.Module):
     def __init__(self):
@@ -35,6 +36,9 @@ class Model(nn.Module):
         return x, y, z, w
 
 def test():
+    if version.parse(torch.__version__) < version.parse('1.12'):
+        return True
+
     net = Model()
     net.eval()
 
