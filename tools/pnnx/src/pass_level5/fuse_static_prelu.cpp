8d4eaae7c174b2d85ed57eb8513d7ff9e08dba71
nihuini <nihuini@tencent.com>
2024-07-08 19:34:03 +0800

    pnnx convert onnx hardshirnk hardsigmoid hardswish

--- tools/pnnx/src/pass_level5/fuse_static_prelu.cpp
+++ tools/pnnx/src/pass_level5/fuse_static_prelu.cpp
@@ -29,7 +29,7 @@ public:
         return R"PNNXIR(7767517
 4 3
 pnnx.Input              input       0 1 input
-pnnx.Attribute          op_weight   0 1 weight @data
+pnnx.Attribute          op_weight   0 1 weight @data=(%num_parameters)f32
 F.prelu                 op_0        2 1 input weight out
 pnnx.Output             output      1 0 out
 )PNNXIR";
@@ -40,7 +40,7 @@ pnnx.Output             output      1 0 out
         return R"PNNXIR(7767517
 3 2
 pnnx.Input              input       0 1 input
-nn.PReLU                prelu       1 1 input out @weight=%op_weight.data
+nn.PReLU                prelu       1 1 input out num_parameters=%num_parameters @weight=%op_weight.data
 pnnx.Output             output      1 0 out
 )PNNXIR";
     }
