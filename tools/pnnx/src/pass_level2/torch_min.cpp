b50632a265f56c8c0c2848952ac270ec9444c459
nihuini <nihuini@tencent.com>
2024-07-10 20:03:18 +0800

    reducemax reducemin reduceprod

--- tools/pnnx/src/pass_level2/torch_min.cpp
+++ tools/pnnx/src/pass_level2/torch_min.cpp
@@ -60,4 +60,53 @@ pnnx.Output             output      1 0 out
 
 REGISTER_GLOBAL_PNNX_GRAPH_REWRITER_PASS(torch_min_1, 20)
 
+class torch_min_onnx : public GraphRewriterPass
+{
+public:
+    const char* match_pattern_graph() const
+    {
+        return R"PNNXIR(7767517
+3 2
+pnnx.Input              input       0 1 input
+ReduceMin               op_0        1 1 input out %*=%*
+pnnx.Output             output      1 0 out
+)PNNXIR";
+    }
+
+    const char* type_str() const
+    {
+        return "torch.min";
+    }
+
+    void write(Operator* op, const std::map<std::string, Parameter>& captured_params) const
+    {
+        if (captured_params.find("op_0.axes") != captured_params.end())
+        {
+            op->params["dim"] = captured_params.at("op_0.axes");
+        }
+        else
+        {
+            // reduce all
+            const int input_rank = (int)op->inputs[0]->shape.size();
+            std::vector<int> dim(input_rank);
+            for (int i = 0; i < input_rank; i++)
+            {
+                dim[i] = i;
+            }
+            op->params["dim"] = dim;
+        }
+
+        if (captured_params.find("op_0.keepdims") != captured_params.end())
+        {
+            op->params["keepdim"] = captured_params.at("op_0.keepdims").i ? true : false;
+        }
+        else
+        {
+            op->params["keepdim"] = true;
+        }
+    }
+};
+
+REGISTER_GLOBAL_PNNX_GRAPH_REWRITER_PASS(torch_min_onnx, 20)
+
 } // namespace pnnx
