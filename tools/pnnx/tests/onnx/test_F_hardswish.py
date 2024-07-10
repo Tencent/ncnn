ed6fd32c5d70a0c62947da18560affa82a8c3032
nihuini <nihuini@tencent.com>
2024-07-10 19:44:07 +0800

    wip

--- tools/pnnx/tests/onnx/test_F_hardswish.py
+++ tools/pnnx/tests/onnx/test_F_hardswish.py
@@ -15,6 +15,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from packaging import version
 
 def hardswish_forward_0(x):
     return x * F.hardsigmoid(x)
@@ -46,6 +47,9 @@ class Model(nn.Module):
         return x, y, z, w
 
 def test():
+    if version.parse(torch.__version__) < version.parse('1.10'):
+        return True
+
     net = Model()
     net.eval()
 
