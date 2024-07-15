569617f212b2878137813b6cb16a5bd6a0076fc7
nihui <nihuini@tencent.com>
2024-07-15 16:00:11 +0800

    pnnx convert onnx expand/permute/repeat/reshape/select/slice/cat/ceil/chunk/flatten/floor/maximum/minimum/split/squeeze/stack/transpose/unbind/unsqueeze (#5583)

--- tools/pnnx/tests/onnx/test_torch_ceil.py
+++ tools/pnnx/tests/onnx/test_torch_ceil.py
@@ -1,6 +1,6 @@
 # Tencent is pleased to support the open source community by making ncnn available.
 #
-# Copyright (C) 2022 THL A29 Limited, a Tencent company. All rights reserved.
+# Copyright (C) 2024 THL A29 Limited, a Tencent company. All rights reserved.
 #
 # Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
 # in compliance with the License. You may obtain a copy of the License at
@@ -20,45 +20,33 @@ class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
 
-    def forward(self, x, y):
-        x0, x1, x2 = torch.unbind(x, dim=0)
-        y0, y1, y2, y3, y4, y5, y6, y7, y8 = torch.unbind(y, dim=1)
-
-        x0 = F.relu(x0)
-        x1 = F.relu(x1)
-        x2 = F.relu(x2)
-        y0 = F.relu(y0)
-        y1 = F.relu(y1)
-        y2 = F.relu(y2)
-        y3 = F.relu(y3)
-        y4 = F.relu(y4)
-        y5 = F.relu(y5)
-        y6 = F.relu(y6)
-        y7 = F.relu(y7)
-        y8 = F.relu(y8)
-        return x0, x1, x2, y0, y1, y2, y3, y4, y5, y6, y7, y8
+    def forward(self, x, y, z):
+        x = torch.ceil(x * 10)
+        y = torch.ceil(y * 10)
+        z = torch.ceil(z * 10)
+        return x, y, z
 
 def test():
     net = Model()
     net.eval()
 
     torch.manual_seed(0)
-    x = torch.rand(3, 16)
-    y = torch.rand(5, 9, 11)
+    x = torch.rand(1, 3, 16)
+    y = torch.rand(1, 5, 9, 11)
+    z = torch.rand(14, 8, 5, 9, 10)
 
-    a = net(x, y)
+    a = net(x, y, z)
 
-    # export torchscript
-    mod = torch.jit.trace(net, (x, y))
-    mod.save("test_torch_unbind.pt")
+    # export onnx
+    torch.onnx.export(net, (x, y, z), "test_torch_ceil.onnx")
 
-    # torchscript to pnnx
+    # onnx to pnnx
     import os
-    os.system("../../src/pnnx test_torch_unbind.pt inputshape=[3,16],[5,9,11]")
+    os.system("../../src/pnnx test_torch_ceil.onnx inputshape=[1,3,16],[1,5,9,11],[14,8,5,9,10]")
 
-    # ncnn inference
-    import test_torch_unbind_ncnn
-    b = test_torch_unbind_ncnn.test_inference()
+    # pnnx inference
+    import test_torch_ceil_pnnx
+    b = test_torch_ceil_pnnx.test_inference()
 
     for a0, b0 in zip(a, b):
         if not torch.equal(a0, b0):
--- tools/pnnx/tests/onnx/test_torch_maximum.py
+++ tools/pnnx/tests/onnx/test_torch_maximum.py
@@ -1,6 +1,6 @@
 # Tencent is pleased to support the open source community by making ncnn available.
 #
-# Copyright (C) 2022 THL A29 Limited, a Tencent company. All rights reserved.
+# Copyright (C) 2024 THL A29 Limited, a Tencent company. All rights reserved.
 #
 # Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
 # in compliance with the License. You may obtain a copy of the License at
@@ -15,50 +15,42 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from packaging import version
 
 class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
 
-    def forward(self, x, y):
-        x0, x1, x2 = torch.unbind(x, dim=0)
-        y0, y1, y2, y3, y4, y5, y6, y7, y8 = torch.unbind(y, dim=1)
-
-        x0 = F.relu(x0)
-        x1 = F.relu(x1)
-        x2 = F.relu(x2)
-        y0 = F.relu(y0)
-        y1 = F.relu(y1)
-        y2 = F.relu(y2)
-        y3 = F.relu(y3)
-        y4 = F.relu(y4)
-        y5 = F.relu(y5)
-        y6 = F.relu(y6)
-        y7 = F.relu(y7)
-        y8 = F.relu(y8)
-        return x0, x1, x2, y0, y1, y2, y3, y4, y5, y6, y7, y8
+    def forward(self, x, y, z):
+        out0 = torch.maximum(x, y)
+        out1 = torch.maximum(y, y)
+        out2 = torch.maximum(z, torch.ones_like(z) + 0.1)
+        return out0, out1, out2
 
 def test():
+    if version.parse(torch.__version__) < version.parse('1.12'):
+        return True
+
     net = Model()
     net.eval()
 
     torch.manual_seed(0)
     x = torch.rand(3, 16)
-    y = torch.rand(5, 9, 11)
+    y = torch.rand(3, 16)
+    z = torch.rand(5, 9, 3)
 
-    a = net(x, y)
+    a = net(x, y, z)
 
-    # export torchscript
-    mod = torch.jit.trace(net, (x, y))
-    mod.save("test_torch_unbind.pt")
+    # export onnx
+    torch.onnx.export(net, (x, y, z), "test_torch_maximum.onnx")
 
-    # torchscript to pnnx
+    # onnx to pnnx
     import os
-    os.system("../../src/pnnx test_torch_unbind.pt inputshape=[3,16],[5,9,11]")
+    os.system("../../src/pnnx test_torch_maximum.onnx inputshape=[3,16],[3,16],[5,9,3]")
 
-    # ncnn inference
-    import test_torch_unbind_ncnn
-    b = test_torch_unbind_ncnn.test_inference()
+    # pnnx inference
+    import test_torch_maximum_pnnx
+    b = test_torch_maximum_pnnx.test_inference()
 
     for a0, b0 in zip(a, b):
         if not torch.equal(a0, b0):
