fdf0df3079f779a19ffb0ef8b68e59916fbad1d8
nihui <nihuini@tencent.com>
2024-08-15 16:52:06 +0800

    RMSNorm (#5630)

--- tools/pnnx/tests/ncnn/test_F_rms_norm.py
+++ tools/pnnx/tests/ncnn/test_F_rms_norm.py
@@ -1,6 +1,6 @@
 # Tencent is pleased to support the open source community by making ncnn available.
 #
-# Copyright (C) 2021 THL A29 Limited, a Tencent company. All rights reserved.
+# Copyright (C) 2024 THL A29 Limited, a Tencent company. All rights reserved.
 #
 # Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
 # in compliance with the License. You may obtain a copy of the License at
@@ -15,24 +15,26 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from packaging import version
 
 class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
 
         self.w3 = nn.Parameter(torch.rand(24))
-        self.b3 = nn.Parameter(torch.rand(24))
         self.w4 = nn.Parameter(torch.rand(12, 16))
-        self.b4 = nn.Parameter(torch.rand(12, 16))
 
     def forward(self, x, y):
-        x = F.layer_norm(x, (24,), self.w3, self.b3)
+        x = F.rms_norm(x, (24,), self.w3)
 
-        y = F.layer_norm(y, (16,), None, None)
-        z = F.layer_norm(y, (12,16), self.w4, self.b4, eps=1e-3)
+        y = F.rms_norm(y, (16,), None)
+        z = F.rms_norm(y, (12,16), self.w4, eps=1e-3)
         return x, y, z
 
 def test():
+    if version.parse(torch.__version__) < version.parse('2.4'):
+        return True
+
     net = Model()
     net.eval()
 
@@ -44,15 +46,15 @@ def test():
 
     # export torchscript
     mod = torch.jit.trace(net, (x, y))
-    mod.save("test_F_layer_norm.pt")
+    mod.save("test_F_rms_norm.pt")
 
     # torchscript to pnnx
     import os
-    os.system("../../src/pnnx test_F_layer_norm.pt inputshape=[1,12,24],[1,3,12,16]")
+    os.system("../../src/pnnx test_F_rms_norm.pt inputshape=[1,12,24],[1,3,12,16]")
 
     # ncnn inference
-    import test_F_layer_norm_ncnn
-    b = test_F_layer_norm_ncnn.test_inference()
+    import test_F_rms_norm_ncnn
+    b = test_F_rms_norm_ncnn.test_inference()
 
     for a0, b0 in zip(a, b):
         if not torch.allclose(a0, b0, 1e-4, 1e-4):
