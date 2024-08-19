395f20afe657ef0389494edbc543c23fe6b28fc0
nihui <shuizhuyuanluo@126.com>
2024-08-19 23:19:00 +0800

    Update examples/yolov8.cpp

--- examples/yolov8.cpp
+++ examples/yolov8.cpp
@@ -175,7 +175,7 @@ static void parse_yolov8_detections(
 
     for (int i = 0; i < num_anchors; i++)
     {
-        const float* row_ptr = output.row(i);
+        const float* row_ptr = output.row(i).ptr<float>();
         const float* bboxes_ptr = row_ptr;
         const float* scores_ptr = row_ptr + 4;
         const float* max_s_ptr = std::max_element(scores_ptr, scores_ptr + num_labels);
