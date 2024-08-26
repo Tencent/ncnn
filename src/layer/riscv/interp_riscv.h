8c68cf421ef5f0f040d9343fbd90ff933d5d17d0
nihuini <nihuini@tencent.com>
2024-08-26 15:14:32 +0800

    x

--- src/layer/riscv/interp_riscv.h
+++ src/layer/riscv/interp_riscv.h
@@ -27,7 +27,7 @@ public:
     virtual int forward(const std::vector<Mat>& bottom_blobs, std::vector<Mat>& top_blobs, const Option& opt) const;
 
 protected:
-#if __riscv_vector && __riscv_zvfh
+#if NCNN_ZVFH
     int forward_fp16s(const std::vector<Mat>& bottom_blobs, std::vector<Mat>& top_blobs, const Option& opt) const;
     int forward_fp16sa(const std::vector<Mat>& bottom_blobs, std::vector<Mat>& top_blobs, const Option& opt) const;
 #endif
