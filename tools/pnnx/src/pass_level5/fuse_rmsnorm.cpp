1da205cb2c130208061c10caf53397a5c46db58b
nihuini <nihuini@tencent.com>
2024-09-03 19:35:19 +0800

    p++

--- tools/pnnx/src/pass_level5/fuse_rmsnorm.cpp
+++ tools/pnnx/src/pass_level5/fuse_rmsnorm.cpp
@@ -49,6 +49,23 @@ pnnx.Output             output      1 0 out
     }
 };
 
+class fuse_rmsnorm_pass_1 : public fuse_rmsnorm_pass
+{
+public:
+    const char* match_pattern_graph() const
+    {
+        return R"PNNXIR(7767517
+6 5
+pnnx.Input              input       0 1 input
+pnnx.Attribute          op_0        0 1 weight @data #weight=(%c)f32
+pnnx.Expression         op_1        1 1 input sq expr=pow(@0,2.000000e+00)
+torch.mean              op_2        1 1 sq sqmean dim=(-1) keepdim=True
+pnnx.Expression         op_3        3 1 weight input sqmean out expr=mul(@0,mul(@1,reciprocal(sqrt(add(@2,%eps)))))
+pnnx.Output             output      1 0 out
+)PNNXIR";
+    }
+};
+
 class fuse_rmsnorm_pass_onnx : public fuse_rmsnorm_pass
 {
 public:
@@ -69,10 +86,12 @@ pnnx.Output             output      1 0 out
 void fuse_rmsnorm(Graph& graph)
 {
     fuse_rmsnorm_pass a;
+    fuse_rmsnorm_pass_1 a1;
     fuse_rmsnorm_pass_onnx b;
     int opindex = 0;
 
     pnnx_graph_rewrite(graph, &a, opindex);
+    pnnx_graph_rewrite(graph, &a1, opindex);
     pnnx_graph_rewrite(graph, &b, opindex);
 }
 
