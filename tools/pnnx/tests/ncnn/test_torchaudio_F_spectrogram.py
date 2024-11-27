eac8d2cba7642c6431b0e73e82b21dfa08644eee
nihui <nihuini@tencent.com>
2024-11-27 19:39:03 +0800

    fffffffff

--- tools/pnnx/tests/ncnn/test_torchaudio_F_spectrogram.py
+++ tools/pnnx/tests/ncnn/test_torchaudio_F_spectrogram.py
@@ -31,7 +31,7 @@ class Model(nn.Module):
             out1 = torchaudio.functional.spectrogram(x, n_fft=128, window=torch.hann_window(128), win_length=128, hop_length=3, pad=0, center=False, onesided=True, normalized=False, power=None)
         out2 = torchaudio.functional.spectrogram(y, n_fft=512, window=torch.hamming_window(256), win_length=256, hop_length=128, pad=0, center=True, pad_mode='constant', onesided=True, normalized='frame_length', power=2)
         out3 = torchaudio.functional.spectrogram(y, n_fft=512, window=torch.hamming_window(512), win_length=512, hop_length=128, pad=32, center=True, onesided=False, normalized=False, power=2)
-        if version.parse(torchaudio.__version__) >= version.parse('0.12.0'):
+        if version.parse(torchaudio.__version__) >= version.parse('0.11.0'):
             out1 = torch.view_as_real(out1)
         return out0, out1, out2, out3
 
--- tools/pnnx/tests/ncnn/test_torchaudio_Spectrogram.py
+++ tools/pnnx/tests/ncnn/test_torchaudio_Spectrogram.py
@@ -36,7 +36,7 @@ class Model(nn.Module):
         out1 = self.s1(x)
         out2 = self.s2(y)
         out3 = self.s3(y)
-        if version.parse(torchaudio.__version__) >= version.parse('0.12.0'):
+        if version.parse(torchaudio.__version__) >= version.parse('0.11.0'):
             out1 = torch.view_as_real(out1)
         return out0, out1, out2, out3
 
