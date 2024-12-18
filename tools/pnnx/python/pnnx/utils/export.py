bd75b1074eb9f0d37be473c106d4085f8f7d8db2
Joson <1250377062@qq.com>
2024-12-19 06:49:30 +0800

    Update export.py

--- tools/pnnx/python/pnnx/utils/export.py
+++ tools/pnnx/python/pnnx/utils/export.py
@@ -13,21 +13,58 @@
 # specific language governing permissions and limitations under the License.
 
 import torch
-import os
+import onnx
 
-def export(model, ptpath, inputs = None, inputs2 = None, input_shapes = None, input_types = None,
-           input_shapes2 = None, input_types2 = None, device = None, customop = None,
-           moduleop = None, optlevel = None, pnnxparam = None, pnnxbin = None,
-           pnnxpy = None, pnnxonnx = None, ncnnparam = None, ncnnbin = None, ncnnpy = None,
-           check_trace = True, fp16 = True):
+def export(ptpath_or_model, output_model_path, inputs=None, inputs2=None, input_shapes=None, input_types=None,
+           input_shapes2=None, input_types2=None, device=None, customop=None, moduleop=None,
+           optlevel=None, pnnxparam=None, pnnxbin=None, pnnxpy=None, pnnxonnx=None,
+           ncnnparam=None, ncnnbin=None, ncnnpy=None, check_trace=True, fp16=True):
+
     if (inputs is None) and (input_shapes is None):
         raise Exception("inputs or input_shapes should be specified.")
     if not (input_shapes is None) and (input_types is None):
         raise Exception("when input_shapes is specified, then input_types should be specified correspondingly.")
 
-    model.eval()
-    mod = torch.jit.trace(model, inputs, check_trace=check_trace)
-    mod.save(ptpath)
+    # Check if the input is a path or a model instance
+    if isinstance(ptpath_or_model, torch.nn.Module):
+        ptpath_or_model.eval()
+        mod = torch.jit.trace(ptpath_or_model, inputs, check_trace=check_trace)
+        mod.save(output_model_path)
+        from . import convert
+        return convert(output_model_path, inputs, inputs2, input_shapes, input_types,
+                        input_shapes2, input_types2, device, customop, moduleop,
+                        optlevel, pnnxparam, pnnxbin, pnnxpy, pnnxonnx,
+                        ncnnparam, ncnnbin, ncnnpy, fp16)
+    elif isinstance(ptpath_or_model, str):
+        ptpath = ptpath_or_model
+    else:
+        raise TypeError("`ptpath_or_model` must be a PyTorch model instance or a file path.")
 
-    from . import convert
-    return convert(ptpath, inputs, inputs2, input_shapes, input_types, input_shapes2, input_types2, device, customop, moduleop, optlevel, pnnxparam, pnnxbin, pnnxpy, pnnxonnx, ncnnparam, ncnnbin, ncnnpy, fp16)
+    # If it is a PyTorch model file (.pt)
+    if ptpath.endswith('.pt'):
+        model = torch.jit.load(ptpath)
+        model.eval()
+        mod = torch.jit.trace(model, inputs, check_trace=check_trace)
+        mod.save(output_model_path)
+        ptpath = output_model_path
+        from . import convert
+        return convert(ptpath, inputs, inputs2, input_shapes, input_types,
+                       input_shapes2, input_types2, device, customop, moduleop,
+                       optlevel, pnnxparam, pnnxbin, pnnxpy, pnnxonnx,
+                       ncnnparam, ncnnbin, ncnnpy, fp16)
+
+    # If the input is an ONNX model file (.onnx)
+    elif ptpath.endswith('.onnx'):
+        model = onnx.load(ptpath)
+        onnx.checker.check_model(model)
+        from . import convert
+        return convert(ptpath, inputs=inputs, inputs2=inputs2,
+                       input_shapes=input_shapes, input_types=input_types,
+                       input_shapes2=input_shapes2, input_types2=input_types2,
+                       device=device, customop=customop, moduleop=moduleop,
+                       optlevel=optlevel, pnnxparam=pnnxparam, pnnxbin=pnnxbin,
+                       pnnxpy=pnnxpy, pnnxonnx=pnnxonnx, ncnnparam=ncnnparam,
+                       ncnnbin=ncnnbin, ncnnpy=ncnnpy, fp16=fp16)
+    else:
+        raise Exception("Unsupported model file type. Only .pt and .onnx files are supported.")
+
