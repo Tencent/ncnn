08c330a9e49d41def11c64be7d577fe50e2209a7
佰阅 <43716063+Baiyuetribe@users.noreply.github.com>
2024-12-21 11:11:02 +0800

    fix type error

--- src/layer/topk.cpp
+++ src/layer/topk.cpp
@@ -12,8 +12,7 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
-#include "TopK.h"
-// #include "layer_type.h"
+#include "topk.h"
 
 namespace ncnn {
 
