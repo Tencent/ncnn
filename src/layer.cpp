a7127f2c58f12e613beccf8b3bdf3b017f0e0b52
nihui <shuizhuyuanluo@126.com>
2024-12-22 22:03:39 +0800

    always prefer xtheadvector

--- src/layer.cpp
+++ src/layer.cpp
@@ -546,13 +546,6 @@ Layer* create_layer_cpu(int index)
     }
     else
 #endif // NCNN_RUNTIME_CPU && NCNN_MSA
-#if NCNN_RUNTIME_CPU && NCNN_RVV
-    if (ncnn::cpu_support_riscv_v())
-    {
-        layer_creator = layer_registry_rvv[index].creator;
-    }
-    else
-#endif // NCNN_RUNTIME_CPU && NCNN_RVV
 #if NCNN_RUNTIME_CPU && NCNN_XTHEADVECTOR
     if (ncnn::cpu_support_riscv_xtheadvector())
     {
@@ -560,6 +553,13 @@ Layer* create_layer_cpu(int index)
     }
     else
 #endif // NCNN_RUNTIME_CPU && NCNN_XTHEADVECTOR
+#if NCNN_RUNTIME_CPU && NCNN_RVV
+    if (ncnn::cpu_support_riscv_v())
+    {
+        layer_creator = layer_registry_rvv[index].creator;
+    }
+    else
+#endif // NCNN_RUNTIME_CPU && NCNN_RVV
     {
         layer_creator = layer_registry_arch[index].creator;
     }
