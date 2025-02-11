4dc0e2c21257de0cad9f3e6e8160760bb310a01f
nihuini <nihuini@tencent.com>
2025-02-11 11:38:19 +0800

    w

--- tools/pnnx/src/pass_level2/torch_matmul.cpp
+++ tools/pnnx/src/pass_level2/torch_matmul.cpp
@@ -69,7 +69,7 @@ public:
 4 3
 pnnx.Input              input_0     0 1 input
 pnnx.Input              input_1     0 1 other
-tnn.MatMul              op_0        2 1 input other out arg0=%weight_position
+tnn.MatMul              op_0        2 1 input other out %*=%*
 pnnx.Output             output      1 0 out
 )PNNXIR";
     }
@@ -81,16 +81,16 @@ pnnx.Output             output      1 0 out
 
     void write(Operator* op, const std::map<std::string, Parameter>& captured_params) const
     {
-        const int weight_position = captured_params.at("weight_position").i;
-        if (weight_position == 0)
+        if (captured_params.find("op_0.arg0") != captured_params.end())
         {
-            // swap input and weight
-            std::swap(op->inputs[0], op->inputs[1]);
+            const int weight_position = captured_params.at("op_0.arg0").i;
+            if (weight_position == 0)
+            {
+                // swap input and weight
+                std::swap(op->inputs[0], op->inputs[1]);
+            }
         }
     }
-
-protected:
-    mutable int weight_position;
 };
 
 REGISTER_GLOBAL_PNNX_GRAPH_REWRITER_PASS(torch_matmul_tnn, 90)
