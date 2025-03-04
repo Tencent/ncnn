d4d89f0f1e3b5479f599e8861301ee6c85cb8216
nihui <171016+nihui@users.noreply.github.com>
2025-03-04 06:30:30 +0000

    apply code-format changes

--- tests/test_expression.cpp
+++ tests/test_expression.cpp
@@ -34,8 +34,7 @@ static int test_expression_0()
     return 0
            || test_count_expression_blobs("1,2,3,4,5,6", 0)
            || test_count_expression_blobs("-1,1h,2w", 3)
-           || test_count_expression_blobs("2,9d,2c,-1", 10)
-           ;
+           || test_count_expression_blobs("2,9d,2c,-1", 10);
 }
 
 static int test_eval_list_expression(const std::string& expr, std::vector<ncnn::Mat>& blobs, int ndim, ...)
@@ -74,8 +73,7 @@ static int test_expression_1()
     blobs[1] = ncnn::Mat(10, 20, 2, 4);
 
     return 0
-           || test_eval_list_expression("+(trunc(*(0w,0.5)),-(0c,10)),floor(/(1h,0.5)),+(0c,1c),round(2.0)", blobs, 4, 84, 40, 48, 2)
-           ;
+           || test_eval_list_expression("+(trunc(*(0w,0.5)),-(0c,10)),floor(/(1h,0.5)),+(0c,1c),round(2.0)", blobs, 4, 84, 40, 48, 2);
 }
 
 int main()
