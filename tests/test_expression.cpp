085b66c30a091d0a84340344b1c4e857036d4e36
nihuini <nihuini@tencent.com>
2025-03-04 16:17:49 +0800

    tt

--- tests/test_expression.cpp
+++ tests/test_expression.cpp
@@ -73,7 +73,18 @@ static int test_expression_1()
     blobs[1] = ncnn::Mat(10, 20, 2, 4);
 
     return 0
-           || test_eval_list_expression("+(trunc(*(0w,0.5)),-(0c,10)),floor(/(1h,0.5)),+(0c,1c),round(2.0)", blobs, 4, 84, 40, 48, 2);
+           || test_eval_list_expression("+(trunc(*(0w,0.5)),-(0c,10)),floor(/(1h,0.5)),+(0c,1c),round(2.0)", blobs, 4, 84, 40, 48, 2)
+           || test_eval_list_expression("//(0w,3),+(0w,1w),-(0h,1h),*(0c,1c)", blobs, 4, 33, 110, 180, 176)
+           || test_eval_list_expression("floor(//(0w,3.0)),round(+(0w,1.0)),trunc(-(0h,2.0)),ceil(*(1d,3.0))", blobs, 4, 33, 101, 198, 6)
+           || test_eval_list_expression("round(*(abs(asin(sin(0w))),10)),ceil(*(abs(acos(cos(+(0w,3)))),10))", blobs, 2, 5, 25)
+           || test_eval_list_expression("floor(*(abs(asinh(sinh(/(0w,100)))),10)),trunc(*(abs(acosh(cosh(*(0w,0.004)))),10))", blobs, 2, 10, 3)
+           || test_eval_list_expression("round(*(abs(atan(tan(0w))),10)),ceil(*(abs(atanh(tanh(-(0w,99)))),10))", blobs, 2, 5, 10)
+           || test_eval_list_expression("floor(min(max(*(square(sqrt(0w)),1.2121),100),120))", blobs, 1, 120)
+           || test_eval_list_expression("min(max(trunc(*(log(exp(*(neg(0w),0.001))),-144)),15),20)", blobs, 1, 15)
+           || test_eval_list_expression("round(*(erf(reciprocal(log10(1h))),999))", blobs, 1, 722)
+           || test_eval_list_expression("ceil(pow(fmod(atan2(0w,1d),1c),14.14)),floor(logaddexp(remainder(0c,10),6))", blobs, 2, 495, 6)
+           || test_eval_list_expression("floor(*(square(sqrt(0w)),1.2121))", blobs, 1, 121)
+           || test_eval_list_expression("rshift(lshift(xor(or(and(1d,18),9),4),4),2)", blobs, 1, 60);
 }
 
 int main()
