400195ecf5ec8d5100d1a80408b34167d6088b31
佰阅 <43716063+Baiyuetribe@users.noreply.github.com>
2025-03-17 19:58:56 +0800

    fix cstdint

--- tools/pnnx/src/save_ncnn.cpp
+++ tools/pnnx/src/save_ncnn.cpp
@@ -13,6 +13,7 @@
 // specific language governing permissions and limitations under the License.
 
 #include "save_ncnn.h"
+#include <cstdint>
 
 namespace pnnx {
 
@@ -317,7 +318,7 @@ int save_ncnn(const Graph& g, const std::string& parampath, const std::string& b
                 continue;
             }
 
-            if (fp16 && attr.type == 0 && attr.data == std::vector<char> {0, 0, 0, 0})
+            if (fp16 && attr.type == 0 && attr.data == std::vector<char>{0, 0, 0, 0})
             {
                 // write fp16 flag
                 unsigned int fp16_flag = 0x01306B47;
