524001bf54ea75b817a3cb35c6e05283313fc582
nihui <nihuini@tencent.com>
2025-04-01 11:01:27 +0800

    pnnx convert onnx conv with asymmatric pads (#5965)
    * pnnx convert onnx conv with asymmatric pads
    
    * pad + conv1d and conv3d
    
    * onnx conv auto_pad

--- tools/pnnx/src/pass_level2/F_conv3d.cpp
+++ tools/pnnx/src/pass_level2/F_conv3d.cpp
@@ -171,6 +171,16 @@ pnnx.Output             output      1 0 out
                 return false;
         }
 
+        if (captured_params.find("op_0.auto_pad") != captured_params.end())
+        {
+            if (captured_params.at("op_0.auto_pad").type != 4)
+                return false;
+
+            const std::string& auto_pad = captured_params.at("op_0.auto_pad").s;
+            if (auto_pad == "SAME_LOWER")
+                return false;
+        }
+
         return true;
     }
 
@@ -202,6 +212,19 @@ pnnx.Output             output      1 0 out
         else
         {
             op->params["padding"] = {0, 0, 0};
+        }
+
+        if (captured_params.find("op_0.auto_pad") != captured_params.end())
+        {
+            const std::string& auto_pad = captured_params.at("op_0.auto_pad").s;
+            if (auto_pad == "VALID")
+            {
+                op->params["padding"] = "valid";
+            }
+            if (auto_pad == "SAME_UPPER")
+            {
+                op->params["padding"] = "same";
+            }
         }
 
         if (captured_params.find("op_0.group") != captured_params.end())
@@ -240,5 +263,146 @@ pnnx.Output             output      1 0 out
 };
 
 REGISTER_GLOBAL_PNNX_GRAPH_REWRITER_PASS(F_conv3d_onnx_1, 140)
+
+class F_conv3d_onnx_pad : public GraphRewriterPass
+{
+public:
+    const char* match_pattern_graph() const
+    {
+        return R"PNNXIR(7767517
+5 4
+pnnx.Input              input_0     0 1 input
+pnnx.Input              input_1     0 1 weight
+pnnx.Input              input_2     0 1 bias
+Conv                    op_0        3 1 input weight bias out %*=%*
+pnnx.Output             output      1 0 out
+)PNNXIR";
+    }
+
+    const char* replace_pattern_graph() const
+    {
+        return R"PNNXIR(7767517
+6 5
+pnnx.Input              input_0     0 1 input
+pnnx.Input              input_1     0 1 weight
+pnnx.Input              input_2     0 1 bias
+F.pad                   pad         1 1 input pad
+F.conv3d                conv        3 1 pad weight bias out
+pnnx.Output             output      1 0 out
+)PNNXIR";
+    }
+
+    bool match(const std::map<std::string, Parameter>& captured_params) const
+    {
+        if (captured_params.find("op_0.kernel_shape") != captured_params.end())
+        {
+            if (captured_params.at("op_0.kernel_shape").type != 5 || captured_params.at("op_0.kernel_shape").ai.size() != 3)
+                return false;
+        }
+
+        if (captured_params.find("op_0.dilations") != captured_params.end())
+        {
+            if (captured_params.at("op_0.dilations").type != 5 || captured_params.at("op_0.dilations").ai.size() != 3)
+                return false;
+        }
+
+        if (captured_params.find("op_0.strides") != captured_params.end())
+        {
+            if (captured_params.at("op_0.strides").type != 5 || captured_params.at("op_0.strides").ai.size() != 3)
+                return false;
+        }
+
+        if (captured_params.find("op_0.pads") == captured_params.end())
+            return false;
+
+        if (captured_params.at("op_0.pads").type != 5 || captured_params.at("op_0.pads").ai.size() != 6)
+            return false;
+
+        const std::vector<int>& pads = captured_params.at("op_0.pads").ai;
+        if (pads[0] == pads[3] && pads[1] == pads[4] && pads[2] == pads[5])
+            return false;
+
+        return true;
+    }
+
+    void write(const std::map<std::string, Operator*>& ops, const std::map<std::string, Parameter>& captured_params) const
+    {
+        Operator* op_pad = ops.at("pad");
+        Operator* op_conv = ops.at("conv");
+
+        const std::vector<int>& pads = captured_params.at("op_0.pads").ai;
+        op_pad->params["mode"] = "constant";
+        op_pad->params["pad"] = std::vector<int>{pads[2], pads[5], pads[1], pads[4], pads[0], pads[3]};
+        op_pad->params["value"] = Parameter();
+
+        if (captured_params.find("op_0.dilations") != captured_params.end())
+        {
+            op_conv->params["dilation"] = captured_params.at("op_0.dilations");
+        }
+        else
+        {
+            op_conv->params["dilation"] = {1, 1, 1};
+        }
+
+        if (captured_params.find("op_0.strides") != captured_params.end())
+        {
+            op_conv->params["stride"] = captured_params.at("op_0.strides");
+        }
+        else
+        {
+            op_conv->params["stride"] = {1, 1, 1};
+        }
+
+        op_conv->params["padding"] = {0, 0, 0};
+
+        if (captured_params.find("op_0.group") != captured_params.end())
+        {
+            op_conv->params["groups"] = captured_params.at("op_0.group");
+        }
+        else
+        {
+            op_conv->params["groups"] = 1;
+        }
+    }
+};
+
+REGISTER_GLOBAL_PNNX_GRAPH_REWRITER_PASS(F_conv3d_onnx_pad, 140)
+
+class F_conv3d_onnx_pad_1 : public F_conv3d_onnx_pad
+{
+public:
+    const char* match_pattern_graph() const
+    {
+        return R"PNNXIR(7767517
+4 3
+pnnx.Input              input_0     0 1 input
+pnnx.Input              input_1     0 1 weight
+Conv                    op_0        2 1 input weight out %*=%*
+pnnx.Output             output      1 0 out
+)PNNXIR";
+    }
+
+    const char* replace_pattern_graph() const
+    {
+        return R"PNNXIR(7767517
+5 4
+pnnx.Input              input_0     0 1 input
+pnnx.Input              input_1     0 1 weight
+F.pad                   pad         1 1 input pad
+F.conv3d                conv        2 1 pad weight out
+pnnx.Output             output      1 0 out
+)PNNXIR";
+    }
+
+    void write(const std::map<std::string, Operator*>& ops, const std::map<std::string, Parameter>& captured_params) const
+    {
+        F_conv3d_onnx_pad::write(ops, captured_params);
+
+        Operator* op_conv = ops.at("conv");
+        op_conv->params["bias"] = Parameter();
+    }
+};
+
+REGISTER_GLOBAL_PNNX_GRAPH_REWRITER_PASS(F_conv3d_onnx_pad_1, 140)
 
 } // namespace pnnx
