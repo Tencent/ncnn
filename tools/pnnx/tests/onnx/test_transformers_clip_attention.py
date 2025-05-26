f352627ccee7d7557fad227128e590ad22fd8140
nihuini <nihuini@tencent.com>
2025-05-26 11:28:17 +0800

    skip onnx clip sdpa

--- tools/pnnx/tests/onnx/test_transformers_clip_attention.py
+++ tools/pnnx/tests/onnx/test_transformers_clip_attention.py
@@ -30,52 +30,59 @@ class Model(nn.Module):
         text_config0 = CLIPTextConfig(hidden_size=192, num_attention_heads=8, attention_dropout=0.0)
         self.text_attn0 = CLIPAttention(text_config0)
 
-        text_config0_sdpa = CLIPTextConfig(hidden_size=192, num_attention_heads=8, attention_dropout=0.0, attn_implementation='sdpa')
-        self.text_attn0_sdpa = CLIPAttention(text_config0_sdpa)
-
         text_config1 = CLIPTextConfig(hidden_size=66, num_attention_heads=11, attention_dropout=0.0)
         self.text_attn1 = CLIPAttention(text_config1)
-
-        text_config1_sdpa = CLIPTextConfig(hidden_size=66, num_attention_heads=11, attention_dropout=0.0, attn_implementation='sdpa')
-        self.text_attn1_sdpa = CLIPAttention(text_config1_sdpa)
 
         text_config2 = CLIPTextConfig(hidden_size=66, num_attention_heads=33, attention_dropout=0.0)
         self.text_attn2 = CLIPAttention(text_config2)
 
-        text_config2_sdpa = CLIPTextConfig(hidden_size=66, num_attention_heads=33, attention_dropout=0.0, attn_implementation='sdpa')
-        self.text_attn2_sdpa = CLIPAttention(text_config2_sdpa)
-
         text_config3 = CLIPTextConfig(hidden_size=66, num_attention_heads=22, attention_dropout=0.0)
         self.text_attn3 = CLIPAttention(text_config3)
-
-        text_config3_sdpa = CLIPTextConfig(hidden_size=66, num_attention_heads=22, attention_dropout=0.0, attn_implementation='sdpa')
-        self.text_attn3_sdpa = CLIPAttention(text_config3_sdpa)
 
         text_config4 = CLIPTextConfig(hidden_size=66, num_attention_heads=6, attention_dropout=0.0)
         self.text_attn4 = CLIPAttention(text_config4)
 
-        text_config4_sdpa = CLIPTextConfig(hidden_size=66, num_attention_heads=6, attention_dropout=0.0, attn_implementation='sdpa')
-        self.text_attn4_sdpa = CLIPAttention(text_config4_sdpa)
-
         vision_config0 = CLIPVisionConfig(hidden_size=14, num_attention_heads=2, attention_dropout=0.0)
         self.vision_attn0 = CLIPAttention(vision_config0)
 
-        vision_config0_sdpa = CLIPVisionConfig(hidden_size=14, num_attention_heads=2, attention_dropout=0.0, attn_implementation='sdpa')
-        self.vision_attn0_sdpa = CLIPAttention(vision_config0_sdpa)
+        if version.parse(torch.__version__) >= version.parse('2.5'):
+            text_config0_sdpa = CLIPTextConfig(hidden_size=192, num_attention_heads=8, attention_dropout=0.0, attn_implementation='sdpa')
+            self.text_attn0_sdpa = CLIPAttention(text_config0_sdpa)
+
+            text_config1_sdpa = CLIPTextConfig(hidden_size=66, num_attention_heads=11, attention_dropout=0.0, attn_implementation='sdpa')
+            self.text_attn1_sdpa = CLIPAttention(text_config1_sdpa)
+
+            text_config2_sdpa = CLIPTextConfig(hidden_size=66, num_attention_heads=33, attention_dropout=0.0, attn_implementation='sdpa')
+            self.text_attn2_sdpa = CLIPAttention(text_config2_sdpa)
+
+            text_config3_sdpa = CLIPTextConfig(hidden_size=66, num_attention_heads=22, attention_dropout=0.0, attn_implementation='sdpa')
+            self.text_attn3_sdpa = CLIPAttention(text_config3_sdpa)
+
+            text_config4_sdpa = CLIPTextConfig(hidden_size=66, num_attention_heads=6, attention_dropout=0.0, attn_implementation='sdpa')
+            self.text_attn4_sdpa = CLIPAttention(text_config4_sdpa)
+
+            vision_config0_sdpa = CLIPVisionConfig(hidden_size=14, num_attention_heads=2, attention_dropout=0.0, attn_implementation='sdpa')
+            self.vision_attn0_sdpa = CLIPAttention(vision_config0_sdpa)
 
     def forward(self, x, y, mask0, casual_mask0, z):
         out0, _ = self.text_attn0(x, attention_mask=None, causal_attention_mask=None, output_attentions=True)
-        out0_sdpa, _ = self.text_attn0_sdpa(x, attention_mask=None, causal_attention_mask=None, output_attentions=False)
         out1, _ = self.text_attn1(y, attention_mask=None, causal_attention_mask=None, output_attentions=False)
-        out1_sdpa, _ = self.text_attn1_sdpa(y, attention_mask=None, causal_attention_mask=None, output_attentions=False)
         out2, _ = self.text_attn2(y, attention_mask=mask0, causal_attention_mask=None, output_attentions=True)
-        out2_sdpa, _ = self.text_attn2_sdpa(y, attention_mask=mask0, causal_attention_mask=None, output_attentions=False)
         out3, _ = self.text_attn3(y, attention_mask=None, causal_attention_mask=casual_mask0, output_attentions=True)
-        out3_sdpa, _ = self.text_attn3_sdpa(y, attention_mask=None, causal_attention_mask=casual_mask0, output_attentions=False)
         out4, _ = self.text_attn4(y, attention_mask=mask0, causal_attention_mask=casual_mask0, output_attentions=True)
-        out4_sdpa, _ = self.text_attn4_sdpa(y, attention_mask=mask0, causal_attention_mask=casual_mask0, output_attentions=False)
         out5, _ = self.vision_attn0(z, attention_mask=None, causal_attention_mask=None)
+
+        # sdpa onnx export failed
+        if version.parse(torch.__version__) < version.parse('2.5'):
+            return out0, out1, out2, out3, out4, out5
+
+        out0_sdpa, _ = self.text_attn0_sdpa(x, attention_mask=None, causal_attention_mask=None, output_attentions=False)
+        out1_sdpa, _ = self.text_attn1_sdpa(y, attention_mask=None, causal_attention_mask=None, output_attentions=False)
+        out2_sdpa, _ = self.text_attn2_sdpa(y, attention_mask=mask0, causal_attention_mask=None, output_attentions=False)
+        out3_sdpa, _ = self.text_attn3_sdpa(y, attention_mask=None, causal_attention_mask=casual_mask0, output_attentions=False)
+        out4_sdpa, _ = self.text_attn4_sdpa(y, attention_mask=mask0, causal_attention_mask=casual_mask0, output_attentions=False)
         out5_sdpa, _ = self.vision_attn0_sdpa(z, attention_mask=None, causal_attention_mask=None, output_attentions=False)
+
         return out0, out0_sdpa, out1, out1_sdpa, out2, out2_sdpa, out3, out3_sdpa, out4, out4_sdpa, out5, out5_sdpa
 
 def test():
