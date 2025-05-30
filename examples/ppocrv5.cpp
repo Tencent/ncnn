b5cc25f06351e0341126851e24b00b7cb1641abc
nihui <nihuini@tencent.com>
2025-05-30 16:21:53 +0800

    Update ppocrv5.cpp

--- examples/ppocrv5.cpp
+++ examples/ppocrv5.cpp
@@ -167,6 +167,7 @@ protected:
 void PPOCRv5::init()
 {
     // the ncnn model https://github.com/nihui/ncnn-assets/tree/master/models
+    // https://github.com/nihui/ncnn-android-ppocrv5/tree/master/app/src/main/assets
 
     ppocrv5_det.opt.use_vulkan_compute = true;
     // ppocrv5_det.opt.use_bf16_storage = true;
