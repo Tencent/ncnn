da499ada74554e98ff578894267b85c76b2dae40
Molly Sophia <mollysophia379@gmail.com>
2025-09-10 22:49:33 +0800

    SPDX stype license header for amx_usability.h
    Signed-off-by: Molly Sophia <mollysophia379@gmail.com>

--- src/layer/arm/amx_usability.h
+++ src/layer/arm/amx_usability.h
@@ -1,16 +1,6 @@
-// Tencent is pleased to support the open source community by making ncnn available.
-//
-// Copyright (C) 2021 THL A29 Limited, a Tencent company. All rights reserved.
-//
-// Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
-// in compliance with the License. You may obtain a copy of the License at
-//
-// https://opensource.org/licenses/BSD-3-Clause
-//
-// Unless required by applicable law or agreed to in writing, software distributed
-// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-// CONDITIONS OF ANY KIND, either express or implied. See the License for the
-// specific language governing permissions and limitations under the License.
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2022 Corsix <https://github.com/corsix>
+// Copyright (c) 2025 Molly Sophia <mollysophia@gmail.com>
 
 #ifndef AMX_USABILITY_H
 #define AMX_USABILITY_H
