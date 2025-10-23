66172fa271067f7977a53267edcfbc901d7febb1
nihui <nihuini@tencent.com>
2025-10-23 11:18:44 +0800

    Change external data file name type to ORTCHAR_T

--- tools/pnnx/src/pass_onnx/shape_inference.cpp
+++ tools/pnnx/src/pass_onnx/shape_inference.cpp
@@ -227,7 +227,7 @@ void shape_inference(onnx::ModelProto& model, const std::string& external_data_p
 
         if (!external_data.empty())
         {
-            const char* external_initializer_file_names[] = {external_data_path.c_str()};
+            const ORTCHAR_T* external_initializer_file_names[] = {(const ORTCHAR_T*)external_data_path.c_str()};
             char* external_initializer_file_buffer_array[] = {(char*)external_data.data()};
             const size_t external_initializer_file_lengths[] = {external_data.size()};
 
