90bd7d1a444a201373e5d5f91928bd5c3d26b28f
nihuini <nihuini@tencent.com>
2025-10-27 16:31:51 +0800

    pnnx convert torch.unflatten

---
+++ tools/pnnx/tests/ncnn/test_torch_unflatten.py
@@ -0,0 +1,50 @@
+# Copyright 2025 Tencent
+# SPDX-License-Identifier: BSD-3-Clause
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+
+    def forward(self, x, y, z):
+        x = torch.unflatten(x, dim=0, sizes=(2,1,2,-1))
+        y = torch.unflatten(y, dim=1, sizes=(3,4))
+        z = torch.unflatten(z, dim=-2, sizes=(3,-1))
+        return x, y, z
+
+def test():
+    net = Model()
+    net.eval()
+
+    torch.manual_seed(0)
+    x = torch.rand(16)
+    y = torch.rand(9, 12)
+    z = torch.rand(8, 9, 10)
+
+    a = net(x, y, z)
+
+    # export torchscript
+    mod = torch.jit.trace(net, (x, y, z))
+    mod.save("test_torch_unflatten.pt")
+
+    # torchscript to pnnx
+    import os
+    os.system("../../src/pnnx test_torch_unflatten.pt inputshape=[16],[9,12],[8,9,10]")
+
+    # ncnn inference
+    import test_torch_unflatten_ncnn
+    b = test_torch_unflatten_ncnn.test_inference()
+
+    for a0, b0 in zip(a, b):
+        if not torch.equal(a0, b0):
+            return False
+    return True
+
+if __name__ == "__main__":
+    if test():
+        exit(0)
+    else:
+        exit(1)
