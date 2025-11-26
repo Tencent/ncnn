d71840e3bee5a032af4978c6ee3b3f3fe70b4d76
nihui <nihuini@tencent.com>
2025-11-26 15:26:22 +0800

    Refactor output matrix handling in arcface.cpp
    Refactor output matrix creation and transpose logic.

--- examples/arcface.cpp
+++ examples/arcface.cpp
@@ -313,9 +313,14 @@ static DetectionResult parse_yolo_keypoints_results(ncnn::Mat& result,
         float iou_threshold,
         std::vector<std::string> class_names)
 {
-    cv::Mat output0 = cv::Mat((int)result.h, (int)result.w, CV_32FC1, result);
-    cv::Mat output;
-    cv::transpose(output0, output);
+    cv::Mat output((int)result.w, (int)result.h, CV_32FC1);
+    for (int i = 0; i < output.cols; i++)
+    {
+        for (int j = 0; j < output.rows; j++)
+        {
+            output.ptr<float>(j)[i] = result.row(i)[j];
+        }
+    }
     std::vector<Bbox> detections;
     std::vector<std::vector<float> > all_keypoints;
 
