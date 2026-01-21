363ae349e506622c92dd73eb61c5ed84f145b37a
nihui <nihuini@tencent.com>
2026-01-21 10:31:20 +0800

    Rope vk (#62)
    * rope
    
    * apply code-format changes
    
    * make nihui happy
    
    * fix code style
    
    * update glslang
    
    ---------
    
    Co-authored-by: futz12 <1391525377@qq.com>
    Co-authored-by: futz12 <56149058+futz12@users.noreply.github.com>

---
+++ src/layer/vulkan/rotaryembed_vulkan.h
@@ -0,0 +1,29 @@
+// Copyright 2026 Futz12 <pchar.cn>
+// SPDX-License-Identifier: BSD-3-Clause
+
+#ifndef LAYER_ROTARYEMBED_VULKAN_H
+#define LAYER_ROTARYEMBED_VULKAN_H
+
+#include "rotaryembed.h"
+
+namespace ncnn {
+
+class RotaryEmbed_vulkan : public RotaryEmbed
+{
+public:
+    RotaryEmbed_vulkan();
+
+    virtual int create_pipeline(const Option& opt);
+    virtual int destroy_pipeline(const Option& opt);
+
+    using RotaryEmbed::forward;
+    virtual int forward(const std::vector<VkMat>& bottom_blobs, std::vector<VkMat>& top_blobs, VkCompute& cmd, const Option& opt) const;
+
+public:
+    Pipeline* pipeline_rotaryembed;
+    Pipeline* pipeline_rotaryembed_pack4;
+};
+
+} // namespace ncnn
+
+#endif // LAYER_ROTARYEMBED_VULKAN_H
