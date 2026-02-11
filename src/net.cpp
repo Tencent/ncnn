0ed9702f82bf353c05c50e50b440f1df28f88fa5
nihui <nihuini@tencent.com>
2026-02-11 18:39:58 +0800

    commit vulkan compute command as soon as enough pending dispatches collected for avoiding driver timeout (#6541)

--- src/net.cpp
+++ src/net.cpp
@@ -247,6 +247,31 @@ int NetPrivate::forward_layer(int layer_index, std::vector<Mat>& blob_mats, std:
         }
     }
 
+    // for avoiding driver timeout
+    // commit as soon as we collect enough pending
+    const uint32_t rough_score = vkdev->info.rough_score();
+    uint32_t pending_dispatch_threshold = 32 * 1024; // 32K
+    if (rough_score > 75)
+    {
+        pending_dispatch_threshold = 8 * 1024 * 1024; // 8M
+    }
+    else if (rough_score > 50)
+    {
+        pending_dispatch_threshold = 4 * 1024 * 1024; // 4M
+    }
+    else if (rough_score > 15)
+    {
+        pending_dispatch_threshold = 1 * 1024 * 1024; // 1M
+    }
+    else if (rough_score > 10)
+    {
+        pending_dispatch_threshold = 256 * 1024; // 256K
+    }
+    if (cmd.pending_dispatch_total() > pending_dispatch_threshold)
+    {
+        cmd_submit_and_wait = true;
+    }
+
     int ret;
     if (cmd_submit_and_wait)
     {
