75d97af101740c5147b0dedf7b76bc3ec96b8396
nihui <nihuini@tencent.com>
2026-02-28 08:26:36 +0000

    opt

--- src/layer/x86/deformableconv2d_packed.h
+++ src/layer/x86/deformableconv2d_packed.h
@@ -18,6 +18,7 @@ static void deformableconv2d_packed(const std::vector<Mat>& bottom_blobs, Mat& t
     const int outh = top_blob.h;
     const int outch = top_blob.c;
     const int out_elempack = top_blob.elempack;
+
     const float* bias_data_ptr = bias_data;
 
     #pragma omp parallel for num_threads(opt.num_threads)
@@ -36,30 +37,28 @@ static void deformableconv2d_packed(const std::vector<Mat>& bottom_blobs, Mat& t
 #if __AVX__
 #if __AVX512F__
                 __m512 _sum_avx512 = _mm512_setzero_ps();
-                if (out_elempack == 16)
-                {
-                    if (bias_data_ptr)
-                        _sum_avx512 = _mm512_load_ps(bias_data_ptr + oc * out_elempack);
-                }
 #endif // __AVX512F__
                 __m256 _sum_avx = _mm256_setzero_ps();
-                if (out_elempack == 8)
-                {
-                    if (bias_data_ptr)
-                        _sum_avx = _mm256_load_ps(bias_data_ptr + oc * out_elempack);
-                }
 #endif // __AVX__
                 __m128 _sum_sse = _mm_setzero_ps();
-                if (out_elempack == 4)
-                {
-                    if (bias_data_ptr)
-                        _sum_sse = _mm_load_ps(bias_data_ptr + oc * out_elempack);
-                }
 #endif // __SSE2__
                 float _sum_scalar = 0.f;
-                if (out_elempack == 1)
+
+                if (bias_data_ptr)
                 {
-                    if (bias_data_ptr)
+#if __SSE2__
+#if __AVX__
+#if __AVX512F__
+                    if (out_elempack == 16)
+                        _sum_avx512 = _mm512_load_ps(bias_data_ptr + oc * out_elempack);
+#endif // __AVX512F__
+                    if (out_elempack == 8)
+                        _sum_avx = _mm256_load_ps(bias_data_ptr + oc * out_elempack);
+#endif // __AVX__
+                    if (out_elempack == 4)
+                        _sum_sse = _mm_load_ps(bias_data_ptr + oc * out_elempack);
+#endif // __SSE2__
+                    if (out_elempack == 1)
                         _sum_scalar = *(bias_data_ptr + oc);
                 }
 
@@ -147,48 +146,126 @@ static void deformableconv2d_packed(const std::vector<Mat>& bottom_blobs, Mat& t
                         {
                             const float* data_im_ptr = bottom_blob.channel(ic);
 
-                            for (int ez = 0; ez < elempack; ez++)
-                            {
-                                float val = 0.f;
-                                if (cond)
-                                {
-                                    float v1 = v1_cond ? *(data_im_ptr + v1_pos * elempack + ez) : 0.f;
-                                    float v2 = v2_cond ? *(data_im_ptr + v2_pos * elempack + ez) : 0.f;
-                                    float v3 = v3_cond ? *(data_im_ptr + v3_pos * elempack + ez) : 0.f;
-                                    float v4 = v4_cond ? *(data_im_ptr + v4_pos * elempack + ez) : 0.f;
-                                    val = bw1 * v1 + bw2 * v2 + bw3 * v3 + bw4 * v4;
-                                }
-                                if (has_mask)
-                                {
-                                    val *= mask_;
-                                }
-
 #if __SSE2__
 #if __AVX__
 #if __AVX512F__
-                                if (out_elempack == 16)
+                            if (out_elempack == 16)
+                            {
+                                __m512 _bw1 = _mm512_set1_ps(bw1);
+                                __m512 _bw2 = _mm512_set1_ps(bw2);
+                                __m512 _bw3 = _mm512_set1_ps(bw3);
+                                __m512 _bw4 = _mm512_set1_ps(bw4);
+                                __m512 _mask = _mm512_set1_ps(mask_);
+
+                                for (int ez = 0; ez < elempack; ez++)
                                 {
+                                    __m512 _val = _mm512_setzero_ps();
+                                    if (cond)
+                                    {
+                                        __m512 _v1 = v1_cond ? _mm512_set1_ps(*(data_im_ptr + v1_pos * elempack + ez)) : _mm512_setzero_ps();
+                                        __m512 _v2 = v2_cond ? _mm512_set1_ps(*(data_im_ptr + v2_pos * elempack + ez)) : _mm512_setzero_ps();
+                                        __m512 _v3 = v3_cond ? _mm512_set1_ps(*(data_im_ptr + v3_pos * elempack + ez)) : _mm512_setzero_ps();
+                                        __m512 _v4 = v4_cond ? _mm512_set1_ps(*(data_im_ptr + v4_pos * elempack + ez)) : _mm512_setzero_ps();
+
+                                        _val = _mm512_fmadd_ps(_v1, _bw1, _val);
+                                        _val = _mm512_fmadd_ps(_v2, _bw2, _val);
+                                        _val = _mm512_fmadd_ps(_v3, _bw3, _val);
+                                        _val = _mm512_fmadd_ps(_v4, _bw4, _val);
+                                    }
+                                    if (has_mask)
+                                    {
+                                        _val = _mm512_mul_ps(_val, _mask);
+                                    }
                                     __m512 _conv_w = _mm512_load_ps(kptr);
-                                    _sum_avx512 = _mm512_fmadd_ps(_mm512_set1_ps(val), _conv_w, _sum_avx512);
+                                    _sum_avx512 = _mm512_fmadd_ps(_val, _conv_w, _sum_avx512);
                                     kptr += 16;
                                 }
+                            }
 #endif // __AVX512F__
-                                if (out_elempack == 8)
+                            if (out_elempack == 8)
+                            {
+                                __m256 _bw1 = _mm256_set1_ps(bw1);
+                                __m256 _bw2 = _mm256_set1_ps(bw2);
+                                __m256 _bw3 = _mm256_set1_ps(bw3);
+                                __m256 _bw4 = _mm256_set1_ps(bw4);
+                                __m256 _mask = _mm256_set1_ps(mask_);
+
+                                for (int ez = 0; ez < elempack; ez++)
                                 {
+                                    __m256 _val = _mm256_setzero_ps();
+                                    if (cond)
+                                    {
+                                        __m256 _v1 = v1_cond ? _mm256_broadcast_ss(data_im_ptr + v1_pos * elempack + ez) : _mm256_setzero_ps();
+                                        __m256 _v2 = v2_cond ? _mm256_broadcast_ss(data_im_ptr + v2_pos * elempack + ez) : _mm256_setzero_ps();
+                                        __m256 _v3 = v3_cond ? _mm256_broadcast_ss(data_im_ptr + v3_pos * elempack + ez) : _mm256_setzero_ps();
+                                        __m256 _v4 = v4_cond ? _mm256_broadcast_ss(data_im_ptr + v4_pos * elempack + ez) : _mm256_setzero_ps();
+
+                                        _val = _mm256_comp_fmadd_ps(_v1, _bw1, _val);
+                                        _val = _mm256_comp_fmadd_ps(_v2, _bw2, _val);
+                                        _val = _mm256_comp_fmadd_ps(_v3, _bw3, _val);
+                                        _val = _mm256_comp_fmadd_ps(_v4, _bw4, _val);
+                                    }
+                                    if (has_mask)
+                                    {
+                                        _val = _mm256_mul_ps(_val, _mask);
+                                    }
                                     __m256 _conv_w = _mm256_load_ps(kptr);
-                                    _sum_avx = _mm256_comp_fmadd_ps(_mm256_set1_ps(val), _conv_w, _sum_avx);
+                                    _sum_avx = _mm256_comp_fmadd_ps(_val, _conv_w, _sum_avx);
                                     kptr += 8;
                                 }
+                            }
 #endif // __AVX__
-                                if (out_elempack == 4)
+                            if (out_elempack == 4)
+                            {
+                                __m128 _bw1 = _mm_set1_ps(bw1);
+                                __m128 _bw2 = _mm_set1_ps(bw2);
+                                __m128 _bw3 = _mm_set1_ps(bw3);
+                                __m128 _bw4 = _mm_set1_ps(bw4);
+                                __m128 _mask = _mm_set1_ps(mask_);
+
+                                for (int ez = 0; ez < elempack; ez++)
                                 {
+                                    __m128 _val = _mm_setzero_ps();
+                                    if (cond)
+                                    {
+                                        __m128 _v1 = v1_cond ? _mm_load1_ps(data_im_ptr + v1_pos * elempack + ez) : _mm_setzero_ps();
+                                        __m128 _v2 = v2_cond ? _mm_load1_ps(data_im_ptr + v2_pos * elempack + ez) : _mm_setzero_ps();
+                                        __m128 _v3 = v3_cond ? _mm_load1_ps(data_im_ptr + v3_pos * elempack + ez) : _mm_setzero_ps();
+                                        __m128 _v4 = v4_cond ? _mm_load1_ps(data_im_ptr + v4_pos * elempack + ez) : _mm_setzero_ps();
+
+                                        _val = _mm_comp_fmadd_ps(_v1, _bw1, _val);
+                                        _val = _mm_comp_fmadd_ps(_v2, _bw2, _val);
+                                        _val = _mm_comp_fmadd_ps(_v3, _bw3, _val);
+                                        _val = _mm_comp_fmadd_ps(_v4, _bw4, _val);
+                                    }
+                                    if (has_mask)
+                                    {
+                                        _val = _mm_mul_ps(_val, _mask);
+                                    }
                                     __m128 _conv_w = _mm_load_ps(kptr);
-                                    _sum_sse = _mm_comp_fmadd_ps(_mm_set1_ps(val), _conv_w, _sum_sse);
+                                    _sum_sse = _mm_comp_fmadd_ps(_val, _conv_w, _sum_sse);
                                     kptr += 4;
                                 }
+                            }
 #endif // __SSE2__
-                                if (out_elempack == 1)
+
+                            if (out_elempack == 1)
+                            {
+                                for (int ez = 0; ez < elempack; ez++)
                                 {
+                                    float val = 0.f;
+                                    if (cond)
+                                    {
+                                        float v1 = v1_cond ? *(data_im_ptr + v1_pos * elempack + ez) : 0.f;
+                                        float v2 = v2_cond ? *(data_im_ptr + v2_pos * elempack + ez) : 0.f;
+                                        float v3 = v3_cond ? *(data_im_ptr + v3_pos * elempack + ez) : 0.f;
+                                        float v4 = v4_cond ? *(data_im_ptr + v4_pos * elempack + ez) : 0.f;
+                                        val = bw1 * v1 + bw2 * v2 + bw3 * v3 + bw4 * v4;
+                                    }
+                                    if (has_mask)
+                                    {
+                                        val *= mask_;
+                                    }
                                     _sum_scalar += val * *(kptr);
                                     kptr += 1;
                                 }
