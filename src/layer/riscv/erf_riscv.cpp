af425018e527bcbc68fe785352015ab47e354ae7
futz12 <1391525377@qq.com>
2026-03-11 00:22:22 +0800

    fix erf

--- src/layer/riscv/erf_riscv.cpp
+++ src/layer/riscv/erf_riscv.cpp
@@ -6,9 +6,6 @@
 #if __riscv_vector
 #include <riscv_vector.h>
 #include "rvv_mathfun.h"
-#if __riscv_zvfh
-#include "rvv_mathfun_fp16s.h"
-#endif
 #endif // __riscv_vector
 
 #include "cpu.h"
@@ -79,85 +76,5 @@ int Erf_riscv::forward_inplace(Mat& bottom_top_blob, const Option& opt) const
 
     return 0;
 }
-
-#if NCNN_ZFH
-int Erf_riscv::forward_inplace_fp16s(Mat& bottom_top_blob, const Option& opt) const
-{
-    int w = bottom_top_blob.w;
-    int h = bottom_top_blob.h;
-    int d = bottom_top_blob.d;
-    int channels = bottom_top_blob.c;
-    int elempack = bottom_top_blob.elempack;
-    int size = w * h * d * elempack;
-
-    #pragma omp parallel for num_threads(opt.num_threads)
-    for (int q = 0; q < channels; q++)
-    {
-        __fp16* ptr = bottom_top_blob.channel(q);
-
-#if __riscv_zvfh
-        int n = size;
-        while (n > 0)
-        {
-            size_t vl = __riscv_vsetvl_e16m4(n);
-
-            vfloat32m8_t _p = __riscv_vfwcvt_f_f_v_f32m8(__riscv_vle16_v_f16m4(ptr, vl), vl);
-            _p = erf_ps(_p, vl);
-            __riscv_vse16_v_f16m4(ptr, __riscv_vfncvt_f_f_w_f16m4(_p, vl), vl);
-
-            ptr += vl;
-            n -= vl;
-        }
-#else  // __riscv_zvfh
-        for (int i = 0; i < size; i++)
-        {
-            *ptr = (__fp16)erff((float)*ptr);
-            ptr++;
-        }
-#endif // __riscv_zvfh
-    }
-
-    return 0;
-}
-
-int Erf_riscv::forward_inplace_fp16sa(Mat& bottom_top_blob, const Option& opt) const
-{
-    int w = bottom_top_blob.w;
-    int h = bottom_top_blob.h;
-    int d = bottom_top_blob.d;
-    int channels = bottom_top_blob.c;
-    int elempack = bottom_top_blob.elempack;
-    int size = w * h * d * elempack;
-
-    #pragma omp parallel for num_threads(opt.num_threads)
-    for (int q = 0; q < channels; q++)
-    {
-        __fp16* ptr = bottom_top_blob.channel(q);
-
-#if __riscv_zvfh
-        int n = size;
-        while (n > 0)
-        {
-            size_t vl = __riscv_vsetvl_e16m8(n);
-
-            vfloat16m8_t _p = __riscv_vle16_v_f16m8(ptr, vl);
-            _p = erf_ps(_p, vl);
-            __riscv_vse16_v_f16m8(ptr, _p, vl);
-
-            ptr += vl;
-            n -= vl;
-        }
-#else  // __riscv_zvfh
-        for (int i = 0; i < size; i++)
-        {
-            *ptr = (__fp16)erff((float)*ptr);
-            ptr++;
-        }
-#endif // __riscv_zvfh
-    }
-
-    return 0;
-}
-#endif // NCNN_ZFH
 
 } // namespace ncnn
