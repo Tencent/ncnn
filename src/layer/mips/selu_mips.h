2426a8bd32a31b11c3f49f9c00a1440b1a3816d7
風的彷徨 <56149058+futz12@users.noreply.github.com>
2026-03-18 22:25:43 +0800

    support mips elu erf gelu selu (#6607)

---
+++ src/layer/mips/selu_mips.h
@@ -0,0 +1,21 @@
+// Copyright 2025 Tencent
+// SPDX-License-Identifier: BSD-3-Clause
+
+#ifndef LAYER_SELU_MIPS_H
+#define LAYER_SELU_MIPS_H
+
+#include "selu.h"
+
+namespace ncnn {
+
+class SELU_mips : public SELU
+{
+public:
+    SELU_mips();
+
+    virtual int forward_inplace(Mat& bottom_top_blob, const Option& opt) const;
+};
+
+} // namespace ncnn
+
+#endif // LAYER_SELU_MIPS_H
