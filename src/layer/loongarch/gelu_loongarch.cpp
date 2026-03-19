b166ecdc0929d88ea5745a4ac21004878950a910
futz12 <1391525377@qq.com>
2026-03-19 20:18:27 +0800

    1. use erfc instead of erf + 1 2. avoid exp overflow

--- src/layer/loongarch/gelu_loongarch.cpp
+++ src/layer/loongarch/gelu_loongarch.cpp
@@ -60,16 +60,14 @@ int GELU_loongarch::forward_inplace(Mat& bottom_top_blob, const Option& opt) con
         else
         {
             __m128 _half = (__m128)__lsx_vreplfr2vr_s(0.5f);
-            __m128 _one = (__m128)__lsx_vreplfr2vr_s(1.f);
-            __m128 _inv_sqrt2 = (__m128)__lsx_vreplfr2vr_s(0.70710678f);
+            __m128 _inv_sqrt2 = (__m128)__lsx_vreplfr2vr_s(-0.70710678f);
             for (; i + 3 < size; i += 4)
             {
                 __builtin_prefetch(ptr + 16);
                 __m128 _p = (__m128)__lsx_vld(ptr, 0);
 
                 __m128 _blob = __lsx_vfmul_s(_inv_sqrt2, _p);
-                _blob = erf_ps(_blob);
-                _blob = __lsx_vfadd_s(_one, _blob);
+                _blob = erfc_ps(_blob);
                 _blob = __lsx_vfmul_s(_half, __lsx_vfmul_s(_blob, _p));
                 __lsx_vst(_blob, ptr, 0);
 
@@ -85,7 +83,7 @@ int GELU_loongarch::forward_inplace(Mat& bottom_top_blob, const Option& opt) con
             }
             else
             {
-                *ptr = 0.5f * *ptr * (1.0f + erff(0.70710678f * *ptr));
+                *ptr = 0.5f * *ptr * erfcf(-0.70710678f * *ptr);
             }
 
             ptr++;
--- src/layer/loongarch/selu_loongarch.cpp
+++ src/layer/loongarch/selu_loongarch.cpp
@@ -36,22 +36,22 @@ int SELU_loongarch::forward_inplace(Mat& bottom_top_blob, const Option& opt) con
 #if __loongarch_sx
         __m128 _zero = (__m128)__lsx_vreplgr2vr_w(0);
         __m128 _one = (__m128)__lsx_vreplfr2vr_s(1.f);
-        __m128 _alphaxlambda = (__m128)__lsx_vreplfr2vr_s(alphaxlambda);
+        __m128 _alpha = (__m128)__lsx_vreplfr2vr_s(alpha);
         __m128 _lambda = (__m128)__lsx_vreplfr2vr_s(lambda);
         for (; i + 3 < size; i += 4)
         {
             __builtin_prefetch(ptr + 16);
             __m128 _p = (__m128)__lsx_vld(ptr, 0);
-            __m128i _lemask = __lsx_vfcmp_cle_s(_p, _zero);
 
-            __m128 _nps = exp_ps(_p);
-            _nps = __lsx_vfsub_s(_nps, _one);
-            _nps = __lsx_vfmul_s(_nps, _alphaxlambda);
+            __m128 _pos = __lsx_vfmax_s(_p, _zero);
+            __m128 _neg = __lsx_vfmin_s(_p, _zero);
 
-            _p = __lsx_vfmul_s(_p, _lambda);
+            __m128 _blob = exp_ps(_neg);
+            _blob = __lsx_vfsub_s(_blob, _one);
+            _blob = __lsx_vfmul_s(_alpha, _blob);
+            _blob = __lsx_vfmul_s(_lambda, __lsx_vfadd_s(_pos, _blob));
 
-            _p = (__m128)__lsx_vbitsel_v((__m128i)_p, (__m128i)_nps, (__m128i)_lemask);
-            __lsx_vst(_p, ptr, 0);
+            __lsx_vst(_blob, ptr, 0);
 
             ptr += 4;
         }
