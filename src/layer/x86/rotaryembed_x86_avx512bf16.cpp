3a654e6b734343823b1c7cb5d15354014a4afc1e
nihui <nihuini@tencent.com>
2026-03-31 08:40:22 +0000

    cc

--- src/layer/x86/rotaryembed_x86_avx512bf16.cpp
+++ src/layer/x86/rotaryembed_x86_avx512bf16.cpp
@@ -1,22 +1,9 @@
 // Copyright 2026 pchar.cn
 // SPDX-License-Identifier: BSD-3-Clause
 
-#include "rotaryembed_x86.h"
-
-#if __SSE2__
-#include <emmintrin.h>
-#if __SSE3__
-#include <pmmintrin.h>
-#if __AVX__
-#include <immintrin.h>
-#endif // __AVX__
-#endif // __SSE3__
-#endif // __SSE2__
-
-#include "x86_usability.h"
-
 #include "cpu.h"
 #include "mat.h"
+#include "x86_usability.h"
 
 namespace ncnn {
 
