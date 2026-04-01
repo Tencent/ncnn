f6a11a564c298fb63842efcef9b8cb5c15de5a95
nihui <nihuini@tencent.com>
2026-04-01 15:33:45 +0800

    deconvolution x86 support bf16 storage, clean includes (#6627)

--- src/layer/x86/tanh_x86_avx512bf16.cpp
+++ src/layer/x86/tanh_x86_avx512bf16.cpp
@@ -1,11 +1,12 @@
 // Copyright 2026 Tencent
 // SPDX-License-Identifier: BSD-3-Clause
 
-#include "tanh_x86.h"
-
-#include "x86_activation.h"
-
 #include "cpu.h"
+#include "layer.h"
+#include "layer_type.h"
+#include "mat.h"
+#include "x86_activation.h"
+#include "x86_usability.h"
 
 namespace ncnn {
 
