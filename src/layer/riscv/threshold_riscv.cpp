2fb8b5a378d0f516415cf4eb43f796afd641404e
ihb2032 <hebome@foxmail.com>
2026-04-13 20:58:34 +0800

    rvv: add threshold
    Signed-off-by: ihb2032 <hebome@foxmail.com>

---
+++ src/layer/riscv/threshold_riscv.cpp
@@ -0,0 +1,81 @@
+// Copyright 2026 ihb2032 <hebome@foxmail.com>
+// SPDX-License-Identifier: BSD-3-Clause
+
+#include "threshold_riscv.h"
+
+#if __riscv_vector
+#include <riscv_vector.h>
+#endif // __riscv_vector
+
+#include "cpu.h"
+
+namespace ncnn {
+
+Threshold_riscv::Threshold_riscv()
+{
+#if __riscv_vector
+    support_packing = true;
+#endif // __riscv_vector
+#if NCNN_ZFH
+#if __riscv_vector
+    support_fp16_storage = cpu_support_riscv_zvfh();
+#else
+    support_fp16_storage = cpu_support_riscv_zfh();
+#endif
+#endif
+}
+
+int Threshold_riscv::forward_inplace(Mat& bottom_top_blob, const Option& opt) const
+{
+#if NCNN_ZFH
+    int elembits = bottom_top_blob.elembits();
+
+    if (opt.use_fp16_storage && elembits == 16)
+    {
+        if (opt.use_fp16_arithmetic)
+            return forward_inplace_fp16sa(bottom_top_blob, opt);
+        else
+            return forward_inplace_fp16s(bottom_top_blob, opt);
+    }
+#endif
+
+    int w = bottom_top_blob.w;
+    int h = bottom_top_blob.h;
+    int d = bottom_top_blob.d;
+    int channels = bottom_top_blob.c;
+    int elempack = bottom_top_blob.elempack;
+    int size = w * h * d * elempack;
+
+    #pragma omp parallel for num_threads(opt.num_threads)
+    for (int q = 0; q < channels; q++)
+    {
+        float* ptr = bottom_top_blob.channel(q);
+
+#if __riscv_vector
+        int n = size;
+        while (n > 0)
+        {
+            size_t vl = __riscv_vsetvl_e32m8(n);
+
+            vfloat32m8_t _p = __riscv_vle32_v_f32m8(ptr, vl);
+            vbool4_t _mask = __riscv_vmfgt_vf_f32m8_b4(_p, threshold, vl);
+
+            vfloat32m8_t _out = __riscv_vfmv_v_f_f32m8(0.f, vl);
+            _out = __riscv_vfmerge_vfm_f32m8(_out, 1.f, _mask, vl);
+            __riscv_vse32_v_f32m8(ptr, _out, vl);
+
+            ptr += vl;
+            n -= vl;
+        }
+#else  // __riscv_vector
+        for (int i = 0; i < size; i++)
+        {
+            ptr[i] = ptr[i] > threshold ? 1.f : 0.f;
+        }
+#endif // __riscv_vector
+    }
+
+    return 0;
+}
+
+} // namespace ncnn
---
+++ src/layer/riscv/threshold_riscv_zfh.cpp
@@ -0,0 +1,98 @@
+// Copyright 2026 ihb2032 <hebome@foxmail.com>
+// SPDX-License-Identifier: BSD-3-Clause
+
+#include "threshold_riscv.h"
+
+#if __riscv_vector
+#include <riscv_vector.h>
+#endif // __riscv_vector
+
+namespace ncnn {
+
+#if NCNN_ZFH
+int Threshold_riscv::forward_inplace_fp16s(Mat& bottom_top_blob, const Option& opt) const
+{
+    int w = bottom_top_blob.w;
+    int h = bottom_top_blob.h;
+    int d = bottom_top_blob.d;
+    int channels = bottom_top_blob.c;
+    int elempack = bottom_top_blob.elempack;
+    int size = w * h * d * elempack;
+
+    #pragma omp parallel for num_threads(opt.num_threads)
+    for (int q = 0; q < channels; q++)
+    {
+        __fp16* ptr = bottom_top_blob.channel(q);
+
+#if __riscv_zvfh
+        int n = size;
+        while (n > 0)
+        {
+            size_t vl = __riscv_vsetvl_e16m4(n);
+
+            vfloat32m8_t _p = __riscv_vfwcvt_f_f_v_f32m8(__riscv_vle16_v_f16m4(ptr, vl), vl);
+            vbool4_t _mask = __riscv_vmfgt_vf_f32m8_b4(_p, threshold, vl);
+
+            vfloat32m8_t _out = __riscv_vfmv_v_f_f32m8(0.f, vl);
+            _out = __riscv_vfmerge_vfm_f32m8(_out, 1.f, _mask, vl);
+            __riscv_vse16_v_f16m4(ptr, __riscv_vfncvt_f_f_w_f16m4(_out, vl), vl);
+
+            ptr += vl;
+            n -= vl;
+        }
+#else  // __riscv_zvfh
+        for (int i = 0; i < size; i++)
+        {
+            ptr[i] = (__fp16)((float)ptr[i] > threshold ? 1.f : 0.f);
+        }
+#endif // __riscv_zvfh
+    }
+
+    return 0;
+}
+
+int Threshold_riscv::forward_inplace_fp16sa(Mat& bottom_top_blob, const Option& opt) const
+{
+    int w = bottom_top_blob.w;
+    int h = bottom_top_blob.h;
+    int d = bottom_top_blob.d;
+    int channels = bottom_top_blob.c;
+    int elempack = bottom_top_blob.elempack;
+    int size = w * h * d * elempack;
+
+    const __fp16 _threshold = (__fp16)threshold;
+
+    #pragma omp parallel for num_threads(opt.num_threads)
+    for (int q = 0; q < channels; q++)
+    {
+        __fp16* ptr = bottom_top_blob.channel(q);
+
+#if __riscv_zvfh
+        int n = size;
+        while (n > 0)
+        {
+            size_t vl = __riscv_vsetvl_e16m8(n);
+
+            vfloat16m8_t _p = __riscv_vle16_v_f16m8(ptr, vl);
+            vbool2_t _mask = __riscv_vmfgt_vf_f16m8_b2(_p, _threshold, vl);
+
+            vfloat16m8_t _out = __riscv_vfmv_v_f_f16m8((__fp16)0.f, vl);
+            _out = __riscv_vfmerge_vfm_f16m8(_out, (__fp16)1.f, _mask, vl);
+            __riscv_vse16_v_f16m8(ptr, _out, vl);
+
+            ptr += vl;
+            n -= vl;
+        }
+#else  // __riscv_zvfh
+        for (int i = 0; i < size; i++)
+        {
+            ptr[i] = ptr[i] > _threshold ? (__fp16)1.f : (__fp16)0.f;
+        }
+#endif // __riscv_zvfh
+    }
+
+    return 0;
+}
+#endif // NCNN_ZFH
+
+} // namespace ncnn
