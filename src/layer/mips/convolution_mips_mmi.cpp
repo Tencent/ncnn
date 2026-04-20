433b874cfc8564841d40a33bde23a336339c0a9d
nihui <nihuini@tencent.com>
2026-04-20 20:01:41 +0800

    f

--- src/layer/mips/convolution_mips_mmi.cpp
+++ src/layer/mips/convolution_mips_mmi.cpp
@@ -11,9 +11,7 @@
 namespace ncnn {
 
 #include "convolution_im2col_gemm_int8.h"
-#include "convolution_winograd_transform_int8.h"
-#include "convolution_winograd_dot_int8.h"
-#include "convolution_3x3_int8.h"
+#include "convolution_3x3_winograd_int8.h"
 
 void convolution_gemm_transB_packed_tile_int8_loongson_mmi(const Mat& AT_tile, const Mat& BT_tile, Mat& topT_tile, int i, int max_ii, int j, int max_jj, int k, int max_kk)
 {
@@ -25,14 +23,19 @@ void convolution_im2col_gemm_transform_kernel_int8_loongson_mmi(const Mat& kerne
     convolution_im2col_gemm_transform_kernel_int8(kernel, AT, inch, outch, kernel_w, kernel_h, opt);
 }
 
-void conv3x3s1_winograd43_transform_kernel_int8_loongson_mmi(const Mat& kernel, Mat& kernel_tm_packed, int inch, int outch, const Option& opt)
+void conv3x3s1_winograd23_transform_kernel_int8_loongson_mmi(const Mat& kernel, Mat& AT, int inch, int outch, const Option& opt)
 {
-    conv3x3s1_winograd43_transform_kernel_int8_msa(kernel, kernel_tm_packed, inch, outch, opt);
+    conv3x3s1_winograd23_transform_kernel_int8(kernel, AT, inch, outch, opt);
 }
 
-void convolution_winograd_dot_int8_loongson_mmi(Mat& bottom_blob_tm, int outch, const Mat& kernel_tm, Mat& top_blob_tm, const Option& opt)
+void conv3x3s1_winograd43_transform_kernel_int8_loongson_mmi(const Mat& kernel, Mat& AT, int inch, int outch, const Option& opt)
 {
-    convolution_winograd_dot_int8_msa(bottom_blob_tm, outch, kernel_tm, top_blob_tm, opt);
+    conv3x3s1_winograd43_transform_kernel_int8(kernel, AT, inch, outch, opt);
+}
+
+void gemm_transB_packed_tile_int8_loongson_mmi(const Mat& AT_tile, const Mat& BT_tile, Mat& top_blob, int batch, int max_ii, int max_jj, int k, int max_kk, bool k_end)
+{
+    gemm_transB_packed_tile_int8(AT_tile, BT_tile, top_blob, batch, max_ii, max_jj, k, max_kk, k_end);
 }
 
 } // namespace ncnn
