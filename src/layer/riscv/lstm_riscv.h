c76a349eaece934f5d3adda40e3520d8faece9bb
chenglimin <clmngu@163.com>
2026-05-22 10:43:36 +0800

    add riscv rvv support for lstm operator

---
+++ src/layer/riscv/lstm_riscv.h
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+
+#ifndef LAYER_LSTM_RISCV_H
+#define LAYER_LSTM_RISCV_H
+
+#include "lstm.h"
+
+namespace ncnn {
+
+class LSTM_riscv : public LSTM
+{
+public:
+    LSTM_riscv();
+
+    virtual int forward(const Mat& bottom_blob, Mat& top_blob, const Option& opt) const;
+
+    virtual int forward(const std::vector<Mat>& bottom_blobs, std::vector<Mat>& top_blobs, const Option& opt) const;
+};
+
+} // namespace ncnn
+
+#endif // LAYER_LSTM_RISCV_H
+
+
+
+
+
+
+
