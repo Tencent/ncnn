443159601e422db9b9fda15b6562f79434425fef
nihui <nihuini@tencent.com>
2026-06-15 19:38:27 +0800

    check feature bit

--- src/layer/vulkan/gemm_vulkan.cpp
+++ src/layer/vulkan/gemm_vulkan.cpp
@@ -74,7 +74,7 @@ int Gemm_vulkan::create_pipeline(const Option& opt)
         // sanitize wired subgroup_size
         use_subgroup_ops = false;
     }
-    if (opt.use_fp16_arithmetic && !opt.use_bf16_storage && !opt.use_bf16_packed && !vkdev->info.support_VK_KHR_shader_subgroup_extended_types())
+    if (opt.use_fp16_arithmetic && !opt.use_bf16_storage && !opt.use_bf16_packed && !vkdev->info.queryShaderSubgroupExtendedTypesFeatures().shaderSubgroupExtendedTypes)
     {
         // gemm_sg shuffles fp16 vectors, which requires subgroup extended types
         use_subgroup_ops = false;
