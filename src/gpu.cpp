5c4c03a880a37d6515303f8cad3e2167b56a3c77
nihui <nihuini@tencent.com>
2026-08-04 14:37:26 +0800

    adapt to vkwebgpu and vkopencl (#6876)
    * support image storage

--- src/gpu.cpp
+++ src/gpu.cpp
@@ -322,6 +322,7 @@ public:
     // property
     bool unified_compute_transfer_queue;
     bool resizable_bar_enabled;
+    bool support_image_storage;
 
     // bug is not feature
     bool bug_storage_buffer_no_l1;
@@ -444,6 +445,10 @@ void GpuInfoPrivate::query_features()
 void GpuInfoPrivate::query_properties()
 {
     vkGetPhysicalDeviceProperties(physicalDevice, &physicalDeviceProperties);
+
+    VkImageFormatProperties imageFormatProperties;
+    VkResult ret = vkGetPhysicalDeviceImageFormatProperties(physicalDevice, VK_FORMAT_R32_SFLOAT, VK_IMAGE_TYPE_3D, VK_IMAGE_TILING_OPTIMAL, VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_STORAGE_BIT | VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT, 0, &imageFormatProperties);
+    support_image_storage = ret == VK_SUCCESS;
 
     // NCNN_LOGE("[%u] apiVersion = %u.%u.%u", i, VK_VERSION_MAJOR(physicalDeviceProperties.apiVersion),
     //     VK_VERSION_MINOR(physicalDeviceProperties.apiVersion), VK_VERSION_PATCH(physicalDeviceProperties.apiVersion));
@@ -1863,6 +1868,11 @@ bool GpuInfo::unified_compute_transfer_queue() const
 bool GpuInfo::resizable_bar_enabled() const
 {
     return d->resizable_bar_enabled;
+}
+
+bool GpuInfo::support_image_storage() const
+{
+    return d->support_image_storage;
 }
 
 uint32_t GpuInfo::subgroup_size() const
@@ -3515,24 +3525,30 @@ int VulkanDevicePrivate::create_dummy_buffer_image()
     dummy_allocator = new VkDummyAllocator(vkdev);
 
     dummy_buffer.create(1, 4u, dummy_allocator);
-    dummy_image.create(1, 4u, dummy_allocator);
+    if (vkdev->info.support_image_storage())
+    {
+        dummy_image.create(1, 4u, dummy_allocator);
 #if __APPLE__
-    if (vkdev->info.type() == 0)
-        dummy_image_readonly.create(1, 4u, dummy_allocator);
+        if (vkdev->info.type() == 0)
+            dummy_image_readonly.create(1, 4u, dummy_allocator);
 #else
-    dummy_image_readonly.create(1, 4u, dummy_allocator);
+        dummy_image_readonly.create(1, 4u, dummy_allocator);
 #endif
+    }
 
     VkDummyCompute cmd(vkdev);
 
     cmd.record_dummy(dummy_buffer);
-    cmd.record_dummy(dummy_image);
+    if (vkdev->info.support_image_storage())
+    {
+        cmd.record_dummy(dummy_image);
 #if __APPLE__
-    if (vkdev->info.type() == 0)
-        cmd.record_dummy_readonly(dummy_image_readonly);
+        if (vkdev->info.type() == 0)
+            cmd.record_dummy_readonly(dummy_image_readonly);
 #else
-    cmd.record_dummy_readonly(dummy_image_readonly);
+        cmd.record_dummy_readonly(dummy_image_readonly);
 #endif
+    }
 
     return cmd.submit_and_wait();
 }
@@ -3942,6 +3958,7 @@ VulkanDevice::VulkanDevice(int device_index)
     }
 
     // prepare immutable texelfetch sampler
+    if (info.support_image_storage())
     {
         VkSamplerCreateInfo samplerCreateInfo;
         samplerCreateInfo.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
@@ -4705,6 +4722,9 @@ const PipelineCache* VulkanDevice::get_pipeline_cache() const
 
 bool VulkanDevice::shape_support_image_storage(const Mat& shape) const
 {
+    if (!info.support_image_storage())
+        return false;
+
     int dims = shape.dims;
     int width = shape.w;
     int height = shape.h;
@@ -6232,7 +6252,7 @@ int compile_spirv_module(const char* comp_data, int comp_data_size, const Option
     {
         custom_exts += "#extension GL_EXT_shader_explicit_arithmetic_types_int64: require\n";
     }
-    if (support_shader_int16)
+    if (support_shader_int16 || (opt.use_bf16_packed && support_fp16_storage))
     {
         custom_exts += "#extension GL_EXT_shader_explicit_arithmetic_types_int16: require\n";
     }
@@ -6240,7 +6260,7 @@ int compile_spirv_module(const char* comp_data, int comp_data_size, const Option
     {
         custom_exts += "#extension GL_EXT_bfloat16: require\n";
     }
-    if (opt.use_fp16_storage || opt.use_bf16_storage || opt.use_int16_storage)
+    if (opt.use_fp16_storage || opt.use_bf16_storage || opt.use_int16_storage || (opt.use_bf16_packed && support_fp16_storage))
     {
         custom_exts += "#extension GL_EXT_shader_16bit_storage: require\n";
     }
