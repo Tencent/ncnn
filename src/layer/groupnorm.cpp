8730747b4653e6cbe2f87fcbd77d50b06513c905
LI <2484593937@qq.com>
2026-09-08 17:10:35 +0800

    fix(layer): reject non-positive divisors in ShuffleChannel/PixelShuffle/Reorg/GroupNorm
    load_param accepted group/upscale_factor/stride=0; forward then SIGFPEs on channels%group, channels/(upscale_factor^2), w/stride, or channels/group.

--- src/layer/groupnorm.cpp
+++ src/layer/groupnorm.cpp
@@ -18,6 +18,12 @@ int GroupNorm::load_param(const ParamDict& pd)
     eps = pd.get(2, 0.001f);
     affine = pd.get(3, 1);
 
+    if (group <= 0)
+    {
+        // reject invalid group (forward divides by group)
+        return -100;
+    }
+
     return 0;
 }
 
--- src/layer/reorg.cpp
+++ src/layer/reorg.cpp
@@ -16,6 +16,12 @@ int Reorg::load_param(const ParamDict& pd)
     stride = pd.get(0, 1);
     mode = pd.get(1, 0);
 
+    if (stride <= 0)
+    {
+        // reject invalid stride (forward divides by stride)
+        return -100;
+    }
+
     return 0;
 }
 
