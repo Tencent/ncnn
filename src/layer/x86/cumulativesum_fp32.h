48e90b80885e67a75254a12b8764ccbb68819ec4
nihui <shuizhuyuanluo@126.com>
2026-09-18 16:03:11 +0800

    Refactor cumulative sum implementation with AVX512 support

--- src/layer/x86/cumulativesum_fp32.h
+++ src/layer/x86/cumulativesum_fp32.h
@@ -5,17 +5,6 @@
 void cumulative_sum_avx2(float* ptr, int w);
 #endif
 
-#if __SSE2__
-static inline __m128 cumulative_sum4_ps(__m128 _p)
-{
-    __m128 _t = _mm_castsi128_ps(_mm_slli_si128(_mm_castps_si128(_p), 4));
-    _p = _mm_add_ps(_p, _t);
-    _t = _mm_castsi128_ps(_mm_slli_si128(_mm_castps_si128(_p), 8));
-    _p = _mm_add_ps(_p, _t);
-    return _p;
-}
-#endif
-
 static void cumulative_sum(float* ptr, int w)
 {
 #if NCNN_RUNTIME_CPU && NCNN_AVX2 && __AVX__ && !__AVX2__
@@ -29,48 +18,117 @@ static void cumulative_sum(float* ptr, int w)
     int j = 0;
     float sum = 0.f;
 
-#if __AVX2__
-    __m256 _sum = _mm256_setzero_ps();
+#if __SSE2__
+#if __AVX__
+#if __AVX512F__
+    __m512 _sum_avx512 = _mm512_setzero_ps();
+
+    for (; j + 15 < w; j += 16)
+    {
+        __m512 _p = _mm512_loadu_ps(ptr + j);
+
+        // prefix within each 128-bit quarter
+        __m512 _t = _mm512_maskz_shuffle_ps(
+            (__mmask16)0xeeee, _p, _p, _MM_SHUFFLE(2, 1, 0, 0));
+        _p = _mm512_add_ps(_p, _t);
+
+        _t = _mm512_maskz_shuffle_ps(
+            (__mmask16)0xcccc, _p, _p, _MM_SHUFFLE(1, 0, 0, 0));
+        _p = _mm512_add_ps(_p, _t);
+
+        // propagate quarter prefix
+        __m512 _qsum = _mm512_shuffle_ps(_p, _p, _MM_SHUFFLE(3, 3, 3, 3));
+
+        _t = _mm512_maskz_shuffle_f32x4(
+            (__mmask16)0xfff0, _qsum, _qsum, _MM_SHUFFLE(2, 1, 0, 0));
+        _p = _mm512_add_ps(_p, _t);
+
+        _qsum = _mm512_shuffle_ps(_p, _p, _MM_SHUFFLE(3, 3, 3, 3));
+
+        _t = _mm512_maskz_shuffle_f32x4(
+            (__mmask16)0xff00, _qsum, _qsum, _MM_SHUFFLE(1, 0, 0, 0));
+        _p = _mm512_add_ps(_p, _t);
+
+        _p = _mm512_add_ps(_p, _sum_avx512);
+        _mm512_storeu_ps(ptr + j, _p);
+
+        __m512 _last = _mm512_shuffle_f32x4(_p, _p, _MM_SHUFFLE(3, 3, 3, 3));
+        _sum_avx512 = _mm512_shuffle_ps(_last, _last, _MM_SHUFFLE(3, 3, 3, 3));
+    }
+
+    if (j > 0)
+        sum = ptr[j - 1];
+#endif // __AVX512F__
+
+    __m256 _sum_avx = _mm256_set1_ps(sum);
+
     for (; j + 7 < w; j += 8)
     {
         __m256 _p = _mm256_loadu_ps(ptr + j);
-        __m256 _t = _mm256_castsi256_ps(_mm256_slli_si256(_mm256_castps_si256(_p), 4));
+
+#if __AVX2__
+        __m256 _t = _mm256_castsi256_ps(
+            _mm256_slli_si256(_mm256_castps_si256(_p), 4));
         _p = _mm256_add_ps(_p, _t);
-        _t = _mm256_castsi256_ps(_mm256_slli_si256(_mm256_castps_si256(_p), 8));
+
+        _t = _mm256_castsi256_ps(
+            _mm256_slli_si256(_mm256_castps_si256(_p), 8));
         _p = _mm256_add_ps(_p, _t);
+
         __m256 _lo = _mm256_permute2f128_ps(_p, _p, 0x08);
         _lo = _mm256_shuffle_ps(_lo, _lo, _MM_SHUFFLE(3, 3, 3, 3));
         _p = _mm256_add_ps(_p, _lo);
-        _p = _mm256_add_ps(_p, _sum);
-        _mm256_storeu_ps(ptr + j, _p);
-        __m256 _last = _mm256_permute2f128_ps(_p, _p, 0x11);
-        _sum = _mm256_shuffle_ps(_last, _last, _MM_SHUFFLE(3, 3, 3, 3));
-    }
-    if (j > 0)
-        sum = ptr[j - 1];
-#elif __AVX__
-    for (; j + 7 < w; j += 8)
-    {
-        __m256 _p = _mm256_loadu_ps(ptr + j);
-        __m128 _p0 = cumulative_sum4_ps(_mm256_castps256_ps128(_p));
-        __m128 _p1 = cumulative_sum4_ps(_mm256_extractf128_ps(_p, 1));
+#else
+        __m128 _p0 = _mm256_castps256_ps128(_p);
+        __m128 _p1 = _mm256_extractf128_ps(_p, 1);
+
+        __m128 _t0 = _mm_castsi128_ps(_mm_slli_si128(_mm_castps_si128(_p0), 4));
+        _p0 = _mm_add_ps(_p0, _t0);
+        _t0 = _mm_castsi128_ps(_mm_slli_si128(_mm_castps_si128(_p0), 8));
+        _p0 = _mm_add_ps(_p0, _t0);
+
+        __m128 _t1 = _mm_castsi128_ps(_mm_slli_si128(_mm_castps_si128(_p1), 4));
+        _p1 = _mm_add_ps(_p1, _t1);
+        _t1 = _mm_castsi128_ps(_mm_slli_si128(_mm_castps_si128(_p1), 8));
+        _p1 = _mm_add_ps(_p1, _t1);
+
         _p1 = _mm_add_ps(_p1, _mm_shuffle_ps(_p0, _p0, _MM_SHUFFLE(3, 3, 3, 3)));
 
-        __m256 _out = _mm256_castps128_ps256(_p0);
-        _out = _mm256_insertf128_ps(_out, _p1, 1);
-        _out = _mm256_add_ps(_out, _mm256_set1_ps(sum));
-        _mm256_storeu_ps(ptr + j, _out);
-        sum = ptr[j + 7];
+        _p = _mm256_insertf128_ps(_mm256_castps128_ps256(_p0), _p1, 1);
+#endif
+
+        _p = _mm256_add_ps(_p, _sum_avx);
+        _mm256_storeu_ps(ptr + j, _p);
+
+        __m256 _last = _mm256_permute2f128_ps(_p, _p, 0x11);
+        _sum_avx = _mm256_shuffle_ps(_last, _last, _MM_SHUFFLE(3, 3, 3, 3));
     }
-#elif __SSE2__
+
+    if (j > 0)
+        sum = ptr[j - 1];
+#endif // __AVX__
+
+    __m128 _sum_sse = _mm_set1_ps(sum);
+
     for (; j + 3 < w; j += 4)
     {
-        __m128 _p = cumulative_sum4_ps(_mm_loadu_ps(ptr + j));
-        _p = _mm_add_ps(_p, _mm_set1_ps(sum));
+        __m128 _p = _mm_loadu_ps(ptr + j);
+
+        __m128 _t = _mm_castsi128_ps(_mm_slli_si128(_mm_castps_si128(_p), 4));
+        _p = _mm_add_ps(_p, _t);
+
+        _t = _mm_castsi128_ps(_mm_slli_si128(_mm_castps_si128(_p), 8));
+        _p = _mm_add_ps(_p, _t);
+
+        _p = _mm_add_ps(_p, _sum_sse);
         _mm_storeu_ps(ptr + j, _p);
-        sum = ptr[j + 3];
+
+        _sum_sse = _mm_shuffle_ps(_p, _p, _MM_SHUFFLE(3, 3, 3, 3));
     }
-#endif
+
+    if (j > 0)
+        sum = ptr[j - 1];
+#endif // __SSE2__
 
     for (; j < w; j++)
     {
